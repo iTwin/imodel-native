bc6510bc5967e43963502da53e5807e7c7160291
Casey.Mullen <none@none>
2009-09-29 18:26:59 -0400

    add ECObjectsAPI.h

---
+++ iModelCore/ecobjects/PublicApi/ECObjects/ECObjectsAPI.h
@@ -0,0 +1,18 @@
+/*--------------------------------------------------------------------------------------+
+|
+|     $Source: PublicApi/ECObjects/ECObjectsAPI.h $
+|    $RCSfile: file.tpl,v $
+|   $Revision: 1.10 $
+|       $Date: 2005/11/07 15:38:45 $
+|     $Author: EarlinLutz $
+|
+|  $Copyright: (c) 2009 Bentley Systems, Incorporated. All rights reserved. $
+|
++--------------------------------------------------------------------------------------*/
+#pragma once
+/*__PUBLISH_SECTION_START__*/
+
+#include <ECObjects\ECEnabler.h>
+#include <ECObjects\ECInstance.h>
+#include <ECObjects\ECSchema.h>
+#include <ECObjects\ECValue.h>
