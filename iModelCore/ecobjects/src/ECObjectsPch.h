d0061991e7b1c1c41779118d0faac63617f238aa
Carole.MacDonald <none@none>
2010-01-06 12:54:13 -0500

    Add Bentley Logging

--- iModelCore/ecobjects/src/ECObjectsPch.h
+++ iModelCore/ecobjects/src/ECObjectsPch.h
@@ -2,7 +2,7 @@
 |
 |     $Source: ecobjects/native/ECObjectsPch.h $
 |
-|  $Copyright: (c) 2009 Bentley Systems, Incorporated. All rights reserved. $
+|  $Copyright: (c) 2010 Bentley Systems, Incorporated. All rights reserved. $
 |
 +--------------------------------------------------------------------------------------*/
 #pragma once
@@ -10,6 +10,10 @@
 #include <ECObjects\ECObjectsAPI.h>
 #include <sstream>
 #include "ecxml.h"
+#include "Logger.h"
+#include <BsiLogging\bsilog.h>
+#include <BsiLogging\log4cxx.h>
+
 
 
 
