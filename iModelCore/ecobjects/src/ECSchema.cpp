9ca9b4d9dddb0f4ec6cc7203547b52a5d221bff9
Keith.Bentley <none@none>
2011-03-24 15:33:10 -0400

    moving all SQLite code to DgnDb

--- iModelCore/ecobjects/src/ECSchema.cpp
+++ iModelCore/ecobjects/src/ECSchema.cpp
@@ -1,10 +1,6 @@
 /*--------------------------------------------------------------------------------------+
 |
 |     $Source: src/ECSchema.cpp $
-|    $RCSfile: file.tpl,v $
-|   $Revision: 1.10 $
-|       $Date: 2005/11/07 15:38:45 $
-|     $Author: EarlinLutz $
 |
 |  $Copyright: (c) 2011 Bentley Systems, Incorporated. All rights reserved. $
 |
