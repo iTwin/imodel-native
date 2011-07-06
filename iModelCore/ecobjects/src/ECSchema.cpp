2665b07207bc531ebe4c39f7541f2b4ef804726f
Casey.Mullen <none@none>
2011-07-06 12:18:54 -0400

    test. add blank line

--- iModelCore/ecobjects/src/ECSchema.cpp
+++ iModelCore/ecobjects/src/ECSchema.cpp
@@ -1917,6 +1917,7 @@ bool    ECClassContainer::const_iterator::operator== (const_iterator const& rhs)
     {
     return (m_state->m_mapIterator == rhs.m_state->m_mapIterator);
     }
+
 /*---------------------------------------------------------------------------------**//**
 * @bsimethod                                                   
 +---------------+---------------+---------------+---------------+---------------+------*/
