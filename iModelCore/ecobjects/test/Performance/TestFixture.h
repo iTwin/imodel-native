8e1fe66a883eee7b1007fc2be581fb200e64a742
Carole.MacDonald <none@none>
2011-10-13 11:51:46 -0400

    New performance test for monitoring instance and schema serialization and de-serialization

---
+++ iModelCore/ecobjects/test/Performance/TestFixture.h
@@ -0,0 +1,42 @@
+/*--------------------------------------------------------------------------------------+
+|
+|     $Source: test/Performance/TestFixture.h $
+|
+|  $Copyright: (c) 2011 Bentley Systems, Incorporated. All rights reserved. $
+|
++--------------------------------------------------------------------------------------*/
+#pragma once
+#include <windows.h>
+
+BEGIN_BENTLEY_EC_NAMESPACE
+
+struct ECTestFixture : public ::testing::Test
+    {
+private:
+    static WString s_dllPath;
+    static WString GetDllPath();
+    static bool s_isLoggerInitialized;
+    WString GetLogConfigurationFilename();
+    BentleyStatus CheckProcessDirectory(WCharP filepath, DWORD bufferSize);
+
+protected:
+    ECTestFixture(void);
+    
+public:
+    virtual void            SetUp () override;
+    virtual void            TearDown () override;
+
+    virtual bool            _WantSchemaLeakDetection() { return true; }
+    virtual bool            _WantInstanceLeakDetection() { return true; }
+
+    void    TestForECSchemaLeaks ();
+    void    TestForIECInstanceLeaks ();
+
+    static WString GetTestDataPath(WCharCP fileName);
+    static WString GetWorkingDirectoryPath(WCharCP testFixture, WCharCP fileName);
+    static WString GetDateTime ();
+    static WString GetTestResultsFilePath (WCharCP fileName);
+    static StatusInt MakeDirContainingFile (WCharCP filePath);
+    };
+
+END_BENTLEY_EC_NAMESPACE
