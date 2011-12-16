8303d50475ecb7d09b8a90b6221ce5c56da5477f
Bill.Steinbock <none@none>
2011-12-16 14:52:08 -0500

    WIP: Change to hardcode the number of  bits for allowed PerProperty to 2 for all MemoryBased instances. This allows the flags to be easily loaded into the managed ECDInstance.

--- iModelCore/ecobjects/src/StandaloneECInstance.cpp
+++ iModelCore/ecobjects/src/StandaloneECInstance.cpp
@@ -37,7 +37,7 @@ const UInt32 BITS_PER_FLAGSBITMASK = (sizeof(UInt32) * 8);
 /*---------------------------------------------------------------------------------**//**
 * @bsimethod                                    Bill.Steinbock                  04/2010
 +---------------+---------------+---------------+---------------+---------------+------*/
-MemoryECInstanceBase::MemoryECInstanceBase (byte * data, UInt32 size, ClassLayoutCR classLayout, bool allowWritingDirectlyToInstanceMemory, UInt8 numBitsPerPropertyFlag, MemoryECInstanceBase const* parentInstance) :
+MemoryECInstanceBase::MemoryECInstanceBase (byte * data, UInt32 size, ClassLayoutCR classLayout, bool allowWritingDirectlyToInstanceMemory, MemoryECInstanceBase const* parentInstance) :
         MemoryInstanceSupport (allowWritingDirectlyToInstanceMemory),
         m_bytesAllocated(size)
     {
@@ -47,13 +47,13 @@ MemoryECInstanceBase::MemoryECInstanceBase (byte * data, UInt32 size, ClassLayou
     m_usingSharedMemory = false;
     m_parentInstance.parentInstance = parentInstance;
 
-    InitializePerPropertyFlags (classLayout, numBitsPerPropertyFlag);
+    InitializePerPropertyFlags (classLayout, DEFAULT_NUMBITSPERPROPERTY);
     }
 
 /*---------------------------------------------------------------------------------**//**
 * @bsimethod                                    Bill.Steinbock                  04/2010
 +---------------+---------------+---------------+---------------+---------------+------*/
-MemoryECInstanceBase::MemoryECInstanceBase (ClassLayoutCR classLayout, UInt32 minimumBufferSize, bool allowWritingDirectlyToInstanceMemory, UInt8 numBitsPerPropertyFlag, MemoryECInstanceBase const* parentInstance) :
+MemoryECInstanceBase::MemoryECInstanceBase (ClassLayoutCR classLayout, UInt32 minimumBufferSize, bool allowWritingDirectlyToInstanceMemory, MemoryECInstanceBase const* parentInstance) :
         MemoryInstanceSupport (allowWritingDirectlyToInstanceMemory),
         m_bytesAllocated(0)
     {
@@ -75,7 +75,7 @@ MemoryECInstanceBase::MemoryECInstanceBase (ClassLayoutCR classLayout, UInt32 mi
 
     InitializeMemory (classLayout, m_data.address, m_bytesAllocated);
     
-    InitializePerPropertyFlags (classLayout, numBitsPerPropertyFlag);
+    InitializePerPropertyFlags (classLayout, DEFAULT_NUMBITSPERPROPERTY);
     }
 
 /*---------------------------------------------------------------------------------**//**
@@ -1548,7 +1548,7 @@ size_t                StandaloneECInstance::_GetOffsetToIECInstance () const
 * @bsimethod                                                    CaseyMullen     09/09
 +---------------+---------------+---------------+---------------+---------------+------*/        
 StandaloneECInstance::StandaloneECInstance (StandaloneECEnablerCR enabler, byte * data, UInt32 size) :
-        MemoryECInstanceBase (data, size, enabler.GetClassLayout(), true, 2),
+        MemoryECInstanceBase (data, size, enabler.GetClassLayout(), true),
         m_sharedWipEnabler(const_cast<StandaloneECEnablerP>(&enabler)) // WIP_FUSION: can we get rid of the const cast?
     {
     m_sharedWipEnabler->AddRef ();
@@ -1558,7 +1558,7 @@ StandaloneECInstance::StandaloneECInstance (StandaloneECEnablerCR enabler, byte 
 * @bsimethod                                                    CaseyMullen     01/10
 +---------------+---------------+---------------+---------------+---------------+------*/        
 StandaloneECInstance::StandaloneECInstance (StandaloneECEnablerCR enabler, UInt32 minimumBufferSize) :
-        MemoryECInstanceBase (enabler.GetClassLayout(), minimumBufferSize, true, 2),
+        MemoryECInstanceBase (enabler.GetClassLayout(), minimumBufferSize, true),
         m_sharedWipEnabler(const_cast<StandaloneECEnablerP>(&enabler)) // WIP_FUSION: can we get rid of the const cast?
     {
     m_sharedWipEnabler->AddRef ();
