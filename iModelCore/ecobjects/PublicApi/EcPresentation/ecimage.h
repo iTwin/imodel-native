d938d4d43a24b6e95bdf0cda2c683a2137eff293
Paul.Connelly <none@none>
2012-05-23 09:06:14 -0400

    BF: Another unreferenced local variable.

--- iModelCore/ecobjects/PublicApi/EcPresentation/ecimage.h
+++ iModelCore/ecobjects/PublicApi/EcPresentation/ecimage.h
@@ -85,7 +85,8 @@ struct  ECNativeImage :public IECNativeImage
     ~ECNativeImage ()
         {
         BentleyStatus status = BeIconUtilities::DestroyIcon(GetImage());
-        BeAssert(SUCCESS == status);
+        if (SUCCESS != status)
+            BeAssert(SUCCESS == status);
         }
     };
 
