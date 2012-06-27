624610cf411bbad044e2bcbcd22c442e8cda7251
Paul.Connelly <none@none>
2012-06-27 11:41:07 -0400

    EN: SystemTime::InitFromUnixMillis()

--- iModelCore/ecobjects/src/ECValue.cpp
+++ iModelCore/ecobjects/src/ECValue.cpp
@@ -2087,4 +2087,13 @@ BentleyStatus   SystemTime::InitFromFileTime (_FILETIME const& fileTime)
     return SUCCESS;
     }
 
+/*---------------------------------------------------------------------------------**//**
+* @bsimethod                                                    Paul.Connelly   06/12
++---------------+---------------+---------------+---------------+---------------+------*/
+BentleyStatus SystemTime::InitFromUnixMillis (UInt64 unixMillis)
+    {
+    unixMillisToSystemTime (*this, unixMillis);
+    return SUCCESS;
+    }
+
 END_BENTLEY_EC_NAMESPACE
