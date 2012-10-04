5e9e7de7957fa075cbee14291bec8420b305b8bb
Paul.Connelly <none@none>
2012-10-04 13:14:47 -0400

    EN: ECValuesCollectionIterator caches array size rather than looking it up every time array index is incremented.

--- iModelCore/ecobjects/PublicApi/ECObjects/ECValue.h
+++ iModelCore/ecobjects/PublicApi/ECObjects/ECValue.h
@@ -436,6 +436,7 @@ private:
     friend struct ECValuesCollection;
 
     ECPropertyValue     m_propertyValue;
+    int                 m_arrayCount;
 
     ECValuesCollectionIterator (IECInstanceCR);
     ECValuesCollectionIterator (ECPropertyValueCR parentPropertyValue);
