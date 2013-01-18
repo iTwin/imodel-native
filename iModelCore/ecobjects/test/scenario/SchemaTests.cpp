04c1aaeb0217704ecf229f8bfae48fe827e01e85
Muhammad.Zaighum <none@none>
2013-01-18 18:04:46 +0500

    added test for copySchema

--- iModelCore/ecobjects/test/scenario/SchemaTests.cpp
+++ iModelCore/ecobjects/test/scenario/SchemaTests.cpp
@@ -151,4 +151,86 @@ TEST_F(SchemaTest, TestGetClassCount)
 	ASSERT_EQ(2,schema->GetClassCount());
 
     }
+
+TEST_F (SchemaTest, DISABLED_TestCircularReference)
+{
+	ECSchemaPtr testSchema;
+	ECSchemaReadContextPtr   schemaContext;
+	SearchPathSchemaFileLocaterPtr schemaLocater;
+	bvector<WString> searchPaths;
+	searchPaths.push_back (ECTestFixture::GetTestDataPath(L""));
+	schemaLocater = SearchPathSchemaFileLocater::CreateSearchPathSchemaFileLocater(searchPaths);
+	schemaContext = ECSchemaReadContext::CreateContext();
+	schemaContext->AddSchemaLocater (*schemaLocater);
+	SchemaKey key(L"CircleSchema", 01, 00);
+	testSchema = schemaContext->LocateSchema(key, SCHEMAMATCHTYPE_Latest);
+	EXPECT_FALSE(testSchema.IsValid());
+}
+
+
+
+TEST_F (SchemaTest, TestsLatestCompatible)
+{
+	ECSchemaPtr testSchema;
+	ECSchemaReadContextPtr   schemaContext;
+	SearchPathSchemaFileLocaterPtr schemaLocater;
+	bvector<WString> searchPaths;
+	searchPaths.push_back (ECTestFixture::GetTestDataPath(L""));
+	schemaLocater = SearchPathSchemaFileLocater::CreateSearchPathSchemaFileLocater(searchPaths);
+	schemaContext = ECSchemaReadContext::CreateContext();
+	schemaContext->AddSchemaLocater (*schemaLocater);
+	SchemaKey key(L"Widgets", 01, 00);
+	testSchema = schemaContext->LocateSchema(key, SCHEMAMATCHTYPE_Latest);
+	EXPECT_TRUE(testSchema.IsValid());
+	EXPECT_TRUE(testSchema->GetVersionMajor()==9);
+	EXPECT_TRUE(testSchema->GetVersionMinor()==6);
+}
+
+
+TEST_F (SchemaTest, GetBaseClassPropertyWhenSchemaHaveDuplicatePrefixes)
+{
+	ECSchemaPtr testSchema;
+	ECSchemaReadContextPtr   schemaContext;
+	SearchPathSchemaFileLocaterPtr schemaLocater;
+	bvector<WString> searchPaths;
+	searchPaths.push_back (ECTestFixture::GetTestDataPath(L""));
+	schemaLocater = SearchPathSchemaFileLocater::CreateSearchPathSchemaFileLocater(searchPaths);
+	schemaContext = ECSchemaReadContext::CreateContext();
+	schemaContext->AddSchemaLocater (*schemaLocater);
+	SchemaKey key(L"DuplicatePrefixes", 01, 00);
+	testSchema = schemaContext->LocateSchema(key, SCHEMAMATCHTYPE_Latest);
+	EXPECT_TRUE(testSchema.IsValid());
+	ECClassCP CircleClass = testSchema->GetClassCP (L"Circle");
+	EXPECT_TRUE(CircleClass!=NULL)<<"Cannot Load Ellipse Class";
+
+	IECInstancePtr CircleClassInstance = CircleClass->GetDefaultStandaloneEnabler()->CreateInstance();
+	ECValue v;
+    v.SetString (L"test");
+	CircleClassInstance->SetValue (L"Name", v);
+
+}
+
+TEST_F (SchemaTest, GetBaseClassProperty)
+{
+	ECSchemaPtr testSchema;
+	ECSchemaReadContextPtr   schemaContext;
+	SearchPathSchemaFileLocaterPtr schemaLocater;
+	bvector<WString> searchPaths;
+	searchPaths.push_back (ECTestFixture::GetTestDataPath(L""));
+	schemaLocater = SearchPathSchemaFileLocater::CreateSearchPathSchemaFileLocater(searchPaths);
+	schemaContext = ECSchemaReadContext::CreateContext();
+	schemaContext->AddSchemaLocater (*schemaLocater);
+	SchemaKey key(L"testschema", 01, 00);
+	testSchema = schemaContext->LocateSchema(key, SCHEMAMATCHTYPE_Latest);
+	EXPECT_TRUE(testSchema.IsValid());
+	ECClassCP WheelsChildClass = testSchema->GetClassCP (L"WheelsChild");
+	EXPECT_TRUE(WheelsChildClass!=NULL)<<"Cannot Load WheelsChild Class";
+
+	IECInstancePtr WheelsChildInstance = WheelsChildClass->GetDefaultStandaloneEnabler()->CreateInstance();
+	ECValue v;
+    v.SetString (L"test");
+	WheelsChildInstance->SetValue (L"Name", v);
+
+}
+
 END_BENTLEY_ECOBJECT_NAMESPACE
