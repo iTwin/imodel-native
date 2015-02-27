e673616c3a42eb675c8d940b195aac401d076835
Affan.Khan <none@none>
2015-02-27 20:19:01 +0500

    Fixed AllowDuplicateRelationship for N:N Added a test that extensively test 1:1, 1:N and N:N

--- iModelCore/ECDb/Tests/NonPublished/ECDb/ECDbInstances_Test.cpp
+++ iModelCore/ECDb/Tests/NonPublished/ECDb/ECDbInstances_Test.cpp
@@ -7,7 +7,7 @@
 +--------------------------------------------------------------------------------------*/
 #include <UnitTests/NonPublished/ECDb/ECDbTestProject.h>
 #include <limits>
-
+#include <initializer_list>
 USING_NAMESPACE_BENTLEY_EC
 USING_NAMESPACE_BENTLEY_SQLITE_EC
 
@@ -116,6 +116,271 @@ TEST(ECDbInstances, DeleteECInstances)
         ASSERT_EQ (SUCCESS, deleter.Delete (*importedInstance));
         }
     }   
+
+/*---------------------------------------------------------------------------------**//**
+* @bsimethod                                   Affan.Khan                         02/15
++---------------+---------------+---------------+---------------+---------------+------*/
+void VerifyRelationshipInsertionIntegrity (ECDbCR ecdb, Utf8CP relationshipClass, std::vector<ECInstanceKey> const& sourceKeys, std::vector<ECInstanceKey>const& targetKeys, std::vector<ECSqlStepStatus> const& expected , size_t& rowInserted )
+    {
+    ECSqlStatement stmt;
+    auto sql = SqlPrintfString ("INSERT INTO %s (SourceECInstanceId, SourceECClassId, TargetECInstanceId, TargetECClassId) VALUES(?,?,?,?)", relationshipClass);
+    ASSERT_EQ (stmt.Prepare (ecdb, sql.GetUtf8CP()), ECSqlStatus::Success);
+    ASSERT_EQ (expected.size (), sourceKeys.size () * targetKeys.size ());
+    int n = 0;
+    for (auto& fooKey : sourceKeys)
+        {
+        for (auto& gooKey : targetKeys)
+            {
+            stmt.Reset ();
+            ASSERT_EQ (static_cast<int>(stmt.ClearBindings ()), static_cast<int>(ECSqlStatus::Success));
+            stmt.BindId (1, fooKey.GetECInstanceId ());
+            stmt.BindInt64 (2, fooKey.GetECClassId ());
+            stmt.BindId (3, gooKey.GetECInstanceId ());
+            stmt.BindInt64 (4, gooKey.GetECClassId ());
+            ASSERT_EQ (static_cast<int>(stmt.Step ()), static_cast<int>(expected[n]));
+            if (expected[n] == ECSqlStepStatus::Done)
+                rowInserted++;
+
+            n = n + 1;
+            }
+        }
+    }
+
+/*---------------------------------------------------------------------------------**//**
+* @bsimethod                                   Affan.Khan                         02/15
++---------------+---------------+---------------+---------------+---------------+------*/
+size_t GetRelationshipInstanceCount (ECDbCR ecdb, Utf8CP relationshipClass)
+    {
+    ECSqlStatement stmt;
+    auto sql = SqlPrintfString ("SELECT COUNT(*) FROM ONLY ts.Foo JOIN ts.Goo USING %s", relationshipClass);
+    if (stmt.Prepare (ecdb, sql.GetUtf8CP ()) == ECSqlStatus::Success)
+        {
+        if (stmt.Step () == ECSqlStepStatus::HasRow)
+            return static_cast<size_t>(stmt.GetValueInt (0));
+        }
+
+    return 0;
+    }
+
+/*---------------------------------------------------------------------------------**//**
+* @bsimethod                                   Affan.Khan                         02/15
++---------------+---------------+---------------+---------------+---------------+------*/
+enum  MapStrategy : uint32_t  
+    {
+    MapStrategy_DoNotMap = 0x1,
+    MapStrategy_DoNotMapHierarchy = 0x2,
+    MapStrategy_TablePerHierarchy = 0x4,
+    MapStrategy_TablePerClass = 0x8,
+    MapStrategy_SharedTableForThisClass = 0x10,
+    MapStrategy_MapToExistingTable = 0x20,
+    MapStrategy_TableForThisClass = 40,
+    MapStrategy_WithReuseColumns = 0x100,
+    MapStrategy_WithExclusivelyStoredInThisTable = 0x200,
+    MapStrategy_WithReadonly = 0x400,
+    MapStrategy_NoHint = 0x0,
+    MapStrategy_InParentTable = 0x1000,
+    MapStrategy_RelationshipTargetTable = 0x2000,
+    MapStrategy_RelationshipSourceTable = 0x4000,
+    };
+
+/*---------------------------------------------------------------------------------**//**
+* @bsimethod                                   Affan.Khan                         02/15
++---------------+---------------+---------------+---------------+---------------+------*/
+uint32_t GetMapStrategy (ECDbR ecdb, ECClassId ecClassId)
+    {
+    Statement stmt;
+    stmt.Prepare (ecdb, "SELECT MapStrategy FROM ec_ClassMap WHERE ECClassId = ?");
+    stmt.BindInt64 (1, ecClassId);
+    if (stmt.Step () == BE_SQLITE_ROW)
+        return stmt.GetValueInt (0);
+
+    return 0;
+    }
+
+/*---------------------------------------------------------------------------------**//**
+* @bsimethod                                   Affan.Khan                         02/15
++---------------+---------------+---------------+---------------+---------------+------*/
+void ExecuteRelationshipInsertionIntegrityTest (ECDbR ecdb, bool allowDuplicateRelationships)
+    {
+    ECSchemaPtr testSchema;
+    ECClassP foo = nullptr, goo = nullptr;
+    ECRelationshipClassP oneFooHasOneGoo = nullptr, oneFooHasManyGoo = nullptr, manyFooHasManyGoo = nullptr;
+    PrimitiveECPropertyP prim;
+    auto readContext = ECSchemaReadContext::CreateContext ();
+    readContext->AddSchemaLocater (ecdb.GetSchemaLocater ());
+    auto bscaKey = SchemaKey (L"Bentley_Standard_CustomAttributes", 1, 11);
+    auto bscaSchema = readContext->LocateSchema (bscaKey, SchemaMatchType::SCHEMAMATCHTYPE_LatestCompatible);
+    ASSERT_TRUE (bscaSchema.IsValid ());
+
+    ECSchema::CreateSchema (testSchema, L"TestSchema", 1, 0);
+    ASSERT_TRUE (testSchema.IsValid ());
+
+    testSchema->SetNamespacePrefix (L"ts");
+    testSchema->AddReferencedSchema (*bscaSchema);
+
+    testSchema->CreateClass (foo, L"Foo");
+    testSchema->CreateClass (goo, L"Goo");
+
+    testSchema->CreateRelationshipClass (oneFooHasOneGoo, L"OneFooHasOneGoo");
+    testSchema->CreateRelationshipClass (oneFooHasManyGoo, L"OneFooHasManyGoo");
+    testSchema->CreateRelationshipClass (manyFooHasManyGoo, L"ManyFooHasManyGoo");
+
+    ASSERT_TRUE (foo != nullptr);
+    ASSERT_TRUE (foo != nullptr);
+    ASSERT_TRUE (oneFooHasOneGoo != nullptr);
+    ASSERT_TRUE (oneFooHasManyGoo != nullptr);
+    ASSERT_TRUE (manyFooHasManyGoo != nullptr);
+
+    prim = nullptr;
+    foo->CreatePrimitiveProperty (prim, L"fooProp");
+    prim->SetType (PrimitiveType::PRIMITIVETYPE_String);
+    ASSERT_TRUE (prim != nullptr);
+
+    prim = nullptr;
+    goo->CreatePrimitiveProperty (prim, L"gooProp");
+    prim->SetType (PrimitiveType::PRIMITIVETYPE_String);
+    ASSERT_TRUE (prim != nullptr);
+
+    oneFooHasOneGoo->GetSource ().AddClass (*foo);
+    oneFooHasOneGoo->GetSource ().SetCardinality (L"1");
+    oneFooHasOneGoo->GetTarget ().AddClass (*goo);
+    oneFooHasOneGoo->GetTarget ().SetCardinality (L"1");
+
+    oneFooHasManyGoo->GetSource ().AddClass (*foo);
+    oneFooHasManyGoo->GetSource ().SetCardinality (L"1");
+    oneFooHasManyGoo->GetTarget ().AddClass (*goo);
+    oneFooHasManyGoo->GetTarget ().SetCardinality (L"N");
+
+    manyFooHasManyGoo->GetSource ().AddClass (*foo);
+    manyFooHasManyGoo->GetSource ().SetCardinality (L"N");
+    manyFooHasManyGoo->GetTarget ().AddClass (*goo);
+    manyFooHasManyGoo->GetTarget ().SetCardinality (L"N");
+    readContext->AddSchema (*testSchema);
+
+    if (allowDuplicateRelationships)
+        {
+        auto hintClass = bscaSchema->GetClassCP (L"ECDbRelationshipClassHint");
+        ASSERT_TRUE (hintClass != nullptr);
+        auto hint = hintClass->GetDefaultStandaloneEnabler ()->CreateInstance ();
+        ASSERT_TRUE (hint != nullptr);
+        ASSERT_TRUE (hint->SetValue (L"AllowDuplicateRelationships", ECValue (true)) == ECOBJECTS_STATUS_Success);
+        ASSERT_TRUE (oneFooHasOneGoo->SetCustomAttribute (*hint) == ECOBJECTS_STATUS_Success);
+        ASSERT_TRUE (oneFooHasManyGoo->SetCustomAttribute (*hint) == ECOBJECTS_STATUS_Success);
+        ASSERT_TRUE (manyFooHasManyGoo->SetCustomAttribute (*hint) == ECOBJECTS_STATUS_Success);
+        }
+
+
+    ASSERT_EQ (ecdb.GetSchemaManager ().ImportECSchemas (readContext->GetCache ()), BentleyStatus::SUCCESS);
+
+    std::vector<ECInstanceKey> fooKeys, gooKeys;
+    const int maxFooInstances = 3;
+    const int maxGooInstances = 3;
+
+    ECSqlStatement fooStmt;
+    ASSERT_EQ (fooStmt.Prepare (ecdb, "INSERT INTO ts.Foo(fooProp) VALUES(?)"), ECSqlStatus::Success);
+    for (auto i = 0; i < maxFooInstances; i++)
+        {
+        ECInstanceKey out;
+        ASSERT_EQ (fooStmt.Reset (), ECSqlStatus::Success);
+        ASSERT_EQ (fooStmt.ClearBindings (), ECSqlStatus::Success);
+        ASSERT_EQ (fooStmt.BindText (1, SqlPrintfString ("foo_%d", i), IECSqlBinder::MakeCopy::Yes), ECSqlStatus::Success);
+        ASSERT_EQ (fooStmt.Step (out), ECSqlStepStatus::Done);
+        fooKeys.push_back (out);
+        }
+
+    ECSqlStatement gooStmt;
+    ASSERT_EQ (gooStmt.Prepare (ecdb, "INSERT INTO ts.Goo(gooProp) VALUES(?)"), ECSqlStatus::Success);
+    for (auto i = 0; i < maxGooInstances; i++)
+        {
+        ECInstanceKey out;
+        ASSERT_EQ (gooStmt.Reset (), ECSqlStatus::Success);
+        ASSERT_EQ (gooStmt.ClearBindings (), ECSqlStatus::Success);
+        ASSERT_EQ (gooStmt.BindText (1, SqlPrintfString ("goo_%d", i), IECSqlBinder::MakeCopy::Yes), ECSqlStatus::Success);
+        ASSERT_EQ (gooStmt.Step (out), ECSqlStepStatus::Done);
+        gooKeys.push_back (out);
+        }
+
+    //Compute what are the right valid permutation
+    std::vector<ECSqlStepStatus> oneFooHasOneGooResult;
+    std::vector<ECSqlStepStatus> oneFooHasManyGooResult;
+    std::vector<ECSqlStepStatus> manyFooHasManyGooResult;
+    std::vector<ECSqlStepStatus> reinsertResultError;
+    std::vector<ECSqlStepStatus> reinsertResultDone;
+    for (auto f = 0; f < maxFooInstances; f++)
+        {
+        for (auto g = 0; g < maxGooInstances; g++)
+            {
+            //1:1 is not effected AllowDuplicateRelationships
+            if (f == g)
+                oneFooHasOneGooResult.push_back (ECSqlStepStatus::Done);
+            else
+                oneFooHasOneGooResult.push_back (ECSqlStepStatus::Error);
+
+            //1:N is effected AllowDuplicateRelationships
+            if (f == 0 || allowDuplicateRelationships)
+                oneFooHasManyGooResult.push_back (ECSqlStepStatus::Done);
+            else
+                oneFooHasManyGooResult.push_back (ECSqlStepStatus::Error);
+
+            manyFooHasManyGooResult.push_back (ECSqlStepStatus::Done);
+            reinsertResultError.push_back (ECSqlStepStatus::Error);
+            reinsertResultDone.push_back (ECSqlStepStatus::Done);
+            }
+        }
+
+    //1:1--------------------------------
+    size_t count_OneFooHasOneGoo = 0;
+    VerifyRelationshipInsertionIntegrity (ecdb, "ts.OneFooHasOneGoo", fooKeys, gooKeys, oneFooHasOneGooResult, count_OneFooHasOneGoo);
+    VerifyRelationshipInsertionIntegrity (ecdb, "ts.OneFooHasOneGoo", fooKeys, gooKeys, reinsertResultError, count_OneFooHasOneGoo);
+    ASSERT_EQ (GetMapStrategy (ecdb, oneFooHasOneGoo->GetId ()), MapStrategy_RelationshipTargetTable);    
+    ASSERT_EQ (count_OneFooHasOneGoo, GetRelationshipInstanceCount (ecdb, "ts.OneFooHasOneGoo"));
+
+    //1:N--------------------------------
+    size_t count_OneFooHasManyGoo = 0;
+    VerifyRelationshipInsertionIntegrity (ecdb, "ts.OneFooHasManyGoo", fooKeys, gooKeys, oneFooHasManyGooResult, count_OneFooHasManyGoo);
+    if (allowDuplicateRelationships)
+        {
+        VerifyRelationshipInsertionIntegrity (ecdb, "ts.OneFooHasManyGoo", fooKeys, gooKeys, reinsertResultDone, count_OneFooHasManyGoo);
+        ASSERT_EQ (GetMapStrategy (ecdb, oneFooHasManyGoo->GetId ()), MapStrategy_TableForThisClass);
+        }
+    else
+        {
+        VerifyRelationshipInsertionIntegrity (ecdb, "ts.OneFooHasManyGoo", fooKeys, gooKeys, reinsertResultError, count_OneFooHasManyGoo);
+        ASSERT_EQ (GetMapStrategy (ecdb, oneFooHasManyGoo->GetId ()), MapStrategy_RelationshipTargetTable);
+        }
+    ASSERT_EQ (count_OneFooHasManyGoo, GetRelationshipInstanceCount (ecdb, "ts.OneFooHasManyGoo"));
+
+    //N:N--------------------------------
+    size_t count_ManyFooHasManyGoo = 0;
+    VerifyRelationshipInsertionIntegrity (ecdb, "ts.ManyFooHasManyGoo", fooKeys, gooKeys, manyFooHasManyGooResult, count_ManyFooHasManyGoo);
+    if (allowDuplicateRelationships)
+        VerifyRelationshipInsertionIntegrity (ecdb, "ts.ManyFooHasManyGoo", fooKeys, gooKeys, reinsertResultDone, count_ManyFooHasManyGoo);
+    else
+        VerifyRelationshipInsertionIntegrity (ecdb, "ts.ManyFooHasManyGoo", fooKeys, gooKeys, reinsertResultError, count_ManyFooHasManyGoo);
+
+    ASSERT_EQ (GetMapStrategy (ecdb, manyFooHasManyGoo->GetId ()), MapStrategy_TableForThisClass);
+    ASSERT_EQ (count_ManyFooHasManyGoo, GetRelationshipInstanceCount (ecdb, "ts.ManyFooHasManyGoo"));
+    }
+
+/*---------------------------------------------------------------------------------**//**
+* @bsimethod                                   Affan.Khan                         02/15
++---------------+---------------+---------------+---------------+---------------+------*/
+TEST(ECDbInstances, RelationshipCardinalityTest)
+    {
+    ECDbTestProject test;
+    ECDbR ecdb = test.Create ("RelationshipCardinalityTest.ecdb");
+    ExecuteRelationshipInsertionIntegrityTest (ecdb , false);
+    } 
+
+/*---------------------------------------------------------------------------------**//**
+* @bsimethod                                   Affan.Khan                         02/15
++---------------+---------------+---------------+---------------+---------------+------*/
+TEST (ECDbInstances, RelationshipCardinalityTest_AllowDuplicateRelationships)
+    {
+    ECDbTestProject test;
+    ECDbR ecdb = test.Create ("RelationshipCardinalityTest_AllowDuplicateRelationships.ecdb");
+    ExecuteRelationshipInsertionIntegrityTest (ecdb , true);
+    } 
 
 /*---------------------------------------------------------------------------------**//**
 * @bsimethod                                    Affan.Khan                          04/12
