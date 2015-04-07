4b89e6afad4145079a62c31208a3307d2619ea86
Jeff.Marker <none@none>
2015-04-07 09:30:26 -0400

    Merge

--- iModelCore/ECDb/Tests/Performance/PerformanceBisDesignTestFixture.cpp
+++ iModelCore/ECDb/Tests/Performance/PerformanceBisDesignTestFixture.cpp
@@ -1,8 +1,8 @@
 /*--------------------------------------------------------------------------------------+
 |
-|     $Source: Tests/ECDB/Performance/PerformanceBisDesignTestFixture.cpp $
+|     $Source: Tests/Performance/PerformanceBisDesignTestFixture.cpp $
 |
-|  $Copyright: (c) 2014 Bentley Systems, Incorporated. All rights reserved. $
+|  $Copyright: (c) 2015 Bentley Systems, Incorporated. All rights reserved. $
 |
 +--------------------------------------------------------------------------------------*/
 #include "PerformanceBisDesignTestFixture.h"
