830a7d75cf623406ded5c594d5a8569ba4653fab
Umar.Hayat <none@none>
2015-05-26 21:42:40 +0500

    Added alignment compatibility tests

--- iModelCore/DgnPlatform/Tests/DgnProject/NonPublished/Compatibility_Test.cpp
+++ iModelCore/DgnPlatform/Tests/DgnProject/NonPublished/Compatibility_Test.cpp
@@ -11,16 +11,16 @@
 #include <BeXml/BeXml.h>
 #include <wchar.h>
 
-
 //#ifdef WIP_NOT_PORTABLE
 
-struct TypeNamePair
+struct TypeInfo
 {
     WString m_name;
     int32_t m_size;
     int32_t m_alignment;
-    TypeNamePair() :m_size(-1), m_alignment(-1), m_name(L"UnIntializedName"){}
-    TypeNamePair(int32_t size, WString name) :m_size(size), m_name(name){}
+    TypeInfo() :m_size(-1), m_alignment(-1), m_name(L"UnIntializedName"){}
+    TypeInfo(int32_t size, WString name) :m_size(size), m_name(name){}
+    TypeInfo(int32_t size, int32_t align, WString name) :m_size(size), m_alignment(align), m_name(name){}
 };
 
 #include "BentleyHeaders.h"
@@ -34,7 +34,7 @@ struct Compatibility_Test : public ::testing::Test
      public:
          static BeXmlDomPtr m_dom;
          static xmlXPathContextPtr m_xmlXPathContext;
-         static void GetStructList(bvector<TypeNamePair>& list);
+         static void GetStructList(bvector<TypeInfo>& list);
          static bool IsConfigLoadError;
          static WString GetCompatibilityConfigFile();
          static void SetUpTestCase()
@@ -83,7 +83,7 @@ struct Compatibility_Test : public ::testing::Test
         //---------------------------------------------------------------------------------------
         // @bsimethod                                        Umar.Hayat                02/13
         //---------------------------------------------------------------------------------------
-        int32_t GetAlignOf(WString typeName)
+        int32_t GetAlignOf(WString typeName) 
         {
             BeXmlDom::IterableNodeSet iterableNodeSet;
             WString expressionW = L"//struct[@name='%ls']";
@@ -95,7 +95,6 @@ struct Compatibility_Test : public ::testing::Test
             if (iterableNodeSet.size())
             {
                 //printf("node name %ls \n", (*(iterableNodeSet.begin()++))->GetName());
-                printf("node name %s \n", iterableNodeSet.front()->GetName());
                 int32_t size = -1;
                 iterableNodeSet.front()->GetAttributeInt32Value(size, "alignment");
                 return size;
@@ -114,7 +113,7 @@ WString Compatibility_Test::GetCompatibilityConfigFile()
     {
 #if defined (_X86_)
         return WString(L"CompatibilityTestDataTypes_x86.xml");
-#elif
+#else
         return WString(L"CompatibilityTestDataTypes.xml");
 #endif
     }
@@ -124,14 +123,29 @@ WString Compatibility_Test::GetCompatibilityConfigFile()
 TEST_F(Compatibility_Test, SizeCheck)
     {
     ASSERT_FALSE(IsConfigLoadError) << "Configuration file not loaded properly";
-    bvector<TypeNamePair> structList; 
+    bvector<TypeInfo> structList; 
     GetBentleyStructList(structList);
     GetGeomStructList(structList);
-    for (TypeNamePair param : structList)
+    for (TypeInfo param : structList)
         {
         //printf("size = %d\n", GetSizeOf(param.m_name));
         EXPECT_EQ(GetSizeOf(param.m_name), param.m_size ) << param.m_name.c_str() << " size is not matching";
         }
     }
+//---------------------------------------------------------------------------------------
+// @bsimethod                                        Umar.Hayat                05/15
+//---------------------------------------------------------------------------------------
+TEST_F(Compatibility_Test, AlignCheck) 
+{
+    ASSERT_FALSE(IsConfigLoadError) << "Configuration file not loaded properly";
+    bvector<TypeInfo> structList;
+    GetBentleyStructList(structList);
+    GetGeomStructList(structList);
+    for (TypeInfo param : structList)
+    {
+    //printf("size = %d\n", GetSizeOf(param.m_name));
+    EXPECT_EQ(GetAlignOf(param.m_name), param.m_alignment) << param.m_name.c_str() << " alignment is not matching";
+    }
+}
 
 //#endif // WIP_NOT_PORTABLE
