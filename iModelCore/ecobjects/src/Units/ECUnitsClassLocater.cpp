75b465ba89917315c5274addec3f5893f6e0f9ce
Jeff.Marker <none@none>
2015-05-29 16:18:56 -0400

    EF: At least on iOS, if two compilands define the same string constant, there are actually two copies. Thus, you cannot do an address comparison, you must do a string comparison.

--- iModelCore/ecobjects/src/Units/ECUnitsClassLocater.cpp
+++ iModelCore/ecobjects/src/Units/ECUnitsClassLocater.cpp
@@ -72,9 +72,9 @@ ECClassCP ECUnitsClassLocater::_LocateClass (WCharCP schemaName, WCharCP classNa
     if (!Initialize())
         return NULL;
 
-    if (UNITS_SCHEMA == schemaName)
+    if (0 == wcscmp(UNITS_SCHEMA, schemaName))
         return m_unitsSchema->GetClassCP (className);
-    else if (KOQ_SCHEMA == schemaName)
+    else if (0 == wcscmp(KOQ_SCHEMA, schemaName))
         return m_koqSchema->GetClassCP (className);
     /* else */
     return NULL;
