8d8d33d21cb52f0c43ad051606553cc4ad3a6c9a
Jeff.Marker <none@none>
2015-06-04 13:16:00 -0400

    CM: Cleanup up TextString.h/cpp to match new header publishing/inlining/export standards. CM: Documentation for TextString.h

--- iModelCore/DgnPlatform/DgnCore/TextString.cpp
+++ iModelCore/DgnPlatform/DgnCore/TextString.cpp
@@ -13,7 +13,6 @@ using namespace flatbuffers;
 //---------------------------------------------------------------------------------------
 // @bsimethod                                                   Jeff.Marker     01/2015
 //---------------------------------------------------------------------------------------
-TextStringStylePtr TextStringStyle::Create() { return new TextStringStyle(); }
 TextStringStyle::TextStringStyle() :
     T_Super(),
     m_font(&DgnFontManager::GetLastResortTrueTypeFont()),
@@ -28,9 +27,6 @@ TextStringStyle::TextStringStyle() :
 //---------------------------------------------------------------------------------------
 // @bsimethod                                                   Jeff.Marker     01/2015
 //---------------------------------------------------------------------------------------
-TextStringStylePtr TextStringStyle::Clone() const { return new TextStringStyle(*this); }
-TextStringStyle::TextStringStyle(TextStringStyleCR rhs) : T_Super(rhs) { CopyFrom(rhs); }
-TextStringStyleR TextStringStyle::operator=(TextStringStyleCR rhs) { T_Super::operator=(rhs); if (&rhs != this) CopyFrom(rhs); return *this; }
 void TextStringStyle::CopyFrom(TextStringStyleCR rhs)
     {
     // Making additions or changes? Please check constructor and Reset.
@@ -56,29 +52,6 @@ void TextStringStyle::Reset()
     m_size.Zero();
     }
 
-//---------------------------------------------------------------------------------------
-// @bsimethod                                                   Jeff.Marker     01/2015
-//---------------------------------------------------------------------------------------
-ColorDefCR TextStringStyle::GetColor() const { return m_color; }
-void TextStringStyle::SetColor(ColorDefCR value) { m_color = value; }
-DgnFontCR TextStringStyle::GetFont() const { return *m_font; }
-void TextStringStyle::SetFont(DgnFontCR value) { m_font = &DgnFontManager::ResolveFont(&value); }
-bool TextStringStyle::IsBold() const { return m_isBold; }
-void TextStringStyle::SetIsBold(bool value) { m_isBold = value; }
-bool TextStringStyle::IsItalic() const { return m_isItalic; }
-void TextStringStyle::SetIsItalic(bool value) { m_isItalic = value; }
-bool TextStringStyle::IsUnderlined() const { return m_isUnderlined; }
-void TextStringStyle::SetIsUnderlined(bool value) { m_isUnderlined = value; }
-DPoint2dCR TextStringStyle::GetSize() const { return m_size; }
-void TextStringStyle::SetSize(DPoint2dCR value) { m_size = value; }
-
-double TextStringStyle::GetHeight() const { return m_size.y; }
-void TextStringStyle::SetHeight(double value) { m_size.y = value; }
-double TextStringStyle::GetWidth() const { return m_size.x; }
-void TextStringStyle::SetWidth(double value) { m_size.x = value; }
-void TextStringStyle::SetSize(double widthAndHeight) { SetSize(widthAndHeight, widthAndHeight); }
-void TextStringStyle::SetSize(double width, double height) { m_size.x = width; m_size.y = height; }
-
 //*****************************************************************************************************************************************************************************************************
 //*****************************************************************************************************************************************************************************************************
 
@@ -89,7 +62,6 @@ static const double DEFAULT_UNDERLINE_OFFSET_FACTOR = 0.15;
 //---------------------------------------------------------------------------------------
 // @bsimethod                                                   Jeff.Marker     01/2015
 //---------------------------------------------------------------------------------------
-TextStringPtr TextString::Create() { return new TextString(); }
 TextString::TextString() :
     T_Super(),
     m_isValid(false)
@@ -102,9 +74,6 @@ TextString::TextString() :
 //---------------------------------------------------------------------------------------
 // @bsimethod                                                   Jeff.Marker     01/2015
 //---------------------------------------------------------------------------------------
-TextStringPtr TextString::Clone() const { return new TextString(*this); }
-TextString::TextString(TextStringCR rhs) : T_Super(rhs) { CopyFrom(rhs); }
-TextStringR TextString::operator=(TextStringCR rhs) { T_Super::operator=(rhs); if (&rhs != this) CopyFrom(rhs); return *this; }
 void TextString::CopyFrom(TextStringCR rhs)
     {
     // Making additions or changes? Please check constructor and Reset.
@@ -137,25 +106,6 @@ void TextString::Reset()
     m_glyphIds.clear();
     m_glyphOrigins.clear();
     }
-//---------------------------------------------------------------------------------------
-// @bsimethod                                                   Jeff.Marker     01/2015
-//---------------------------------------------------------------------------------------
-Utf8StringCR TextString::GetText() const { return m_text; }
-void TextString::SetText(Utf8CP value) { Invalidate(); m_text.AssignOrClear(value); }
-TextStringStyleCR TextString::GetStyle() const { return m_style; }
-TextStringStyleR TextString::GetStyleR() { Invalidate(); return m_style; }
-void TextString::SetStyle(TextStringStyleCR value) { Invalidate(); m_style = value; }
-DPoint3dCR TextString::GetOrigin() const { return m_origin; }
-void TextString::SetOrigin(DPoint3dCR value) { m_origin = value; }
-RotMatrixCR TextString::GetOrientation() const { return m_orientation; }
-void TextString::SetOrientation(RotMatrixCR value) { m_orientation = value; }
-
-void TextString::Invalidate() { m_isValid = false; }
-DRange2dCR TextString::GetRange() const { Update(); return m_range; }
-size_t TextString::GetNumGlyphs() const { Update(); return m_glyphIds.size(); }
-DgnGlyphCP const* TextString::GetGlyphs() const { Update(); return &m_glyphs[0]; }
-DgnGlyph::T_Id const* TextString::GetGlyphIds() const { Update(); return &m_glyphIds[0]; }
-DPoint3dCP TextString::GetGlyphOrigins() const { Update(); return &m_glyphOrigins[0]; }
 
 //---------------------------------------------------------------------------------------
 // @bsimethod                                                   Jeff.Marker     01/2015
@@ -174,8 +124,6 @@ void TextString::ComputeGlyphAxes(DVec3dR xAxis, DVec3dR yAxis) const
 //---------------------------------------------------------------------------------------
 // @bsimethod                                                   Jeff.Marker     01/2015
 //---------------------------------------------------------------------------------------
-void TextString::ComputeBoundingShape(DPoint3dP boxPts) const { ComputeBoundingShape(boxPts, 0.0, 0.0); }
-void TextString::ComputeBoundingShape(DPoint3dP boxPts, double uniformPadding) const { ComputeBoundingShape(boxPts, uniformPadding, uniformPadding); }
 void TextString::ComputeBoundingShape(DPoint3dP boxPts, double horizontalPadding, double verticalPadding) const
     {
     Update();
@@ -272,15 +220,6 @@ void TextString::SetOriginFromJustificationOrigin(DPoint3dCR jorigin, Horizontal
 //---------------------------------------------------------------------------------------
 // @bsimethod                                                   Jeff.Marker     01/2015
 //---------------------------------------------------------------------------------------
-Transform TextString::ComputeTransform() const
-    {
-    return Transform::From(m_orientation, m_origin);
-    }
-
-//---------------------------------------------------------------------------------------
-// @bsimethod                                                   Jeff.Marker     01/2015
-//---------------------------------------------------------------------------------------
-void TextString::Update() const { const_cast<TextStringP>(this)->Update(); }
 void TextString::Update()
     {
     if (m_isValid)
