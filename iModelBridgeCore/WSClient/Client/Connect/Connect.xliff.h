a8c91064d233cb91715ce61e422d02e95a7939a6
Keith.Bentley <none@none>
2015-06-05 16:31:03 -0400

    rework TxnManager, change L10N approach to always use strings rather than ids, added DgnTool::ToolId

--- iModelBridgeCore/WSClient/Client/Connect/Connect.xliff.h
+++ iModelBridgeCore/WSClient/Client/Connect/Connect.xliff.h
@@ -14,30 +14,28 @@
 // @bsiclass
 //=======================================================================================
 MOBILEDGN_TRANSLATABLE_STRINGS_START (ConnectL10N, Connect)
-    {
-    ALERT_SignInFailed_ServerError,          // =="Could not connect to server. Check your network connection."==
-    ALERT_SignInFailed_Message,              // =="Please enter the correct email and password."==
-    FIELDAPPS_SingleSignOn,                  // =="Single sign-on"==
-    FIELDAPPS_DontHaveAnAccount,             // =="Don't have an account yet?"==
-    FIELDAPPS_SignUp,                        // =="Sign up"==
-    FIELDAPPS_Email,                         // =="Email"==
-    FIELDAPPS_Password,                      // =="Password"==
-    FIELDAPPS_ForgotPassword,                // =="Forgot Password?"==
-    FIELDAPPS_SignIn,                        // =="Sign in"==
-    FIELDAPPS_ProductDemo,                   // =="Product Demo"==
-    FIELDAPPS_EmailAndPasswordRequired,      // =="You must enter an email address and password."==
-    FIELDAPPS_Error,                         // =="Error"==
-    FIELDAPPS_SignInFailed,                  // =="Sign In Failed"==
-    FIELDAPPS_UnknownError,                  // =="Unknown error."==
-    FIELDAPPS_SignInUsername,                // =="Signed in username:"==
-    FIELDAPPS_SignOut,                       // =="Sign Out"==
-    FIELDAPPS_SigningInDotDotDot,            // =="Signing In..."==
-    FIELDAPPS_EulaAccept,                    // =="Accept"==
-    FIELDAPPS_EulaDecline,                   // =="Decline"==
-    FIELDAPPS_EulaAgree,                     // =="To continue you must accept the terms in the Licence Agreement"==
-    FIELDAPPS_EulaHeader,                    // =="End-user license agreement"==
-    FIELDAPPS_EulaChecking,                  // =="Checking EULA..."==
-    };
+    L10N_STRING(ALERT_SignInFailed_ServerError)          // =="Could not connect to server. Check your network connection."==
+    L10N_STRING(ALERT_SignInFailed_Message)              // =="Please enter the correct email and password."==
+    L10N_STRING(FIELDAPPS_SingleSignOn)                  // =="Single sign-on"==
+    L10N_STRING(FIELDAPPS_DontHaveAnAccount)             // =="Don't have an account yet?"==
+    L10N_STRING(FIELDAPPS_SignUp)                        // =="Sign up"==
+    L10N_STRING(FIELDAPPS_Email)                         // =="Email"==
+    L10N_STRING(FIELDAPPS_Password)                      // =="Password"==
+    L10N_STRING(FIELDAPPS_ForgotPassword)                // =="Forgot Password?"==
+    L10N_STRING(FIELDAPPS_SignIn)                        // =="Sign in"==
+    L10N_STRING(FIELDAPPS_ProductDemo)                   // =="Product Demo"==
+    L10N_STRING(FIELDAPPS_EmailAndPasswordRequired)      // =="You must enter an email address and password."==
+    L10N_STRING(FIELDAPPS_Error)                         // =="Error"==
+    L10N_STRING(FIELDAPPS_SignInFailed)                  // =="Sign In Failed"==
+    L10N_STRING(FIELDAPPS_UnknownError)                  // =="Unknown error."==
+    L10N_STRING(FIELDAPPS_SignInUsername)                // =="Signed in username:"==
+    L10N_STRING(FIELDAPPS_SignOut)                       // =="Sign Out"==
+    L10N_STRING(FIELDAPPS_SigningInDotDotDot)            // =="Signing In..."==
+    L10N_STRING(FIELDAPPS_EulaAccept)                    // =="Accept"==
+    L10N_STRING(FIELDAPPS_EulaDecline)                   // =="Decline"==
+    L10N_STRING(FIELDAPPS_EulaAgree)                     // =="To continue you must accept the terms in the Licence Agreement"==
+    L10N_STRING(FIELDAPPS_EulaHeader)                    // =="End-user license agreement"==
+    L10N_STRING(FIELDAPPS_EulaChecking)                  // =="Checking EULA..."==
 MOBILEDGN_TRANSLATABLE_STRINGS_END
 
-#define ConnectLocalizedString(K) ConnectL10N::GetString(ConnectL10N::K, #K)
+#define ConnectLocalizedString(K) ConnectL10N::GetString(ConnectL10N::K())
--- iModelBridgeCore/WSClient/Client/WSError.xliff.h
+++ iModelBridgeCore/WSClient/Client/WSError.xliff.h
@@ -12,11 +12,9 @@
 // @bsiclass
 //=======================================================================================
 MOBILEDGN_TRANSLATABLE_STRINGS_START (WSErrorL10N, WSError)
-    {
-    MESSAGE_ServerNotSupported,         // =="Server is not supported. Please check server address or contact your server administrator."==
-    MESSAGE_FunctionalityNotSupported,  // =="Requested functionality is not supported for this server version."==
-    MESSAGE_UnknownError,               // =="Unknown error. Please contact your server administrator."==
-    };
+    L10N_STRING(MESSAGE_ServerNotSupported)         // =="Server is not supported. Please check server address or contact your server administrator."==
+    L10N_STRING(MESSAGE_FunctionalityNotSupported)  // =="Requested functionality is not supported for this server version."==
+    L10N_STRING(MESSAGE_UnknownError)               // =="Unknown error. Please contact your server administrator."==
 MOBILEDGN_TRANSLATABLE_STRINGS_END
 
-#define WSErrorLocalizedString(K) WSErrorL10N::GetString (WSErrorL10N::K, #K)
+#define WSErrorLocalizedString(K) WSErrorL10N::GetString(WSErrorL10N::K())
