a943e7203a1633bda9dbd1434550dae25e2c6875
Alain.Robert <none@none>
2015-06-11 11:46:33 -0400

    EF - TFS#22440 - Fixed csmap, geocoord and GCS dictionaries to allow non-earth ellipsoids EN - Introduced two Mars based ellipsoids and two Mars based datums

--- iModelCore/GeoCoord/BaseGeoCoord/GCSLibrary.cpp
+++ iModelCore/GeoCoord/BaseGeoCoord/GCSLibrary.cpp
@@ -2,7 +2,7 @@
 |
 |   $Source: BaseGeoCoord/GCSLibrary.cpp $
 |
-|  $Copyright: (c) 2013 Bentley Systems, Incorporated. All rights reserved. $
+|  $Copyright: (c) 2015 Bentley Systems, Incorporated. All rights reserved. $
 |
 +----------------------------------------------------------------------*/
 
@@ -1635,10 +1635,10 @@ virtual CSEllipsoidDef* GetEllipsoid (WCharCP ellipsoidName) override
     stream = NULL;
 
     /* Check the ellipsoid definition for valitity. */
-    double cs_ERadMax = 7.0E+06;
-    double cs_PRadMax = 7.0E+06;
-    double cs_ERadMin = 6.0E+06;
-    double cs_PRadMin = 6.0E+06;
+    double cs_ERadMax = 7.0E+08; // Modified from original value of 7.0E+06 to support solar system objects other than Earth.
+    double cs_PRadMax = 7.0E+08; // Modified from original value of 7.0E+06 to support solar system objects other than Earth.
+    double cs_ERadMin = 1.0E+02; // Modified from original value of 6.0E+06 to support solar system objects other than Earth.
+    double cs_PRadMin = 1.0E+02; // Modified from original value of 6.0E+06 to support solar system objects other than Earth.
     if (elptr->e_rad < cs_ERadMin || elptr->e_rad > cs_ERadMax ||
         elptr->p_rad < cs_PRadMin || elptr->p_rad > cs_PRadMax)
         {
