da2ba60412970d4c8085581681cb5e4d32683352
Sam.Wilson <none@none>
2015-07-09 07:25:56 -0400

    BF comment out MapQuest in a way that does not break optimized build

--- iModelCore/DgnPlatform/DgnCore/WebMercator.cpp
+++ iModelCore/DgnPlatform/DgnCore/WebMercator.cpp
@@ -1393,18 +1393,18 @@ BentleyStatus dgn_ModelHandler::StreetMap::_CreateUrl (Utf8StringR url, ImageUti
         return BSIERROR;
         }
 
-#ifdef WIP_MAPQUEST_NO_LONGER_AVAILABLE
     if (props.m_mapService[0] == '0') // "(c) OpenStreetMap contributors"
-        url = CreateMapquestUrl (tileid, props);
-#else
-    #ifndef NDEBUG  // *** In a developer build, go ahead and use OSM
-    if (true)
-        url = Utf8PrintfString ("http://tile.openstreetmap.org/%d/%d/%d.png", tileid.zoomLevel, tileid.column, tileid.row);
-    #endif
-#endif
+        {
+        #ifdef WIP_MAPQUEST_NO_LONGER_AVAILABLE
+            url = CreateMapquestUrl (tileid, props);
+        #else
+            #ifndef NDEBUG  // *** In a developer build, go ahead and use OSM
+                url = Utf8PrintfString ("http://tile.openstreetmap.org/%d/%d/%d.png", tileid.zoomLevel, tileid.column, tileid.row);
+            #endif
+        #endif
+        }
 #ifdef WIP_MAP_SERVICE
-    else
-    if (OpenStreetMaps)
+    else if (OpenStreetMaps)
         url = Utf8PrintfString ("http://tile.openstreetmap.org/%d/%d/%d.png", tileid.zoomLevel, tileid.column, tileid.row);
     else if (GoogleMaps)
         url = CreateGoogleMapsUrl (tileid);
