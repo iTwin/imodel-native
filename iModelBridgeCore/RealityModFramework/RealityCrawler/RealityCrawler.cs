5c95725a5479dae3dbd18a9a660739c5533006f5
Martin-Yanick.Guille <none@none>
2015-07-10 14:50:23 -0400

    Add a HowToBuild document and clean the code.

--- iModelBridgeCore/RealityModFramework/RealityCrawler/RealityCrawler.cs
+++ iModelBridgeCore/RealityModFramework/RealityCrawler/RealityCrawler.cs
@@ -14,52 +14,14 @@ using Bentley.RealityPlatform.RealityCrawler;
 using System.Diagnostics;
 using System.Configuration;
 
-// For threading
-//using System.Threading;
-
 namespace Bentley.RealityPlatform.RealityCrawler
     {
-    public class CustomCrawlDecisionMaker : CrawlDecisionMaker
-        {
-        public override CrawlDecision ShouldCrawlPage (PageToCrawl pageToCrawl, CrawlContext crawlContext)
-            {
-            CrawlDecision decision = base.ShouldCrawlPage (pageToCrawl, crawlContext);
-            if ( decision.Allow && !pageToCrawl.IsRoot )
-                {
-                if ( pageToCrawl.Uri.Authority == "google.com" )
-                    decision = new CrawlDecision
-                    {
-                        Allow = false, Reason = "Ignoring google links"
-                    };
-                }
-
-            return decision;
-            }
-
-        public override CrawlDecision ShouldCrawlPageLinks (CrawledPage crawledPage, CrawlContext crawlContext)
-            {
-            CrawlDecision decision = base.ShouldCrawlPageLinks (crawledPage, crawlContext);
-            //if (decision.Allow && !crawledPage.IsRoot)
-            //{
-            //    if (crawledPage.Uri.Authority == "google.com")
-            //        decision = new CrawlDecision { Allow = false, Reason = "Ignoring google links" };
-            //}
-
-            return decision;
-            }
-        }
-
     class RealityCrawler
         {
-        // For threading
-        //private System.Object lockThis = new System.Object();
-
         static void Main (string[] args)
             {
-            Console.Title = "GIS Crawler";
-            //m_dictionaryWMS = new Dictionary();
+            Console.Title = "GIS Crawler";            
             log4net.Config.XmlConfigurator.Configure ();
-            //PrintDisclaimer();
 
             Console.WriteLine ("GIS Crawler");
             Console.WriteLine ("Press 'u' key to update or any key to crawl.");
@@ -112,8 +74,6 @@ namespace Bentley.RealityPlatform.RealityCrawler
 
                     //Start the crawl
                     //This is a synchronous call
-                    //CrawlResult result = crawler.Crawl(uriToCrawl);
-
                     CrawlResult result = crawler.Crawl (seed.uri);
                     Console.ForegroundColor = ConsoleColor.DarkGray;
                     System.Console.WriteLine ("Seed Elapsed Time:{0} milliseconds", result.Elapsed.TotalMilliseconds);
@@ -127,12 +87,6 @@ namespace Bentley.RealityPlatform.RealityCrawler
             Console.WriteLine ();
             Console.WriteLine ("Total Crawling elapsed time: {0} Day(s), {1} hour(s) {2} minute(s) {3} second(s) {4} milisecond(s)", timeTaken.Days, timeTaken.Hours, timeTaken.Minutes, timeTaken.Seconds, timeTaken.Milliseconds);
 
-            //Now go view the log.txt file that is in the same directory as this executable. It has
-            //all the statements that you were trying to read in the console window :).
-            //Not enough data being logged? Change the app.config file's log4net log level from "INFO" TO "DEBUG"
-
-            //PrintDisclaimer();
-
             // Keep the console window open in debug mode.
             Console.WriteLine ("Press any key to exit.");
             Console.ReadKey ();
@@ -140,11 +94,8 @@ namespace Bentley.RealityPlatform.RealityCrawler
 
         private static IWebCrawler GetDefaultWebCrawler (CrawlConfiguration crawlConfig)
             {
-            //CustomCrawlDecisionMaker myCustomCrawlerInstance = new CustomCrawlDecisionMaker ();
-            //return new PoliteWebCrawler();
             //Will use the manually created crawlConfig object created above
             return new  PoliteWebCrawler(crawlConfig, null, null, null, null, null, null, null, null);
-            //return new PoliteWebCrawler (null, myCustomCrawlerInstance, null, null, null, null, null, null, null);
             }
 
         private static void PrintDisclaimer ()
@@ -179,11 +130,6 @@ namespace Bentley.RealityPlatform.RealityCrawler
             {
             CrawledPage crawledPage = e.CrawledPage;
 
-            //if ( crawledPage.WebException != null || crawledPage.HttpWebResponse.StatusCode != HttpStatusCode.OK )
-            //    Console.WriteLine ("Crawl of page failed {0}", crawledPage.Uri.AbsoluteUri);
-            //else
-            //    Console.WriteLine ("Crawl of page succeeded {0}", crawledPage.Uri.AbsoluteUri);
-
             if ( string.IsNullOrEmpty (crawledPage.Content.Text) )
                 Console.WriteLine ("Page had no content {0}", crawledPage.Uri.AbsoluteUri);
             else
@@ -191,7 +137,6 @@ namespace Bentley.RealityPlatform.RealityCrawler
                 Console.WriteLine ("Analyzing the links of the PAGE {0}", crawledPage.Uri.AbsoluteUri);
 
                 KeywordDictionary dictionaryWMS = new KeywordDictionary ("DictionaryWMS.xml");
-                //SeedCreator foundSeed = new SeedCreator ();
                 Dictionary<int, string>CrawledServerList = ServerWMS.GenerateCrawledServerList();
                 if (crawledPage.ParsedLinks != null)
                     {                    
@@ -209,9 +154,6 @@ namespace Bentley.RealityPlatform.RealityCrawler
                                     Console.ResetColor ();
                                     }
                                 WMSKeywordURL.Save ();
-                                //RRHandler Test = new RRHandler(foundLink);
-                                //foundSeed.Add(foundLink);
-                                //Test.GetXMLStream();
                                 }
                             else
                                 {
@@ -222,7 +164,6 @@ namespace Bentley.RealityPlatform.RealityCrawler
                             }
                         }
                     }
-                //foundSeed.Write();
                 }
             }
 
--- iModelBridgeCore/RealityModFramework/RealityCrawler/SeedCreator.cs
+++ iModelBridgeCore/RealityModFramework/RealityCrawler/SeedCreator.cs
@@ -23,24 +23,43 @@ namespace Bentley.RealityPlatform.RealityCrawler
         protected Boolean m_Append;
         #endregion
 
+        #region Constructor
+        /// <summary>
+        /// Constructor of the seed creator that use the defaut seed file 
+        /// name "CrawledSeed.xml" in the current application path and 
+        /// that append the seed at the end of the file by default.
+        /// </summary>
         public SeedCreator()
             : this(DEFAULT_APPEND_VALUE)
         {
 
         }
 
+        /// <summary>
+        /// Constructor of the seed creator that use the defaut seed file name "CrawledSeed.xml" in the current application path.
+        /// </summary>
+        /// <param name="append">True to append at the end of the file, False to overwrite.</param>
         public SeedCreator(Boolean append)
             : this(DEFAULT_SEED_FILE, append)
         {
 
         }
 
+        /// <summary>
+        /// Constructor of the seed creator that append the seed at the end of the file by default.
+        /// </summary>
+        /// <param name="xmlFilePath">Path of the seed file.</param>
         public SeedCreator(String xmlFilePath)
             : this(xmlFilePath, DEFAULT_APPEND_VALUE)
         {
 
         }
 
+        /// <summary>
+        /// Constructor of the seed creator.
+        /// </summary>
+        /// <param name="xmlFilePath">Path of the seed file.</param>
+        /// <param name="append">True to append at the end of the file, False to overwrite.</param>
         public SeedCreator(String xmlFilePath, Boolean append)
         {
             m_XMLFilePath = xmlFilePath;
@@ -61,15 +80,23 @@ namespace Bentley.RealityPlatform.RealityCrawler
                 }
             }
         }
+        #endregion
 
         #region Methods
         #region Public Methods
+        /// <summary>
+        /// Add a new seed to the seed list.
+        /// </summary>
+        /// <param name="seed">Uri of the seed.</param>
         public void Add(Uri seed)
         {
             if (!m_seedList.Contains(seed))
                 m_seedList.Add(seed);
         }
 
+        /// <summary>
+        /// Save the new seed list.
+        /// </summary>
         public void Write() 
         {
             XmlWriter writer = null;
@@ -81,12 +108,7 @@ namespace Bentley.RealityPlatform.RealityCrawler
                 settings.Indent = true;
                 settings.IndentChars = ("\t");
                 settings.OmitXmlDeclaration = false;
-
-                //XmlTextWriter writer = new XmlTextWriter(m_XMLFilePath, System.Text.Encoding.UTF8);
                 writer = XmlWriter.Create(m_XMLFilePath, settings);
-                //writer.WriteStartDocument(true);
-                //writer.Formatting = Formatting.Indented;
-                //writer.Indentation = 2;
                 writer.WriteStartElement(SEED_NODE_ROOT);
                 foreach (Uri seed in m_seedList)
                 {
