15897dd301b436d5cc8af8273efb2fde56c4f0b8
Eric.Paquet <none@none>
2015-07-16 10:58:26 -0400

    Add methods to ImagePPAdmin to correctly handle different paths required by ImagePP.

--- Domains/RealityModeling/RasterSchema/ImagePPAdmin.cpp
+++ Domains/RealityModeling/RasterSchema/ImagePPAdmin.cpp
@@ -57,3 +57,60 @@ ImagePP::IRasterGeoCoordinateServices* MyImageppLibAdmin::_GetIRasterGeoCoordina
 
     //return NULL;
     }
+
+//-----------------------------------------------------------------------------------------
+// @bsimethod                                                   Eric.Paquet         07/2015
+//-----------------------------------------------------------------------------------------
+BentleyStatus MyImageppLibAdmin::_GetDefaultTempDirectory(BeFileName& tempFileName) const
+    {
+    // Return the temp directory name. The directory is created if it does not exist.
+    T_HOST.GetIKnownLocationsAdmin().GetLocalTempDirectory(tempFileName, L"Raster");
+    return BSISUCCESS;
+    }
+
+//-----------------------------------------------------------------------------------------
+// @bsimethod                                                   Eric.Paquet         07/2015
+//-----------------------------------------------------------------------------------------
+BentleyStatus MyImageppLibAdmin::_GetLocalCacheDirPath(BeFileName& tempPath, bool checkForChange) const
+    {
+    //If not empty and it exist, return it 
+    if (!checkForChange && !m_localDirPath.IsEmpty() && BeFileName::IsDirectory(m_localDirPath.GetName()))
+        {
+        tempPath = m_localDirPath;
+        return BSISUCCESS;
+        }
+
+    //Use default cache folder,m_localDirPath will be set by this call
+    return T_Super::_GetLocalCacheDirPath(tempPath); 
+    }
+
+//-----------------------------------------------------------------------------------------
+// @bsimethod                                                   Eric.Paquet         07/2015
+//-----------------------------------------------------------------------------------------
+BentleyStatus MyImageppLibAdmin::_GetGDalDataPath(WStringR gdalDataPath) const
+    {
+    BeFileName path = T_HOST.GetIKnownLocationsAdmin().GetDgnPlatformAssetsDirectory();
+    path.AppendToPath (L"GDalData");
+
+    // Convert BeFileName to WString
+    BeFileName::BuildName (gdalDataPath, path.GetDevice().c_str(), path.GetDirectoryWithoutDevice().c_str(), path.GetFileNameWithoutExtension().c_str(), path.GetExtension().c_str());
+    BeFileName::AppendSeparator (gdalDataPath);
+
+    return BSISUCCESS;
+    }
+
+//-----------------------------------------------------------------------------------------
+// @bsimethod                                                   Eric.Paquet         07/2015
+//-----------------------------------------------------------------------------------------
+BentleyStatus MyImageppLibAdmin::_GetECWDataPath(WStringR ecwDataPath) const
+    {
+    BeFileName path = T_HOST.GetIKnownLocationsAdmin().GetDgnPlatformAssetsDirectory();
+    path.AppendToPath (L"ECWData");
+
+    // Convert BeFileName to WString
+    BeFileName::BuildName (ecwDataPath, path.GetDevice().c_str(), path.GetDirectoryWithoutDevice().c_str(), path.GetFileNameWithoutExtension().c_str(), path.GetExtension().c_str());
+    BeFileName::AppendSeparator (ecwDataPath);
+
+    return BSISUCCESS;
+    }
+
