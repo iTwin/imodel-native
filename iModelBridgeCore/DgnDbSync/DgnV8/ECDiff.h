b440db43d157666df042817faa7b31afe9db0ce6
Carole.MacDonald <none@none>
2015-07-21 13:24:48 -0400

    Merge.

--- iModelBridgeCore/DgnDbSync/DgnV8/ECDiff.h
+++ iModelBridgeCore/DgnDbSync/DgnV8/ECDiff.h
@@ -2,7 +2,7 @@
 |
 |     $Source: PublicApi/ECObjects/ECDiff.h $
 |
-|  $Copyright: (c) 2014 Bentley Systems, Incorporated. All rights reserved. $
+|  $Copyright: (c) 2015 Bentley Systems, Incorporated. All rights reserved. $
 |
 +--------------------------------------------------------------------------------------*/
 #pragma once
