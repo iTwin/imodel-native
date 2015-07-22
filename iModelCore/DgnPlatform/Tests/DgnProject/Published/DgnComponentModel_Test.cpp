d7e7c84fb8195ca77b9eb03299241ee777dce9b0
Sam.Wilson <none@none>
2015-07-22 15:15:37 -0400

    WIP disable DgnComponentModel_Test on platforms that don't support JavaScript

--- iModelCore/DgnPlatform/Tests/DgnProject/Published/DgnComponentModel_Test.cpp
+++ iModelCore/DgnPlatform/Tests/DgnProject/Published/DgnComponentModel_Test.cpp
@@ -5,6 +5,7 @@
 |  $Copyright: (c) 2015 Bentley Systems, Incorporated. All rights reserved. $
 |
 +--------------------------------------------------------------------------------------*/
+#ifndef BENTLEYCONFIG_NO_JAVASCRIPT
 #include "DgnHandlersTests.h"
 #include <DgnPlatform/DgnPlatformLib.h>
 #include <DgnPlatform/DgnCore/DgnScriptContext.h>
@@ -530,3 +531,4 @@ TEST_F(ComponentModelTest, SimulateDeveloperAndClient)
     SimulateDeveloper();
     SimulateClient();
     }
+#endif //ndef BENTLEYCONFIG_NO_JAVASCRIPT
