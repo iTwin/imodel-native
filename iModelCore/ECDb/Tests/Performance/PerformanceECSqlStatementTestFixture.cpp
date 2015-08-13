ab20cfbf5eb70db69e87131baa57795c7ba221fc
Sam.Wilson <none@none>
2015-08-13 11:42:54 -0400

    BF WIP_STUB_FOR_MISSING_CLASS_PerformanceResultRecorder

--- iModelCore/ECDb/Tests/Performance/PerformanceECSqlStatementTestFixture.cpp
+++ iModelCore/ECDb/Tests/Performance/PerformanceECSqlStatementTestFixture.cpp
@@ -16,7 +16,7 @@ BEGIN_ECDBUNITTESTS_NAMESPACE
 // *** WIP_STUB_FOR_MISSING_CLASS_PerformanceResultRecorder
 struct PerformanceResultRecorder
 {
-static void PerformanceResultRecorder::LogToDb(Utf8String,Utf8String,double,Utf8String,int optional=0) {;}
+static void LogToDb(Utf8String,Utf8String,double,Utf8String,int optional=0) {;}
 };
 
 /*---------------------------------------------------------------------------------**//**
