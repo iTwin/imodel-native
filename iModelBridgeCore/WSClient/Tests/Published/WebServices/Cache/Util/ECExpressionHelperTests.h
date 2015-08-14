8430922eec7e278590c262ffe7c01a8c3b360dbc
Vincas Razma <vincas.razma@bentley.com>
2015-08-14 15:05:13 +0300

    ECExpressionHelper to not match whitespace in property accessors SRB: Beneditkas.Lipnickas

---
+++ iModelBridgeCore/WSClient/Tests/Published/WebServices/Cache/Util/ECExpressionHelperTests.h
@@ -0,0 +1,14 @@
+/*--------------------------------------------------------------------------------------+
+|
+|     $Source: Tests/Published/WebServices/Cache/Util/ECExpressionHelperTests.h $
+|
+|  $Copyright: (c) 2015 Bentley Systems, Incorporated. All rights reserved. $
+|
++--------------------------------------------------------------------------------------*/
+
+#pragma once
+
+#include "../../WebServicesTestsHelper.h"
+
+class ECExpressionHelperTests : public WSClientBaseTest
+    {};
