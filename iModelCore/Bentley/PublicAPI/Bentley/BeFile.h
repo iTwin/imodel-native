3959f4fd8cba07c896500a12f211a9a0efef61b5
Keith.Bentley <none@none>
2015-08-28 09:06:18 -0400

    cosmetic

--- iModelCore/Bentley/PublicAPI/Bentley/BeFile.h
+++ iModelCore/Bentley/PublicAPI/Bentley/BeFile.h
@@ -71,45 +71,45 @@ protected:
     BeFileAccess        m_mode;
     mutable BeFileStatus m_lastError;
 
-    static BENTLEYDLL_EXPORT uint32_t GetWinntAccessMode (BeFileAccess m);
+    static BENTLEYDLL_EXPORT uint32_t GetWinntAccessMode(BeFileAccess m);
 
     //! Used only by subclasses.
-    BENTLEYDLL_EXPORT BeFileStatus SetLastError () const;
+    BENTLEYDLL_EXPORT BeFileStatus SetLastError() const;
 
 private:
-    BeFile (BeFile const&) {BeAssert(false);}  // No!
+    BeFile(BeFile const&) {BeAssert(false);}  // No!
     BeFile& operator=(BeFile const&) {BeAssert(false); return *this;} // No!
 
 public:
     //! Construct a BeFile object. @see Create and Open.
-    BENTLEYDLL_EXPORT BeFile ();
+    BENTLEYDLL_EXPORT BeFile();
 
     //! Destroys a BeFile instance.
     ~BeFile() {Close();}
 
     //! Swap file handles between two BeFile instances.
     //! @param[in] other    The other file.
-    BENTLEYDLL_EXPORT void Swap (BeFile& other);
+    BENTLEYDLL_EXPORT void Swap(BeFile& other);
 
     //! Creates a new disk file 
     //! @param[in] filename     The full path of the file to create.
     //! @param[in] createAlways If false, returns a status other than BeFileStatus::Success, and does not create a new file if \a filename already exists. If true, deletes any existing file before creating a new one by the same name.
     //! @return BeFileStatus::Success if the file was created or non-zero if create failed.
-    BENTLEYDLL_EXPORT BeFileStatus Create (WCharCP filename, bool createAlways = true);
-    BeFileStatus Create (Utf8String filename, bool createAlways = true) {return Create(WString(filename.c_str(), true).c_str(), createAlways);}
+    BENTLEYDLL_EXPORT BeFileStatus Create(WCharCP filename, bool createAlways = true);
+    BeFileStatus Create(Utf8String filename, bool createAlways = true) {return Create(WString(filename.c_str(), true).c_str(), createAlways);}
 
     //! Opens an existing file.
     //! @param[in] filename     The full path the file to open.
     //! @param[in] mode         The file open mode.
     //! @return BeFileStatus::Success if the file was opened or non-zero if open failed.
-    BENTLEYDLL_EXPORT BeFileStatus Open (WCharCP filename, BeFileAccess mode);
-    BeFileStatus Open (Utf8String filename, BeFileAccess mode) {return Open(WString(filename.c_str(), true).c_str(), mode);}
+    BENTLEYDLL_EXPORT BeFileStatus Open(WCharCP filename, BeFileAccess mode);
+    BeFileStatus Open(Utf8String filename, BeFileAccess mode) {return Open(WString(filename.c_str(), true).c_str(), mode);}
 
     //! Close the disk file
     BENTLEYDLL_EXPORT BeFileStatus Close();
 
     //! Query whether the disk file is open.
-    BENTLEYDLL_EXPORT bool IsOpen () const;
+    BENTLEYDLL_EXPORT bool IsOpen() const;
 
     //! Gets the access mode used to open the disk file.
     BeFileAccess GetAccess() const {return m_mode;}
@@ -133,42 +133,42 @@ public:
     //! @param[in] position The new read/write position.
     //! @param[in] origin   The origin mode for the move.
     //! @return BeFileStatus::Success if the operation was successful or non-zero if it failed.
-    BENTLEYDLL_EXPORT BeFileStatus SetPointer (uint64_t position, BeFileSeekOrigin origin);
+    BENTLEYDLL_EXPORT BeFileStatus SetPointer(uint64_t position, BeFileSeekOrigin origin);
 
     //! Gets the file read/write position
     //! @param[out] position      the current read/write position
     //! @return BeFileStatus::Success if the operation was successful or non-zero if it failed.
-    BENTLEYDLL_EXPORT BeFileStatus GetPointer (uint64_t& position);
+    BENTLEYDLL_EXPORT BeFileStatus GetPointer(uint64_t& position);
 
-    BENTLEYDLL_EXPORT BeFileStatus ReadEntireFile (bvector<Byte>& buffer);
+    BENTLEYDLL_EXPORT BeFileStatus ReadEntireFile(bvector<Byte>& buffer);
 
     //! Reads bytes from the file at the current position. The file position is advanced by the number of bytes read.
     //! @param[out] buffer      Data is copied from the file into this buffer.
     //! @param[out] bytesRead   If not NULL, the number of bytes actually read is returned here
     //! @param[in]  numBytes    The number of bytes to read from the file into \a buffer
     //! @return BeFileStatus::Success if the operation was successful or non-zero if it failed.
-    BENTLEYDLL_EXPORT BeFileStatus Read (void* buffer, uint32_t* bytesRead, uint32_t numBytes);
+    BENTLEYDLL_EXPORT BeFileStatus Read(void* buffer, uint32_t* bytesRead, uint32_t numBytes);
 
     //! Writes bytes to the file at the current position. The file position is advanced by the number of bytes written.
     //! @param[out] bytesWritten    If not NULL, the number of bytes actually written is returned here.
     //! @param[in]  buffer          The data to be written.
     //! @param[in]  numBytes        The number of bytes to be copied from \a buffer into the file.
     //! @return BeFileStatus::Success if the operation was successful or non-zero if it failed.
-    BENTLEYDLL_EXPORT BeFileStatus Write (uint32_t* bytesWritten, void const* buffer, uint32_t numBytes);
+    BENTLEYDLL_EXPORT BeFileStatus Write(uint32_t* bytesWritten, void const* buffer, uint32_t numBytes);
 
     //! Flushes all pending writes to the file
     //! @return BeFileStatus::Success if the operation was successful or non-zero if it failed.
-    BENTLEYDLL_EXPORT BeFileStatus Flush ();
+    BENTLEYDLL_EXPORT BeFileStatus Flush();
 
     //! Gets the size of the file.
     //! @param[out] length  The size of the file in bytes
     //! @return BeFileStatus::Success if the operation was successful or non-zero if it failed.
-    BENTLEYDLL_EXPORT BeFileStatus GetSize (uint64_t& length) const;
+    BENTLEYDLL_EXPORT BeFileStatus GetSize(uint64_t& length) const;
 
     //! Sets the size of the file.
     //! @param[in] length       The new size of the file in bytes.
     //! @return BeFileStatus::Success if the operation was successful or non-zero if it failed.
-    BENTLEYDLL_EXPORT BeFileStatus SetSize (uint64_t length);
+    BENTLEYDLL_EXPORT BeFileStatus SetSize(uint64_t length);
 };
 
 END_BENTLEY_NAMESPACE
