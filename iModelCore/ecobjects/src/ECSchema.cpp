7089475d8603ceee9dc6fa06d4a9ea37398ce39c
Paul.Connelly <none@none>
2015-08-31 10:50:53 -0400

    CM: Const overload of ECSchema::GetCustomAttributeContainer(). The inheritance is not published so published API users could not CM: obtain custom attributes without casting away const. CM: Interface docs should document what the interface does, not known implementations.

--- iModelCore/ecobjects/src/ECSchema.cpp
+++ iModelCore/ecobjects/src/ECSchema.cpp
@@ -2674,10 +2674,8 @@ SchemaKeyCR ECSchema::GetSchemaKey ()const
 /*---------------------------------------------------------------------------------**//**
 * @bsimethod                                                    Paul.Connelly   06/12
 +---------------+---------------+---------------+---------------+---------------+------*/
-IECCustomAttributeContainer& ECSchema::GetCustomAttributeContainer()
-    {
-    return *this;
-    }
+IECCustomAttributeContainer& ECSchema::GetCustomAttributeContainer() { return *this; }
+IECCustomAttributeContainer const& ECSchema::GetCustomAttributeContainer() const { return *this; }
 
 /*---------------------------------------------------------------------------------**//**
 * @bsimethod                                    Abeesh.Basheer                  03/2012
