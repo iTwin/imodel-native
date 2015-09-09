8099b50493e57a483e14b38fb4128b72750d9dd7
Richard.Bois <none@none>
2015-09-09 15:47:12 -0400

    - Use new cvs bin repositories for third party libraries. - Fix examples install script to reflect PointoolsVortexAPI part name change.

--- iModelBridgeCore/Pointools/PointoolsCommon/BuildScripts/InstallAll.bat
+++ iModelBridgeCore/Pointools/PointoolsCommon/BuildScripts/InstallAll.bat
@@ -8,9 +8,9 @@ rem   platform: x86 or x64
 set PointoolsAction=%1
 set PointoolsVortexDest=%2
 set PointoolsPlatform=%3
-msiexec %PointoolsAction% PointoolsVortexAPI_VS2012\PointoolsVortexAPI_Runtime_VS2012%PointoolsPlatform%.msi INSTALLDIR=%PointoolsVortexDest%\bin\VS2012\%PointoolsPlatform%
-msiexec %PointoolsAction% PointoolsVortexAPI_VS2012\PointoolsVortexAPI_API_VS2012%PointoolsPlatform%.msi INSTALLDIR=%PointoolsVortexDest%
-msiexec %PointoolsAction% PointoolsIO_VS2012\PointoolsIO_PODFormats_Runtime_VS2012%PointoolsPlatform%.msi INSTALLDIR=%PointoolsVortexDest%\bin\VS2012\%PointoolsPlatform%
-msiexec %PointoolsAction% PointoolsIO_VS2012\PointoolsIO_PODFormats_API_VS2012%PointoolsPlatform%.msi INSTALLDIR=%PointoolsVortexDest%
-msiexec %PointoolsAction% PointoolsIO_VS2012\PointoolsIO_PODWriter_Runtime_VS2012%PointoolsPlatform%.msi INSTALLDIR=%PointoolsVortexDest%\bin\VS2012\%PointoolsPlatform%
-msiexec %PointoolsAction% PointoolsIO_VS2012\PointoolsIO_PODWriter_API_VS2012%PointoolsPlatform%.msi INSTALLDIR=%PointoolsVortexDest%
+msiexec %PointoolsAction% PointoolsVortexAPI\PointoolsVortexAPI_Runtime_VS2012%PointoolsPlatform%.msi INSTALLDIR=%PointoolsVortexDest%\bin\VS2012\%PointoolsPlatform%
+msiexec %PointoolsAction% PointoolsVortexAPI\PointoolsVortexAPI_API_VS2012%PointoolsPlatform%.msi INSTALLDIR=%PointoolsVortexDest%
+msiexec %PointoolsAction% PointoolsIO\PointoolsIO_PODFormats_Runtime_VS2012%PointoolsPlatform%.msi INSTALLDIR=%PointoolsVortexDest%\bin\VS2012\%PointoolsPlatform%
+msiexec %PointoolsAction% PointoolsIO\PointoolsIO_PODFormats_API_VS2012%PointoolsPlatform%.msi INSTALLDIR=%PointoolsVortexDest%
+msiexec %PointoolsAction% PointoolsIO\PointoolsIO_PODWriter_Runtime_VS2012%PointoolsPlatform%.msi INSTALLDIR=%PointoolsVortexDest%\bin\VS2012\%PointoolsPlatform%
+msiexec %PointoolsAction% PointoolsIO\PointoolsIO_PODWriter_API_VS2012%PointoolsPlatform%.msi INSTALLDIR=%PointoolsVortexDest%
