0b3c810911a3515a855be6a975dff5e239283b38
Krischan.Eberle <none@none>
2015-09-14 12:47:34 +0200

    ECDb now enforces MinOccurs/MaxOccurs for array ECProperties. TFS#285059

--- iModelCore/ECDb/Tests/Published/ECSqlStatementCrud_Tests.cpp
+++ iModelCore/ECDb/Tests/Published/ECSqlStatementCrud_Tests.cpp
@@ -73,7 +73,7 @@ TEST_F (ECSqlSelectTestFixture, AliasTests)
 //+---------------+---------------+---------------+---------------+---------------+------
 TEST_F (ECSqlSelectTestFixture, ECInstanceIdTests)
     {
-    auto dataset = ECSqlSelectTestDataset::ECInstanceIdTests (PerClassRowCount);
+    auto dataset = ECSqlSelectTestDataset::ECInstanceIdTests (GetTestProject(), PerClassRowCount);
     RunTest (dataset);
     }
 
