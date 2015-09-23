987b12fb9715c529e73cb4f6d64013988db54691
Sam.Wilson <none@none>
2015-09-23 15:34:33 -0400

    WIP get Dgnplatform 'noview' tests building

--- iModelCore/BeGTest/Android/BeTestHost.h
+++ iModelCore/BeGTest/Android/BeTestHost.h
@@ -5,6 +5,7 @@
 |  $Copyright: (c) 2015 Bentley Systems, Incorporated. All rights reserved. $
 |
 +--------------------------------------------------------------------------------------*/
+//__BENTLEY_INTERNAL_ONLY__
 #include <Bentley/BeTest.h>
 #include <Bentley/BeAssert.h>
 #include <Bentley/BeFileName.h>
