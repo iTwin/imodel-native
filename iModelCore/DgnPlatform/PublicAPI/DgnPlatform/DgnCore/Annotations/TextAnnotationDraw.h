29f65e5cc612eccc7bc25dacff80f44473698564
Jeff.Marker <none@none>
2015-09-24 15:40:22 -0400

    EF: Fixing TextAnnotationDraw.h to work with published API consumers like sample walls editor.

--- iModelCore/DgnPlatform/PublicAPI/DgnPlatform/DgnCore/Annotations/TextAnnotationDraw.h
+++ iModelCore/DgnPlatform/PublicAPI/DgnPlatform/DgnCore/Annotations/TextAnnotationDraw.h
@@ -7,7 +7,6 @@
 //__PUBLISH_SECTION_START__
 
 #include "TextAnnotation.h"
-#include "../ElementGraphics.h"
 
 DGNPLATFORM_TYPEDEFS(TextAnnotationDraw);
 DGNPLATFORM_REF_COUNTED_PTR(TextAnnotationDraw);
@@ -23,29 +22,6 @@ BEGIN_BENTLEY_DGNPLATFORM_NAMESPACE
 //=======================================================================================
 struct TextAnnotationDraw : public RefCountedBase
 {
-    //=======================================================================================
-    //! Allows for the ViewContext-based TextAnnotationDraw to be used with an ElementGeometryBuilder.
-    // @bsiclass                                                    Jeff.Marker     09/2015
-    //=======================================================================================
-    struct DrawToElementGeometry : IElementGraphicsProcessor
-    {
-    private:
-        TextAnnotationDrawCR m_annotationDraw;
-        ElementGeometryBuilderR m_builder;
-        DgnCategoryId m_categoryId;
-        Transform m_transform;
-
-    public:
-        DrawToElementGeometry(TextAnnotationDrawCR annotationDraw, ElementGeometryBuilderR builder, DgnCategoryId categoryId) :
-            m_annotationDraw(annotationDraw), m_builder(builder), m_categoryId(categoryId), m_transform(Transform::FromIdentity()) {}
-
-        virtual void _AnnounceTransform(TransformCP transform) override { if (nullptr != transform) { m_transform = *transform; } else { m_transform.InitIdentity(); } }
-        virtual void _AnnounceElemDisplayParams(ElemDisplayParamsCR params) override { m_builder.Append(params); }
-        virtual BentleyStatus _ProcessTextString(TextStringCR) override;
-        virtual BentleyStatus _ProcessCurveVector(CurveVectorCR, bool isFilled) override;
-        virtual void _OutputGraphics(ViewContextR) override;
-    };
-
 private:
     DEFINE_T_SUPER(RefCountedBase)
 
