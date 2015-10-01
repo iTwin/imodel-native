efa1530239e5be04000be78dc0981477cdcab1dc
Majd.Uddin <none@none>
2015-10-01 10:45:28 +0500

    Updated logging of Performance result to go to CSV instead of a Db

--- iModelCore/BeSQLite/Tests/Performance/PerformanceTestingHelpers.cpp
+++ iModelCore/BeSQLite/Tests/Performance/PerformanceTestingHelpers.cpp
@@ -205,6 +205,33 @@ bool PerformanceResultRecorder::writeTodb(double timeInSeconds, Utf8String testc
 
 }
 
+/*---------------------------------------------------------------------------------**//**
+* Writes time to a csv file
+*@bsimethod                                            Majd.Uddin          10/2015
++---------------+---------------+---------------+---------------+---------------+------*/
+void PerformanceResultRecorder::writeResults(Utf8String testcaseName, Utf8String testName, double timeInSeconds, Utf8String testDescription, int opCount)
+{
+    FILE* logFile = NULL;
+
+    BeFileName dir;
+    BeTest::GetHost().GetOutputRoot(dir);
+    dir.AppendToPath(L"PerformanceTestResults");
+    if (!dir.DoesPathExist())
+        BeFileName::CreateNewDirectory(dir.c_str());
+
+    dir.AppendToPath(L"PerformanceResults.csv");
+
+    bool existingFile = dir.DoesPathExist();
+
+    logFile = fopen(dir.GetNameUtf8().c_str(), "a+");
+    PERFORMANCELOG.infov(L"CSV Results filename: %ls\n", dir.GetName());
+
+    if (!existingFile)
+        fprintf(logFile, "TestCaseName, TestName, ExecutionTime, TestDescription, opCount\n");
+    fprintf(logFile, "%s, %s, %.4f, \"%s\", %ld\n", testcaseName.c_str(), testName.c_str(), timeInSeconds, testDescription.c_str(), opCount);
+
+    fclose(logFile);
+}
 
 //---------------------------------------------------------------------------------------
 // Creating a new Db for the test
