4ffdd7aaddb0fa4c251cb573d22053c36d1608e6
Mathieu.Marchand <none@none>
2015-10-05 12:40:03 -0400

    Added ImageBuffer to share pixels buffer instead of copying it.

--- iModelCore/DgnPlatform/DgnCore/RenderMaterial.cpp
+++ iModelCore/DgnPlatform/DgnCore/RenderMaterial.cpp
@@ -284,6 +284,76 @@ uintptr_t  JsonRenderMaterial::_GetQvMaterialId (DgnDbR dgnDb, bool createIfNotF
     return qvMaterialId;
     }
 
+//=======================================================================================
+// ImageBuffer...
+//=======================================================================================
+
+//----------------------------------------------------------------------------------------
+// @bsimethod                                                   Mathieu.Marchand  10/2015
+//----------------------------------------------------------------------------------------
+uint32_t            ImageBuffer::GetWidth() const       {return m_width;}
+uint32_t            ImageBuffer::GetHeight() const      {return m_height;}
+Byte*               ImageBuffer::GetDataP()             {return m_data.data();}
+Byte const*         ImageBuffer::GetDataCP() const      {return m_data.data();}
+size_t              ImageBuffer::GetDataSize() const    {return m_data.size();}
+ImageBuffer::Format ImageBuffer::GetFormat() const      {return m_format;}
+
+//----------------------------------------------------------------------------------------
+// @bsimethod                                                   Mathieu.Marchand  10/2015
+//----------------------------------------------------------------------------------------
+ImageBuffer::ImageBuffer(uint32_t width, uint32_t height, ImageBuffer::Format const& format, bvector<Byte>& data)
+:m_width(width),
+ m_height(height),
+ m_format(format)
+    {
+    BeAssert(data.size() == m_width*m_height*ImageBuffer::BytesPerPixel(m_format));
+
+    m_data = std::move(data);
+    }
+
+//----------------------------------------------------------------------------------------
+// @bsimethod                                                   Mathieu.Marchand  10/2015
+//----------------------------------------------------------------------------------------
+ImageBufferPtr ImageBuffer::Create(uint32_t width, uint32_t height, ImageBuffer::Format const& format)
+    {
+    bvector<Byte> data(width*height*ImageBuffer::BytesPerPixel(format));
+    return new ImageBuffer(width, height, format, data);
+    }
+
+//----------------------------------------------------------------------------------------
+// @bsimethod                                                   Mathieu.Marchand  10/2015
+//----------------------------------------------------------------------------------------
+ImageBufferPtr ImageBuffer::Create(uint32_t width, uint32_t height, ImageBuffer::Format const& format, bvector<Byte>& data)
+    {
+    return new ImageBuffer(width, height, format, data);
+    }
+
+//----------------------------------------------------------------------------------------
+// @bsimethod                                                   Mathieu.Marchand  10/2015
+//----------------------------------------------------------------------------------------
+size_t ImageBuffer::BytesPerPixel(Format const& format)
+    {
+    switch(format)
+        {
+        case Format::Rgba:
+        case Format::Bgra:
+            return 4;
+        case Format::Rgb:
+        case Format::Bgr:
+            return 3;
+        case Format::Gray:
+            return 1;
+        default:
+            BeAssert(!"ImageBuffer::BytesPerPixel -> unknown pixel format");
+            return 4;
+        }   
+    }
+
+
+//=======================================================================================
+// JsonRenderMaterialMap...
+//=======================================================================================
+
 /*---------------------------------------------------------------------------------**//**
 * @bsimethod                                                    RayBentley      09/2015
 +---------------+---------------+---------------+---------------+---------------+------*/
@@ -291,14 +361,6 @@ double      JsonRenderMaterialMap::_GetDouble (char const* key, BentleyStatus* s
 bool        JsonRenderMaterialMap::_GetBool (char const* key, BentleyStatus* status) const    {  return getBoolValue (m_value, key, status); }
 DPoint2d    JsonRenderMaterialMap::_GetScale (BentleyStatus* status) const                    {  return getDPoint2dValue (m_value, RENDER_MATERIAL_PatternScale, status); }
 DPoint2d    JsonRenderMaterialMap::_GetOffset(BentleyStatus* status) const                    {  return getDPoint2dValue (m_value, RENDER_MATERIAL_PatternOffset, status); }
- 
-
-
-
-//=======================================================================================
-// JsonRenderMaterialMap...
-//=======================================================================================
-
 
 /*---------------------------------------------------------------------------------**//**
 * @bsimethod                                                    RayBentley      08/2015
@@ -333,12 +395,12 @@ RenderMaterialMap::Units JsonRenderMaterialMap::_GetUnits () const
 /*---------------------------------------------------------------------------------**//**
 * @bsimethod                                                    RayBentley      08/2015
 +---------------+---------------+---------------+---------------+---------------+------*/
-BentleyStatus   JsonRenderMaterialMap::_GetImage (bvector<Byte>& data, Point2dR imageSize, DgnDbR dgnDb) const
+ImageBufferPtr  JsonRenderMaterialMap::_GetImage (DgnDbR dgnDb) const
     {
     Json::Value const&     textureIdValue = m_value[RENDER_MATERIAL_TextureId];
 
     if (textureIdValue.isNull())     
-        return ERROR;                 // No external file support for now.
+        return nullptr;                 // No external file support for now.
 
     DgnTextureId            textureId = (DgnTextureId) textureIdValue.asUInt64();
     DgnTextures::Texture    texture = dgnDb.Textures().Query (textureId);
@@ -346,12 +408,14 @@ BentleyStatus   JsonRenderMaterialMap::_GetImage (bvector<Byte>& data, Point2dR 
     if (!texture.IsValid())
         {
         BeAssert (false);
-        return ERROR;
+        return nullptr;
         }
 
-    imageSize.Init(texture.GetData().GetWidth(), texture.GetData().GetHeight());
+    bvector<Byte> data;
+    if(SUCCESS != texture.GetImage (data))
+        return nullptr;        
 
-    return texture.GetImage (data);
+    return ImageBuffer::Create(texture.GetData().GetWidth(), texture.GetData().GetHeight(), ImageBuffer::Format::Rgba, data);
     }
 
 /*---------------------------------------------------------------------------------**//**
@@ -379,12 +443,8 @@ uintptr_t  JsonRenderMaterialMap::_GetQvTextureId (DgnDbR dgnDb, bool createIfNo
 /*---------------------------------------------------------------------------------**//**
 * @bsimethod                                                    RayBentley      09/2015
 +---------------+---------------+---------------+---------------+---------------+------*/
-SimpleBufferPatternMap::SimpleBufferPatternMap (Byte const* imageData, Point2dCR imageSize) : m_imageSize (imageSize), m_qvTextureId (0)
+SimpleBufferPatternMap::SimpleBufferPatternMap (ImageBufferR buffer) :m_imageBuffer (&buffer), m_qvTextureId (0)
     {
-    size_t      dataSize = 4 * imageSize.x * imageSize.y;
-
-    m_imageData.resize (dataSize);                     
-    memcpy (&m_imageData.front(), imageData, dataSize);
     }
 
 /*---------------------------------------------------------------------------------**//**
@@ -399,13 +459,11 @@ SimpleBufferPatternMap::~SimpleBufferPatternMap ()
 /*---------------------------------------------------------------------------------**//**
 * @bsimethod                                                    RayBentley      09/2015
 +---------------+---------------+---------------+---------------+---------------+------*/
-BentleyStatus  SimpleBufferPatternMap::_GetImage (bvector<Byte>& imageData, Point2dR imageSize, DgnDbR dgnDb) const
+ImageBufferPtr  SimpleBufferPatternMap::_GetImage (DgnDbR dgnDb) const
     {
-    imageData = m_imageData;
-    imageSize = m_imageSize;
-
-    return SUCCESS;
+    return m_imageBuffer;
     }
+
 /*---------------------------------------------------------------------------------**//**
 * @bsimethod                                                    RayBentley      09/2015
 +---------------+---------------+---------------+---------------+---------------+------*/
@@ -420,9 +478,9 @@ uintptr_t SimpleBufferPatternMap::_GetQvTextureId (DgnDbR dgnDb, bool createIfNo
 /*---------------------------------------------------------------------------------**//**
 * @bsimethod                                                    RayBentley      09/2015
 +---------------+---------------+---------------+---------------+---------------+------*/
-RenderMaterialMapPtr  SimpleBufferPatternMap::Create (Byte const* imageData, Point2dCR imageSize)
+RenderMaterialMapPtr  SimpleBufferPatternMap::Create (ImageBufferR buffer)
     {
-    return new SimpleBufferPatternMap (imageData, imageSize);
+    return new SimpleBufferPatternMap (buffer);
     }
 
 //=======================================================================================
--- iModelCore/DgnPlatform/PublicAPI/DgnPlatform/DgnCore/RenderMaterial.h
+++ iModelCore/DgnPlatform/PublicAPI/DgnPlatform/DgnCore/RenderMaterial.h
@@ -51,6 +51,53 @@ DGNPLATFORM_EXPORT    virtual     uintptr_t               _GetQvMaterialId (DgnD
 };
 
 //=======================================================================================
+// @bsiclass                                                    BentleySystems
+//=======================================================================================
+struct ImageBuffer: public RefCountedBase
+{   
+public:
+    // __PUBLISH_SECTION_END__
+    // These enum values are expected to match with QV_*_FORMAT.
+    // __PUBLISH_SECTION_START__
+    enum class Format
+        {
+        Rgba        = 0,
+        Bgra        = 1,
+        Rgb         = 2,
+        Bgr         = 3,
+        Gray        = 4,
+        };
+
+protected:
+    uint32_t        m_width;
+    uint32_t        m_height;
+    Format          m_format;
+    bvector<Byte>   m_data;  
+
+    ImageBuffer(); 
+    ImageBuffer(uint32_t width, uint32_t height, Format const& format, bvector<Byte>& data);
+
+    static size_t BytesPerPixel(Format const& format);
+
+public:
+
+    //! Create an uninitialized buffer that it is going to be filled later.
+    DGNPLATFORM_EXPORT static ImageBufferPtr Create(uint32_t width, uint32_t height, Format const& format);
+
+    //! Create from a vector. 'data' is not copied, std::move is used to avoid a buffer copy.
+    DGNPLATFORM_EXPORT static ImageBufferPtr Create(uint32_t width, uint32_t height, Format const& format, bvector<Byte>& data);
+       
+    DGNPLATFORM_EXPORT uint32_t     GetWidth() const;
+    DGNPLATFORM_EXPORT uint32_t     GetHeight() const;
+
+    DGNPLATFORM_EXPORT Format       GetFormat() const;
+    DGNPLATFORM_EXPORT Byte*        GetDataP();
+    DGNPLATFORM_EXPORT Byte const*  GetDataCP() const;
+    DGNPLATFORM_EXPORT size_t       GetDataSize() const; 
+
+};
+
+//=======================================================================================
 // @bsiclass                                            Ray.Bentley     09/2015
 //=======================================================================================
 struct RenderMaterialMap : RefCountedBase
@@ -115,16 +162,6 @@ struct RenderMaterialMap : RefCountedBase
         Inches                 = 6,
         };
 
-    enum class ImageFormat
-        {
-        Rgba                    = 0,
-        Bgra                    = 1,
-        Rgb                     = 2,
-        Bgr                     = 3,
-        Gray                    = 4,
-        };
-
-
     virtual Mode                        _GetMode () const        { return Mode::Parametric; }
     virtual Units                       _GetUnits () const       { return Units::Relative; }
 
@@ -140,8 +177,8 @@ struct RenderMaterialMap : RefCountedBase
     //! You can turn OFF alpha testing if your image does not have transparent pixels.
     virtual bool                        _GetEnableAlphaTesting() const {return true;}
 
-    virtual ImageFormat                 _GetImageFormat() const { return ImageFormat::Rgba;}
-    virtual BentleyStatus               _GetImage (bvector<Byte>& data, Point2dR size, DgnDbR dgnDb) const = 0;
+    //! Return an ImageBufferPtr or an invalid instance if an error occurs.
+    virtual ImageBufferPtr              _GetImage (DgnDbR dgnDb) const = 0;
     virtual uintptr_t                   _GetQvTextureId (DgnDbR dgnDb, bool createIfNotFound) const = 0;
 
 
@@ -169,7 +206,7 @@ public:
     DGNPLATFORM_EXPORT    virtual DPoint2d            _GetOffset (BentleyStatus* status = NULL) const override;
     DGNPLATFORM_EXPORT    virtual double              _GetDouble (char const* key, BentleyStatus* status = NULL) const override;
     DGNPLATFORM_EXPORT    virtual bool                _GetBool (char const* key, BentleyStatus* status = NULL) const override;
-    DGNPLATFORM_EXPORT    virtual BentleyStatus       _GetImage (bvector<Byte>& data, Point2dR size, DgnDbR dgnDb) const override;
+    DGNPLATFORM_EXPORT    virtual ImageBufferPtr      _GetImage (DgnDbR dgnDb) const override;
     DGNPLATFORM_EXPORT    virtual uintptr_t           _GetQvTextureId (DgnDbR dgnDb, bool createIfNotFound) const override;
 
 };
@@ -181,20 +218,18 @@ struct SimpleBufferPatternMap : RenderMaterialMap
 {
 protected:
     mutable uintptr_t   m_qvTextureId;
-    bvector<Byte>       m_imageData;        // Rgba
-    Point2d             m_imageSize;
+    ImageBufferPtr      m_imageBuffer;
 
-    SimpleBufferPatternMap (Byte const* imageData, Point2dCR imageSize);
-
-public:
-    //! Create a material map from a Rgba buffer.
-    DGNPLATFORM_EXPORT static RenderMaterialMapPtr  Create (Byte const* imageData, Point2dCR imageSize);
-
+    DGNPLATFORM_EXPORT SimpleBufferPatternMap (ImageBufferR buffer);
     DGNPLATFORM_EXPORT ~SimpleBufferPatternMap ();
 
-    virtual uintptr_t   _GetQvTextureId (DgnDbR dgnDb, bool createIfNotFound) const override;
+public:
+    //! Create a material map from an ImageBuffer. No copy occurs, 'buffer' refcount will be incremented.
+    DGNPLATFORM_EXPORT static RenderMaterialMapPtr  Create (ImageBufferR buffer);
+    
+    DGNPLATFORM_EXPORT virtual uintptr_t        _GetQvTextureId (DgnDbR dgnDb, bool createIfNotFound) const override;
 
-    DGNPLATFORM_EXPORT virtual BentleyStatus _GetImage (bvector<Byte>& data, Point2dR size, DgnDbR dgnDb) const override;
+    DGNPLATFORM_EXPORT virtual ImageBufferPtr   _GetImage (DgnDbR dgnDb) const override;
                                                                                                  
 };
 
