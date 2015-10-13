92294ac296821c7404db60934718591d0d035f33
Sam.Wilson <none@none>
2015-10-13 16:44:14 -0400

    BF iOS build break

--- iModelCore/DgnPlatform/Tools/ToolSubs/fileutil/findfile.cpp
+++ iModelCore/DgnPlatform/Tools/ToolSubs/fileutil/findfile.cpp
@@ -48,13 +48,13 @@ void util_parseFileName (WCharCP string, WStringP dev, WStringP dir, WStringP na
     ---------------------------------------------------------------*/
     WCharCP lastDirSep;
 
-    if ((lastDirSep = ::wcsrchr (string, WCSDIR_SEPARATOR_CHAR)) == NULL)
-        lastDirSep = ::wcsrchr (string, WCSALT_DIR_SEPARATOR_CHAR);
+    if ((lastDirSep = wcsrchr (string, WCSDIR_SEPARATOR_CHAR)) == NULL)
+        lastDirSep = wcsrchr (string, WCSALT_DIR_SEPARATOR_CHAR);
 
     if (NULL == lastDirSep)
         return;
 
-    WCharCP lastDot = ::wcsrchr (string, L'.');
+    WCharCP lastDot = wcsrchr (string, L'.');
 
     if (NULL == lastDot || lastDot < lastDirSep)
         return;
