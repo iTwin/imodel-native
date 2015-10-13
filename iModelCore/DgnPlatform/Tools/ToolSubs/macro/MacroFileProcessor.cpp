77edd1f888557341cae08c7e0e00dc9e1e603e2f
Sam.Wilson <none@none>
2015-10-13 14:59:37 -0400

    Fix various compile errors caught by clang

--- iModelCore/DgnPlatform/Tools/ToolSubs/macro/MacroFileProcessor.cpp
+++ iModelCore/DgnPlatform/Tools/ToolSubs/macro/MacroFileProcessor.cpp
@@ -549,7 +549,9 @@ void            MacroFileProcessor::ShowEndFileMessage (WStringP endFileMsg, boo
 /*---------------------------------------------------------------------------------**//**
 * @bsimethod                                                    Keith.Bentley   10/92
 +---------------+---------------+---------------+---------------+---------------+------*/
+#ifdef _WIN32
 #pragma warning(disable:4702)  // unreachable code warning triggered by final return statement in Release build only.
+#endif
 MacroOperation  MacroFileProcessor::GetMacroNameAndOperation (WStringR macroName, WStringP endFileMsg)
     {
     macroName.clear();
@@ -605,7 +607,9 @@ MacroOperation  MacroFileProcessor::GetMacroNameAndOperation (WStringR macroName
     // Without this : warning C4715: ...  : not all control paths return a value
     return MacroOperation::EndOfFile;
     }
+#ifdef _WIN32
 #pragma warning(default:4702)
+#endif
 
 /*---------------------------------------------------------------------------------**//**
 * @bsimethod                                                    kab             10/89
