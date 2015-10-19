7bad7c26808035a46a0f2e2ee6e9b4be6521a881
Vincas Razma <vincas.razma@bentley.com>
2015-10-19 14:35:02 +0300

    Add WSChangesetBuilder SRB:Benediktas.Lipnickas

---
+++ iModelBridgeCore/WSClient/Tests/Published/WebServices/Client/WSChangesetBuilderTests.h
@@ -0,0 +1,15 @@
+/*--------------------------------------------------------------------------------------+
+|
+|     $Source: Tests/Published/WebServices/Client/WSChangesetBuilderTests.h $
+|
+|  $Copyright: (c) 2015 Bentley Systems, Incorporated. All rights reserved. $
+|
++--------------------------------------------------------------------------------------*/
+
+#pragma once
+
+#include "../../Utils/WebServicesTestsHelper.h"
+
+class WSChangesetBuilderTests : public WSClientBaseTest
+    {
+    };
