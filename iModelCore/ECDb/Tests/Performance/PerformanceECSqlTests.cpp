c6fd4fd0d6ec4d14018799575575d925afb10112
Krischan.Eberle <none@none>
2015-10-28 16:03:18 +0100

    ECSQL vs SQLite performance tests now separate out creation of the seed file into its own test which can be run completely separate.

--- iModelCore/ECDb/Tests/Performance/PerformanceECSqlTests.cpp
+++ iModelCore/ECDb/Tests/Performance/PerformanceECSqlTests.cpp
@@ -37,28 +37,89 @@ protected:
 struct PerformanceECSqlVsSqliteTests : PerformanceECSqlTestFixture
     {
 private:
-    static const int s_instanceCount = 1000000;
+    static const int s_initialInstanceCount = 1000000;
+    static const int s_opCount = 500000;
     static BeFileName s_seedFilePath;
 
 protected:
-    static bmap<ECClassId,std::vector<ECInstanceId>> s_instanceIds;
+    bmap<ECClassId,std::vector<ECInstanceId>> s_instanceIds;
 
+    //---------------------------------------------------------------------------------------
+    // @bsimethod                                      Krischan.Eberle                  10/15
+    //+---------------+---------------+---------------+---------------+---------------+------
+    BentleyStatus CreateSampleIdPopulation(ECDbCR ecdb)
+        {
+        if (!s_instanceIds.empty())
+            return SUCCESS;
+
+        ECDb seed;
+        if (BE_SQLITE_OK != seed.OpenBeSQLiteDb(s_seedFilePath, ECDb::OpenParams(Db::OpenMode::Readonly)))
+            return ERROR;
+
+        ECSqlStatement stmt;
+        if (ECSqlStatus::Success != stmt.Prepare(seed, "SELECT GetECClassId(), ECInstanceId FROM ONLY ecsql.TH3"))
+            return ERROR;
+
+        const int increment = s_initialInstanceCount / s_opCount;
+        if (increment * s_opCount > s_initialInstanceCount)
+            {
+            EXPECT_LE(increment * s_opCount, s_initialInstanceCount);
+            return ERROR;
+            }
+
+        for (int i = 0; i < s_opCount; i++)
+            {
+            for (int j = 0; j < increment; j++)
+                {
+                if (BE_SQLITE_ROW != stmt.Step())
+                    return ERROR;
+                }
+
+            ECClassId classId = stmt.GetValueInt64(0);
+            ECInstanceId id = stmt.GetValueId<ECInstanceId>(1);
+            if (classId == ECClass::UNSET_ECCLASSID || !id.IsValid())
+                return ERROR;
+
+            s_instanceIds[classId].push_back(id);
+            }
+
+        return SUCCESS;
+        }
+
+    //---------------------------------------------------------------------------------------
+    // @bsimethod                                      Krischan.Eberle                  10/15
+    //+---------------+---------------+---------------+---------------+---------------+------
     BentleyStatus SetupTestECDb(ECDbR ecdb)
         {
+        Utf8String seedFileName;
+        bool mustCreateSeed = false;
         if (s_seedFilePath.empty())
             {
-            if (SUCCESS != CreateSeedECDb(s_seedFilePath, "ecsqlperformance_seed.ecdb", BeFileName("ECSqlTest.01.00.ecschema.xml"), 0))
+            seedFileName.Sprintf("ecsqlvssqliteperformance_seed_%d.ecdb", DateTime::GetCurrentTimeUtc().GetDayOfYear());
+
+            BeFileName seedPath = ECDbTestUtility::BuildECDbPath(seedFileName.c_str());
+            //if seed file exists on disk, we reuse it. This is risky if other tests happen to create file with same name
+            //but we add the current day of the year to the file name, to make sure it would never be reused after 24h.
+            if (seedPath.DoesPathExist())
+                s_seedFilePath = seedPath;
+            else
+                mustCreateSeed = true;
+            }
+
+        if (mustCreateSeed)
+            {
+            if (SUCCESS != CreateSeedECDb(s_seedFilePath, seedFileName.c_str(), BeFileName("ECSqlTest.01.00.ecschema.xml"), 0))
                 return ERROR;
 
             ECDb seed;
             if (BE_SQLITE_OK != seed.OpenBeSQLiteDb(s_seedFilePath, ECDb::OpenParams(Db::OpenMode::ReadWrite)))
-            return ERROR;
+                return ERROR;
 
             ECSqlStatement stmt;
             if (ECSqlStatus::Success != stmt.Prepare(seed, "INSERT INTO ecsql.TH3(S,S1,S2,S3) VALUES(?,?,?,?)"))
-            return ERROR;
+                return ERROR;
 
-            for (int i = 0; i < s_instanceCount; i++)
+            for (int i = 0; i < s_initialInstanceCount; i++)
                 {
                 for (int j = 0; j < 4; j++)
                     {
@@ -74,19 +135,41 @@ protected:
 
                 stmt.Reset();
                 stmt.ClearBindings();
-
-                s_instanceIds[newKey.GetECClassId()].push_back(newKey.GetECInstanceId());
                 }
 
             seed.SaveChanges();
             }
+        
+        if (SUCCESS != CreateSampleIdPopulation(ecdb))
+            return ERROR;
 
-        return CloneECDb(ecdb, "ecsqlperformance.ecdb", s_seedFilePath, ECDb::OpenParams(Db::OpenMode::ReadWrite)) == BE_SQLITE_OK ? SUCCESS : ERROR;
+        return CloneECDb(ecdb, "ecsqlvssqliteperformance.ecdb", s_seedFilePath, ECDb::OpenParams(Db::OpenMode::ReadWrite)) == BE_SQLITE_OK ? SUCCESS : ERROR;
+        }
+
+    //---------------------------------------------------------------------------------------
+    // @bsimethod                                      Krischan.Eberle                  10/15
+    //+---------------+---------------+---------------+---------------+---------------+------
+    void LogTiming(StopWatch& timer, Utf8CP testDescription, int actualOpCount)
+        {
+        ASSERT_EQ(s_opCount, actualOpCount) << "Unexpected actual op count";
+        Utf8String totalTestDescr;
+        totalTestDescr.Sprintf("%s [initial instance count: %d]", testDescription, s_initialInstanceCount);
+        LOGTODB(TEST_DETAILS, timer.GetElapsedSeconds(), totalTestDescr, actualOpCount);
         }
     };
 
 BeFileName PerformanceECSqlVsSqliteTests::s_seedFilePath;
-bmap<ECClassId, std::vector<ECInstanceId>> PerformanceECSqlVsSqliteTests::s_instanceIds;
+
+//---------------------------------------------------------------------------------------
+// @bsimethod                                      Krischan.Eberle                  10/15
+//+---------------+---------------+---------------+---------------+---------------+------
+TEST_F(PerformanceECSqlVsSqliteTests, CreateSeedFile)
+    {
+    //separate out code that creates and populates the seed files, so that multiple runs of the actual
+    //perf timings can be done without influence of the heavy work to create the seed file.
+    ECDb ecdb;
+    ASSERT_EQ(SUCCESS, SetupTestECDb(ecdb));
+    }
 
 //---------------------------------------------------------------------------------------
 // @bsimethod                                      Krischan.Eberle                  10/15
@@ -142,7 +225,7 @@ TEST_F(PerformanceECSqlVsSqliteTests, SqliteUpdateWithClassIdFilter)
         }
 
     timer.Stop();
-    LOGTODB(TEST_DETAILS, timer.GetElapsedSeconds(), "SQLite UPDATE with ECClassId filter", (int) instanceIds.size());
+    LogTiming(timer, "SQLite UPDATE with ECClassId filter", (int) instanceIds.size());
     }
 
 //---------------------------------------------------------------------------------------
@@ -181,7 +264,7 @@ TEST_F(PerformanceECSqlVsSqliteTests, SqliteUpdateWithoutClassIdFilter)
         }
 
     timer.Stop();
-    LOGTODB(TEST_DETAILS, timer.GetElapsedSeconds(), "SQLite UPDATE w/o ECClassId filter", (int) instanceIds.size());
+    LogTiming(timer, "SQLite UPDATE w/o ECClassId filter", (int) instanceIds.size());
     }
 
 //---------------------------------------------------------------------------------------
@@ -220,7 +303,7 @@ TEST_F(PerformanceECSqlVsSqliteTests, ECSqlUpdateWithClassIdFilter)
         }
 
     timer.Stop();
-    LOGTODB(TEST_DETAILS, timer.GetElapsedSeconds(), "ECSQL UPDATE", (int) instanceIds.size());
+    LogTiming(timer, "ECSQL UPDATE", (int) instanceIds.size());
     }
 
 //---------------------------------------------------------------------------------------
@@ -239,6 +322,7 @@ TEST_F(PerformanceECSqlVsSqliteTests, ECSqlUpdateWithoutClassIdFilter)
     ASSERT_EQ(ECSqlStatus::Success, stmt.Prepare(ecdb, "UPDATE ecsql.TH3 SET S='S', S1='S1', S2='S2', S3='S3' WHERE ECInstanceId=? ECSQLOPTIONS NoECClassIdFilter"));
     LOG.infov("Translated SQL: %s", stmt.GetNativeSql());
 
+    //printf("Attach to profiler...\r\n"); getchar();
     StopWatch timer(true);
     for (ECInstanceId const& id : instanceIds)
         {
@@ -259,7 +343,8 @@ TEST_F(PerformanceECSqlVsSqliteTests, ECSqlUpdateWithoutClassIdFilter)
         }
 
     timer.Stop();
-    LOGTODB(TEST_DETAILS, timer.GetElapsedSeconds(), "ECSQL UPDATE OPTIONS NoECClassIdFilter", (int) instanceIds.size());
+    //printf("Detach...\r\n"); getchar();
+    LogTiming(timer, "ECSQL UPDATE OPTIONS NoECClassIdFilter", (int) instanceIds.size());
     }
 
 END_ECDBUNITTESTS_NAMESPACE
