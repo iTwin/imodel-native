c3ab1ef40e1c272f4ac8bc8a6ad4b8f2ca91679b
Dalius.Dobravolskas <none@none>
2015-11-10 17:47:34 +0200

    Punchlist QA pointed to https://qa-punchlist-eus.cloudapp.net

--- iModelBridgeCore/WSClient/Configuration/UrlProvider.cpp
+++ iModelBridgeCore/WSClient/Configuration/UrlProvider.cpp
@@ -73,7 +73,7 @@ const UrlProvider::UrlDescriptor UrlProvider::Urls::ConnectWsgProjectContent(
 const UrlProvider::UrlDescriptor UrlProvider::Urls::ConnectWsgPunchList(
     "Mobile.PunchListWsg",
     "https://dev-punchlist-eus.cloudapp.net",
-    "https://qa-wsg20-eus.cloudapp.net",
+    "https://qa-punchlist-eus.cloudapp.net",
     "https://connect-wsg20.bentley.com",
     &s_urlRegistry
     );
