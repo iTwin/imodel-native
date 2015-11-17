362074ee98d755b11e338694693e47584426852d
Vincas Razma <vincas.razma@bentley.com>
2015-11-17 10:16:44 +0200

    Ensure tests mocks are building when API is changed

---
+++ iModelBridgeCore/WSClient/Tests/Published/Utils/MockTests.cpp
@@ -0,0 +1,46 @@
+/*--------------------------------------------------------------------------------------+
+|
+|     $Source: Tests/Published/Utils/MockTests.cpp $
+|
+|  $Copyright: (c) 2015 Bentley Systems, Incorporated. All rights reserved. $
+|
++--------------------------------------------------------------------------------------*/
+
+#include "MockTests.h"
+
+#ifdef USE_GTEST
+
+#include "../WebServices/Cache/MockCachingDataSource.h"
+#include "../WebServices/Cache/MockQueryProvider.h"
+#include "../WebServices/Cache/Persistence/MockChangeManager.h"
+#include "../WebServices/Cache/Persistence/MockDataSourceCache.h"
+#include "../WebServices/Cache/Persistence/MockRepositoryInfoStore.h"
+#include "../WebServices/Client/MockServerInfoListener.h"
+#include "../WebServices/Client/MockWSClient.h"
+#include "../WebServices/Client/MockWSRepositoryClient.h"
+#include "../WebServices/Client/MockWSSchemaProvider.h"
+#include "../WebServices/Configuration/MockBuddiClient.h"
+#include "../WebServices/Connect/MockConnectAuthenticationPersistence.h"
+#include "../WebServices/Connect/MockConnectTokenProvider.h"
+#include "../WebServices/Connect/MockLocalState.h"
+
+USING_NAMESPACE_BENTLEY_WEBSERVICES
+
+TEST_F(MockTests, Ctor_Default_Builds)
+    {
+    MockCachingDataSource();
+    MockQueryProvider();
+    MockChangeManager();
+    MockDataSourceCache();
+    MockRepositoryInfoStore();
+    MockServerInfoListener();
+    MockWSClient();
+    MockWSRepositoryClient();
+    MockWSSchemaProvider();
+    MockBuddiClient();
+    MockConnectAuthenticationPersistence();
+    MockConnectTokenProvider();
+    MockLocalState();
+    }
+
+#endif
