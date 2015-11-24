324e7e8731a811ac31eef3b5c45c04c7318ad495
Paul.Connelly <none@none>
2015-11-24 13:37:45 -0500

    EN: Adjust tests for extracting locks from changesets to use DgnRevision, since we will typically be extracting them at the time EN: when the user prepares a revision. BF: Missing exports on virtual fns.

--- iModelCore/DgnPlatform/Tests/DgnProject/Published/LocksManager_Test.cpp
+++ iModelCore/DgnPlatform/Tests/DgnProject/Published/LocksManager_Test.cpp
@@ -899,13 +899,35 @@ TEST_F(DoubleBriefcaseTest, Dynamics)
     ExpectLevel(*elemA, LockLevel::Exclusive);
     }
 
-#ifdef WIP_LOCKS_FROM_CHANGESET
+/*---------------------------------------------------------------------------------**//**
+* @bsistruct                                                    Paul.Connelly   11/15
++---------------+---------------+---------------+---------------+---------------+------*/
+struct ExtractLocksTest : SingleBriefcaseLocksTest
+{
+    DgnDbStatus ExtractLocks(LockRequestR req)
+        {
+        if (BE_SQLITE_OK != m_db->SaveChanges())
+            return DgnDbStatus::WriteError;
+
+        DgnRevisionPtr rev = m_db->Revisions().StartCreateRevision();
+        if (rev.IsNull())
+            return DgnDbStatus::BadRequest;
+
+        ChangeStreamFileReader stream(rev->GetChangeStreamFile());
+        DgnDbStatus status = req.FromChangeSet(stream, *m_db);
+
+        m_db->Revisions().AbandonCreateRevision();
+
+        return status;
+        }
+};
+
 /*---------------------------------------------------------------------------------**//**
 * Test functions which query the set of locks which are required by the changes actually
 * made in the briefcase. (Excluding locks obtained but not actually used).
 * @bsimethod                                                    Paul.Connelly   11/15
 +---------------+---------------+---------------+---------------+---------------+------*/
-TEST_F(SingleBriefcaseLocksTest, UsedLocks)
+TEST_F(ExtractLocksTest, UsedLocks)
     {
     struct AssertScope
     {
@@ -929,7 +951,7 @@ TEST_F(SingleBriefcaseLocksTest, UsedLocks)
     DgnElementCPtr cpEl = db.Elements().GetElement(m_elemId);
 
     LockRequest req;
-    EXPECT_EQ(DgnDbStatus::BadArg, req.FromChangeSet(db));
+    EXPECT_EQ(DgnDbStatus::Success, ExtractLocks(req));
     EXPECT_TRUE(req.IsEmpty());
 
     // Modify an elem (it's a DgnCategory...)
@@ -941,7 +963,7 @@ TEST_F(SingleBriefcaseLocksTest, UsedLocks)
         cpEl = pEl->Update();
         ASSERT_TRUE(cpEl.IsValid());
 
-        EXPECT_EQ(DgnDbStatus::Success, req.FromChangeSet(db));
+        EXPECT_EQ(DgnDbStatus::Success, ExtractLocks(req));
         EXPECT_FALSE(req.IsEmpty());
         EXPECT_EQ(3, req.Size());
         EXPECT_EQ(LockLevel::Shared, req.GetLockLevel(LockableId(model)));
@@ -950,14 +972,14 @@ TEST_F(SingleBriefcaseLocksTest, UsedLocks)
         }
 
     // Change reversed on exit above scope
-    EXPECT_EQ(DgnDbStatus::BadArg, req.FromChangeSet(db));
+    EXPECT_EQ(DgnDbStatus::Success, ExtractLocks(req));
     EXPECT_TRUE(req.IsEmpty());
 
     // Create a new model
     DgnModelPtr newModel = CreateModel("NewModel");
     DgnElementCPtr newElem = CreateElement(*newModel);
 
-    EXPECT_EQ(DgnDbStatus::Success, req.FromChangeSet(db));
+    EXPECT_EQ(DgnDbStatus::Success, ExtractLocks(req));
     EXPECT_EQ(3, req.Size());
     EXPECT_EQ(LockLevel::Exclusive, req.GetLockLevel(LockableId(*newModel)));
     EXPECT_EQ(LockLevel::Exclusive, req.GetLockLevel(LockableId(*newElem)));
@@ -965,15 +987,14 @@ TEST_F(SingleBriefcaseLocksTest, UsedLocks)
 
     // Delete the new element
     EXPECT_EQ(DgnDbStatus::Success, newElem->Delete());
-    EXPECT_EQ(DgnDbStatus::Success, req.FromChangeSet(db));
+    EXPECT_EQ(DgnDbStatus::Success, ExtractLocks(req));
     EXPECT_EQ(2, req.Size());
     EXPECT_EQ(LockLevel::Exclusive, req.GetLockLevel(LockableId(*newModel)));
     EXPECT_EQ(LockLevel::Shared, req.GetLockLevel(LockableId(db)));
 
     // Delete the new model
     EXPECT_EQ(DgnDbStatus::Success, newModel->Delete());
-    EXPECT_EQ(DgnDbStatus::Success, req.FromChangeSet(db));
+    EXPECT_EQ(DgnDbStatus::Success, ExtractLocks(req));
     EXPECT_TRUE(req.IsEmpty());
     }
-#endif
 
