a71a4a6950403d2272d6003fcbb22e679658c9ac
Vincas Razma <vincas.razma@bentley.com>
2015-11-25 12:42:19 +0200

    JsonUtil::DeepCopy to use CopyValues and fix issues found in new use case SRB:Benediktas.Lipnickas

---
+++ iModelCore/WSClient/Tests/Published/WebServices/Cache/Util/JsonUtilTests.h
@@ -0,0 +1,15 @@
+/*--------------------------------------------------------------------------------------+
+|
+|     $Source: Tests/Published/WebServices/Cache/Util/JsonUtilTests.h $
+|
+|  $Copyright: (c) 2015 Bentley Systems, Incorporated. All rights reserved. $
+|
++--------------------------------------------------------------------------------------*/
+
+#pragma once
+
+#include "../../../Utils/WebServicesTestsHelper.h"
+
+class JsonUtilTests : public ::testing::Test
+    {
+    };
