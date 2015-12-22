a1282b31dde81808e9507c0dd9c0884818ce3a40
Jeff.Marker <none@none>
2015-12-22 10:59:32 -0500

    EF: Use ResolveFont instead of querying directly to allow hosts to go find the font data and give you a font object you can use.

--- iModelCore/DgnPlatform/DgnCore/AnnotationTable.cpp
+++ iModelCore/DgnPlatform/DgnCore/AnnotationTable.cpp
@@ -1747,15 +1747,11 @@ double          TextBlockHolder::ComputeDescenderAdjustment (AnnotationTextStyle
 #endif
 
     double      height = textStyle.GetHeight();
-    DgnFontId   fontId = textStyle.GetFontId();
-    DgnFontCP   font   = textStyle.GetDgnDb().Fonts().FindFontById(fontId);
-
-    if (UNEXPECTED_CONDITION (nullptr == font))
-        return 0.0;
-
 #if defined (NEEDSWORK)
-    DgnFontStyle    fontStyle   = DgnFont::FontStyleFromBoldItalic(textStyle.IsBold(), textStyle.IsItalic());
-    return font->GetDescenderRatio(fontStyle) * height;
+    DgnFontCR   font   = textStyle.ResolveFont();
+
+    DgnFontStyle fontStyle = DgnFont::FontStyleFromBoldItalic(textStyle.IsBold(), textStyle.IsItalic());
+    return font.GetDescenderRatio(fontStyle) * height;
 #else
     return 0.3 * height;
 #endif
