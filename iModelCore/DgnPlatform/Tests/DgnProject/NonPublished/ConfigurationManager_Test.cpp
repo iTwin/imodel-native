88279a9f993125e440453c906e1d9f2599788e8d
Jeff.Marker <none@none>
2015-12-22 14:48:49 -0500

    EF: #if'ing out some unused and non-portable configuration manager test code

--- iModelCore/DgnPlatform/Tests/DgnProject/NonPublished/ConfigurationManager_Test.cpp
+++ iModelCore/DgnPlatform/Tests/DgnProject/NonPublished/ConfigurationManager_Test.cpp
@@ -52,6 +52,8 @@ ConfigurationVariableLevel GetLevelFromString (WCharCP levelString)
     return (ConfigurationVariableLevel)-5;
     }
 
+#if defined (BENTLEY_WIN32) && defined (UNUSED)
+
 /*---------------------------------------------------------------------------------**//**
 * @bsimethod                                    Barry.Bentley                   02/12
 +---------------+---------------+---------------+---------------+---------------+------*/
@@ -232,6 +234,10 @@ void    GetPredefinedMacrosFromMsDebug (MacroConfigurationAdmin& macroCfgAdmin)
         }
     }
 
+#endif
+
+#if defined (UNUSED)
+
 /*---------------------------------------------------------------------------------**//**
 * @bsiclass                                    Barry.Bentley                   06/12
 +---------------+---------------+---------------+---------------+---------------+------*/
@@ -259,6 +265,8 @@ virtual void        ShowDebugMessage (int indent,WCharCP format, ...) override
         m_debugFile->PrintfTo (false, message.c_str());
     }
 };
+
+#endif
 
 /*---------------------------------------------------------------------------------**//**
 * @bsimethod                                    Barry.Bentley                   02/12
