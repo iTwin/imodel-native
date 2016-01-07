b2056f385ce98a2529271c6ba3cb4e07ff959097
Vytautas.Barkauskas <none@none>
2016-01-07 15:52:36 +0200

    Change ConnectSignInManager SignInWithToken to return error result SRB: vincas.razma

--- iModelBridgeCore/WSClient/Connect/Connect.xliff.h
+++ iModelBridgeCore/WSClient/Connect/Connect.xliff.h
@@ -2,7 +2,7 @@
 |
 |     $Source: Connect/Connect.xliff.h $
 |
-|  $Copyright: (c) 2015 Bentley Systems, Incorporated. All rights reserved. $
+|  $Copyright: (c) 2016 Bentley Systems, Incorporated. All rights reserved. $
 |
 +--------------------------------------------------------------------------------------*/
 
@@ -15,6 +15,7 @@
 //=======================================================================================
 MOBILEDGN_TRANSLATABLE_STRINGS_START(ConnectL10N, Connect)
     {
+    ALERT_UnsupportedToken,                  // =="Could not sign in with provided token."==
     ALERT_SignInFailed_ServerError,          // =="Could not connect to server. Check your network connection."==
     ALERT_SignInFailed_Message,              // =="Please enter the correct email and password."==
     FIELDAPPS_SingleSignOn,                  // =="Single sign-on"==
