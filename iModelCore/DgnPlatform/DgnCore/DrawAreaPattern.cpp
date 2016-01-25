e53582651c1120d833454c2e1451108a878e2b82
Paul.Connelly <none@none>
2016-01-25 12:10:45 -0500

    CM: GeomPart => GeometryPart

--- iModelCore/DgnPlatform/DgnCore/DrawAreaPattern.cpp
+++ iModelCore/DgnPlatform/DgnCore/DrawAreaPattern.cpp
@@ -2,7 +2,7 @@
 |
 |     $Source: DgnCore/DrawAreaPattern.cpp $
 |
-|  $Copyright: (c) 2015 Bentley Systems, Incorporated. All rights reserved. $
+|  $Copyright: (c) 2016 Bentley Systems, Incorporated. All rights reserved. $
 |
 +--------------------------------------------------------------------------------------*/
 #include    <DgnPlatformInternal.h>
@@ -427,7 +427,7 @@ struct PatternSymbol : IDisplaySymbol
 {
 private:
 
-DgnGeomPartId       m_partId;
+DgnGeometryPartId   m_partId;
 mutable DRange3d    m_range;
 
 public:
@@ -458,7 +458,7 @@ virtual StatusInt _GetRange(DRange3dR range) const override
 /*---------------------------------------------------------------------------------**//**
 * @bsimethod                                                    Brien.Bastings  11/07
 +---------------+---------------+---------------+---------------+---------------+------*/
-PatternSymbol(DgnGeomPartId partId, DgnDbR project)
+PatternSymbol(DgnGeometryPartId partId, DgnDbR project)
     {
 #if defined (NEEDSWORK_REVISIT_PATTERN_SYMBOLS_SCDEF)
     DgnElementPtr edP;
