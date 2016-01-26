36713cf87414925091dd9f5d094df35423543a1d
Krischan.Eberle <none@none>
2016-01-26 12:19:39 +0100

    Disable determination of joined table PK column name until preparation issue is fixed where wrong table is used.

--- iModelCore/ECDb/ECDb/ClassMapInfo.cpp
+++ iModelCore/ECDb/ECDb/ClassMapInfo.cpp
@@ -249,7 +249,8 @@ BentleyStatus ClassMapInfo::DoEvaluateMapStrategy(bool& baseClassesNotMappedYet,
                         return ERROR;
                         }
 
-                    m_ecInstanceIdColumnName.Sprintf("%s%s", rootClassMap->GetClass().GetName().c_str(), m_ecInstanceIdColumnName.c_str());
+                    //WIP: Can only commented out when prepare issues with picking the right table are solved.
+                    //m_ecInstanceIdColumnName.Sprintf("%s%s", rootClassMap->GetClass().GetName().c_str(), m_ecInstanceIdColumnName.c_str());
                     }
                 }
             }
