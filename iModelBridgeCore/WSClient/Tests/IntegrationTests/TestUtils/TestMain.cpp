9b4f6c437c5f255e984dbc8c0707497dba3b529e
Vincas.Razma <none@none>
2016-01-26 14:18:44 +0200

    Make integration tests print assers same way as BeTest

--- iModelBridgeCore/WSClient/Tests/IntegrationTests/TestUtils/TestMain.cpp
+++ iModelBridgeCore/WSClient/Tests/IntegrationTests/TestUtils/TestMain.cpp
@@ -2,13 +2,20 @@
 |
 |     $Source: Tests/IntegrationTests/TestUtils/TestMain.cpp $
 |
-|  $Copyright: (c) 2015 Bentley Systems, Incorporated. All rights reserved. $
+|  $Copyright: (c) 2016 Bentley Systems, Incorporated. All rights reserved. $
 |
 +--------------------------------------------------------------------------------------*/
 
 #include <WebServicesTestsHelper.h>
 
 #include "WSClientIntegrationTestsHost.h"
+
+struct GtestFailureHandler : BeTest::IFailureHandler
+    {
+    virtual void _OnAssertionFailure(WCharCP msg) { FAIL() << msg; }
+    virtual void _OnUnexpectedResult(WCharCP msg) { FAIL() << msg; }
+    virtual void _OnFailureHandled() {}
+    };
 
 extern "C"
 int main(int argc, char** argv)
@@ -17,6 +24,9 @@ int main(int argc, char** argv)
 
     auto host = WSClientIntegrationTestsHost::Create(argv[0]);
     BeTest::Initialize(*host);
+
+    GtestFailureHandler gtestFailureHandler;
+    BeTest::SetIFailureHandler(gtestFailureHandler);
 
     return RUN_ALL_TESTS();
     }
