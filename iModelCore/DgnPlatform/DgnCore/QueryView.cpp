8555d526ed14c89c68790853f1f9606609dca64e
Sam.Wilson <none@none>
2016-01-28 09:42:36 -0500

    Fix merge mistake

--- iModelCore/DgnPlatform/DgnCore/QueryView.cpp
+++ iModelCore/DgnPlatform/DgnCore/QueryView.cpp
@@ -272,7 +272,7 @@ Utf8String QueryViewController::_GetRTreeMatchSql(DgnViewportR)
     {
     return Utf8String("SELECT r.ElementId FROM "
            DGN_VTABLE_RTree3d " AS r, " DGN_TABLE(DGN_CLASSNAME_Element) " AS e, " DGN_TABLE(DGN_CLASSNAME_SpatialElement) " AS g "
-           "WHERE r.ElementId MATCH rTreeMatch(1) AND e.Id=r.ElementId AND g.Id=r.ElementId"
+           "WHERE r.ElementId MATCH DGN_rTree(@matcher) AND e.Id=r.ElementId AND g.ElementId=r.ElementId"
            " AND InVirtualSet(@vSet,e.ModelId,g.CategoryId)");
     }
 
