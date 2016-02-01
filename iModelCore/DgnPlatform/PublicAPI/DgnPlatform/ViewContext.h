5578084e8c041a0215a22fc2d40371942030a7b1
Carole.MacDonald <none@none>
2016-02-01 13:48:57 -0500

    Merge.

--- iModelCore/DgnPlatform/PublicAPI/DgnPlatform/ViewContext.h
+++ iModelCore/DgnPlatform/PublicAPI/DgnPlatform/ViewContext.h
@@ -2,7 +2,7 @@
 |
 |     $Source: PublicAPI/DgnPlatform/ViewContext.h $
 |
-|  $Copyright: (c) 2015 Bentley Systems, Incorporated. All rights reserved. $
+|  $Copyright: (c) 2016 Bentley Systems, Incorporated. All rights reserved. $
 |
 +--------------------------------------------------------------------------------------*/
 #pragma once
