64553574c13161f2eb426d82dde507ca65ae5eae
Umar.Hayat <none@none>
2016-02-04 18:01:38 +0500

    cosmetic

--- iModelCore/Bentley/PublicAPI/Bentley/ValueFormat.h
+++ iModelCore/Bentley/PublicAPI/Bentley/ValueFormat.h
@@ -2,7 +2,7 @@
 |
 |     $Source: PublicAPI/Bentley/ValueFormat.h $
 |
-|  $Copyright: (c) 2015 Bentley Systems, Incorporated. All rights reserved. $
+|  $Copyright: (c) 2016 Bentley Systems, Incorporated. All rights reserved. $
 |
 +--------------------------------------------------------------------------------------*/
 #pragma once
@@ -63,8 +63,8 @@ public:
 protected:
     BENTLEYDLL_EXPORT DoubleFormatterBase ();
 
-//! Get the precision used by this formatter.
-public: 
+public:
+    //! Get the precision used by this formatter.
     BENTLEYDLL_EXPORT PrecisionFormat GetPrecision () const;
 
     //! Get the decimal separator used by this formatter.
