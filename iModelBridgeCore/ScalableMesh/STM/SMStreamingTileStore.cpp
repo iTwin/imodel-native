243d953f6060801be0055c78d11eda1ac1c18e1d
Richard.Bois <none@none>
2016-02-17 10:29:21 -0500

    Specialized streaming tile store in .cpp

---
+++ iModelBridgeCore/ScalableMesh/STM/SMStreamingTileStore.cpp
@@ -0,0 +1,14 @@
+//:>--------------------------------------------------------------------------------------+
+//:>
+//:>     $Source: STM/SMStreamingTileStore.cpp $
+//:>
+//:>  $Copyright: (c) 2016 Bentley Systems, Incorporated. All rights reserved. $
+//:>
+//:>+--------------------------------------------------------------------------------------
+
+#include <ScalableMeshPCH.h>
+#include "ImagePPHeaders.h"
+#include "SMStreamingTileStore.h"
+
+template class SMStreamingPointTaggedTileStore<DPoint3d, IDTMFile::Extent3d64f>;
+template class SMStreamingPointTaggedTileStore<int32_t, IDTMFile::Extent3d64f>;
