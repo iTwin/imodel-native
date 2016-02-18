683323e35544b3be9a1c07cce6b2d005507ff585
Vincas.Razma <none@none>
2016-02-18 12:41:08 +0200

    Add initial ConnectSingInManagerTests

---
+++ iModelCore/WSClient/Tests/UnitTests/Published/WebServices/Connect/ConnectSignInManagerTests.h
@@ -0,0 +1,26 @@
+/*--------------------------------------------------------------------------------------+
+|
+|     $Source: Tests/UnitTests/Published/WebServices/Connect/ConnectSignInManagerTests.h $
+|
+|  $Copyright: (c) 2016 Bentley Systems, Incorporated. All rights reserved. $
+|
++--------------------------------------------------------------------------------------*/
+
+#pragma once
+
+#include "ConnectTestsHelper.h"
+#include <WebServices/Configuration/UrlProvider.h>
+#include "StubSecureStore.h"
+
+class ConnectSignInManagerTests : public BaseMockHttpHandlerTest
+    {
+    private:
+        std::shared_ptr<StubBuddiClient> m_client;
+        StubLocalState m_localState;
+        std::shared_ptr<StubSecureStore> m_secureStore;
+
+    public:
+        void StubUrlProviderEnvironment(UrlProvider::Environment env);
+        virtual void SetUp () override;
+        virtual void TearDown () override;
+    };
