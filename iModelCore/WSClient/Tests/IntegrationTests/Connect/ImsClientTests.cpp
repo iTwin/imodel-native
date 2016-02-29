67cba12954e1f1730853955a6ffb624a98f35ff9
Vincas.Razma <none@none>
2016-02-29 13:49:39 +0200

    Add ImsClient tests cases to reproduce issue

--- iModelCore/WSClient/Tests/IntegrationTests/Connect/ImsClientTests.cpp
+++ iModelCore/WSClient/Tests/IntegrationTests/Connect/ImsClientTests.cpp
@@ -13,7 +13,7 @@
 #include <MobileDgn/Utils/Http/ProxyHttpHandler.h>
 #include <curl/curl.h>
 
-TEST_F(ImsClientTests, GetToken_ProdUrls_RetrievesToken)
+TEST_F(ImsClientTests, RequestToken_ProdUrls_RetrievesToken)
     {
     StubLocalState localState;
     UrlProvider::Initialize(UrlProvider::Release, UrlProvider::DefaultTimeout, &localState);
@@ -31,7 +31,7 @@ TEST_F(ImsClientTests, GetToken_ProdUrls_RetrievesToken)
     BeDebugLog(tokenAuthStr.c_str());
     }
 
-TEST_F(ImsClientTests, GetToken_QaUrls_RetrievesToken)
+TEST_F(ImsClientTests, RequestToken_QaUrls_RetrievesToken)
     {
     StubLocalState localState;
     UrlProvider::Initialize(UrlProvider::Qa, UrlProvider::DefaultTimeout, &localState);
@@ -47,4 +47,63 @@ TEST_F(ImsClientTests, GetToken_QaUrls_RetrievesToken)
     SamlTokenPtr token = result.GetValue();
     auto tokenAuthStr = token->ToAuthorizationString();
     BeDebugLog(tokenAuthStr.c_str());
+    }
+
+TEST_F(ImsClientTests, RequestToken_NoLifetimeSpecified_RetrievesTokenWithDefaultLifetime)
+    {
+    StubLocalState localState;
+    UrlProvider::Initialize(UrlProvider::Qa, UrlProvider::DefaultTimeout, &localState);
+
+    auto proxy = ProxyHttpHandler::GetFiddlerProxyIfReachable();
+    auto client = ImsClient::Create(StubValidClientInfo(), proxy);
+
+    Credentials credentials("8cc45bd041514b58947ea6c09c@gmail.com", "qwe12312");
+
+    auto result = client->RequestToken(credentials, nullptr, 0)->GetResult();
+    ASSERT_TRUE(result.IsSuccess());
+
+    SamlTokenPtr token = result.GetValue();
+    EXPECT_TRUE(token->IsSupported());
+    EXPECT_TRUE(token->IsValidNow(0));
+    EXPECT_NE(0, token->GetLifetime());
+    }
+
+TEST_F(ImsClientTests, RequestToken_LifetimeSpecified_RetrievesTokenWithSpecifiedLifetime)
+    {
+    StubLocalState localState;
+    UrlProvider::Initialize(UrlProvider::Qa, UrlProvider::DefaultTimeout, &localState);
+
+    auto proxy = ProxyHttpHandler::GetFiddlerProxyIfReachable();
+    auto client = ImsClient::Create(StubValidClientInfo(), proxy);
+
+    Credentials credentials("8cc45bd041514b58947ea6c09c@gmail.com", "qwe12312");
+
+    auto result = client->RequestToken(credentials, nullptr, 5)->GetResult();
+    ASSERT_TRUE(result.IsSuccess());
+
+    SamlTokenPtr token = result.GetValue();
+    EXPECT_TRUE(token->IsSupported());
+    EXPECT_TRUE(token->IsValidNow(0));
+    EXPECT_EQ(5, token->GetLifetime());
+    }
+
+TEST_F(ImsClientTests, RequestToken_UsingParentTokenAndLifetimeSpecified_RetrievesTokenWithSpecifiedLifetime)
+    {
+    StubLocalState localState;
+    UrlProvider::Initialize(UrlProvider::Qa, UrlProvider::DefaultTimeout, &localState);
+
+    auto proxy = ProxyHttpHandler::GetFiddlerProxyIfReachable();
+    auto client = ImsClient::Create(StubValidClientInfo(), proxy);
+
+    Credentials credentials("8cc45bd041514b58947ea6c09c@gmail.com", "qwe12312");
+    auto parentTokenResult = client->RequestToken(credentials)->GetResult();
+    ASSERT_TRUE(parentTokenResult.IsSuccess());
+
+    auto result = client->RequestToken(*parentTokenResult.GetValue(), nullptr, 5)->GetResult();
+    ASSERT_TRUE(result.IsSuccess());
+
+    SamlTokenPtr token = result.GetValue();
+    EXPECT_TRUE(token->IsSupported());
+    EXPECT_TRUE(token->IsValidNow(0));
+    EXPECT_EQ(5, token->GetLifetime());
     }
