93747ffc1013fe41fc3148d1f43d20fc72929c2c
Grigas.Petraitis <none@none>
2016-03-01 15:07:13 +0200

    Fixed a build break.

--- iModelCore/ECDb/Tests/Published/ECDbSymbolProviderTests.cpp
+++ iModelCore/ECDb/Tests/Published/ECDbSymbolProviderTests.cpp
@@ -5,7 +5,6 @@
 |  $Copyright: (c) 2016 Bentley Systems, Incorporated. All rights reserved. $
 |
 +--------------------------------------------------------------------------------------*/
-#pragma once
 #include "ECDbPublishedTests.h"
 #include <ECDb/ECDbExpressionSymbolProviders.h>
 
