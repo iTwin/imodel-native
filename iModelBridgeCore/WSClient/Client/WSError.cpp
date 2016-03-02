fb74fc0ee0f0c7d4777931b685749a002bfa3c7f
Vincas.Razma <none@none>
2016-03-02 10:29:08 +0200

    Add better logging Network errors should be logged as errors

--- iModelBridgeCore/WSClient/Client/WSError.cpp
+++ iModelBridgeCore/WSClient/Client/WSError.cpp
@@ -80,9 +80,9 @@ m_id(errorId)
 WSError::WSError(HttpResponseCR httpResponse) : WSError()
     {
     if (ConnectionStatus::OK == httpResponse.GetConnectionStatus() &&
-        LOG.isSeverityEnabled(NativeLogging::SEVERITY::LOG_TRACE))
+        LOG.isSeverityEnabled(NativeLogging::SEVERITY::LOG_ERROR))
         {
-        LOG.tracev
+        LOG.errorv
             (
             "WSError - received error:\n"
             "From Url: %s\n"
