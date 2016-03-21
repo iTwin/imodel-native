cdd17d9d8ad8b16c601e9fdabc4cf91062e0350e
Sam.Wilson <none@none>
2016-03-21 10:25:37 -0400

    Removed char const* operator from Utf8PrintfString and wchar_t const* operator from WPrintfString. While convenient, the implicit casts make code harder to understand. They also make the Utf8PrintfString inconsistent with the Utf8String class, which does not define cast operators. (Similaryly, std::basic_string does not define cast operators.) They also lead to confusing compile errors on some platforms, where the index operator becomes ambiguous, since both char*[int] and Utf8String[size_t] are both in play.

--- iModelCore/ECDb/ECDb/ECDbMapAnalyser.cpp
+++ iModelCore/ECDb/ECDb/ECDbMapAnalyser.cpp
@@ -1239,9 +1239,9 @@ SqlViewBuilder ECDbMapAnalyser::BuildView (Class& nclass)
             else
                 {
                 if (rootE->GetColumn () != nullptr)
-                    select.Append (Utf8PrintfString ("%lld [%s]", childE->GetValue ().GetLong (), rootE->GetColumn ()->GetName ().c_str ()));
+                    select.Append (Utf8PrintfString ("%lld [%s]", childE->GetValue ().GetLong (), rootE->GetColumn ()->GetName ().c_str ()).c_str());
                 else
-                    select.Append (Utf8PrintfString ("%lld [%s]", childE->GetValue ().GetLong (), (rootE->GetAccessString ().c_str ())));
+                    select.Append (Utf8PrintfString ("%lld [%s]", childE->GetValue ().GetLong (), (rootE->GetAccessString ().c_str ())).c_str());
                 }
 
             if (rootE != rootEndPoints.back ())
