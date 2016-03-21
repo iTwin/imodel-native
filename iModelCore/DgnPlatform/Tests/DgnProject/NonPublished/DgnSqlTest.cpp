9caa566350f7b7e33dfb845cd4f93880c756b84f
Sam.Wilson <none@none>
2016-03-21 14:44:23 -0400

    Removed char const* operator from Utf8PrintfString and wchar_t const* operator from WPrintfString. While convenient, the implicit casts make code harder to understand. They also make the Utf8PrintfString inconsistent with the Utf8String class, which does not define cast operators. (Similaryly, std::basic_string does not define cast operators.) They also lead to confusing compile errors on some platforms, where the index operator becomes ambiguous, since both char*[int] and Utf8String[size_t] are both in play.

--- iModelCore/DgnPlatform/Tests/DgnProject/NonPublished/DgnSqlTest.cpp
+++ iModelCore/DgnPlatform/Tests/DgnProject/NonPublished/DgnSqlTest.cpp
@@ -120,7 +120,7 @@ TEST_F(SqlFunctionsTest, placement_areaxy)
         DbResult rc = stmt.Step(); // rc will be BE_SQLITE_ERROR, and m_db->GetLastError() will return "Illegal input to DGN_bbox_areaxy"
         //__PUBLISH_EXTRACT_END__
         ASSERT_EQ( BE_SQLITE_ERROR , rc );
-        BeTest::Log("SqlFunctionsTest", BeTest::PRIORITY_INFO, Utf8PrintfString("SQLite error: %s\n", m_db->GetLastError().c_str())); // displays "SQLite error: Illegal input to DGN_bbox_areaxy"
+        BeTest::Log("SqlFunctionsTest", BeTest::PRIORITY_INFO, Utf8PrintfString("SQLite error: %s\n", m_db->GetLastError().c_str()).c_str()); // displays "SQLite error: Illegal input to DGN_bbox_areaxy"
         }
 
     //  The X-Y area is width (X) time depth (Y)
@@ -142,7 +142,7 @@ TEST_F(SqlFunctionsTest, placement_areaxy)
             totalAreaXy += areaxy;
             }
 
-        ASSERT_EQ( BE_SQLITE_DONE , rc ) << (Utf8CP)Utf8PrintfString("SQLite error: %s", m_db->GetLastError().c_str());
+        ASSERT_EQ( BE_SQLITE_DONE , rc ) << Utf8PrintfString("SQLite error: %s", m_db->GetLastError().c_str()).c_str();
         EXPECT_DOUBLE_EQ( 2*obstacleXyArea , totalAreaXy );
         }
 
