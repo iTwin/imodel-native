d9479e9bea6787839f9d3f44577a2a2dbcca9d99
Sam.Wilson <none@none>
2016-03-28 14:24:09 -0400

    back out recent change to remove std::move - doesn't compile under Windows

--- iModelBridgeCore/WSClient/Tests/UnitTests/Published/WebServices/Cache/MockCachingDataSource.h
+++ iModelBridgeCore/WSClient/Tests/UnitTests/Published/WebServices/Cache/MockCachingDataSource.h
@@ -69,7 +69,7 @@ struct MockCachingDataSource : public ICachingDataSource
 
         CacheTransaction StartCacheTransaction () override
             {
-            return *StartCacheTransactionProxy ();
+            return std::move(*StartCacheTransactionProxy());
             }
 
         //! Use this method to mock StartCacheTransaction that returns non-copyable CacheTransaction
