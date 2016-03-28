83d3dcb6d1a1848d879c23667006d1c61b3b3c32
David.Jones <none@none>
2016-03-28 10:36:27 -0500

    Added SolrClient for making Solr REST calls, specifically to IMS Search API endpoints Added SolrQuery for building Solr REST query strings Added Unit and Integration tests to validate SolrClient and SolrQuery

---
+++ iModelCore/WSClient/Tests/IntegrationTests/Ims/SolrClientTests.h
@@ -0,0 +1,21 @@
+/*--------------------------------------------------------------------------------------+
+|
+|     $Source: Tests/IntegrationTests/Ims/SolrClientTests.h $
+|
+|  $Copyright: (c) 2016 Bentley Systems, Incorporated. All rights reserved. $
+|
++--------------------------------------------------------------------------------------*/
+
+#pragma once
+
+#include <WebServicesTestsHelper.h>
+#include <DgnClientFx/Utils/Http/ProxyHttpHandler.h>
+
+struct SolrClientTests : public WSClientBaseTest
+    {
+    StubLocalState m_localState;
+    std::shared_ptr<ProxyHttpHandler> m_proxy;
+    Utf8String m_serverUrl;
+    Credentials m_credentials;
+    void SetUp();
+    };
---
+++ iModelCore/WSClient/Tests/UnitTests/Published/WebServices/Ims/SolrQueryTests.h
@@ -0,0 +1,15 @@
+/*--------------------------------------------------------------------------------------+
+|
+|     $Source: Tests/UnitTests/Published/WebServices/Ims/SolrQueryTests.h $
+|
+|  $Copyright: (c) 2016 Bentley Systems, Incorporated. All rights reserved. $
+|
++--------------------------------------------------------------------------------------*/
+
+#pragma once
+
+#include "../../Utils/WebServicesTestsHelper.h"
+
+class SolrQueryTests : public WSClientBaseTest
+    {};
+
