a8c1d020b1fd30edf8cc33e00faaa919612b9fd4
Muhammad.Hassan <none@none>
2016-04-05 16:23:15 +0500

    enhanced JsonReader test. #TFS 383266

--- iModelCore/ECDb/Tests/Published/JsonReaderTests.cpp
+++ iModelCore/ECDb/Tests/Published/JsonReaderTests.cpp
@@ -12,9 +12,15 @@ USING_NAMESPACE_BENTLEY_EC
 
 BEGIN_ECDBUNITTESTS_NAMESPACE
 
+//---------------------------------------------------------------------------------------
+// @bsiclass                                   Muhammad Hassan                     03/16
+//+---------------+---------------+---------------+---------------+---------------+------
 struct JsonReaderTests : public SchemaImportTestFixture
     {};
 
+//---------------------------------------------------------------------------------------
+// @bsimethod                                   Muhammad Hassan                     03/16
+//+---------------+---------------+---------------+---------------+---------------+------
 TEST_F(JsonReaderTests, ReadInstanceAlongWithRelatedInstances)
     {
     ECDb db;
@@ -28,9 +34,31 @@ TEST_F(JsonReaderTests, ReadInstanceAlongWithRelatedInstances)
 
     {
     SchemaItem testItem("<?xml version='1.0' encoding='utf-8'?>"
-                        "<ECSchema schemaName='test' nameSpacePrefix='ts' version='1.0' xmlns='http://www.bentley.com/schemas/Bentley.ECXML.3.0'>"
+                        "<ECSchema schemaName='TestSchema' nameSpacePrefix='ts' version='1.0' xmlns='http://www.bentley.com/schemas/Bentley.ECXML.3.0'>"
+                        "<ECSchemaReference name='Bentley_Standard_CustomAttributes' version='01.13' prefix='bsca' />"
+                        "        <ECCustomAttributes>"
+                        "            <RelatedItemsDisplaySpecifications xmlns='Bentley_Standard_CustomAttributes.01.13'>"
+                        "                <Specifications>"
+                        "                   <RelatedItemsDisplaySpecification>"
+                        "                   <ParentClass>ts:A1</ParentClass>"
+                        "                   <RelationshipPath>ts:AHasA</RelationshipPath>"
+                        "                   <DerivedClasses>"
+                        "                   <string>ts:A2</string>"
+                        "                   </DerivedClasses>"
+                        "                   </RelatedItemsDisplaySpecification>"
+                        "                </Specifications>"
+                        "            </RelatedItemsDisplaySpecifications>"
+                        "        </ECCustomAttributes>"
                         "    <ECEntityClass typeName='A' >"
-                        "        <ECProperty propertyName='P1' typeName='int' />"
+                        "        <ECProperty propertyName='Aprop' typeName='int' />"
+                        "    </ECEntityClass>"
+                        "    <ECEntityClass typeName='A1' >"
+                        "        <BaseClass>A</BaseClass>"
+                        "        <ECProperty propertyName='A1prop' typeName='int' />"
+                        "    </ECEntityClass>"
+                        "    <ECEntityClass typeName='A2' >"
+                        "        <BaseClass>A</BaseClass>"
+                        "        <ECProperty propertyName='A2prop' typeName='int' />"
                         "    </ECEntityClass>"
                         "    <ECRelationshipClass typeName='AHasA' strength='referencing'>"
                         "        <ECProperty propertyName='Name' typeName='string' />"
@@ -45,8 +73,7 @@ TEST_F(JsonReaderTests, ReadInstanceAlongWithRelatedInstances)
     ASSERT_FALSE(asserted);
 
     ECSqlStatement stmt;
-    ASSERT_EQ(ECSqlStatus::Success, stmt.Prepare(db, "INSERT INTO ts.A (P1) VALUES(?)"));
-
+    ASSERT_EQ(ECSqlStatus::Success, stmt.Prepare(db, "INSERT INTO ts.A (Aprop) VALUES(?)"));
 
     ASSERT_EQ(ECSqlStatus::Success, stmt.BindInt(1, 111));
     ASSERT_EQ(BE_SQLITE_DONE, stmt.Step(sourceKey));
@@ -54,11 +81,9 @@ TEST_F(JsonReaderTests, ReadInstanceAlongWithRelatedInstances)
 
     stmt.Reset();
     stmt.ClearBindings();
-
     ASSERT_EQ(ECSqlStatus::Success, stmt.BindInt(1, 222));
     ASSERT_EQ(BE_SQLITE_DONE, stmt.Step(targetKey));
     targetInstanceId = targetKey.GetECInstanceId();
-
     stmt.Finalize();
 
     ASSERT_EQ(ECSqlStatus::Success, stmt.Prepare(db, "INSERT INTO ts.AHasA (SourceECInstanceId, TargetECInstanceId, Name) VALUES(?,?,'good morning')"));
@@ -71,7 +96,7 @@ TEST_F(JsonReaderTests, ReadInstanceAlongWithRelatedInstances)
     relInstanceId = relKey.GetECInstanceId();
     }
 
-    ECClassCP entityClass = db.Schemas().GetECClass("test", "A");
+    ECClassCP entityClass = db.Schemas().GetECClass("TestSchema", "A");
     ASSERT_TRUE(entityClass != nullptr);
     JsonReader reader(db, entityClass->GetId());
     Json::Value classJsonWithRelatedInstances;
