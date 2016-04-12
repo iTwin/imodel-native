e125d7d7496cb1ababb9324c7881efa8b2baa1df
Paul.Connelly <none@none>
2016-04-12 15:20:26 -0400

    CM: _OnDbOpened() => _OnDbOpening()

--- iModelCore/ECDb/PublicAPI/ECDb/ECDb.h
+++ iModelCore/ECDb/PublicAPI/ECDb/ECDb.h
@@ -88,7 +88,7 @@ private:
 
 #if !defined (DOCUMENTATION_GENERATOR)
 protected:
-    ECDB_EXPORT virtual DbResult _OnDbOpened() override;
+    ECDB_EXPORT virtual DbResult _OnDbOpening() override;
     //! Fired when an ECDb file was created. Creates the %EC profile.
     ECDB_EXPORT virtual DbResult _OnDbCreated(CreateParams const& params) override;
     ECDB_EXPORT virtual DbResult _OnBriefcaseIdChanged(BeBriefcaseId newBriefcaseId) override;
