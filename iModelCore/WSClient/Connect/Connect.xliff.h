d58f11391aaec53760199764ade781632599294c
Vincas.Razma <none@none>
2016-04-12 16:54:26 +0300

    CM: Move Connect.xliff.h strings to FieldApps FieldAppsUi.xliff.h where they belong SRB:Vytautas.Barkauskas

--- iModelCore/WSClient/Connect/Connect.xliff.h
+++ iModelCore/WSClient/Connect/Connect.xliff.h
@@ -18,33 +18,6 @@ MOBILEDGN_TRANSLATABLE_STRINGS_START(ConnectL10N, Connect)
     ALERT_UnsupportedToken,                  // =="Could not sign in with provided token."==
     ALERT_SignInFailed_ServerError,          // =="Could not connect to server. Check your network connection."==
     ALERT_SignInFailed_Message,              // =="Please enter the correct email and password."==
-    FIELDAPPS_SingleSignOn,                  // =="Single sign-on"==
-    FIELDAPPS_DontHaveAnAccount,             // =="Don't have an account yet?"==
-    FIELDAPPS_SignUp,                        // =="Sign up"==
-    FIELDAPPS_SignUpSuggestion,              // =="Don't have a profile?"==
-    FIELDAPPS_SignUpSuggestionLink,          // =="Register"==
-    FIELDAPPS_Email,                         // =="Email"==
-    FIELDAPPS_Password,                      // =="Password"==
-    FIELDAPPS_ForgotPassword,                // =="Forgot Password?"==
-    FIELDAPPS_SignIn,                        // =="Sign in"==
-    FIELDAPPS_ProductDemo,                   // =="Demo"==
-    FIELDAPPS_EmailAndPasswordRequired,      // =="You must enter an email address and password."==
-    FIELDAPPS_Error,                         // =="Error"==
-    FIELDAPPS_Warning,                       // =="Warning"==
-    FIELDAPPS_SignInFailed,                  // =="Sign In Failed"==
-    FIELDAPPS_UnknownError,                  // =="Unknown error."==
-    FIELDAPPS_SignInUsername,                // =="Signed in username:"==
-    FIELDAPPS_SignOut,                       // =="Sign Out"==
-    FIELDAPPS_SigningInDotDotDot,            // =="Signing In..."==
-    FIELDAPPS_EulaAccept,                    // =="Accept"==
-    FIELDAPPS_EulaDecline,                   // =="Decline"==
-    FIELDAPPS_EulaAgree,                     // =="To continue you must accept the terms in the Licence Agreement"==
-    FIELDAPPS_EulaHeader,                    // =="End-user license agreement"==
-    FIELDAPPS_EulaChecking,                  // =="Checking EULA..."==
-    FIELDAPPS_EulaAccepting,                 // =="Accepting EULA..."==
-    FIELDAPPS_SignInConnecting,              // =="Connecting to sign-in page..."==
-    FIELDAPPS_SigningOut,                    // =="Signing out..."==
-    FIELDAPPS_TokenExpired,                  // =="Session expired. Please sign in again."==
     };
 MOBILEDGN_TRANSLATABLE_STRINGS_END
 
