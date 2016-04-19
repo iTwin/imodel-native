9641bd05b8a9bf41983afa2aa428821222b9c7c4
Carole.MacDonald <none@none>
2016-04-19 09:15:11 -0400

    TFS#406819: Fixed which list of supplemental schemas to check to verify there are any.

--- iModelCore/ecobjects/src/SupplementalSchema.cpp
+++ iModelCore/ecobjects/src/SupplementalSchema.cpp
@@ -392,7 +392,7 @@ bool createCopyOfSupplementalCustomAttribute
     if (SupplementedSchemaStatus::Success != status)
         return status;
 
-    if (1 > supplementalSchemaList.size())
+    if (1 > schemasByPrecedence.size() && 1 > localizationSchemas.size())
         return status;
 
     SupplementalSchemaInfoPtr info = SupplementalSchemaInfo::Create(primarySchema.GetFullSchemaName().c_str(), m_supplementalSchemaNamesAndPurposes);
