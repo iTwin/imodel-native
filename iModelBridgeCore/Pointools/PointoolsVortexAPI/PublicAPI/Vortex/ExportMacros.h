a9b764743950b725724f59cedcb2a1a1786d2f16
Mathieu.Marchand <none@none>
2016-04-29 14:37:46 -0400

    - cleanup 2005, 2008.... parts - Added prewire for PublicAPI\Vortex
    --HG--
    branch : PointoolsVortexAPI

---
+++ iModelBridgeCore/Pointools/PointoolsVortexAPI/PublicAPI/Vortex/ExportMacros.h
@@ -0,0 +1,36 @@
+//:>--------------------------------------------------------------------------------------+
+//:>
+//:>     $Source: PublicAPI/Vortex/ExportMacros.h $
+//:>
+//:>  $Copyright: (c) 2016 Bentley Systems, Incorporated. All rights reserved. $
+//:>
+//:>+--------------------------------------------------------------------------------------
+#pragma once
+
+// Should have come from bentley.h but we do not want to depend on it for now.
+//#include <Bentley/Bentley.h>
+#if defined (__GNUC__) || defined (__clang__)
+    #define EXPORT_ATTRIBUTE        __attribute__((visibility ("default")))
+    #define IMPORT_ATTRIBUTE        __attribute__((visibility ("default")))
+
+#elif defined (_WIN32) // Windows && WinRT
+    #define EXPORT_ATTRIBUTE        __declspec(dllexport)
+    #define IMPORT_ATTRIBUTE        __declspec(dllimport)
+#else
+    #error unknown compiler
+#endif
+
+#   if defined (__PointoolsVortexAPI_BUILD__)
+#       define VORTEX_EXPORT EXPORT_ATTRIBUTE
+#   else
+#       define VORTEX_EXPORT IMPORT_ATTRIBUTE
+#   endif
+
+#if defined(CREATE_STATIC_LIBRARIES)
+    #undef VORTEX_EXPORT
+    #define VORTEX_EXPORT
+#endif
+
+
+
+
