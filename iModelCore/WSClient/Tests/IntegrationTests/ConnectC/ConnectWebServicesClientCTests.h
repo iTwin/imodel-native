ccf8259727b780847fce15094b47495c69f2fc0e
David.Jones <none@none>
2016-05-05 11:33:32 -0500

    Updated integration tests to adapt to changes

--- iModelCore/WSClient/Tests/IntegrationTests/ConnectC/ConnectWebServicesClientCTests.h
+++ iModelCore/WSClient/Tests/IntegrationTests/ConnectC/ConnectWebServicesClientCTests.h
@@ -14,8 +14,15 @@
 
 struct ConnectWebServicesClientC : public WSClientBaseTest
     {
-    static const int CCPRODUCTID = 1805;
-    static const int NAVPRODUCTID = 2545;
+    const WString m_username = L"david.jones@bentley.com";
+    const WString m_password = L"testdfijEr34";
+    const WString m_temporaryDirectory = L"C:/Users/David.Jones/AppData/Local/Bentley/WSApi";
+    const WString m_assetsRootDirectory = L"D:/dev/dgndb0601dev/out/Winx64/Product/DgnClientSdk-Winx64/assets";
+    const WString m_ccProductId = L"1805";
+    const WString m_navProductId = L"2545";
+    const WString m_applicationName = L"Bentley-Test";
+    const WString m_applicationVersion = L"1.0.0.0";
+    const WString m_applicationGuid = L"TestAppGUID";
 
     CWSCCHANDLE m_api;
     void SetUp();
