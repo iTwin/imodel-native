68de85994a2606620c887de653a55e8e85df37ef
Sam.Wilson <none@none>
2016-05-10 10:26:31 -0400

    WIP RunAndroidJUnit now displays a progress window

--- iModelCore/BeGTest/Android/RunAndroidJUnitTest.py
+++ iModelCore/BeGTest/Android/RunAndroidJUnitTest.py
@@ -7,71 +7,63 @@
 #--------------------------------------------------------------------------------------
 import os, glob, sys, string, stat, re, subprocess
 
-import sys
 sys.path.append(os.path.join (os.getenv("SrcRoot"), "bsicommon", "build"))
 from bentleybuild.utils import *
 
 #-------------------------------------------------------------------------------------------
 # bsimethod                                     Sam.Wilson              04/2016
 #-------------------------------------------------------------------------------------------
-def printProgress(procStdOutLine, status_len):
-    msg = procStdOutLine.strip('\n')
-    msg = msg.replace('\t', ' ')
-    showRewritableLine(msg, INFO_LEVEL_Important)
+def main():
+    if (len (sys.argv) < 4):
+        print "Syntax: ", sys.argv[0], " logfilename installscript logcatscript"
+        exit(1)
 
-#-------------------------------------------------------------------------------------------
-# bsimethod                                     Sam.Wilson              04/2016
-#-------------------------------------------------------------------------------------------
-def RunTest(adbCmd, logfile):
-    status_len = 132
+    exeDir = os.path.dirname(sys.argv[0])
 
-    failureCount = 0
-    testCount = 0
-    testSuiteCount = 0
+    logfilename = sys.argv[1]
+    installscript = sys.argv[2]
+    logcatscript = sys.argv[3]
 
+    with open (logfilename, 'w') as logfile:
+        with open(installscript, 'r') as installCmds:
+            for installCmd in installCmds.readlines():
+                print installCmd,
+                proc = subprocess.Popen(installCmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell = True)
+                procStdOutLine = proc.stdout.readline ()
+                while procStdOutLine:
+                    print procStdOutLine,
+                    logfile.write(procStdOutLine)
+                    procStdOutLine = proc.stdout.readline ()
+                proc.wait()
+
+
+    adbCmd = os.path.join(os.path.join(os.environ["ANDROID_SDK_ROOT"], "platform-tools"), "adb");
     cmd = [adbCmd, "shell", "am", "instrument", "-w", "com.bentley.unittest/com.bentley.unittest.BeInstrumentationTestRunner"]
     print " ".join(cmd)
     proc = subprocess.Popen(" ".join(cmd), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell = True)
+
+    # NEEDS WORK logcat always hangs. I can't seem to kill it
+    #logcatCmd = ""
+    #with open (logcatscript, 'r') as logcatscriptfile:
+    #    logcatCmd = logcatscriptfile.read()
+    #print logcatCmd
+    #logcatproc = subprocess.Popen(logcatCmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell = True, creationflags = subprocess.CREATE_NEW_PROCESS_GROUP)
+    #
+    #teeCmd = os.path.join (os.getenv("SrcRoot"), "bsicommon", "build", "Tee.py")
+    #tee = subprocess.Popen ("python " + teeCmd + " " + logfilename, shell=True, stdin=logcatproc.stdout, creationflags = subprocess.CREATE_NEW_PROCESS_GROUP)
+
+    failureCount = 0
     procStdOutLine = proc.stdout.readline ()
     while procStdOutLine:
-        
-        # We often get an error when copying resources to the device the first time we try to run a given set of tests
-        if procStdOutLine.startswith('com.bentley.unittest'):
-            printProgress(procStdOutLine, status_len)
-            testCount = testCount + 1
-        else:
-            if procStdOutLine.startswith("Failure"):
-                printProgress(' ', status_len)
-                print procStdOutLine,
-                failureCount = failureCount + 1
-        
-        logfile.write(procStdOutLine)
-
+        showRewritableLine(procStdOutLine.replace('\n', ' ').replace('\r', ' '), INFO_LEVEL_Important)
+        if procStdOutLine.lower().startswith("failure"):
+            failureCount = failureCount + 1
         procStdOutLine = proc.stdout.readline ()
+    proc.wait()
 
-    retval = proc.wait()
+    #logcatproc.kill()
 
-    printProgress(' ', status_len)
-    print '{0} failures'.format(failureCount)
-
-    # if any test fails to build or if any test does not succeed, the xcodebuild test command should return a non-zero status. 
-    return retval + failureCount
-
-#-------------------------------------------------------------------------------------------
-# bsimethod                                     Sam.Wilson              04/2016
-#-------------------------------------------------------------------------------------------
-def main():
-    if (len (sys.argv) < 2):
-        print "Syntax: ", sys.argv[0], " logfilename"
-        exit(1)
-
-    adbCmd = os.path.join(os.path.join(os.environ["ANDROID_SDK_ROOT"], "platform-tools"), "adb");
-    logfilename = sys.argv[1]
-
-    with open (logfilename, 'w') as logfile:
-        failureCount = RunTest(adbCmd, logfile)
-
-    exit (failureCount)
+    exit(failureCount)
 
 if __name__ == '__main__':
     main()
