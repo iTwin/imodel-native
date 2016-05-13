d150bf10a33b71b1fc7c3c569315e42543a4a544
Jeehwan.Cho <none@none>
2016-05-13 14:22:28 -0400

    EventServiceClient revamped

--- iModelCore/WSClient/Azure/EventServiceClient.cpp
+++ iModelCore/WSClient/Azure/EventServiceClient.cpp
@@ -10,13 +10,17 @@
 #include <iomanip>
 #include <Bentley/Base64Utilities.h>
 
-EventServiceClient::EventServiceClient(Utf8StringCR nameSpace, Utf8StringCR repoId, Utf8StringCR userId)
+EventServiceClient::EventServiceClient(Utf8StringCR nameSpace, Utf8StringCR repoId, Utf8StringCR userId, int longPollingTimeout)
 {
+	m_timeout = longPollingTimeout;
+	char numBuffer[10];
+	itoa(longPollingTimeout, numBuffer, 10);
+	Utf8String timeout = Utf8String(numBuffer);
 	m_nameSpace = nameSpace;
 	m_repoId = repoId;
 	m_userId = userId;
 	Utf8String baseAddress = "https://" + nameSpace + "." + "servicebus.windows.net/";
-	m_fullAddress = baseAddress + repoId + "/Subscriptions/" + userId + "/messages/head" + "?timeout=60";
+	m_fullAddress = baseAddress + repoId + "/Subscriptions/" + userId + "/messages/head?timeout=" + timeout;
 }
 
 bool EventServiceClient::Receive (Utf8StringR msgOut, Utf8StringCR token)
@@ -26,6 +30,7 @@ bool EventServiceClient::Receive (Utf8StringR msgOut, Utf8StringCR token)
 	request.GetHeaders ().SetValue ("Content-Length", "0");
 	request.GetHeaders ().SetValue ("Content-Type", "application/atom+xml;type=entry;charset=utf-8");
 	request.GetHeaders ().SetAuthorization (token);
+	// request.SetTimeoutSeconds(m_timeout);
 	HttpResponse response = request.Perform();
 
 	if (!response.IsSuccess())
@@ -36,17 +41,4 @@ bool EventServiceClient::Receive (Utf8StringR msgOut, Utf8StringCR token)
 	else
 		printf("response.IsSuccess(): %s\n", response.GetBody().AsString().c_str());
 	return true;
-// Utf8String baseAddress = "https://" + nameSpace + "." + "servicebus.windows.net/";
-// Utf8String fullAddress = baseAddress + repoId + "/Subscriptions/" + userId + "/messages/head" + "?timeout=60";
-// HttpRequest request(fullAddress.c_str(), "DELETE", nullptr);
-// request.GetHeaders ().Clear ();
-// request.GetHeaders ().SetValue ("Content-Length", "0");
-// request.GetHeaders ().SetValue ("Content-Type", "application/atom+xml;type=entry;charset=utf-8");
-// request.GetHeaders ().SetAuthorization (token);
-// HttpResponse response = request.Perform();
-
-// if (!response.IsSuccess())
-//     printf("!!!!!response.IsSuccess()");
-// else
-// 	printf("response.IsSuccess()");
 }
--- iModelCore/WSClient/Tests/UnitTests/Published/WebServices/Azure/EventServiceClientTests.cpp
+++ iModelCore/WSClient/Tests/UnitTests/Published/WebServices/Azure/EventServiceClientTests.cpp
@@ -14,19 +14,16 @@ USING_NAMESPACE_BENTLEY_DGNCLIENTFX_UTILS
 
 TEST_F(EventServiceClientTests, Test1)
 {
-	//Utf8String SA_KEY_NAME = "RootManageSharedAccessKey";
-	//Utf8String SA_KEY_VALUE = "Pz2HmbIvXLj7ntcgiwgxKzYgcXZxyJrobQLkO+u1jVc=";
 	Utf8String SERVICE_NAMESPACE = "testhubjeehwan-ns";
 	Utf8String REPO_ID = "TEST";
 	Utf8String USER_ID1 = "USER_ID1";
-	Utf8String token = "SharedAccessSignature sig=Z7WLDGx%2f3Vas2o8pLN8YCL5CBxEWA4uLcNKobWFTGRA%3d&se=1463160725&skn=RootManageSharedAccessKey&sr=https%3a%2f%2ftesthubjeehwan-ns.servicebus.windows.net%2ftest";
-
-	EventServiceClient* client = new EventServiceClient(SERVICE_NAMESPACE, REPO_ID, USER_ID1);
+	Utf8String token = "SharedAccessSignature sig=XpLyYfnpVhrvLOh5Gbz18aSPz6H2zkOKomBCu9%2bdyIA%3d&se=1463239626&skn=EventReceivePolicy&sr=https%3a%2f%2ftesthubjeehwan-ns.servicebus.windows.net%2ftest";
+	EventServiceClient* client = new EventServiceClient(SERVICE_NAMESPACE, REPO_ID, USER_ID1, 300);
 	printf("receiving msgs..\n");
 	Utf8String msg = "";
 	while (client->Receive(msg, token))
 	{
 		;
 	}
-	// EXPECT_TRUE(false);
+	EXPECT_TRUE(false);
 }
