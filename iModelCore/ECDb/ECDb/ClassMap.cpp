041a195b82c9260d1644407d45139c60576228ba
Affan.Khan <none@none>
2016-05-23 12:38:54 +0500

    Fixed a warning message in a class view

--- iModelCore/ECDb/ECDb/ClassMap.cpp
+++ iModelCore/ECDb/ECDb/ClassMap.cpp
@@ -632,7 +632,7 @@ BentleyStatus  ClassMap::CreateOrReplaceView() const
         }
 
     Utf8String sql;
-    sql.Sprintf("CREATE VIEW [%s.%s] (%s)\n\t--### ECCLASS VIEW is for only debugging purpose.\n\tAS %s;", GetClass().GetSchema().GetNamespacePrefix().c_str(), GetClass().GetName().c_str(), columns.c_str(), viewSql.ToString());
+    sql.Sprintf("CREATE VIEW [%s.%s] (%s)\n\t--### ECCLASS VIEW is for debugging purpose only!.\n\tAS %s;", GetClass().GetSchema().GetNamespacePrefix().c_str(), GetClass().GetName().c_str(), columns.c_str(), viewSql.ToString());
     if (GetECDbMap().GetECDb().ExecuteSql(sql.c_str()) != BE_SQLITE_OK)
         return ERROR;
 
