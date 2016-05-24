3be02c85ae9083f177089c26fc6bc8602ec2b0da
Vincas.Razma <none@none>
2016-05-24 14:01:53 +0300

    Make UsageTracking API async so it would not block app code

--- iModelBridgeCore/WSClient/Tests/UnitTests/Published/WebServices/Licensing/UsageTrackingTests.cpp
+++ iModelBridgeCore/WSClient/Tests/UnitTests/Published/WebServices/Licensing/UsageTrackingTests.cpp
@@ -2,7 +2,7 @@
 |
 |     $Source: Tests/UnitTests/Published/WebServices/Licensing/UsageTrackingTests.cpp $
 |
-|  $Copyright: (c) 2015 Bentley Systems, Incorporated. All rights reserved. $
+|  $Copyright: (c) 2016 Bentley Systems, Incorporated. All rights reserved. $
 |
 +--------------------------------------------------------------------------------------*/
 #include "UsageTrackingTests.h"
@@ -15,62 +15,66 @@
 USING_NAMESPACE_BENTLEY_WEBSERVICES
 USING_NAMESPACE_BENTLEY_MOBILEDGN_UTILS
 
-void UsageTrackingTests::SetUp ()
+void UsageTrackingTests::SetUp()
     {
-    UsageTracking::Initialize (GetHandlerPtr ());
+    UsageTracking::Initialize(GetHandlerPtr());
     m_client = std::make_shared<StubBuddiClient>();
     UrlProvider::Initialize(UrlProvider::Environment::Dev, UrlProvider::DefaultTimeout, &m_localState, m_client);
     }
 
-void UsageTrackingTests::TearDown ()
+void UsageTrackingTests::TearDown()
     {
-    UsageTracking::Uninintialize ();
+    UsageTracking::Uninintialize();
     }
 
-TEST_F (UsageTrackingTests, PostSingleUsage)
+TEST_F(UsageTrackingTests, PostSingleUsage)
     {
-    GetHandler ().ForFirstRequest ([&] (HttpRequestCR request)
+    GetHandler().ForFirstRequest([&] (HttpRequestCR request)
         {
-        auto bodyJson = request.GetRequestBody ()->AsJson ();
-        EXPECT_EQ (1, (int)bodyJson.size ());
-        EXPECT_EQ ("2321DDDD-B37B-49AC-B4A8-591C4EBC9062", bodyJson[0]["DeviceID"].asString ());
+        auto bodyJson = request.GetRequestBody()->AsJson();
+        EXPECT_EQ(1, (int) bodyJson.size());
+        EXPECT_EQ("2321DDDD-B37B-49AC-B4A8-591C4EBC9062", bodyJson[0]["DeviceID"].asString());
 
-        return StubHttpResponse (HttpStatus::OK);
+        return StubHttpResponse(HttpStatus::OK);
         });
 
-    DateTime dt (DateTime::GetCurrentTimeUtc ());
-    auto status = UsageTracking::RegisterUserUsages ("2321DDDD-B37B-49AC-B4A8-591C4EBC9062",
-           "7409333D-EF73-4083-B377-22CFDF4ED4B7", "1654", "", dt, "5.3.1.11");
+    DateTime dt(DateTime::GetCurrentTimeUtc());
+    auto status = UsageTracking::RegisterUserUsages("2321DDDD-B37B-49AC-B4A8-591C4EBC9062",
+                                                    "7409333D-EF73-4083-B377-22CFDF4ED4B7",
+                                                    "1654",
+                                                    "",
+                                                    dt,
+                                                    "5.3.1.11")->GetResult();
 
-    EXPECT_EQ (SUCCESS, status);
+    EXPECT_EQ(UsageTracking::Status::Success, status);
     }
 
 
-TEST_F (UsageTrackingTests, PostMultipleUsage)
+TEST_F(UsageTrackingTests, PostMultipleUsage)
     {
-    GetHandler ().ForFirstRequest ([&] (HttpRequestCR request)
+    GetHandler().ForFirstRequest([&] (HttpRequestCR request)
         {
-        auto bodyJson = request.GetRequestBody ()->AsJson ();
-        EXPECT_LT (1, (int)bodyJson.size ());
-        EXPECT_EQ ("2321DDDD-B37B-49AC-B4A8-591C4EBC9062", bodyJson[1]["DeviceID"].asString ());
+        auto bodyJson = request.GetRequestBody()->AsJson();
+        EXPECT_LT(1, (int) bodyJson.size());
+        EXPECT_EQ("2321DDDD-B37B-49AC-B4A8-591C4EBC9062", bodyJson[1]["DeviceID"].asString());
 
-        return StubHttpResponse (HttpStatus::OK);
+        return StubHttpResponse(HttpStatus::OK);
         });
 
-    DateTime dt (DateTime::GetCurrentTimeUtc ());
-    MobileTracking mt1 ("2321DDDD-B37B-49AC-B4A8-591C4EBC9062", "7409333D-EF73-4083-B377-22CFDF4ED4B7", "1654", "", dt, "5.3.1.11");
-    MobileTracking mt2 ("2321DDDD-B37B-49AC-B4A8-591C4EBC9062", "7409333D-EF73-4083-B377-22CFDF4ED4B7", "1654", "", dt, "5.3.1.11");
+    DateTime dt(DateTime::GetCurrentTimeUtc());
+    MobileTracking mt1("2321DDDD-B37B-49AC-B4A8-591C4EBC9062", "7409333D-EF73-4083-B377-22CFDF4ED4B7", "1654", "", dt, "5.3.1.11");
+    MobileTracking mt2("2321DDDD-B37B-49AC-B4A8-591C4EBC9062", "7409333D-EF73-4083-B377-22CFDF4ED4B7", "1654", "", dt, "5.3.1.11");
     bvector<MobileTracking> usages;
-    usages.push_back (mt1);
-    usages.push_back (mt2);
+    usages.push_back(mt1);
+    usages.push_back(mt2);
 
-    auto status = UsageTracking::RegisterUserUsages (usages);
-    EXPECT_EQ (SUCCESS, status);
+    auto status = UsageTracking::RegisterUserUsages(usages)->GetResult();
+    EXPECT_EQ(UsageTracking::Status::Success, status);
     }
 
-TEST_F (UsageTrackingTests, PostEmptyUsage)
+TEST_F(UsageTrackingTests, PostEmptyUsage)
     {
     MobileTracking mt;
-    auto status = UsageTracking::RegisterUserUsages (mt);
-    EXPECT_EQ (UsageTracking::USAGE_NO_USAGES, status);
+    auto status = UsageTracking::RegisterUserUsages(mt)->GetResult();
+    EXPECT_EQ(UsageTracking::Status::NoUsages, status);
     }
