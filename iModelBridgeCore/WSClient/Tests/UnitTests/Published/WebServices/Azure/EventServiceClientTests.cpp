40557a4ddff66319b0279545b6a1c1f645429823
Jeehwan.Cho <none@none>
2016-05-25 13:06:20 -0400

    Modified EventServiceClientTests according to the recent change

--- iModelBridgeCore/WSClient/Tests/UnitTests/Published/WebServices/Azure/EventServiceClientTests.cpp
+++ iModelBridgeCore/WSClient/Tests/UnitTests/Published/WebServices/Azure/EventServiceClientTests.cpp
@@ -14,9 +14,12 @@ USING_NAMESPACE_BENTLEY_DGNCLIENTFX_UTILS
 
 TEST_F(EventServiceClientTests, Test1)
     {
-    Utf8String REPO_ID = "TEST0";
+    Utf8String NAMESPACE1 = "bentleyeventservice-792202";
+    Utf8String REPO_ID = "TEST1";
     Utf8String USER_ID1 = "USER_ID1";
-    EventServiceClient* client = new EventServiceClient(REPO_ID, USER_ID1);
+    Utf8String TOKEN = "SharedAccessSignature sr=https%3A%2F%2FBentleyEventService-792202.servicebus.windows.net%2FTEST1&sig=FuJ4YftYW%2FMm6mUKtVLY3tLxzbWPHHmeK8%2F5ejfDsBA%3D&se=1464198677&skn=EventReceivePolicy";
+    EventServiceClient* client = new EventServiceClient(NAMESPACE1, REPO_ID, USER_ID1);
+    client->UpdateSASToken(TOKEN);
     printf("receiving msgs..\n");
     Utf8String msg = "";
     while (client->Receive(msg))
