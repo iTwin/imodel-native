c1d9d3954925436003ceb9b7d54558a434e97b32
Jeehwan.Cho <none@none>
2016-05-25 13:43:15 -0400

    BF: EventServiceClientTests connectionId

--- iModelBridgeCore/WSClient/Tests/UnitTests/Published/WebServices/Azure/EventServiceClientTests.cpp
+++ iModelBridgeCore/WSClient/Tests/UnitTests/Published/WebServices/Azure/EventServiceClientTests.cpp
@@ -10,13 +10,12 @@
 #include <WebServices/Azure/EventServiceClient.h>
 
 USING_NAMESPACE_BENTLEY_WEBSERVICES
-USING_NAMESPACE_BENTLEY_DGNCLIENTFX_UTILS
 
 TEST_F(EventServiceClientTests, Test1)
     {
     Utf8String NAMESPACE1 = "bentleyeventservice-792202";
     Utf8String REPO_ID = "TEST1";
-    Utf8String USER_ID1 = "USER_ID1";
+    Utf8String USER_ID1 = "CONNECTION_ID1";
     Utf8String TOKEN = "SharedAccessSignature sr=https%3A%2F%2FBentleyEventService-792202.servicebus.windows.net%2FTEST1&sig=FuJ4YftYW%2FMm6mUKtVLY3tLxzbWPHHmeK8%2F5ejfDsBA%3D&se=1464198677&skn=EventReceivePolicy";
     EventServiceClient* client = new EventServiceClient(NAMESPACE1, REPO_ID, USER_ID1);
     client->UpdateSASToken(TOKEN);
