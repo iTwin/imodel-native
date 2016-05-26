8bf97e18849518df16415284e76db6478022fa2d
Arvind.Venkateswaran <none@none>
2016-05-26 17:36:36 -0400

    Added missing files

--- iModelBridgeCore/WSClient/DgnDbServerClient/EventServiceConnection.cpp
+++ iModelBridgeCore/WSClient/DgnDbServerClient/EventServiceConnection.cpp
@@ -1,11 +1,11 @@
 /*--------------------------------------------------------------------------------------+
 |
-|     $Source: DgnDbServerClient/EventServiceInfo.cpp $
+|     $Source: DgnDbServerClient/EventServiceConnection.cpp $
 |
 |  $Copyright: (c) 2016 Bentley Systems, Incorporated. All rights reserved. $
 |
 +--------------------------------------------------------------------------------------*/
-#include <DgnDbServer/Client/EventServiceInfo.h>
+#include <DgnDbServer/Client/EventServiceConnection.h>
 #include "DgnDbServerUtils.h"
 
 USING_NAMESPACE_BENTLEY_DGNDBSERVER
@@ -13,26 +13,43 @@ USING_NAMESPACE_BENTLEY_DGNDBSERVER
 //---------------------------------------------------------------------------------------
 //@bsimethod                                   Arvind.Venkateswaran             05/2016
 //---------------------------------------------------------------------------------------
-EventServiceInfo::EventServiceInfo(Utf8String sasToken, Utf8String nameSpace)
+EventServiceConnection::EventServiceConnection(Utf8String sasToken, Utf8String nameSpace)
     {
     m_sasToken = sasToken;
     m_nameSpace = nameSpace;
+    m_connectionId = nullptr;
     }
 
 //---------------------------------------------------------------------------------------
 //@bsimethod                                   Arvind.Venkateswaran             05/2016
 //---------------------------------------------------------------------------------------
-EventServiceInfoPtr EventServiceInfo::Create(Utf8String sasToken, Utf8String nameSpace)
+EventServiceConnection::EventServiceConnection(Utf8String sasToken, Utf8String nameSpace, Utf8String connectionId) : EventServiceConnection(sasToken, nameSpace)
     {
-    return EventServiceInfoPtr(new EventServiceInfo(sasToken, nameSpace));
+    m_connectionId = connectionId;
     }
 
 //---------------------------------------------------------------------------------------
 //@bsimethod                                   Arvind.Venkateswaran             05/2016
 //---------------------------------------------------------------------------------------
-EventServiceInfoPtr EventServiceInfo::CreateDefaultInfo()
+EventServiceConnectionPtr EventServiceConnection::Create(Utf8String sasToken, Utf8String nameSpace)
     {
-    return EventServiceInfoPtr(new EventServiceInfo
+    return EventServiceConnectionPtr(new EventServiceConnection(sasToken, nameSpace));
+    }
+
+//---------------------------------------------------------------------------------------
+//@bsimethod                                   Arvind.Venkateswaran             05/2016
+//---------------------------------------------------------------------------------------
+EventServiceConnectionPtr EventServiceConnection::Create(Utf8String sasToken, Utf8String nameSpace, Utf8String connectionId)
+    {
+    return EventServiceConnectionPtr(new EventServiceConnection(sasToken, nameSpace, connectionId));
+    }
+
+//---------------------------------------------------------------------------------------
+//@bsimethod                                   Arvind.Venkateswaran             05/2016
+//---------------------------------------------------------------------------------------
+EventServiceConnectionPtr EventServiceConnection::CreateDefaultInfo()
+    {
+    return EventServiceConnectionPtr(new EventServiceConnection
                                (
                                "SharedAccessSignature sig=TOk40ce29TwpOYCFG7EWqHL5%2bmi9fIDX%2fYA0Ckv7Urs%3d&se=1463758026&skn=EventReceivePolicy&sr=https%3a%2f%2ftesthubjeehwan-ns.servicebus.windows.net%2ftest", 
                                "testhubjeehwan-ns"
@@ -42,7 +59,7 @@ EventServiceInfoPtr EventServiceInfo::CreateDefaultInfo()
 //---------------------------------------------------------------------------------------
 //@bsimethod                                   Arvind.Venkateswaran             05/2016
 //---------------------------------------------------------------------------------------
-Utf8String EventServiceInfo::GetSasToken()
+Utf8String EventServiceConnection::GetSasToken()
     {
     return m_sasToken;
     }
@@ -50,7 +67,7 @@ Utf8String EventServiceInfo::GetSasToken()
 //---------------------------------------------------------------------------------------
 //@bsimethod                                   Arvind.Venkateswaran             05/2016
 //---------------------------------------------------------------------------------------
-Utf8String EventServiceInfo::GetNamespace()
+Utf8String EventServiceConnection::GetNamespace()
     {
     return m_nameSpace;
     }
@@ -58,32 +75,7 @@ Utf8String EventServiceInfo::GetNamespace()
 //---------------------------------------------------------------------------------------
 //@bsimethod                                   Arvind.Venkateswaran             05/2016
 //---------------------------------------------------------------------------------------
-EventServiceReceive::EventServiceReceive(bool isSuccess, Utf8String receivedMsg)
+Utf8String EventServiceConnection::GetConnectionId()
     {
-    m_isSuccess = isSuccess;
-    m_receivedMsg = receivedMsg;
-    }
-
-//---------------------------------------------------------------------------------------
-//@bsimethod                                   Arvind.Venkateswaran             05/2016
-//---------------------------------------------------------------------------------------
-EventServiceReceivePtr EventServiceReceive::Create(bool isSuccess, Utf8String receivedMsg)
-    {
-    return EventServiceReceivePtr(new EventServiceReceive(isSuccess, receivedMsg));
-    }
-
-//---------------------------------------------------------------------------------------
-//@bsimethod                                   Arvind.Venkateswaran             05/2016
-//---------------------------------------------------------------------------------------
-bool EventServiceReceive::IsSuccess()
-    {
-    return m_isSuccess;
-    }
-
-//---------------------------------------------------------------------------------------
-//@bsimethod                                   Arvind.Venkateswaran             05/2016
-//---------------------------------------------------------------------------------------
-Utf8String EventServiceReceive::ReceivedMessage()
-    {
-    return m_receivedMsg;
+    return m_connectionId;
     }
---
+++ iModelBridgeCore/WSClient/PublicAPI/DgnDbServer/Client/EventServiceConnection.h
@@ -0,0 +1,37 @@
+/*--------------------------------------------------------------------------------------+
+|
+|     $Source: PublicAPI/DgnDbServer/Client/EventServiceConnection.h $
+|
+|  $Copyright: (c) 2016 Bentley Systems, Incorporated. All rights reserved. $
+|
++--------------------------------------------------------------------------------------*/
+#pragma once
+//__PUBLISH_SECTION_START__
+#include <DgnDbServer/DgnDbServerCommon.h>
+
+BEGIN_BENTLEY_DGNDBSERVER_NAMESPACE
+USING_NAMESPACE_BENTLEY_DGNPLATFORM
+
+typedef std::shared_ptr<struct EventServiceConnection> EventServiceConnectionPtr;
+
+struct EventServiceConnection
+    {
+    //__PUBLISH_SECTION_END__
+    private:
+        Utf8String m_sasToken;
+        Utf8String m_nameSpace;
+        Utf8String m_connectionId;
+
+        EventServiceConnection(Utf8String sasToken, Utf8String nameSpace);
+        EventServiceConnection(Utf8String sasToken, Utf8String nameSpace, Utf8String connectionId);
+        //__PUBLISH_SECTION_START__
+    public:
+        DGNDBSERVERCLIENT_EXPORT static EventServiceConnectionPtr Create(Utf8String sasToken, Utf8String nameSpace);
+        DGNDBSERVERCLIENT_EXPORT static EventServiceConnectionPtr Create(Utf8String sasToken, Utf8String nameSpace, Utf8String connectionId);
+        DGNDBSERVERCLIENT_EXPORT static EventServiceConnectionPtr CreateDefaultInfo();
+        DGNDBSERVERCLIENT_EXPORT Utf8String GetSasToken();
+        DGNDBSERVERCLIENT_EXPORT Utf8String GetNamespace();
+        DGNDBSERVERCLIENT_EXPORT Utf8String GetConnectionId();
+    };
+
+END_BENTLEY_DGNDBSERVER_NAMESPACE
