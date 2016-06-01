2d3791c2bcd91fc84fdbb36670dd81f8bed3e851
David.Jones <none@none>
2016-06-01 07:48:33 -0500

    Run pyCApiGen tool to generate newly refactored Api Updated integration tests to include correct files

--- iModelBridgeCore/WSClient/ConnectC/WSLocalState.h
+++ iModelBridgeCore/WSClient/ConnectC/WSLocalState.h
@@ -5,6 +5,7 @@
 |  $Copyright: (c) 2016 Bentley Systems, Incorporated. All rights reserved. $
 |
 +--------------------------------------------------------------------------------------*/
+#pragma once
 
 #include <DgnClientFx/DgnClientApp.h>
 
