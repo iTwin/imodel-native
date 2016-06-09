b189cb1112796410f2fb2bea4b3a069c963dbbc0
Caleb.Shafer <none@none>
2016-06-09 16:00:14 -0400

    Backout revision 951

--- iModelBridgeCore/WSClient/DgnDbServerClient/DgnDbBriefcase.cpp
+++ iModelBridgeCore/WSClient/DgnDbServerClient/DgnDbBriefcase.cpp
@@ -294,22 +294,22 @@ DgnDbServerBoolTaskPtr DgnDbBriefcase::IsBriefcaseUpToDate (ICancellationTokenPt
 //---------------------------------------------------------------------------------------
 //@bsimethod                                 Arvind.Venkateswaran	              06/2016
 //---------------------------------------------------------------------------------------
-DgnDbServerEventStringTaskPtr DgnDbBriefcase::GetEvent(bool longPolling, ICancellationTokenPtr cancellationToken) const
+DgnDbServerEventValueTaskPtr DgnDbBriefcase::GetEvent(bool longPolling, ICancellationTokenPtr cancellationToken) const
     {
     BeAssert(DgnDbServerHost::IsInitialized());
     if (!m_db.IsValid() || !m_db->IsDbOpen())
         {
-        return CreateCompletedAsyncTask<DgnDbServerEventStringResult>(DgnDbServerEventStringResult::Error(DgnDbServerError::Id::FileNotFound));
+        return CreateCompletedAsyncTask<DgnDbServerEventValueResult>(DgnDbServerEventValueResult::Error(DgnDbServerError::Id::FileNotFound));
         }
     if (!m_repositoryConnection)
         {
-        return CreateCompletedAsyncTask<DgnDbServerEventStringResult>(DgnDbServerEventStringResult::Error(DgnDbServerError::Id::InvalidRepositoryConnection));
+        return CreateCompletedAsyncTask<DgnDbServerEventValueResult>(DgnDbServerEventValueResult::Error(DgnDbServerError::Id::InvalidRepositoryConnection));
         }
 
     IDgnDbServerEventTaskPtr currentEventTask = m_repositoryConnection->GetEvent(longPolling, cancellationToken);
     auto result = currentEventTask->GetResult();
     if (!result.IsSuccess())
-        return CreateCompletedAsyncTask<DgnDbServerEventStringResult>(DgnDbServerEventStringResult::Error(DgnDbServerError::Id::InternalServerError));
+        return CreateCompletedAsyncTask<DgnDbServerEventValueResult>(DgnDbServerEventValueResult::Error(DgnDbServerError::Id::InternalServerError));
 
     IDgnDbServerEventPtr currentEvent = result.GetValue();
     const type_info& eventType = currentEvent->GetEventType();
@@ -320,17 +320,18 @@ DgnDbServerEventStringTaskPtr DgnDbBriefcase::GetEvent(bool longPolling, ICancel
         {
         DgnDbServerLockEvent& lockEvent = dynamic_cast<DgnDbServerLockEvent&>(*currentEvent);
         Utf8String resultString = "Lock Info-> LockId: " + lockEvent.GetLockId() + " LockType: " + lockEvent.GetLockType();
-        return CreateCompletedAsyncTask<DgnDbServerEventStringResult>(DgnDbServerEventStringResult::Success(resultString));
+        return CreateCompletedAsyncTask<DgnDbServerEventValueResult>(DgnDbServerEventValueResult::Success(resultString));
         }
     else if (returnedEventName.ContainsI(revisionEventName))
         {
+        //Todo:: Complete revision event
         DgnDbServerRevisionEvent& revisionEvent = dynamic_cast<DgnDbServerRevisionEvent&>(*currentEvent);
         Utf8String resultString = "Revision Info-> RevisionId: " + revisionEvent.GetRevisionId();
-        return CreateCompletedAsyncTask<DgnDbServerEventStringResult>(DgnDbServerEventStringResult::Success(resultString));
+        return CreateCompletedAsyncTask<DgnDbServerEventValueResult>(DgnDbServerEventValueResult::Success(resultString));
         }
     else
         {
-        return CreateCompletedAsyncTask<DgnDbServerEventStringResult>(DgnDbServerEventStringResult::Error(DgnDbServerError::Id::NoEventsFound));
+        return CreateCompletedAsyncTask<DgnDbServerEventValueResult>(DgnDbServerEventValueResult::Error(DgnDbServerError::Id::NoEventsFound));
         }
     }
 
