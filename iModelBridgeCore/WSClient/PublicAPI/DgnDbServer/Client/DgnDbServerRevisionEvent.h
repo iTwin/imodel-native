e493ad30c0594bf0696dce06881e7dade1487543
Arvind.Venkateswaran <none@none>
2016-06-15 15:46:10 -0400

    Added changes of LockEvents and RevisionEvents to IDgnDbServerEventParser

--- iModelBridgeCore/WSClient/PublicAPI/DgnDbServer/Client/DgnDbServerRevisionEvent.h
+++ iModelBridgeCore/WSClient/PublicAPI/DgnDbServer/Client/DgnDbServerRevisionEvent.h
@@ -21,10 +21,24 @@ struct DgnDbServerRevisionEvent : public IDgnDbServerEvent
         Utf8String m_revisionId;
         Utf8String m_revisionIndex;
         Utf8String m_date;
-        DgnDbServerRevisionEvent(Utf8String repoId, Utf8String userId, Utf8String revisionId, Utf8String revisionIndex, Utf8String date);
+        DgnDbServerRevisionEvent
+                                (
+                                Utf8String repoId,
+                                Utf8String userId, 
+                                Utf8String revisionId, 
+                                Utf8String revisionIndex, 
+                                Utf8String date
+                                );
 
     public:
-        DGNDBSERVERCLIENT_EXPORT static std::shared_ptr<struct DgnDbServerRevisionEvent> Create(Utf8String repoId, Utf8String userId, Utf8String revisionId, Utf8String revisionIndex, Utf8String date);
+        DGNDBSERVERCLIENT_EXPORT static std::shared_ptr<struct DgnDbServerRevisionEvent> Create
+                                                                                               (
+                                                                                               Utf8String repoId, 
+                                                                                               Utf8String userId, 
+                                                                                               Utf8String revisionId, 
+                                                                                               Utf8String revisionIndex, 
+                                                                                               Utf8String date
+                                                                                               );
         DGNDBSERVERCLIENT_EXPORT virtual Utf8String GetRepoId();
         DGNDBSERVERCLIENT_EXPORT virtual Utf8String GetUserId();
         DGNDBSERVERCLIENT_EXPORT virtual const type_info& GetEventType();
