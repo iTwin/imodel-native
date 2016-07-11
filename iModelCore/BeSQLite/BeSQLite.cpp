1fcdc4def7e269d8cdd2517dded51c44e4dbc1a5
Keith.Bentley <none@none>
2016-07-11 12:19:58 -0400

    inline methods in BeHttp. Rename HttpRequest and HttpResponse to Http::Request and Http::Response

--- iModelCore/BeSQLite/BeSQLite.cpp
+++ iModelCore/BeSQLite/BeSQLite.cpp
@@ -3082,11 +3082,13 @@ void StatementCache::AddStatement(CachedStatementPtr& newEntry, Utf8CP sql) cons
 +---------------+---------------+---------------+---------------+---------------+------*/
 DbResult StatementCache::GetPreparedStatement(CachedStatementPtr& stmt, DbFile const& dbFile, Utf8CP sqlString) const
     {
+#if defined (NO_NOT_THREAD_SAFE)
     if (!dbFile.CheckImplicitTxn())
         {
         BeAssert(false);
         return BE_SQLITE_ERROR_NoTxnActive;
         }
+#endif
 
     FindStatement(stmt, sqlString);
     if (stmt.IsValid())
