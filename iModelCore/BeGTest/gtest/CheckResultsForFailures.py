7236439c0126df116a03fd50be335f2a54a3885c
Sam.Wilson <none@none>
2016-07-12 08:43:28 -0400

    BF parsing of gtest results

--- iModelCore/BeGTest/gtest/CheckResultsForFailures.py
+++ iModelCore/BeGTest/gtest/CheckResultsForFailures.py
@@ -6,44 +6,59 @@
 #
 #--------------------------------------------------------------------------------------
 import os, sys, re
+from sets import Set
 
 #-------------------------------------------------------------------------------------------
 # bsimethod                                     Sam.Wilson      05/2016
 #-------------------------------------------------------------------------------------------
 if __name__ == '__main__':
-    failureCount = 0
-    failedtests = ''
-    comma = ''
-    lookingforsummary = True
-    # patterns to match
+    failedtests = Set()
+    foundSummary = False
+    lastTestSeen = ""
+    summarypat = re.compile (r"\[==========\].*ran", re.I)
+    runpat = re.compile (r"RUN\s*]\s*(\w+\.\w+)", re.I)
+    # Failure patterns to match
     # [  FAILED  ] RleEditorTester.MaxRleRunSetPixels    
     # [  FAILED  ] FileFormatTests/ExportTester.ToBestiTiff/21, where GetParam() = L"D:\\ATP\\Dataset\\Images_Files\\_forATPs\\Images\\jpeg\\Mosaic_Jpeg_WF\\TC06L0\\15.jpg"
     errpat = re.compile (r"FAILED\s*]\s*(\w+\.\w+|\w+/\w+\.\w+/\d+)", re.I)
-    summarypat = re.compile(r"\[==========\]\s*(\d+)\s+tests?\s+from\s+(\d+)\s+test\s+cases?\s+ran\.\s*\((\d+)\s+ms\s+total\)")
     with open(sys.argv[1], 'r') as logfile:
         for line in logfile.readlines():
 
-            if lookingforsummary:
-                if summarypat.match(line) != None:
-                    lookingforsummary = False
-                continue
+            if summarypat.search(line) != None:
+                 foundSummary = True
+            else:
+                run = runpat.search(line)
+                if run != None:
+                    lastTestSeen = run.group(1)
+                else:
+                    err = errpat.search(line)
+                    if err != None:
+                        failedtests.add(err.group(1))
 
-            err = errpat.search(line)
-            if err != None:
-                failedtests = failedtests + comma + err.group(1)
-                comma = ","
-                failureCount = failureCount + 1
+    if not foundSummary:
+        print ""
+        print "*** TEST CRASHED ***"
+        print ""
+        print lastTestSeen
+        print ""
+        print "To run just the crashing test, run the following command in your debugger:"
+        print "    " + sys.argv[2] + " --gtest_break_on_failure --gtest_filter=" + lastTestSeen
+        print ""
 
     if len(failedtests) != 0:
         print ""
         print "*** TESTS FAILED ***"
-        for t in failedtests.split(","):
+        comma = ""
+        faileTestsStr = ""
+        for t in failedtests:
             print t
+            faileTestsStr = faileTestsStr + comma + t
+            comma = ","
         print "********************"
         print ""
         print "To re-run failing tests, run the following command in your debugger:"
-        print "    " + sys.argv[2] + " --gtest_break_on_failure --gtest_filter=" + failedtests
+        print "    " + sys.argv[2] + " --gtest_break_on_failure --gtest_filter=" + faileTestsStr
         print ""
 
-    exit (failureCount)
+    exit (len(failedtests))
 
