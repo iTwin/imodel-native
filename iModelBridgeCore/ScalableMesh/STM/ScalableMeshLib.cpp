a1cec939e3c7ee2ef0a008b0fb9fd9a28da00c5f
Elenie.Godzaridis <none@none>
2016-07-15 08:23:01 -0400

    added init of basegeocoord for SDK

--- iModelBridgeCore/ScalableMesh/STM/ScalableMeshLib.cpp
+++ iModelBridgeCore/ScalableMesh/STM/ScalableMeshLib.cpp
@@ -38,6 +38,8 @@ void ScalableMeshLib::Host::Initialize()
     BeAssert (NULL == m_scalableTerrainModelAdmin);   
     m_scalableTerrainModelAdmin = &_SupplyScalableMeshAdmin();  
     InitializeProgressiveQueries();
+        BeFileName geocoordinateDataPath(L".\\GeoCoordinateData\\");
+        GeoCoordinates::BaseGCS::Initialize(geocoordinateDataPath.c_str());
     //BENTLEY_NAMESPACE_NAME::TerrainModel::Element::DTMElementHandlerManager::InitializeDgnPlatform();
     }
     
