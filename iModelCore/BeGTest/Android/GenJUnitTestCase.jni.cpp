42bf8e012664d162b373d816f51e68e69d33be27
Sam.Wilson <none@none>
2016-07-20 08:51:22 -0400

    unit tests - handle test case setup/teardown more like gtest does it. This is necessary to make test subclasses work the same on all platforms

--- iModelCore/BeGTest/Android/GenJUnitTestCase.jni.cpp
+++ iModelCore/BeGTest/Android/GenJUnitTestCase.jni.cpp
@@ -2,7 +2,7 @@
 |
 |     $Source: Android/GenJUnitTestCase.jni.cpp $
 |
-|  $Copyright: (c) 2015 Bentley Systems, Incorporated. All rights reserved. $
+|  $Copyright: (c) 2016 Bentley Systems, Incorporated. All rights reserved. $
 |
 +--------------------------------------------------------------------------------------*/
 /* 
@@ -25,36 +25,32 @@ extern "C" void begtest_uninitialize ();
 #define BE_TEST_TEARDOWN_JNI_WRAPPER(JTC)
 #undef  BE_TEST_RUNNER_JNI_WRAPPER
 #define BE_TEST_RUNNER_JNI_WRAPPER(JTC,JTN)
-
-// declare the actual test case setup and teardown functions
 #undef  BE_TEST_SETUP
-#define BE_TEST_SETUP(TC)                       extern "C" void setUpTestCase_##TC ();
 #undef  BE_TEST_TEARDOWN
-#define BE_TEST_TEARDOWN(TC)                    extern "C" void tearDownTestCase_##TC ();
+#define BE_TEST_SETUP(TC)
+#define BE_TEST_TEARDOWN(TC)
 
 // declare the actual  test runner functions
 #undef  BE_TEST_RUNNER
-#define BE_TEST_RUNNER(TC,TN)                   extern "C" int  run_TEST_##TC##_##TN ();
+#define BE_TEST_RUNNER(TC,TN)                   extern "C" int BETEST_TEST_RUNNER_FUNC(TC,TN) ();
 #include "__REPO__.list.h"
 
 // define jni functions that call test case setup and teardown functions
 #undef  BE_TEST_SETUP_JNI_WRAPPER               
 #undef  BE_TEST_SETUP
-#define BE_TEST_SETUP_JNI_WRAPPER(JTC)          extern "C" void Java_com_bentley_unittest___REPO___##JTC##_jniSetUpTestCase (JNIEnv*,jclass) {
-#define BE_TEST_SETUP(TC)                       if (begtest_initialize () == 0) setUpTestCase_##TC (); }
+#define BE_TEST_SETUP_JNI_WRAPPER(JTC)          extern "C" EXPORT_ATTRIBUTE void Java_com_bentley_unittest___REPO___##JTC##_jniSetUpTestCase (JNIEnv*,jclass) {
+#define BE_TEST_SETUP(TC)                       if (begtest_initialize () == 0) BeTest::SetUpTestCase(#TC); }
 
 #undef  BE_TEST_TEARDOWN_JNI_WRAPPER
 #undef  BE_TEST_TEARDOWN
-#define BE_TEST_TEARDOWN_JNI_WRAPPER(JTC)       extern "C" void Java_com_bentley_unittest___REPO___##JTC##_jniTearDownTestCase (JNIEnv*,jclass) {
-#define BE_TEST_TEARDOWN(TC)                    tearDownTestCase_##TC (); begtest_uninitialize (); }
+#define BE_TEST_TEARDOWN_JNI_WRAPPER(JTC)       extern "C" EXPORT_ATTRIBUTE void Java_com_bentley_unittest___REPO___##JTC##_jniTearDownTestCase (JNIEnv*,jclass) {
+#define BE_TEST_TEARDOWN(TC)                    BeTest::TearDownTestCase(#TC); begtest_uninitialize (); }
 
 // define jni functions that call the test runner functions
 #undef  BE_TEST_RUNNER_JNI_WRAPPER
 #undef  BE_TEST_RUNNER
-#define BE_TEST_RUNNER_JNI_WRAPPER(JTC,JTN)     extern "C" jint Java_com_bentley_unittest___REPO___##JTC##_jniTest##JTN (JNIEnv* jni, jobject jobj) { \
-                                                    if (begtest_initialize() != 0) return 1; \
+#define BE_TEST_RUNNER_JNI_WRAPPER(JTC,JTN)     extern "C" EXPORT_ATTRIBUTE jint Java_com_bentley_unittest___REPO___##JTC##_jniTest##JTN (JNIEnv* jni, jobject jobj) { \
                                                     ((BeTestHost&) BeTest::GetHost ()).SetJniEnvAndObject (jni, jobj); 
-#define BE_TEST_RUNNER(TC,TN)                       try { return run_TEST_##TC##_##TN(); } catch (...) {return 1;} \
-                                                    begtest_uninitialize(); \
+#define BE_TEST_RUNNER(TC,TN)                       try { return BETEST_TEST_RUNNER_FUNC(TC,TN) (); } catch (...) {return 1;}
                                                 }
 #include "__REPO__.list.h"
