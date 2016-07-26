2b830216b0e06f78e10f3ab60d4196ff9279a33a
Jeff.Marker <none@none>
2016-07-26 15:34:27 -0400

    WIP: rename winrt to uwp TFS#496511

--- iModelCore/BeGTest/uwp/VisualStudioProject/BeTestTest1/BeTestHost.cpp
+++ iModelCore/BeGTest/uwp/VisualStudioProject/BeTestTest1/BeTestHost.cpp
@@ -1,8 +1,8 @@
 /*----------------------------------------------------------------------+
 |
-|     $Source: WinRT/VisualStudioProject/BeTestTest1/BeTestHost.cpp $
+|     $Source: uwp/VisualStudioProject/BeTestTest1/BeTestHost.cpp $
 |
-|  $Copyright: (c) 2015 Bentley Systems, Incorporated. All rights reserved. $
+|  $Copyright: (c) 2016 Bentley Systems, Incorporated. All rights reserved. $
 |
 +----------------------------------------------------------------------*/
 #include "BeTestHost.h"
