447f69db977f1826d287050d3ab52c546446c0b7
Ridha.Malik <none@none>
2016-08-03 16:46:25 +0500

    Tests added for Linestyles components

--- iModelCore/DgnPlatform/Tests/DgnProject/NonPublished/DgnStyles_Test.cpp
+++ iModelCore/DgnPlatform/Tests/DgnProject/NonPublished/DgnStyles_Test.cpp
@@ -256,7 +256,7 @@ TEST_F(DgnLineStyleTest, IteratorLineStyleElement)
     }
 /*---------------------------------------------------------------------------------**//**
 * Test InsertRasterComponentAsJson
-* @bsimethod                                    Ridha.Malik                          03/16
+* @bsimethod                                    Ridha.Malik                          07/16
 +---------------+---------------+---------------+---------------+---------------+------*/
 TEST_F(DgnLineStyleTest, InsertRasterComponentAsJson)
     {
@@ -292,6 +292,7 @@ TEST_F(DgnLineStyleTest, InsertRasterComponentAsJson)
     jsonValue["x"] = image.GetWidth();
     jsonValue["y"] = image.GetHeight();
     jsonValue["flags"] = 12;
+    jsonValue["trueWidth"] = 100;
     LsComponentId componentId(LsComponentType::RasterImage, 7);
     EXPECT_TRUE(BSISUCCESS == LsComponent::AddRasterComponentAsJson(componentId, *project, jsonValue, image.GetByteStream().GetData(), image.GetByteStream().GetSize()));
     EXPECT_TRUE(DbResult::BE_SQLITE_OK == project->SaveChanges());
@@ -313,4 +314,190 @@ TEST_F(DgnLineStyleTest, InsertRasterComponentAsJson)
     ASSERT_TRUE(checkComponentype->IsOfType(LsComponentType::RasterImage));
     checkComponentype = LsCache::FindInMap(*project, newStyleId);
     ASSERT_TRUE(checkComponentype->IsOfType(LsComponentType::RasterImage));
+    LsRasterImageComponent *rasterimage = (LsRasterImageComponent *)lsComponent;
+    ASSERT_TRUE(image.GetWidth() == rasterimage->GetWidth());
+    ASSERT_TRUE(image.GetHeight() == rasterimage->GetHeight());
+    ASSERT_TRUE(12 == rasterimage->GetFlags());
+    Byte const* src1 = image.GetByteStream().GetData();
+    Byte const* dst1=rasterimage->GetImage();
+    for (uint8_t y = 0; y<height; ++y)
+    {
+        for (uint8_t x = 0; x<width; ++x)
+        {
+            ASSERT_TRUE(*src1++ == *dst1++);
+            ASSERT_TRUE(*src1++ == *dst1++);
+            ASSERT_TRUE(*src1++ == *dst1++);
+        }
+    }
+    }
+/*---------------------------------------------------------------------------------**//**
+* @bsimethod                                    Ridha.Malik                          08/16
++---------------+---------------+---------------+---------------+---------------+------*/
+TEST_F(DgnLineStyleTest, InsertLineCodeComponentAsJson)
+    {
+    //SetupSeedProject();
+    SetupWithPrePublishedFile(L"3dMetricGeneral.ibim", L"LineCodeComponentAsJson.ibim", Db::OpenMode::ReadWrite);
+    DgnDbPtr      project = m_db;
+    DgnLineStyles& styleTable = project->Styles().LineStyles();
+    LsCacheP cache = styleTable.GetLsCacheP();
+    LsCacheStyleIterator iterLineStyles = cache->begin();
+    DgnModelPtr model = project->Models().GetModel(project->Models().QueryFirstModelId());
+    static Utf8CP STYLE_NAME = "TestLineStyle";
+    DgnStyleId newStyleId;
+    LsComponentId componentId(LsComponentType::LineCode, 10);
+    ASSERT_TRUE(componentId.IsValid());
+    BeFileName outFileName= (BeFileName)cache->GetFileName();
+    Json::Value     jsonValue(Json::objectValue);
+    double phase = 0.0;
+    uint32_t options = 0;
+    uint32_t  maxIterate = 5;
+
+    // Set phase, maxIterate, and options
+
+    options |= LCOPT_ITERATION;
+
+    options |= LCOPT_SEGMENT;
+    options |= LsStrokePatternComponent::PhaseMode::PHASEMODE_Fixed;
+    phase = 10;
+  
+
+    jsonValue["phase"] = phase;
+    jsonValue["options"] = options;
+    jsonValue["maxIter"] = maxIterate;
+    Json::Value strokes(Json::arrayValue);
+    for (int i = 0; i < 4; i++)
+        {
+        Json::Value  entry(Json::objectValue);
+        entry["length"] = 10;
+        entry["orgWidth"] = 5;
+        entry["endWidth"] = 5;
+        int32_t strokeMode = 0;
+        strokeMode |= LCSTROKE_DASH;
+        strokeMode |= LCSTROKE_RAY;
+        strokeMode |= LCSTROKE_SCALE;
+        entry["strokeMode"] = strokeMode;
+        entry["widthMode"]= LCWIDTH_FULL;
+        entry["capMode"] = LCCAP_CLOSED;
+        strokes[i] = entry;
+        }
+
+    jsonValue["strokes"] = strokes;
+
+    LsComponent::AddComponentAsJsonProperty(componentId, *project, LsComponentType::LineCode, jsonValue);
+    EXPECT_TRUE(DbResult::BE_SQLITE_OK == project->SaveChanges());
+
+    EXPECT_EQ(SUCCESS, styleTable.Insert(newStyleId, STYLE_NAME, componentId, 53, 0.0)) << "Insert line style return value should be SUCCESS";
+    ASSERT_TRUE(newStyleId.IsValid());
+
+    EXPECT_TRUE(DbResult::BE_SQLITE_OK == project->SaveChanges());
+    uint32_t id = componentId.GetValue();
+    uint32_t nextid = id + 1;
+    BeSQLite::PropertySpec spec = LineStyleProperty::LineCode();
+    LsComponent::GetNextComponentNumber(id,*project, spec);
+    ASSERT_TRUE(nextid==id);
+    
+    CloseDb();
+   
+    BeSQLite::Db::OpenMode mode = BeSQLite::Db::OpenMode::ReadWrite;
+
+    OpenDb(m_db, outFileName, mode);
+   
+    cache = m_db->Styles().LineStyles().GetLsCacheP();
+    LsDefinitionP  lsDef = cache->GetLineStyleP(newStyleId);
+    ASSERT_TRUE(nullptr != lsDef);
+    LsComponentCP lsComponent = lsDef->GetComponentCP(model.get());
+    ASSERT_TRUE(nullptr != lsComponent);
+    ASSERT_TRUE(LsComponentType::LineCode == lsComponent->GetComponentType());
+
+    LsStrokePatternComponent * strokePattern = (LsStrokePatternComponent *)lsComponent;
+    ASSERT_TRUE(nullptr != strokePattern);
+    uint32_t nStrokes = (uint32_t)strokePattern->GetNumberStrokes();
+    ASSERT_TRUE(strokePattern->IsSingleSegment());
+    ASSERT_TRUE(LsStrokePatternComponent::PhaseMode::PHASEMODE_Fixed == strokePattern->GetPhaseMode());
+    ASSERT_TRUE(0 == strokePattern->GetDistancePhase());
+    ASSERT_TRUE(strokePattern->HasIterationLimit());
+    ASSERT_TRUE(5 == strokePattern->GetIterationLimit());
+ 
+    ASSERT_TRUE(LsStrokePatternComponent::PhaseMode::PHASEMODE_Fixed == strokePattern->GetPhaseMode());
+    for (uint32_t index = 0; index < nStrokes; ++index)
+    {
+        LsStroke * stroke = strokePattern->GetStrokeP(index);
+        ASSERT_TRUE(nullptr != stroke);
+        ASSERT_TRUE(10==stroke->GetLength());
+        ASSERT_TRUE(3 == stroke->GetWidthMode());
+        ASSERT_TRUE(5 == stroke->GetStartWidth());
+        ASSERT_TRUE(5 == stroke->GetEndWidth());
+        ASSERT_TRUE(stroke->IsDash());
+        ASSERT_TRUE(stroke->IsDashFirst());
+        ASSERT_TRUE(stroke->IsDashLast());
+        ASSERT_TRUE(stroke->IsRigid());
+        ASSERT_TRUE(stroke->IsStretchable());
+    }
+    }
+/*---------------------------------------------------------------------------------**//**
+* @bsimethod                                    Ridha.Malik                          08/16
++---------------+---------------+---------------+---------------+---------------+------*/
+TEST_F(DgnLineStyleTest, InsertCompoundComponentAsJson)
+    {
+    SetupSeedProject();
+    DgnDbPtr      project = m_db;
+    //Get line styles
+    DgnLineStyles& styleTable = project->Styles().LineStyles();
+    LsCacheP cache = styleTable.GetLsCacheP();
+    LsCacheStyleIterator iterLineStyles = cache->begin();
+    DgnModelPtr model = project->Models().GetModel(project->Models().QueryFirstModelId());
+    static Utf8CP STYLE_NAME = "TestLineStyle";
+    DgnStyleId newStyleId;
+    BeFileName outFileName = (BeFileName)cache->GetFileName();
+    LsComponentId componentId(LsComponentType::Compound, 4);
+    Json::Value     jsonValue(Json::objectValue);
+
+    Json::Value components(Json::arrayValue);
+
+    for (int i = 0; i < 2; ++i)
+        {
+        LsComponentId componentId(LsComponentType::Internal, i);
+        Json::Value  entry(Json::objectValue);
+        entry["offset"] = i+1;
+        entry["id"] = i;
+        uint32_t type=(uint32_t)componentId.GetType();
+        entry["type"] = type;
+        components.append(entry);
+        }
+
+    jsonValue["comps"] = components;
+
+    LsComponent::AddComponentAsJsonProperty(componentId, *project, LsComponentType::Compound, jsonValue);
+    EXPECT_TRUE(DbResult::BE_SQLITE_OK == project->SaveChanges());
+
+    EXPECT_EQ(SUCCESS, styleTable.Insert(newStyleId, STYLE_NAME, componentId, 53, 0.0)) << "Insert line style return value should be SUCCESS";
+    ASSERT_TRUE(newStyleId.IsValid());
+
+    EXPECT_TRUE(DbResult::BE_SQLITE_OK == project->SaveChanges());
+    CloseDb();
+
+    BeSQLite::Db::OpenMode mode = BeSQLite::Db::OpenMode::ReadWrite;
+
+    OpenDb(m_db, outFileName, mode);
+
+    cache = m_db->Styles().LineStyles().GetLsCacheP();
+    LsDefinitionP  lsDef = cache->GetLineStyleP(newStyleId);
+    ASSERT_TRUE(nullptr != lsDef);
+    LsComponentCP lsComponent = lsDef->GetComponentCP(model.get());
+    ASSERT_TRUE(nullptr != lsComponent);
+    ASSERT_TRUE(LsComponentType::Compound == lsComponent->GetComponentType());
+    LsCompoundComponent * compoundComponent = (LsCompoundComponent *)lsComponent;
+    ASSERT_TRUE(nullptr != compoundComponent);
+    uint32_t numComponents = (uint32_t)compoundComponent->GetNumComponents();
+    ASSERT_TRUE(2 == compoundComponent->GetNumComponents());
+    for (size_t i = 0; i < numComponents; ++i)
+        {
+        LsComponent const* child = compoundComponent->GetComponentCP(i);
+        ASSERT_TRUE(nullptr != child);
+        ASSERT_TRUE(i+1 == compoundComponent->GetOffsetToComponent(i));
+        ASSERT_TRUE(LsComponentType::Internal == child->GetComponentType());
+        LsInternalComponent * internalComponent = (LsInternalComponent*)child;
+        ASSERT_TRUE(nullptr != internalComponent);
+        ASSERT_TRUE(0 == internalComponent->GetHardwareStyle());
+        }
     }
