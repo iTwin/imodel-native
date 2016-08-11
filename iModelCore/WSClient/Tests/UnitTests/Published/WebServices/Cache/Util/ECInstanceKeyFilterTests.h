0caaff9bccc94a1d3cf69222259109e472a783f7
Benediktas.Lipnickas <none@none>
2016-08-11 13:43:27 +0300

    ECInstanceKeyFilter refactorings and tests

---
+++ iModelCore/WSClient/Tests/UnitTests/Published/WebServices/Cache/Util/ECInstanceKeyFilterTests.h
@@ -0,0 +1,15 @@
+/*--------------------------------------------------------------------------------------+
+|
+|     $Source: Tests/UnitTests/Published/WebServices/Cache/Util/ECInstanceKeyFilterTests.h $
+|
+|  $Copyright: (c) 2016 Bentley Systems, Incorporated. All rights reserved. $
+|
++--------------------------------------------------------------------------------------*/
+
+#pragma once
+
+#include "../BaseCachingDataSourceTest.h"
+
+class ECInstanceKeyFilterTests : public BaseCachingDataSourceTest
+    {
+    };
