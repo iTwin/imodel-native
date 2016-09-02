86db9f71a949befee99c599b33b1dcf51eba8430
Vytautas.Strimaitis <none@none>
2016-09-02 15:03:27 +0300

    TFS#519266:Add logging to ClientAPI

--- iModelCore/WSClient/DgnDbServerClient/DgnDbRepositoryManager.cpp
+++ iModelCore/WSClient/DgnDbServerClient/DgnDbRepositoryManager.cpp
@@ -7,6 +7,7 @@
 +--------------------------------------------------------------------------------------*/
 #include <DgnDbServer/Client/DgnDbRepositoryManager.h>
 #include "DgnDbServerUtils.h"
+#include <DgnDbServer/Client/Logging.h>
 
 USING_NAMESPACE_BENTLEY_DGNDBSERVER
 USING_NAMESPACE_BENTLEY_DGNPLATFORM
@@ -50,16 +51,27 @@ void SetCodesLocksStates (IBriefcaseManager::Response& response, IBriefcaseManag
 +---------------+---------------+---------------+---------------+---------------+------*/
 DgnDbServerStatusResult DgnDbRepositoryManager::Connect (DgnDbCR db)
     {
+    const Utf8String methodName = "DgnDbRepositoryManager::Connect";
+    DgnDbServerLogHelper::Log(SEVERITY::LOG_DEBUG, methodName, "Method called.");
+    double start = BeTimeUtilities::GetCurrentTimeAsUnixMillisDouble();
     RepositoryInfo repositoryInfo;
     auto readResult = RepositoryInfo::ReadRepositoryInfo(repositoryInfo, db);
 
     if (!readResult.IsSuccess())
+        {
+        double end = BeTimeUtilities::GetCurrentTimeAsUnixMillisDouble();
+        DgnDbServerLogHelper::Log(SEVERITY::LOG_INFO, methodName, end - start, "");
         return readResult;
+        }
 
     if (m_connection)
         {
         if (repositoryInfo == m_connection->GetRepositoryInfo())
+            {
+            double end = BeTimeUtilities::GetCurrentTimeAsUnixMillisDouble();
+            DgnDbServerLogHelper::Log(SEVERITY::LOG_INFO, methodName, end - start, "");
             return DgnDbServerStatusResult::Success();
+            }
         }
 
     auto connectionResult = DgnDbRepositoryConnection::Create(repositoryInfo, m_credentials, m_clientInfo, m_cancellationToken, m_authenticationHandler)->GetResult();
@@ -67,11 +79,14 @@ DgnDbServerStatusResult DgnDbRepositoryManager::Connect (DgnDbCR db)
         {
         DgnDbRepositoryConnectionPtr connection = connectionResult.GetValue();
         m_connection = connection;
+        double end = BeTimeUtilities::GetCurrentTimeAsUnixMillisDouble();
+        DgnDbServerLogHelper::Log(SEVERITY::LOG_INFO, methodName, end - start, "");
         return DgnDbServerStatusResult::Success();
         }
     else
         {
         m_connection = nullptr;
+        DgnDbServerLogHelper::Log(SEVERITY::LOG_ERROR, methodName, connectionResult.GetError().GetMessage().c_str());
         return DgnDbServerStatusResult::Error(connectionResult.GetError());
         }
     }
