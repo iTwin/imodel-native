846b116d5265792dfa56544572790c8e4be68dd2
Mathieu.Marchand <none@none>
2016-09-09 14:17:08 -0400

    wait for idle only when a root was created

--- Domains/RealityModeling/RasterSchema/RasterHandler.cpp
+++ Domains/RealityModeling/RasterSchema/RasterHandler.cpp
@@ -302,11 +302,13 @@ RasterModel::RasterModel(CreateParams const& params) : T_Super (params)
 //----------------------------------------------------------------------------------------
 RasterModel::~RasterModel()
     {
-    // Wait for tasks that we may have queued. 
-    //&&MM bogus in WaitForIdle it will deadlock if task queue is not empty.
-    BeFolly::IOThreadPool::GetPool().WaitForIdle();
-
-    m_root = nullptr;
+    if (m_root.IsValid())
+        {
+        // Wait for tasks that we may have queued. 
+        //&&MM bogus in WaitForIdle it will deadlock if task queue is not empty.
+        BeFolly::IOThreadPool::GetPool().WaitForIdle();
+        m_root = nullptr;
+        }
     }
 
 //----------------------------------------------------------------------------------------
