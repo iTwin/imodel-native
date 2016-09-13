90a8f31886f364e742989672021f7f8ad0991aba
Paul.Connelly <none@none>
2016-09-13 15:47:15 -0400

    BF: FacetCount no longer in Render namespace.

--- iModelCore/DgnPlatform/Tests/DgnProject/Published/ElementGeometry_Tests.cpp
+++ iModelCore/DgnPlatform/Tests/DgnProject/Published/ElementGeometry_Tests.cpp
@@ -120,7 +120,7 @@ TEST_F(GeometricPrimitiveTests, FacetCounts)
     ASSERT_TRUE(solidPrimitive.IsValid());
 
     IFacetOptionsPtr opts = CreateFacetOptions(0.01);
-    Render::FacetCounter facetCounter(*opts);
+    FacetCounter facetCounter(*opts);
     size_t facetCountApprox = facetCounter.GetFacetCount(*solidPrimitive);
 
     IPolyfaceConstructionPtr polyfaceBuilder = IPolyfaceConstruction::Create(*opts);
