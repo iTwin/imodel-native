0e7b2414da841d854d1adc6e856bdc93055f57a5
Krischan.Eberle <none@none>
2016-09-13 15:43:01 +0200

    Added ECClassId cache for fast look ups by schema and class name. TFS#558517

---
+++ iModelCore/ECDb/Tests/Performance/PerformanceSchemaManagerTests.cpp
@@ -0,0 +1,14 @@
+/*--------------------------------------------------------------------------------------+
+|
+|  $Source: Tests/Performance/PerformanceSchemaManagerTests.cpp $
+|
+|  $Copyright: (c) 2016 Bentley Systems, Incorporated. All rights reserved. $
+|
++--------------------------------------------------------------------------------------*/
+#include "PerformanceTests.h"
+
+USING_NAMESPACE_BENTLEY_EC
+
+BEGIN_ECDBUNITTESTS_NAMESPACE
+
+END_ECDBUNITTESTS_NAMESPACE
