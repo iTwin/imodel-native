8a875e526af6c19615b9d345f114c5c2956ab6b9
Jeff.Marker <none@none>
2016-09-15 11:40:14 -0400

    EF: clang/androidarm7a error

--- iModelCore/DgnPlatform/docs/samplecode/TxnManager.sample.cpp
+++ iModelCore/DgnPlatform/docs/samplecode/TxnManager.sample.cpp
@@ -58,7 +58,7 @@ void TestTxnMonitor::CheckClass(TxnManager& txnMgr, DgnClassId classOfInterest)
         {
         if (TxnTable::ChangeType::Delete == element.GetChangeType())
             {
-            DgnElementId eid = element.GetElementId();
+            /* unused - DgnElementId eid = element.GetElementId();*/
 
             // TODO: do something here
             }
@@ -81,7 +81,7 @@ void TestTxnMonitor::CheckClassOrSubClasses(TxnManager& txnMgr, DgnClassId class
         {
         if (TxnTable::ChangeType::Delete == element.GetChangeType())
             {
-            DgnElementId eid = element.GetElementId();
+            /* unused - DgnElementId eid = element.GetElementId();*/
             DgnClassId ecclsid = element.GetECClassId();
 
             ECN::ECClassCP ecclass = txnMgr.GetDgnDb().Schemas().GetECClass(ECN::ECClassId(ecclsid.GetValue()));
