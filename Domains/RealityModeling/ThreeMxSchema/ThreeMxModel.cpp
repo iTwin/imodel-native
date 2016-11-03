642799951c333916d03580ac86790fc8d87ab6a9
Ray.Bentley <none@none>
2016-11-03 16:59:29 -0400

    BF: force parameters on for reality model meshes.

--- Domains/RealityModeling/ThreeMxSchema/ThreeMxModel.cpp
+++ Domains/RealityModeling/ThreeMxSchema/ThreeMxModel.cpp
@@ -483,7 +483,7 @@ virtual TileMeshList _GenerateMeshes(DgnDbR dgnDb, TileGeometry::NormalMode norm
                     {
                     builder = found->second;
                     }
-                }
+                }                                   
 
             if (ClipPlaneContainment_StronglyInside != clipContainment)
                 {
