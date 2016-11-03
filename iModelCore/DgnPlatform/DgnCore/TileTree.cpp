7359f3ff513a9e6549dd186b84b17360caf4b211
Jeff.Marker <none@none>
2016-11-03 13:44:09 -0400

    BF: Add error checking when missing threemx data; also remove assert in that scenario. TFS#614968

--- iModelCore/DgnPlatform/DgnCore/TileTree.cpp
+++ iModelCore/DgnPlatform/DgnCore/TileTree.cpp
@@ -75,13 +75,17 @@ BentleyStatus TileData::ReadFromFile() const
     BeFile dataFile;
     if (BeFileStatus::Success != dataFile.Open(m_fileName.c_str(), BeFileAccess::Read))
         {
-        m_tile->SetNotFound();
+        if (m_tile.IsValid())
+            m_tile->SetNotFound();
+        
         return ERROR;
         }
 
     if (BeFileStatus::Success != dataFile.ReadEntireFile(m_tileBytes))
         {
-        m_tile->SetNotFound();
+        if (m_tile.IsValid())
+            m_tile->SetNotFound();
+
         return ERROR;
         }
 
@@ -91,7 +95,8 @@ BentleyStatus TileData::ReadFromFile() const
         return SUCCESS;
         }
 
-    if (SUCCESS != m_tile->_LoadTile(m_tileBytes, m_root))
+    if (m_tile.IsValid() && 
+        SUCCESS != m_tile->_LoadTile(m_tileBytes, m_root))
         {
         m_tile->SetNotFound();
         return ERROR;
