bc85b83901a643222f2ccfab4704c19948e1f0e4
Sam.Wilson <none@none>
2016-11-11 11:22:25 -0500

    wip clean up generated test wrapper code for uwp, so that you can more easily navigate, set breakpoints, etc.

---
+++ iModelCore/BeGTest/uwp/StripEmptyLines.py
@@ -0,0 +1,16 @@
+import sys,re
+
+if __name__ == '__main__':
+    fileName = sys.argv[1]
+
+    oFile = open(fileName, 'r')
+    lines = oFile.readlines()
+    oFile.close()
+
+    oFile = open(fileName, 'w')
+    for line in lines:
+        testline = line.strip()
+        testline = testline.strip('\n')
+        if 0 != len(testline):
+            oFile.write (line)
+    oFile.close()
