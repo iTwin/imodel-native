6d688572cda21544f6c7a073e33d05a59d642b80
Jeff.Marker <none@none>
2016-11-15 14:08:41 -0500

    BF: Patched how DgnDb terminates its query thread to prevent a race condition wherein the main thread would signal the query thread and wait, but then the query thread would stop and notify the main thread. The main thread would then wake up and destroy the DgnQueryQueue object, potentially before it left the CRT's notify_all call, resulting in an access violation. This was very reproducible on iOS. TFS#624037

--- iModelCore/DgnPlatform/DgnCore/QueryView.cpp
+++ iModelCore/DgnPlatform/DgnCore/QueryView.cpp
@@ -744,6 +744,7 @@ void DgnQueryQueue::Process()
             BeThreadUtilities::BeSleep(delay);
         }
 
+    BeMutexHolder holder(m_cv.GetMutex());
     m_state = State::Terminated;
     m_cv.notify_all();
     }
