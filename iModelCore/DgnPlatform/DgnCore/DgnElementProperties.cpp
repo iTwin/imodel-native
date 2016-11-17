6ca393d8f9e25199cd569dbdcf2bcb503e8ebc4e
Krischan.Eberle <none@none>
2016-11-17 10:49:48 +0100

    In case of success ECSQL adapters return BE_SQLITE_OK instead of BE_SQLITE_DONE

--- iModelCore/DgnPlatform/DgnCore/DgnElementProperties.cpp
+++ iModelCore/DgnPlatform/DgnCore/DgnElementProperties.cpp
@@ -753,7 +753,7 @@ DgnDbStatus ElementAutoHandledPropertiesECInstanceAdapter::UpdateProperties()
         return DgnDbStatus::WrongClass;
         }
 
-    if (BE_SQLITE_DONE != updater->Update(*this))
+    if (BE_SQLITE_OK != updater->Update(*this))
         return DgnDbStatus::WriteError;
 
     m_element.m_flags.m_propState = DgnElement::PropState::InBuffer;
