6f989e2cbccba564d47eca10e5d788cb7a828be9
Paul.Connelly <none@none>
2016-12-30 10:32:26 -0500

    TFS#635673 EN: Enable caching of instanced geometry - cuts tile generation time significantly. (Ex: OfficeBuilding initial view displays in less than half the time with caching enabled).

--- iModelCore/DgnPlatform/DgnCore/ElementTileTree.cpp
+++ iModelCore/DgnPlatform/DgnCore/ElementTileTree.cpp
@@ -437,7 +437,7 @@ constexpr double s_solidPrimitivePartCompareTolerance = 1.0E-5;
 
 // avoid re-facetting repeated geometry - cache and reuse
 // Improves tile generation time - but that was before we enabled concurrent parasolid facetting. Requires mutexes, additional state - may not be worth it.
-static bool s_cacheInstances = false;
+static bool s_cacheInstances = true;
 // Reuse the same Graphic as a SubGraphic for instanced geometry
 // Results in significant worse framerate due to surprisingly poor performance of qv_defineTransclip()
 static bool s_instancesAsSubgraphics = false;
