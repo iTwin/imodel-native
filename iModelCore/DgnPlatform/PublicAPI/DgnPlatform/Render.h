9359d91713ce16077a7a5f8e27d14387f9c282e2
Paul.Connelly <none@none>
2017-01-04 14:16:26 -0500

    TFS#635673 CM: Remove obsolete render tasks.

--- iModelCore/DgnPlatform/PublicAPI/DgnPlatform/Render.h
+++ iModelCore/DgnPlatform/PublicAPI/DgnPlatform/Render.h
@@ -2,7 +2,7 @@
 |
 |     $Source: PublicAPI/DgnPlatform/Render.h $
 |
-|  $Copyright: (c) 2016 Bentley Systems, Incorporated. All rights reserved. $
+|  $Copyright: (c) 2017 Bentley Systems, Incorporated. All rights reserved. $
 |
 +--------------------------------------------------------------------------------------*/
 #pragma once
@@ -153,8 +153,6 @@ struct Task : RefCounted<NonCopyableClass>
         ChangeScene,
         ChangeRenderPlan,
         ChangeDynamics,
-        ChangeDecorations,
-        DrawFrame,
         Redraw,
         DefineGeometryTexture,
         FindNearestZ,
