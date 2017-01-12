8add702ef11187c8f016176ea5002bbb9846443b
David.Fox-Rabinovitz <none@none>
2017-01-12 08:33:59 -0500

    WIP update

--- iModelCore/Units/src/Formatting/FormatUtils.cpp
+++ iModelCore/Units/src/Formatting/FormatUtils.cpp
@@ -145,11 +145,11 @@ Utf8String Utils::DecimalPrecisionName(DecimalPrecision prec)
          case FractionalPrecision::Half: return 2;
          case FractionalPrecision::Quarter: return 4;
          case FractionalPrecision::Eighth: return 8;
-         case FractionalPrecision::Sixteenth: 16;
-         case FractionalPrecision::Over_32: 32;
-         case FractionalPrecision::Over_64: 64;
-         case FractionalPrecision::Over_128: 128;
-         case FractionalPrecision::Over_256: 256;
+         case FractionalPrecision::Sixteenth: return 16;
+         case FractionalPrecision::Over_32: return 32;
+         case FractionalPrecision::Over_64: return 64;
+         case FractionalPrecision::Over_128: return 128;
+         case FractionalPrecision::Over_256: return 256;
          default:
          case FractionalPrecision::Whole: return 1;
          }
@@ -205,6 +205,7 @@ Utf8String Utils::DecimalPrecisionName(DecimalPrecision prec)
       m_integral = dval;
       m_numerator = 0;
       m_denominator = Utils::FractionalPrecisionDenominator(fprec);
+      
      }
 
 END_BENTLEY_FORMATTING_NAMESPACE
