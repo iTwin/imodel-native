699444dd338d75d1411f1a37f93862697dfc8fd3
Majd.Uddin <none@none>
2017-01-19 12:32:13 +0500

    Enabled duplicate symbols to allow BeHttp tests to run as code coverage TestAggregate.

--- iModelCore/BeHttp/Tests/UnitTests/Published/ValuePrinter.h
+++ iModelCore/BeHttp/Tests/UnitTests/Published/ValuePrinter.h
@@ -2,7 +2,7 @@
 |
 |     $Source: Tests/UnitTests/Published/ValuePrinter.h $
 |
-|  $Copyright: (c) 2016 Bentley Systems, Incorporated. All rights reserved. $
+|  $Copyright: (c) 2017 Bentley Systems, Incorporated. All rights reserved. $
 |
 +--------------------------------------------------------------------------------------*/
 #pragma once
@@ -19,6 +19,13 @@
 USING_NAMESPACE_BENTLEY_TASKS
 USING_NAMESPACE_BENTLEY_HTTP
 
+// In case of Aggregate build set Environment Variable (BUILD_AGGREGATE_TESTS) which will make (#define WSCLIENT_ENABLE_DUPLICATING_SYMBOLS) in active
+
+#ifndef BUILD_FOR_AGGREGATE_TESTS
+#define BEHTTP_ENABLE_DUPLICATING_SYMBOLS
+#endif
+
+#ifdef BEHTTP_ENABLE_DUPLICATING_SYMBOLS
 // Http
 std::ostream& operator << (std::ostream &o, CredentialsCR creds);
 std::ostream& operator << (std::ostream &o, ConnectionStatus status);
@@ -32,8 +39,9 @@ std::ostream& operator << (std::ostream &o, AsyncTask::Priority value);
 BEGIN_BENTLEY_HTTP_UNIT_TESTS_NAMESPACE
 
 
-void PrintTo (const WString& value, ::std::ostream* os);
-void PrintTo (const Utf8String& value, ::std::ostream* os);
-void PrintTo (BentleyStatus value, ::std::ostream* os);
+void PrintTo(const WString& value, ::std::ostream* os);
+void PrintTo(const Utf8String& value, ::std::ostream* os);
+void PrintTo(BentleyStatus value, ::std::ostream* os);
 
 END_BENTLEY_HTTP_UNIT_TESTS_NAMESPACE
+#endif
