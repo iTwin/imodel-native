d44a51134e91e8bacb6bc7a7caa5be5e34454ab6
Sam.Wilson <none@none>
2017-01-26 13:59:31 -0500

    WIP Moved generic dgndb file management functions out of DgnPlatformSeedManager and into DgnDbTestUtils, so that tests in other repos can use them.

--- iModelCore/DgnPlatform/Tests/DgnProject/DgnDbTestUtils/DgnDbTestUtils.cpp
+++ iModelCore/DgnPlatform/Tests/DgnProject/DgnDbTestUtils/DgnDbTestUtils.cpp
@@ -23,7 +23,7 @@ USING_NAMESPACE_BENTLEY_DPTEST
 
 #define MUST_HAVE_HOST(BAD_RETURN) if (nullptr == DgnPlatformLib::QueryHost())\
         {\
-        EXPECT_FALSE(true) << "Your TC_SETUP function must set up a host. Just put an instance of ScopedDgnHost on the stack at the top of your function.";\
+        EXPECT_FALSE(true) << "Your SetUpTestCase function must set up a host. Just put an instance of ScopedDgnHost on the stack at the top of your function.";\
         return BAD_RETURN;\
         }
 
@@ -396,4 +396,216 @@ bool DgnDbTestUtils::CodeValueExists(DgnDbR db, Utf8CP codeValue)
         }
 
     return (BE_SQLITE_ROW == statement.Step());
+    }
+
+//---------------------------------------------------------------------------------------
+// @bsimethod                                           Sam.Wilson             01/2016
+//---------------------------------------------------------------------------------------
+static BeFileName getOutputPath(WStringCR relPath)
+    {
+    BeFileName outputPathName;
+    BeTest::GetHost().GetOutputRoot(outputPathName);
+    outputPathName.AppendToPath(relPath.c_str());
+    return outputPathName;
+    }
+
+//---------------------------------------------------------------------------------------
+// @bsimethod                                           Sam.Wilson             01/2016
+//---------------------------------------------------------------------------------------
+DgnDbStatus DgnDbTestUtils::MakeSeedDbCopy(BeFileNameR actualName, WCharCP relSeedPath, WCharCP newName)
+    {
+    auto db = OpenSeedDbCopy(relSeedPath, newName);
+    if (!db.IsValid())
+        return DgnDbStatus::BadRequest;
+    auto fn = db->GetFileName();
+    actualName.SetName(fn.substr(getOutputPath(L"").length()));
+    return DgnDbStatus::Success;
+    }
+
+/*---------------------------------------------------------------------------------**//**
+* @bsimethod                                                    Sam.Wilson      06/15
++---------------+---------------+---------------+---------------+---------------+------*/
+static void setBriefcase(DgnDbPtr& db, DgnDb::OpenMode mode)
+    {
+    if (db->IsBriefcase())
+        return;
+
+    BeFileName name(db->GetFileName());
+
+    db->ChangeBriefcaseId(BeBriefcaseId(BeBriefcaseId::Standalone()));
+    db->SaveChanges();
+    db->CloseDb();
+
+    DbResult result = BE_SQLITE_OK;
+    db = DgnDb::OpenDgnDb(&result, name, DgnDb::OpenParams(mode));
+    }
+
+//---------------------------------------------------------------------------------------
+// @bsimethod                                           Sam.Wilson             01/2016
+//---------------------------------------------------------------------------------------
+BeFileNameStatus DgnDbTestUtils::CreateSubDirectory(WCharCP relPath)
+    {
+    BeFileName path = getOutputPath(relPath);
+    if (path.IsDirectory() || path.DoesPathExist())
+        {
+        EXPECT_FALSE(true) << WPrintfString(L"%ls - already exists. Use a unique name for your test group's files", path.c_str()).c_str();
+        return BeFileNameStatus::AlreadyExists;
+        }
+    return BeFileName::CreateNewDirectory(path.c_str());
+    }
+
+//---------------------------------------------------------------------------------------
+// @bsimethod                                           Sam.Wilson             01/2016
+//---------------------------------------------------------------------------------------
+void DgnDbTestUtils::EmptySubDirectory(WCharCP relPath)
+    {
+    BeFileName path = getOutputPath(relPath);
+    BeFileName::EmptyDirectory(path.c_str());
+    }
+
+//---------------------------------------------------------------------------------------
+// @bsimethod                                           Sam.Wilson             01/2016
+//---------------------------------------------------------------------------------------
+DgnDbPtr DgnDbTestUtils::CreateDgnDb(WCharCP relPath, bool isRoot, bool mustBeBriefcase)
+    {
+    MUST_HAVE_HOST(nullptr);
+
+    BeFileName fileName = getOutputPath(relPath);
+
+    if (fileName.GetExtension().empty())
+        {
+        EXPECT_FALSE(true) << WPrintfString(L"%ls - missing a file extension", relPath).c_str();
+        return nullptr;
+        }
+
+    if (fileName.DoesPathExist())
+        {
+        EXPECT_FALSE(true) << WPrintfString(L"%ls - already exists. Use a unique name for your test group's files", fileName.c_str()).c_str();
+        return nullptr;
+        }
+
+    if (!isRoot && BeFileName::GetDirectoryName(relPath).empty())
+        {
+        EXPECT_FALSE(true) << "DgnDbTestUtils::CreateDgnDb - the destination must be in a sub-directory with the same name as the test group.";
+        return nullptr;
+        }
+
+    CreateDgnDbParams createDgnDbParams("DgnDbTestUtils");
+    DbResult createStatus;
+    DgnDbPtr db = DgnDb::CreateDgnDb(&createStatus, fileName, createDgnDbParams);
+    if (!db.IsValid())
+        EXPECT_FALSE(true) << WPrintfString(L"%ls - create failed", fileName.c_str()).c_str();
+
+    if (mustBeBriefcase)
+        setBriefcase(db, DgnDb::OpenMode::ReadWrite);
+
+    return db;
+    }
+
+//---------------------------------------------------------------------------------------
+// @bsimethod                                           Sam.Wilson             01/2016
+//---------------------------------------------------------------------------------------
+DgnDbPtr DgnDbTestUtils::OpenDgnDb(WCharCP relPath, DgnDb::OpenMode mode)
+    {
+    BeFileName fileName = getOutputPath(relPath);
+    DbResult openStatus;
+    DgnDb::OpenParams openParams(mode);
+    DgnDbPtr db = DgnDb::OpenDgnDb(&openStatus, fileName, openParams);
+    if (!db.IsValid())
+        EXPECT_FALSE(true) << WPrintfString(L"%ls - open failed with %x", fileName.c_str(), (int)openStatus).c_str();
+    return db;
+    }
+
+//---------------------------------------------------------------------------------------
+// @bsimethod                                           Sam.Wilson             01/2016
+//---------------------------------------------------------------------------------------
+DgnDbPtr DgnDbTestUtils::OpenSeedDb(WCharCP relSeedPath)
+    {
+    return OpenDgnDb(relSeedPath, DgnDb::OpenMode::Readonly);
+    }
+
+//---------------------------------------------------------------------------------------
+// @bsimethod                                           Sam.Wilson             01/2016
+//---------------------------------------------------------------------------------------
+static void supplyMissingDbExtension(WStringR name)
+    {
+    if (name.find(L".") == WString::npos)
+        name.append(L".bim");
+    }
+
+//---------------------------------------------------------------------------------------
+// @bsimethod                                           Sam.Wilson             01/2016
+//---------------------------------------------------------------------------------------
+DgnDbPtr DgnDbTestUtils::OpenSeedDbCopy(WCharCP relSeedPathIn, WCharCP newName)
+    {
+    WString relSeedPath(relSeedPathIn);
+    supplyMissingDbExtension(relSeedPath);
+        
+    BeFileName infileName = getOutputPath(relSeedPath.c_str());
+    if (!infileName.DoesPathExist())
+        {
+        EXPECT_FALSE(true) << WPrintfString(L"%ls - file not found", infileName.c_str()).c_str();
+        return nullptr;
+        }
+
+    //  Create a unique name for the output file, based on the input seed filename.
+
+    //  1. Establish the basename and extension
+    BeFileName ccRelPathBase;
+    if (nullptr == newName)
+        ccRelPathBase.SetName(relSeedPath);
+    else
+        {
+        ccRelPathBase.SetName(BeFileName::GetDirectoryName(relSeedPath.c_str()));
+        ccRelPathBase.AppendToPath(newName);
+        supplyMissingDbExtension(ccRelPathBase);
+        }
+        
+    //  2. Make sure that it is located in a subdirectory that is specific to the current test case. (That's how we keep test groups out of each other's way.)
+    Utf8String tcname = BeTest::GetNameOfCurrentTestCase();
+    if (!tcname.empty())
+        {
+        WString wtcname(tcname.c_str(), BentleyCharEncoding::Utf8);
+        if (!wtcname.Equals(ccRelPathBase.substr(0, tcname.size())))
+            {
+            WString tmp(ccRelPathBase);
+            ccRelPathBase.SetName(wtcname);
+            ccRelPathBase.AppendToPath(tmp.c_str());
+            }
+        }
+    else
+        {
+        // The caller is not a test. Caller must be a SetUpTestCase function. We don't know the caller's TC name. At least check that he's specifying a subdirectory.
+        if (ccRelPathBase.GetDirectoryName().empty())
+            {
+            EXPECT_FALSE(true) << "DgnDbTestUtils::OpenDgnDbCopy - the destination must be in a sub-directory.";
+            return nullptr;
+            }
+        }
+
+    //  Make sure the output subdirectory exists
+    BeFileName subDir = getOutputPath(ccRelPathBase.GetDirectoryName());
+    if (!BeFileName::IsDirectory(subDir.c_str()))
+        {
+        BeFileName::CreateNewDirectory(subDir.c_str());
+        }
+
+    //  3. Make sure it's unique
+    BeFileName ccfileName;
+    BeFileName ccRelPathUnique;
+    int ncopies = 0;
+    do  {
+        if (0 == ncopies)
+            ccRelPathUnique = ccRelPathBase;
+        else
+            ccRelPathUnique.SetName(WPrintfString(L"%ls-%d.%ls", ccRelPathBase.substr(0, ccRelPathBase.find(L".")).c_str(), ncopies, ccRelPathBase.GetExtension().c_str()));
+        ccfileName = getOutputPath(ccRelPathUnique.c_str());
+        ++ncopies;
+        } 
+    while (ccfileName.DoesPathExist());
+
+    BeFileNameStatus fileStatus = BeFileName::BeCopyFile(infileName.c_str(), ccfileName.c_str(), /*failIfFileExists*/true);
+    EXPECT_EQ(BeFileNameStatus::Success, fileStatus) << WPrintfString(L"%ls => %ls - copy failed", infileName.c_str(), ccfileName.c_str()).c_str();
+
+    return OpenDgnDb(ccRelPathUnique.c_str(), DgnDb::OpenMode::ReadWrite);
     }
