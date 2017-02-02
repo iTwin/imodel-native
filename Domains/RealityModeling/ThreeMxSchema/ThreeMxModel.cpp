8e19ddad7f1c91c9ae2ae6e474aff8241e4b65aa
Keith.Bentley <none@none>
2017-02-02 16:20:00 -0500

    rework approach to displaying infostring for hits.

--- Domains/RealityModeling/ThreeMxSchema/ThreeMxModel.cpp
+++ Domains/RealityModeling/ThreeMxSchema/ThreeMxModel.cpp
@@ -205,7 +205,6 @@ void ThreeMxModel::Load(SystemP renderSys) const
     m_scene->SetPickable(true);
     if (SUCCESS != m_scene->LoadScene())
         m_scene = nullptr;
-
     }
 
 /*---------------------------------------------------------------------------------**//**
@@ -268,13 +267,15 @@ void ThreeMxModel::_AddTerrainGraphics(TerrainContextR context) const
     }
 
 /*---------------------------------------------------------------------------------**//**
-* Called whenever the camera moves. Must be fast.
 * @bsimethod                                    Keith.Bentley                   04/16
 +---------------+---------------+---------------+---------------+---------------+------*/
 void ThreeMxModel::_PickTerrainGraphics(PickContextR context) const
     {
-    if (m_scene.IsValid())
-        m_scene->Pick(context, m_scene->GetLocation(), m_clip.get());
+    if (!m_scene.IsValid())
+        return;
+    
+    PickContext::ActiveDescription descr(context, GetName());
+    m_scene->Pick(context, m_scene->GetLocation(), m_clip.get());
     }
 
 /*---------------------------------------------------------------------------------**//**
