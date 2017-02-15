0614c2fdc1ce08e084d8ef7a72c677d5e62d6af1
Krischan.Eberle <none@none>
2017-02-15 16:49:39 +0100

    Removed unused variables/members.

--- iModelCore/ECDb/ECDb/IssueReporter.h
+++ iModelCore/ECDb/ECDb/IssueReporter.h
@@ -20,11 +20,10 @@ struct IssueReporter : NonCopyableClass
     {
 private:
     mutable BeMutex m_mutex;
-    // unused - ECDbCR m_ecdb;
-    ECDb::IIssueListener const* m_issueListener;
+    ECDb::IIssueListener const* m_issueListener = nullptr;
 
 public:
-    explicit IssueReporter(ECDbCR ecdb) : /* unused - m_ecdb(ecdb), */m_issueListener(nullptr) {}
+    IssueReporter() {}
     ~IssueReporter() {}
 
     BentleyStatus AddListener(ECDb::IIssueListener const&);
--- iModelCore/ECDb/ECDb/ViewGenerator.cpp
+++ iModelCore/ECDb/ECDb/ViewGenerator.cpp
@@ -1253,7 +1253,7 @@ bool ViewGenerator::SelectFromViewContext::IsInSelectClause(Utf8StringCR exp) co
 // @bsimethod                                                   Affan.Khan          07/16
 //---------------------------------------------------------------------------------------
 ViewGenerator::ToSqlVisitor::ToSqlVisitor(DbTable const& tableFilter, Utf8CP classIdentifier, bool usePropertyNameAsAliasForSystemPropertyMaps, bool forECClassView)
-    : IPropertyMapVisitor(), m_tableFilter(tableFilter), m_classIdentifier(classIdentifier), m_usePropertyNameAsAliasForSystemPropertyMaps(usePropertyNameAsAliasForSystemPropertyMaps)/* unused - , m_forECClassView(forECClassView)*/
+    : IPropertyMapVisitor(), m_tableFilter(tableFilter), m_classIdentifier(classIdentifier), m_usePropertyNameAsAliasForSystemPropertyMaps(usePropertyNameAsAliasForSystemPropertyMaps)
     {
     if (m_classIdentifier != nullptr && Utf8String::IsNullOrEmpty(m_classIdentifier))
         m_classIdentifier = nullptr;
