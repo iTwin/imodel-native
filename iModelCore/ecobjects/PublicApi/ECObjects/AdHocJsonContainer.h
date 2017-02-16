53ec628329da909837c2370567c4e26bbe829278
Keith.Bentley <none@none>
2017-02-16 12:27:48 -0500

    change UserProperties to JsonProperties

--- iModelCore/ecobjects/PublicApi/ECObjects/AdHocJsonContainer.h
+++ iModelCore/ecobjects/PublicApi/ECObjects/AdHocJsonContainer.h
@@ -10,274 +10,230 @@
 /*__PUBLISH_SECTION_START__*/
 #include <ECObjects/ECObjects.h>
 #include <json/value.h>
-
-#define META_DATA_KEY_PREFIX "$"
-
-#define TYPE_FIELD_NAME "Type"
-#define UNITS_FIELD_NAME "Units"
-#define CATEGORY_FIELD_NAME "Category"
-#define HIDDEN_FIELD_NAME "Hidden"
-#define READONLY_FIELD_NAME "ReadOnly"
-#define EXTENDEDTYPE_FIELD_NAME "ExtendedType"
-#define PRIORITY_FIELD_NAME "Priority"
+#include <json/reader.h>
+#include <json/writer.h>
 
 BEGIN_BENTLEY_ECOBJECT_NAMESPACE
 
-struct AdHocJsonContainer;
-
 //=======================================================================================
-//! A property value in an AdHocJsonContainer with methods to get/set values and attributes. 
+//! A Json::Value with methods to get/set values and attributes. 
 //! Attributes are typically used to store units, type and other presentation information. 
-//! @see AdHocJsonContainer
 // @bsiclass                                                 Ramanujam.Raman   02/16
 //=======================================================================================
-struct AdHocJsonPropertyValue
+struct AdHocJsonValue : Json::Value
 {
-friend struct AdHocJsonContainer;
-
-enum class GetStatus
-    {
-    Found = 0,
-    NotFound = 1
-    };
-
 private:
-    Json::Value& m_value;
-    Utf8String m_name;
-    Utf8String m_attributeKey;
-
-    AdHocJsonPropertyValue(Json::Value& value, Utf8CP name) : m_value(value), m_name(name)
-        {
-        m_attributeKey = GetAttributeKey(name);
-        }
-
-    template<typename AttributeType>
-    void SetAttribute(Utf8CP attributeName, AttributeType const& attributeValue)
-        {
-        Json::Value& entry = m_value[m_attributeKey.c_str()];
-        if (!entry.isObject())
-            entry = Json::objectValue;
-
-        entry[attributeName] = attributeValue;
-        }
-
-    void SetType(ECN::PrimitiveType primitiveType);
-
-    Json::Value const* GetAttribute(Utf8CP attributeName) const;
-    Json::Value const* GetJsonValue() const;
-
-    static Utf8String GetAttributeKey(Utf8CP propertyName)
-        {
-        BeAssert(!Utf8String::IsNullOrEmpty(propertyName));
-        Utf8PrintfString attributeKey("%s%s", META_DATA_KEY_PREFIX, propertyName ? propertyName : "");
-        return attributeKey;
-        }
+    Utf8String Attributes(Utf8CP member) const {return Utf8String("$") + member;}
+    static Utf8CP TYPE_FIELD_NAME() {return "Type";}
+    static Utf8CP UNITS_FIELD_NAME() {return  "Units";}
+    static Utf8CP CATEGORY_FIELD_NAME() {return  "Category";}
+    static Utf8CP HIDDEN_FIELD_NAME() {return  "Hidden";}
+    static Utf8CP READONLY_FIELD_NAME() {return  "ReadOnly";}
+    static Utf8CP EXTENDEDTYPE_FIELD_NAME() {return  "ExtendedType";}
+    static Utf8CP PRIORITY_FIELD_NAME() {return  "Priority";}
 
 public:
-    /* Set values */
+    using Json::Value::Value;
+
+    AdHocJsonValue& GetMemberR(Utf8CP member) {return (AdHocJsonValue&) (*this)[member];}
+    AdHocJsonValue const& GetMember(Utf8CP member) const {return (AdHocJsonValue const&)(*this)[member];}
+    JsonValueCR GetAttributes(Utf8CP member) const {return GetMember(Attributes(member).c_str());}
+    void SetAttribute(Utf8CP member, Utf8CP attributeName, JsonValueCR value) {(*this)[Attributes(member)][attributeName] = value;}
 
     //! Set a Boolean value
+    //! @param[in] member the name of the member
     //! @param[in] value Value of the property
-    ECOBJECTS_EXPORT void SetValueBoolean(bool value);
+    void SetValueBoolean(Utf8CP member, bool value) {GetMemberR(member) = AdHocJsonValue(value); SetType(member, PRIMITIVETYPE_Boolean);}
 
     //! Set an Integer value
+    //! @param[in] member the name of the member
     //! @param[in] value Value of the property
     //! @param[in] units String representing the units of the value
-    ECOBJECTS_EXPORT void SetValueInt(int32_t value, Utf8CP units = nullptr);
+    void SetValueInt(Utf8CP member, int32_t value, Utf8CP units = nullptr) {GetMemberR(member) = value;  SetType(member, PRIMITIVETYPE_Integer); if (units) SetUnits(member, units);}
 
     //! Set an Int64 value
+    //! @param[in] member the name of the member
     //! @param[in] value Value of the property
-    ECOBJECTS_EXPORT void SetValueInt64(int64_t value);
+    void SetValueInt64(Utf8CP member, int64_t value) {GetMemberR(member) = value; SetType(member, PRIMITIVETYPE_Long);}
 
     //! Set a Double value
+    //! @param[in] member the name of the member
     //! @param[in] value Value of the property
     //! @param[in] units String representing the units of the value
-    ECOBJECTS_EXPORT void SetValueDouble(double value, Utf8CP units = nullptr);
+    void SetValueDouble(Utf8CP member, double value, Utf8CP units = nullptr) {GetMemberR(member) = value; SetType(member, PRIMITIVETYPE_Double); if (units) SetUnits(member, units);}
 
     //! Set a Text value
+    //! @param[in] member the name of the member
     //! @param[in] value Value of the property
-    ECOBJECTS_EXPORT void SetValueText(Utf8CP value);
+    void SetValueText(Utf8CP member, Utf8CP value) {GetMemberR(member) = value; SetType(member, PRIMITIVETYPE_String);}
 
     //! Set a DPoint2d value
+    //! @param[in] member the name of the member
     //! @param[in] value Value of the property
-    ECOBJECTS_EXPORT void SetValuePoint2d(DPoint2dCR value);
+    void SetValuePoint2d(Utf8CP member, DPoint2dCR value) {auto& obj=GetMemberR(member); obj["x"] = value.x; obj["y"] = value.y; SetType(member, PRIMITIVETYPE_Point2d);}
 
     //! Set an DPoint3d value
+    //! @param[in] member the name of the member
     //! @param[in] value Value of the property
-    ECOBJECTS_EXPORT void SetValuePoint3d(DPoint3dCR value);
+    void SetValuePoint3d(Utf8CP member, DPoint3dCR value) {auto& obj=GetMemberR(member); obj["x"] = value.x; obj["y"] = value.y; obj["z"] = value.z; SetType(member, PRIMITIVETYPE_Point3d);}
 
     //! Set a DateTime value
+    //! @param[in] member the name of the member
     //! @param[in] value Value of the property
-    ECOBJECTS_EXPORT void SetValueDateTime(DateTimeCR value);
+    void SetValueDateTime(Utf8CP member, DateTimeCR value) {GetMemberR(member) = value.ToString(); SetType(member, PRIMITIVETYPE_DateTime);}
 
     //! Set a value from an ECValue (invariant)
+    //! @param[in] member the name of the member
     //! @param[in] value Value of the property
     //! @return SUCCESS if the setting was successful. ERROR otherwise. 
     //! @remarks Wraps over the other primitive SetValue methods. 
-    ECOBJECTS_EXPORT BentleyStatus SetValueEC(ECN::ECValueCR value);
+    BentleyStatus SetValueEC(Utf8CP member, ECN::ECValueCR value)
+        {
+        if (!value.IsPrimitive())
+            return ERROR;
 
-    //! Set a value to null
-    ECOBJECTS_EXPORT void SetValueNull();
+        if (value.IsNull())
+            {
+            GetMemberR(member) = AdHocJsonValue();
+            return SUCCESS;
+            }
+
+        PrimitiveType primitiveType = value.GetPrimitiveType();
+        switch (primitiveType)
+            {
+            case PRIMITIVETYPE_Boolean:
+                SetValueBoolean(member, value.GetBoolean());
+                break;
+            case PRIMITIVETYPE_DateTime:
+                SetValueDateTime(member, value.GetDateTime());
+                break;
+            case PRIMITIVETYPE_Double:
+                SetValueDouble(member, value.GetDouble());
+                break;
+            case PRIMITIVETYPE_Integer:
+                SetValueInt(member, value.GetInteger());
+                break;
+            case PRIMITIVETYPE_Long:
+                SetValueInt64(member, value.GetLong());
+                break;
+            case PRIMITIVETYPE_Point2d:
+                SetValuePoint2d(member, value.GetPoint2d());
+                break;
+            case PRIMITIVETYPE_Point3d:
+                SetValuePoint3d(member, value.GetPoint3d());
+                break;
+            case PRIMITIVETYPE_String:
+                SetValueText(member, value.GetUtf8CP());
+                break;
+            default:
+                BeAssert(false && "Cannot handle type");
+                return ERROR;
+            }
+
+        return SUCCESS;
+        }
 
     //! Remove the value
-    ECOBJECTS_EXPORT void RemoveValue();
-
-    /* Get values */
-
-    //! Checks if a value by the name exists and is not null
-    //! @return true if the value exists and is not null. false otherwise.
-    bool IsValueNull() const { return !m_value.isMember(m_name.c_str()) || m_value[m_name.c_str()].isNull(); }
-
-    //! Get a Boolean value
-    ECOBJECTS_EXPORT bool GetValueBoolean() const;
-
-    //! Get a Text value
-    ECOBJECTS_EXPORT Utf8CP GetValueText() const;
-
-    //! Get a Double value
-    ECOBJECTS_EXPORT double GetValueDouble() const;
-
-    //! Get an Integer value
-    ECOBJECTS_EXPORT int GetValueInt() const;
-
-    //! Get an Int64 value
-    ECOBJECTS_EXPORT int64_t GetValueInt64() const;
+    void RemoveMember(Utf8CP member) {removeMember(member); removeMember(Attributes(member));}
 
     //! Get a DPoint2d value
-    ECOBJECTS_EXPORT DPoint2d GetValuePoint2d() const;
+    DPoint2d GetValuePoint2d(Utf8CP member) const {auto& obj = GetMember(member); return DPoint2d::From(obj["x"].asDouble(), obj["y"].asDouble());}
 
     //! Get a DPoint3d value
-    ECOBJECTS_EXPORT DPoint3d GetValuePoint3d() const;
+    DPoint3d GetValuePoint3d(Utf8CP member) const {auto& obj = GetMember(member); return DPoint3d::From(obj["x"].asDouble(), obj["y"].asDouble(), obj["z"].asDouble());}
 
     //! Get a DateTime value
-    ECOBJECTS_EXPORT DateTime GetValueDateTime() const;
+    DateTime GetValueDateTime(Utf8CP member) const {DateTime value; DateTime::FromString(value, GetMember(member).asCString()); return value;}
 
     //! Get the value as an ECValue (invariant)
-    ECOBJECTS_EXPORT ECValue GetValueEC() const;
+    ECValue GetValueEC(Utf8CP member) const 
+        {
+        auto& obj = GetMember(member);
+        if (obj.isNull())
+            return ECValue();
 
-    /* Set attributes */
+        switch (GetType(member))
+            {
+            case PRIMITIVETYPE_Boolean:
+                return ECValue(obj.asBool());
+            case PRIMITIVETYPE_DateTime:
+                return ECValue(GetValueDateTime(member));
+            case PRIMITIVETYPE_Double:
+                return ECValue(obj.asDouble());
+            case PRIMITIVETYPE_Integer:
+                return ECValue(obj.asInt());
+            case PRIMITIVETYPE_Long:
+                return ECValue(obj.asInt64());
+            case PRIMITIVETYPE_Point2d:
+                return ECValue(GetValuePoint2d(member));
+            case PRIMITIVETYPE_Point3d:
+                return ECValue(GetValuePoint3d(member));
+            case PRIMITIVETYPE_String:
+                return ECValue(obj.asCString());
+            default:
+                return ECValue();
+            }
+        }
+
+    void SetType(Utf8CP member, ECN::PrimitiveType primitiveType) {SetAttribute(member, TYPE_FIELD_NAME(), Json::Value((int) primitiveType));}
 
     //! Set the units of the property value
+    //! @param[in] member the name of the member
     //! @param[in] units String representing the units of the property value
-    void SetUnits(Utf8CP units) { SetAttribute<Utf8CP>(UNITS_FIELD_NAME, units); }
+    void SetUnits(Utf8CP member, Utf8CP units) {SetAttribute(member, UNITS_FIELD_NAME(), units);}
 
     //! Set the extended type to be used to show or edit the property value
+    //! @param[in] member the name of the member
     //! @param[in] extendedType Name of the extended type
-    void SetExtendedType(Utf8CP extendedType) { SetAttribute<Utf8CP>(EXTENDEDTYPE_FIELD_NAME, extendedType); }
+    void SetExtendedType(Utf8CP member, Utf8CP extendedType) {SetAttribute(member, EXTENDEDTYPE_FIELD_NAME(), Json::Value(extendedType));}
 
     //! Set the category for the property value (typically used for presentation)
+    //! @param[in] member the name of the member
     //! @param[in] category Name of the category
-    void SetCategory(Utf8CP category) { SetAttribute<Utf8CP>(CATEGORY_FIELD_NAME, category); }
+    void SetCategory(Utf8CP member, Utf8CP category) {SetAttribute(member, CATEGORY_FIELD_NAME(), Json::Value(category));}
 
     //! Set the property value to be hidden (or shown)
-    //! @param[in] isHidden Pass true to hide the property.
-    void SetHidden(bool isHidden) { SetAttribute<bool>(HIDDEN_FIELD_NAME, isHidden); }
+    //! @param[in] member the name of the member
+    //! @param[in] hidden Pass true to hide the property.
+    void SetHidden(Utf8CP member, bool hidden) {SetAttribute(member, HIDDEN_FIELD_NAME(), Json::Value(hidden));}
 
     //! Set the priority of the property value (typically used for presentation)
+    //! @param[in] member the name of the member
     //! @param[in] priority Pass a number to indicate the priority. @see PropertySortPriority
-    void SetPriority(int priority) { SetAttribute<int>(PRIORITY_FIELD_NAME, priority); }
+    void SetPriority(Utf8CP member, int priority) {SetAttribute(member, PRIORITY_FIELD_NAME(), Json::Value(priority));}
 
     //! Set that the property value is to be treated as read only.
+    //! @param[in] member the name of the member
     //! @param[in] isReadOnly Pass true to make the property read only.
-    void SetReadOnly(bool isReadOnly) { SetAttribute<bool>(READONLY_FIELD_NAME, isReadOnly); }
-
-    /* Get attributes */
+    void SetReadOnly(Utf8CP member, bool isReadOnly) {SetAttribute(member, READONLY_FIELD_NAME(), Json::Value(isReadOnly));}
 
     //! Get the type of the property value
-    //! @param[out] type Type of the property value
-    //! @return GetStatus::Found if the property was set. GetStatus::NotFound otherwise. 
-    ECOBJECTS_EXPORT GetStatus GetType(PrimitiveType& type) const;
+    PrimitiveType GetType(Utf8CP member) const {return (PrimitiveType) GetAttributes(member)[TYPE_FIELD_NAME()].asInt();}
 
     //! Get the extended type used to show or edit the property value 
-    //! @param[out] extendedType Name of the extended type
-    //! @return GetStatus::Found if the property was set. GetStatus::NotFound otherwise. 
-    ECOBJECTS_EXPORT GetStatus GetExtendedType(Utf8StringR extendedType) const;
+    Utf8String GetExtendedType(Utf8CP member) const {return GetAttributes(member)[EXTENDEDTYPE_FIELD_NAME()].asString();}
 
     //! Get the units of the property value
-    //! @param[out] units String representing the units of the property
-    //! @return GetStatus::Found if the property was set. GetStatus::NotFound otherwise. 
-    ECOBJECTS_EXPORT GetStatus GetUnits(Utf8StringR units) const;
+    Utf8String GetUnits(Utf8CP member) const {return GetAttributes(member)[UNITS_FIELD_NAME()].asString();}
 
     //! Get the category for the property value (typically used for presentation)
-    //! @param[out] category Name of the category
-    //! @return GetStatus::Found if the property was set. GetStatus::NotFound otherwise. 
-    ECOBJECTS_EXPORT GetStatus GetCategory(Utf8StringR category) const;
+    Utf8String GetCategory(Utf8CP member) const {return GetAttributes(member)[CATEGORY_FIELD_NAME()].asString();}
 
     //! Get the flag indicating if the property value should be hidden (or shown)
-    //! @param[out] isHidden true if the property is to be hidden.
-    //! @return GetStatus::Found if the property was set. GetStatus::NotFound otherwise. 
-    ECOBJECTS_EXPORT GetStatus GetHidden(bool& isHidden) const;
+    bool GetHidden(Utf8CP member) const {return GetAttributes(member)[HIDDEN_FIELD_NAME()].asBool();}
 
     //! Get the priority of the property value (typically used for presentation)
-    //! @param[out] priority A number that indicates the the priority. @see PropertySortPriority
-    //! @return GetStatus::Found if the property was set. GetStatus::NotFound otherwise. 
-    ECOBJECTS_EXPORT GetStatus GetPriority(int& priority) const;
+    int GetPriority(Utf8CP member) const {return GetAttributes(member)[PRIORITY_FIELD_NAME()].asInt();}
     
     //! Get the flag indicating if the property value should be treated as read only.
-    //! @param[out] isReadOnly true if the property should be read only.
-    //! @return GetStatus::Found if the property was set. GetStatus::NotFound otherwise. 
-    ECOBJECTS_EXPORT GetStatus GetReadOnly(bool& isReadOnly) const;
+    bool GetReadOnly(Utf8CP member) const {return GetAttributes(member)[READONLY_FIELD_NAME()].asBool();}
+
+    BentleyStatus FromString(Utf8CP jsonStr) {return Json::Reader::Parse(jsonStr, *this) ? SUCCESS : ERROR;}
+
+    Utf8String ToString() const {return Json::FastWriter::ToString(*this);}
 };
 
-
-//=======================================================================================
-//! AdHocJsonContainer represents a collection of property values used by the AdHocJson 
-//! ExtendedType. It wraps around a standard JSON objectValue, and includes additional, 
-//! optional attributes on the contained property values - units, type, and other 
-//! presentation information. 
-//!
-//! The AdHocJsonContainer contains a collection of name-value pairs:
-//! "name": \<value\>
-//! the optional attributes are setup in another node at the same level:
-//! "$name": \<attributes\>
-//! where the attributes is in-turn a JSON struct value, with the following format:
-//! {"type": <Int|Boolean|Text|DPoint3d|DPoint2d>, "units": "unitsString", "category": "CategoryLabel", "hidden": true/false ...} 
-//! 
-//! Example: 
-//! {
-//! "FirstName" : "John Doe",
-//! "$FirstName": {"type": "Text", "priority" : 1000, "category": "Personal Info"}
-//! "Age" : 67,
-//! "Employed" : true,
-//! "Location" : {"x": "1.1", "y": "2.2", "z" : "3.3"},
-//! "$Location" : {"type": "DPoint3d", "hidden": true},
-//! }
-//! 
-// @bsiclass                                                 Ramanujam.Raman   02/16
-//=======================================================================================
-struct AdHocJsonContainer
-{
-private:
-    Json::Value m_value;
-
-public:
-    //! Constructor
-    AdHocJsonContainer() : m_value(Json::objectValue) {}
-
-    //! Initialize from a JSON string
-    ECOBJECTS_EXPORT BentleyStatus FromString(Utf8CP jsonStr);
-
-    //! Convert to a JSON string
-    ECOBJECTS_EXPORT Utf8String ToString();
-
-    //! Get the property value by name
-    //! @param[in] name Name of the property
-    AdHocJsonPropertyValue Get(Utf8CP name) { return AdHocJsonPropertyValue(m_value, name); }
-
-    //! Check if property value is contained
-    //! @param[in] name Name of the property
-    //! @return true if the value exists. false otherwise.
-    bool Contains(Utf8CP name) const { return m_value.isMember(name); }
-
-    //! Remove a property value
-    //! @param[in] name Name of the property
-    void Remove(Utf8CP name) { AdHocJsonPropertyValue(m_value, name).RemoveValue(); }
-
-    //! Removes all the property values
-    void Clear() { m_value = Json::objectValue; }
-
-    //! Returns true if there aren't any property values and the container is empty
-    bool IsEmpty() { return m_value.empty(); }
-};
+typedef AdHocJsonValue& AdHocJsonValueR;
+typedef AdHocJsonValue const& AdHocJsonValueCR;
 
 END_BENTLEY_ECOBJECT_NAMESPACE
