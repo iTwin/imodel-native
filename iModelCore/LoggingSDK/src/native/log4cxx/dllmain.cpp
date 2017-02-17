c6f6e16a0e14781dc5b669045bcde4a114009ed6
Jeff.Marker <none@none>
2017-02-17 13:11:00 -0500

    EF: stop ignoring MSVC C4996; fix code or selectively disable instead.

--- iModelCore/LoggingSDK/src/native/log4cxx/dllmain.cpp
+++ iModelCore/LoggingSDK/src/native/log4cxx/dllmain.cpp
@@ -2,7 +2,7 @@
 |
 |     $Source: src/native/log4cxx/dllmain.cpp $
 |
-|  $Copyright: (c) 2012 Bentley Systems, Incorporated. All rights reserved. $
+|  $Copyright: (c) 2017 Bentley Systems, Incorporated. All rights reserved. $
 |
 +--------------------------------------------------------------------------------------*/
 
@@ -28,19 +28,3 @@ LPVOID /*lpReserved*/
         }
     return TRUE;
     }
-
-/*---------------------------------------------------------------------------------**//**
-* @description  This function is required to be defined with the correct 24 character
-*               password functions so that it can be loaded by applications such as
-*               PowerDraft that require protected DLM's.
-* @bsimethod                                                    VijaiKalyan     04/06
-+---------------+---------------+---------------+---------------+---------------+------*/
-extern "C" void __declspec(dllexport) dlmPassword
-(
-char*           passwordP,      /* <==> Password back                     */
-char*           /*appNameP*/        /* <==> Application name                  */
-)
-    {                  //UStation   Draft       PowerMap    PowerMapFld View        Navigator
-    strcpy (passwordP, "78TB204409CO70XRB48PD97O9BZ4P6RNZ3QU87Q9S3WYO0LR96R8T2VZN7UQB9WO51UKQBZP");
-    }
-
