feb1ccdf6f6f2feb57e8803515b01f6d45a640f3
Krischan.Eberle <none@none>
2017-02-28 19:48:34 +0100

    Log issue type desc instead of issue type int.

--- iModelCore/ECDb/ECDb/ECDbMap.cpp
+++ iModelCore/ECDb/ECDb/ECDbMap.cpp
@@ -975,8 +975,8 @@ BentleyStatus ECDbMap::LogInvalidDbMappings() const
                              ecdbFileName, schemaName, schemaAlias,
                              className, tableName, issueType, issueTypeDesc, issue);
 
-        LOG.messagev(logSeverity, "ECClass with invalid DB mapping resulting in data corruption: %s:%s - Issue type: %d - Table: %s - Corrupted mapping: %s",
-                     schemaName, className, issueType, tableName, issue);
+        LOG.messagev(logSeverity, "ECClass with invalid DB mapping resulting in data corruption: %s:%s - Issue type: %s - Table: %s - Corrupted mapping: %s",
+                     schemaName, className, issueTypeDesc, tableName, issue);
         }
 
     return SUCCESS;
