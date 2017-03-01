6cd7e32938d725ed3b776e6d96ab95026d872572
Brien.Bastings <none@none>
2017-03-01 17:25:55 -0500

    wip: Need to be able to check some locks (grid, acs context, etc.) in wip:  DgnPlatform so couldn't ask ToolAdmin. wip: StandardViewRotation tool now check acs context lock to apply wip:  rotation relative to the current acs (like in MicroStation).

--- iModelCore/DgnPlatform/DgnHandlers/DgnHandlers.cpp
+++ iModelCore/DgnPlatform/DgnHandlers/DgnHandlers.cpp
@@ -27,6 +27,7 @@ void DgnPlatformLib::Host::InitializeDgnHandlers()
     BeAssert(NULL == m_formatterAdmin);        m_formatterAdmin        = &_SupplyFormatterAdmin();
     BeAssert(NULL == m_repositoryAdmin);       m_repositoryAdmin       = &_SupplyRepositoryAdmin();
     BeAssert(NULL == m_codeAdmin);             m_codeAdmin             = &_SupplyCodeAdmin();
+    BeAssert(NULL == m_sessionSettingsAdmin);  m_sessionSettingsAdmin  = &_SupplySessionSettingsAdmin();
     }
 
 /*---------------------------------------------------------------------------------**//**
@@ -52,6 +53,7 @@ void DgnPlatformLib::Host::Terminate(bool onProgramExit)
     ON_HOST_TERMINATE(m_scriptingAdmin, onProgramExit);
     ON_HOST_TERMINATE(m_repositoryAdmin, onProgramExit);
     ON_HOST_TERMINATE(m_codeAdmin, onProgramExit);
+    ON_HOST_TERMINATE(m_sessionSettingsAdmin, onProgramExit);
 
     // UnRegister Symbol Provider for ECExpressions
     IECSymbolProvider::UnRegisterExternalSymbolPublisher();
