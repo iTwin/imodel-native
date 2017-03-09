b7ad8440a599a40d64ce31c1c41486a6662a6db6
Krischan.Eberle <none@none>
2017-03-09 10:06:30 +0100

    Preparational work for ECSqlStatement refactor for overflow tables. TFS#656524

---
+++ iModelCore/ECDb/ECDb/ECSql/ECSqlPreparedStatement.cpp
@@ -0,0 +1,12 @@
+/*--------------------------------------------------------------------------------------+
+|
+|     $Source: ECDb/ECSql/ECSqlPreparedStatement.cpp $
+|
+|  $Copyright: (c) 2017 Bentley Systems, Incorporated. All rights reserved. $
+|
++--------------------------------------------------------------------------------------*/
+#include "ECDbPch.h"
+
+BEGIN_BENTLEY_SQLITE_EC_NAMESPACE
+
+END_BENTLEY_SQLITE_EC_NAMESPACE
---
+++ iModelCore/ECDb/ECDb/ECSql/ECSqlPreparedStatement.h
@@ -0,0 +1,15 @@
+/*--------------------------------------------------------------------------------------+
+|
+|     $Source: ECDb/ECSql/ECSqlPreparedStatement.h $
+|
+|  $Copyright: (c) 2017 Bentley Systems, Incorporated. All rights reserved. $
+|
++--------------------------------------------------------------------------------------*/
+#pragma once
+//__BENTLEY_INTERNAL_ONLY__
+
+#include "ECSqlPrepareContext.h"
+
+BEGIN_BENTLEY_SQLITE_EC_NAMESPACE
+
+END_BENTLEY_SQLITE_EC_NAMESPACE
