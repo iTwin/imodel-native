1ced19eb4f2f94b326111a450b9f0e493b0df586
Diego.Diaz <none@none>
2017-03-15 13:55:32 -0400

    React to DgnPlatform API changes.

--- Domains/LinearReferencing/PublicAPI/LinearReferencing/LinearReferencingApi.h
+++ Domains/LinearReferencing/PublicAPI/LinearReferencing/LinearReferencingApi.h
@@ -62,6 +62,7 @@ END_BENTLEY_LINEARREFERENCING_NAMESPACE
 #define BLR_SCHEMA_NAME                             "LinearReferencing"
 #define BLR_SCHEMA_FILE                             L"LinearReferencing.01.00.00.ecschema.xml"
 #define BLR_SCHEMA_LOCATION                         L"ECSchemas/Domain/"
+#define BLR_SCHEMA_PATH                             BLR_SCHEMA_LOCATION BLR_SCHEMA_FILE
 #define BLR_SCHEMA(name)                            BLR_SCHEMA_NAME "." name
 #define BLR_SCHEMA_CODE(name)                       BLR_SCHEMA_NAME "_" name
 
