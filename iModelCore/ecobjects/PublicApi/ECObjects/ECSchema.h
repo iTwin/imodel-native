5db985fe85ce966ed3acfac3fff672e90a51ccf7
Carole.MacDonald <none@none>
2017-03-21 14:04:13 -0400

    TFS#673846 - Fixed logic problem with previous commit.  The rename now properly checks the width of the tree, without going up to the base of the tree.

--- iModelCore/ecobjects/PublicApi/ECObjects/ECSchema.h
+++ iModelCore/ecobjects/PublicApi/ECObjects/ECSchema.h
@@ -1281,6 +1281,7 @@ private:
     void FindUniquePropertyName(Utf8StringR newName, Utf8CP prefix, Utf8CP originalName);
     ECObjectsStatus RenameConflictProperty(ECPropertyP thisProperty, bool renameDerivedProperties);
     ECObjectsStatus RenameConflictProperty(ECPropertyP thisProperty, bool renameDerivedProperties, Utf8String newName);
+    void RenameDerivedProperties(Utf8String newName);
 
     static bool     SchemaAllowsOverridingArrays(ECSchemaCP schema);
 
--- iModelCore/ecobjects/src/ECClass.cpp
+++ iModelCore/ecobjects/src/ECClass.cpp
@@ -385,27 +385,31 @@ ECObjectsStatus ECClass::RenameConflictProperty(ECPropertyP prop, bool renameDer
         delete newProperty;
         return status;
         }
-
+    LOG.infov("Renamed conflict property %s:%s to %s\n", GetFullName(), prop->GetName().c_str(), newName.c_str());
     if (renameDerivedProperties)
         {
-        for (ECClassP derivedClass : m_derivedClasses)
-            {
-            ECPropertyP fromDerived = derivedClass->GetPropertyP(newName.c_str(), false);
-            if (nullptr != fromDerived && !fromDerived->GetName().Equals(newName))
-                derivedClass->RenameConflictProperty(fromDerived, renameDerivedProperties, newName);
-            for (ECClassP derivedClassBaseClass : derivedClass->GetBaseClasses())
-                {
-                ECPropertyP fromBaseDerived = derivedClassBaseClass->GetPropertyP(newName.c_str(), true);
-                if (nullptr == fromBaseDerived || fromBaseDerived->GetName().Equals(newName))
-                    continue;
-                derivedClassBaseClass->RenameConflictProperty(fromBaseDerived, true, newName);
-                }
-            }
+        RenameDerivedProperties(newName);
         }
     return ECObjectsStatus::Success;
     }
 
-
+void ECClass::RenameDerivedProperties(Utf8String newName)
+    {
+    for (ECClassP derivedClass : m_derivedClasses)
+        {
+        ECPropertyP fromDerived = derivedClass->GetPropertyP(newName.c_str(), false);
+        if (nullptr != fromDerived && !fromDerived->GetName().Equals(newName))
+            derivedClass->RenameConflictProperty(fromDerived, true, newName);
+        for (ECClassP derivedClassBaseClass : derivedClass->GetBaseClasses())
+            {
+            ECPropertyP fromBaseDerived = derivedClassBaseClass->GetPropertyP(newName.c_str(), true);
+            if (nullptr == fromBaseDerived || fromBaseDerived->GetName().Equals(newName))
+                continue;
+            derivedClassBaseClass->RenameConflictProperty(fromBaseDerived, true, newName);
+            }
+        derivedClass->RenameDerivedProperties(newName);
+        }
+    }
 /*---------------------------------------------------------------------------------**//**
 * @bsimethod                                                    Paul.Connelly   03/13
 +---------------+---------------+---------------+---------------+---------------+------*/
