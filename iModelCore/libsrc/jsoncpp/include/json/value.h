056c190023bdc27b925953d734a45e6b087b8208
Keith.Bentley <none@none>
2017-04-03 17:02:26 -0400

    treat json properties on models the same way we treat them on DgnElement

--- iModelCore/libsrc/jsoncpp/include/json/value.h
+++ iModelCore/libsrc/jsoncpp/include/json/value.h
@@ -69,7 +69,7 @@ namespace Json {
    private:
       Utf8CP m_str;
 
-   public:
+   public:          
       constexpr explicit StaticString(Utf8CP czstring) : m_str(czstring) {}
       operator Utf8CP() const {return m_str;}
       Utf8CP c_str() const {return m_str;}
