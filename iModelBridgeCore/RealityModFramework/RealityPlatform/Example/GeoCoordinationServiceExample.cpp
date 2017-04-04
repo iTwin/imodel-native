3f9592572e33c108a421ee6df034104a7ab0fa1b
Spencer.Mason <none@none>
2017-04-04 16:52:11 -0400

    GCS Example and minor bug fixing

--- iModelBridgeCore/RealityModFramework/RealityPlatform/Example/GeoCoordinationServiceExample.cpp
+++ iModelBridgeCore/RealityModFramework/RealityPlatform/Example/GeoCoordinationServiceExample.cpp
@@ -10,12 +10,20 @@
 
 #include <BeJsonCpp/BeJsonUtilities.h>
 #include <RealityPlatform/GeoCoordinationService.h>
+#include <RealityPlatform/SpatioTemporalSelector.h>
+#include <RealityPlatform/SpatioTemporalData.h>
 
 #include <stdio.h>
 #include <conio.h>
 #include <iostream>
 
 USING_NAMESPACE_BENTLEY_REALITYPLATFORM
+
+static bool FilterFunc(SpatialEntityPtr entity) 
+    {
+    Json::Value provider = entity->GetProvider(); 
+    return provider.isString() ? provider.asString().EqualsI("Amazon Landsat 8") : false;
+    }
 
 /*-----------------------------------------------------------------//
 * Bentley GeoCoordinationServiceExample
@@ -38,6 +46,7 @@ int main(int argc, char *argv[])
     space.push_back(GeoPoint2d::From(115.73, 50.18));
     space.push_back(GeoPoint2d::From(116.53, 50.18));
     space.push_back(GeoPoint2d::From(116.53, 49.44));
+    space.push_back(GeoPoint2d::From(115.73, 49.44));
 
     SpatialEntityWithDetailsSpatialRequest spatialReq = SpatialEntityWithDetailsSpatialRequest(space, Classification::Imagery);
     RawServerResponse spatialResponse = RawServerResponse();
@@ -51,38 +60,38 @@ int main(int argc, char *argv[])
     else
         std::cout << "error performing spatial request" << std::endl;
 
-    SpatialEntityWithDetailsByIdRequest idReq = SpatialEntityWithDetailsByIdRequest("1");
+    SpatialEntityWithDetailsByIdRequest idReq = SpatialEntityWithDetailsByIdRequest("100");
     RawServerResponse idResponse = RawServerResponse();
     SpatialEntityPtr idEntity = GeoCoordinationService::Request(idReq, idResponse);
 
     if (idResponse.status != RequestStatus::BADREQ)
         {
-        std::cout << "Entity found for id '1' with provider :" << std::endl;
+        std::cout << "Entity found for id '100' with provider :" << std::endl;
         std::cout << idEntity->GetProvider() << std::endl << std::endl;
         }
     else
         std::cout << "error SpatialEntityWithDetailsByIdRequest" << std::endl;
 
 
-    SpatialEntityByIdRequest idBasicReq = SpatialEntityByIdRequest("1");
+    SpatialEntityByIdRequest idBasicReq = SpatialEntityByIdRequest("100");
     RawServerResponse idBasicResponse = RawServerResponse();
     SpatialEntityPtr idBasicEntity = GeoCoordinationService::Request(idBasicReq, idBasicResponse);
 
     if (idBasicResponse.status != RequestStatus::BADREQ)
         {
-        std::cout << "Entity found for id '1' with provider :" << std::endl;
+        std::cout << "Entity found for id '100' with provider :" << std::endl;
         std::cout << idBasicEntity->GetProvider() << std::endl << std::endl;
         }
     else
         std::cout << "error SpatialEntityByIdRequest" << std::endl;
 
-    SpatialEntityDataSourceByIdRequest dsReq = SpatialEntityDataSourceByIdRequest("1");
+    SpatialEntityDataSourceByIdRequest dsReq = SpatialEntityDataSourceByIdRequest("100");
     RawServerResponse dsResponse = RawServerResponse();
     SpatialEntityDataSourcePtr dSource = GeoCoordinationService::Request(dsReq, dsResponse);
 
     if (dsResponse.status != RequestStatus::BADREQ)
     {
-        std::cout << "DataSource found for id '1' with Url :" << std::endl;
+        std::cout << "DataSource found for id '100' with Url :" << std::endl;
         std::cout << dSource->GetUrl() << std::endl << std::endl;
     }
     else
@@ -113,16 +122,87 @@ int main(int argc, char *argv[])
         std::cout << "error SpatialEntityMetadataByIdRequest" << std::endl;
 
 
-    /*PackagePreparationRequest prepReq = PackagePreparationRequest(space, );
+    SpatialEntityDatasetPtr dataset = SpatialEntityDataset::CreateFromJson(spatialResponse.body.c_str());
+    if (dataset.IsNull())
+        return -1;
+
+    auto imageryIt(dataset->GetImageryGroupR().begin());
+    while (imageryIt != dataset->GetImageryGroupR().end())
+        {
+        double occlusion = (*imageryIt)->GetOcclusion();
+        if ((occlusion  > 50.0) || FilterFunc(*imageryIt))
+            {
+            imageryIt = dataset->GetImageryGroupR().erase(imageryIt);
+            }
+        else
+            {
+            imageryIt++;
+            }
+        }
+
+    RealityPlatform::SpatioTemporalSelector::ResolutionMap selectedIds = SpatioTemporalSelector::GetIDsByRes(*dataset, space);
+    
+    PackagePreparationRequest prepReq = PackagePreparationRequest(space, selectedIds[RealityPlatform::ResolutionCriteria::Low]);
     RawServerResponse prepResponse = RawServerResponse();
     Utf8String prep = GeoCoordinationService::Request(prepReq, prepResponse);
 
     if (prepResponse.status != RequestStatus::BADREQ)
-    {
+        {
         std::cout << "PackageId  :" << std::endl;
-        std::cout << metaEntity->GetProvenance() << std::endl << std::endl;
-    }
+        std::cout << prep << std::endl << std::endl;
+        }
     else
-        std::cout << "error SpatialEntityMetadataByIdRequest" << std::endl;*/
+        std::cout << "error PackagePreparationRequest" << std::endl;
 
+    PreparedPackageRequest packReq = PreparedPackageRequest(prep);
+    RawServerResponse packResponse = RawServerResponse();
+
+    WChar exePath[MAX_PATH];
+    GetModuleFileNameW(NULL, exePath, MAX_PATH);
+
+    WString exeDir = exePath;
+    size_t pos = exeDir.find_last_of(L"/\\");
+    exeDir = exeDir.substr(0, pos + 1);
+
+    BeFileName baseFile = BeFileName(exeDir);
+    BeFileName packageName = baseFile;
+    packageName.AppendToPath(L"GCSExamplePackage.xrdp");
+
+    GeoCoordinationService::Request(packReq, packageName, packResponse);
+
+    if (packageName.DoesPathExist())
+        std::cout << "Package downloaded" << std::endl;
+    else
+        std::cout << "error with PreparedPackageRequest" << std::endl;
+
+    Utf8String report = "<RealityDataDownload_DownloadReport PackageId=\"0\" Date=\"2017 - 04 - 04T20:07 : 17.807Z\"><File FileName = \"LC80160332016095LGN00_B2.TIF\" url = \"https://s3-us-west-2.amazonaws.com/landsat-pds/L8/016/033/LC80160332016095LGN00/LC80160332016095LGN00_B2.TIF\" filesize = \"62748461\" timeSpent = \"7\"><DownloadAttempt attemptNo = \"1\" CURLcode = \"0\" downloadProgress = \"1\" / >< / File>< / RealityDataDownload_DownloadReport>";
+
+    packageName.BeDeleteFile();
+
+    BeFileName reportName = baseFile;
+    baseFile.AppendToPath(L"report.xml");
+
+    BeFile stream;
+    stream.Create(baseFile.c_str(), true);
+    stream.Open(baseFile.GetNameUtf8(), BeFileAccess::Write);
+    uint32_t bytesWritten;
+    uint32_t numBytes = (uint32_t)report.length();
+
+    stream.Write(&bytesWritten, report.c_str(), numBytes);
+    stream.Close();
+
+    DownloadReportUploadRequest upReq = DownloadReportUploadRequest(prep, "report.xml", baseFile);
+    RawServerResponse upResponse = RawServerResponse();
+    GeoCoordinationService::Request(upReq, upResponse);
+
+    if (upResponse.status != RequestStatus::BADREQ)
+        {
+        std::cout << "Download Report Uploaded" << std::endl;
+        }
+    else
+        std::cout << "error DownloadReportUpload" << std::endl;
+
+    baseFile.BeDeleteFile();
+
+    getch();
     }
