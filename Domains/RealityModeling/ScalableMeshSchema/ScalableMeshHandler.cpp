cd96cb33a3fdc0d5b384984e7dfc2aba48fbfd8e
Elenie.Godzaridis <none@none>
2017-04-06 09:37:06 +0200

    Add function to determine if model owns terrain subparts

--- Domains/RealityModeling/ScalableMeshSchema/ScalableMeshHandler.cpp
+++ Domains/RealityModeling/ScalableMeshSchema/ScalableMeshHandler.cpp
@@ -1940,6 +1940,14 @@ bool ScalableMeshModel::IsTerrain()
     }
 
 //----------------------------------------------------------------------------------------
+// @bsimethod                                                 Elenie.Godzaridis     4/2017
+//----------------------------------------------------------------------------------------
+bool ScalableMeshModel::HasTerrain()
+{
+    return m_terrainParts.size() > 0;
+}
+
+//----------------------------------------------------------------------------------------
 // @bsimethod                                                 Mathieu.St-Pierre    10/2016
 //----------------------------------------------------------------------------------------
 void ScalableMeshModel::SetProgressiveDisplay(bool isProgressiveDisplayOn)
