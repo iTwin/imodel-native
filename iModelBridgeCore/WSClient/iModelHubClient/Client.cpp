e9ff5d4842164c9dfa1c59895ef069d1cc87e4e6
Sam.Wilson <none@none>
2017-05-01 13:34:02 -0400

    TFS #682733 - try a different approach to schema upgrades

--- iModelBridgeCore/WSClient/iModelHubClient/Client.cpp
+++ iModelBridgeCore/WSClient/iModelHubClient/Client.cpp
@@ -715,7 +715,7 @@ StatusResult Client::DownloadBriefcase(iModelConnectionPtr connection, BeFileNam
         }
 
     BeSQLite::DbResult status;
-    Dgn::DgnDbPtr db = Dgn::DgnDb::OpenDgnDb(&status, filePath, Dgn::DgnDb::OpenParams(Dgn::DgnDb::OpenMode::ReadWrite, BeSQLite::DefaultTxn::Yes, DgnDb::OpenParams::EnableSchemaUpgrade::Yes));
+    Dgn::DgnDbPtr db = Dgn::DgnDb::OpenDgnDb(&status, filePath, Dgn::DgnDb::OpenParams(Dgn::DgnDb::OpenMode::ReadWrite));
     if (BeSQLite::DbResult::BE_SQLITE_OK != status)
         {
         StatusResult result = StatusResult::Error(Error(*db, status));
