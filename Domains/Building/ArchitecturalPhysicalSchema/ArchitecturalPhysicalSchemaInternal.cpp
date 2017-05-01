ae3ee8c87d3c395d636d6f6b2286b5dee75d5890
Vern.Francisco <none@none>
2017-05-01 13:35:42 -0400

    EH:Initial set of files

---
+++ Domains/Building/ArchitecturalPhysicalSchema/ArchitecturalPhysicalSchemaInternal.cpp
@@ -0,0 +1,8 @@
+/*--------------------------------------------------------------------------------------+
+|
+|     $Source: ArchitecturalPhysicalSchema/ArchitecturalPhysicalSchemaInternal.cpp $
+|
+|  $Copyright: (c) 2017 Bentley Systems, Incorporated. All rights reserved. $
+|
++--------------------------------------------------------------------------------------*/
+#include "DataCaptureSchemaInternal.h"
