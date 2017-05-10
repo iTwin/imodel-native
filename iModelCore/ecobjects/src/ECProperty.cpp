8184e47235cc98db29b6a286be518afffe61ab38
Carole.MacDonald <none@none>
2017-05-10 10:43:29 -0400

    Fixed problem with referencing a null object in a log message.

--- iModelCore/ecobjects/src/ECProperty.cpp
+++ iModelCore/ecobjects/src/ECProperty.cpp
@@ -1731,15 +1731,15 @@ bool NavigationECProperty::_CanOverride(ECPropertyCR baseProperty) const
     NavigationECPropertyCP baseNavProperty = baseProperty.GetAsNavigationProperty();
     if (nullptr == baseNavProperty)
         {
-        LOG.errorv("The property %s:%s cannot be overriden by a NavigationECProperty %s:%s because it is not a NavigationECProperty.",
-                   baseNavProperty->GetClass().GetFullName(), baseNavProperty->GetName().c_str(), GetClass().GetFullName(), GetName().c_str());
+        LOG.errorv("The property %s:%s cannot be overridden by a NavigationECProperty %s:%s because it is not a NavigationECProperty.",
+                   baseProperty.GetClass().GetFullName(), baseProperty.GetName().c_str(), GetClass().GetFullName(), GetName().c_str());
         return false;
         }
 
     ECRelatedInstanceDirection baseDirection = baseNavProperty->GetDirection();
     if (GetDirection() != baseDirection)
         {
-        LOG.errorv("The NavigationECProperty %s:%s cannot be overriden by %s:%s because they have different directions.",
+        LOG.errorv("The NavigationECProperty %s:%s cannot be overridden by %s:%s because they have different directions.",
                    baseNavProperty->GetClass().GetFullName(), baseNavProperty->GetName().c_str(), GetClass().GetFullName(), GetName().c_str());
         return false;
         }
@@ -1751,7 +1751,7 @@ bool NavigationECProperty::_CanOverride(ECPropertyCR baseProperty) const
     ECRelationshipClassCP baseRelClass = baseNavProperty->GetRelationshipClass();
     if (!m_relationshipClass->Is(baseRelClass))
         {
-        LOG.errorv("The NavigationECProperty %s:%s cannot be overriden by %s:%s because the relationship %s on property %s:%s is not derived from the relationship %s on property %s:%s.",
+        LOG.errorv("The NavigationECProperty %s:%s cannot be overridden by %s:%s because the relationship %s on property %s:%s is not derived from the relationship %s on property %s:%s.",
                    baseNavProperty->GetClass().GetFullName(), baseNavProperty->GetName().c_str(), GetClass().GetFullName(), GetName().c_str(),
                    m_relationshipClass->GetFullName(), GetClass().GetFullName(), GetName().c_str(),
                    baseRelClass->GetFullName(), baseNavProperty->GetClass().GetFullName(), baseNavProperty->GetName().c_str());
