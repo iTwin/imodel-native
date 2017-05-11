a1eb2da17473395be88d4073edde2ca28ceaf1ce
Maxime.Menard <none@none>
2017-05-11 11:54:58 -0400

    Removing tests for ECToSQLMap

--- iModelBridgeCore/RealityModFramework/Tests/RealityDbECPlugin/Tests/Helpers/ECToSQLMapTests.cs
+++ iModelBridgeCore/RealityModFramework/Tests/RealityDbECPlugin/Tests/Helpers/ECToSQLMapTests.cs
@@ -7,7 +7,7 @@ using Bentley.EC.Persistence.Query;
 namespace IndexECPlugin.Tests.Tests.Helpers
     {
     [TestFixture]
-    public class ECToSQLMapTests
+    class ECToSQLMapTests
         {
 
         [SetUp]
@@ -19,20 +19,20 @@ namespace IndexECPlugin.Tests.Tests.Helpers
         [Test]
         public void ECRelationalOperatorToSQLTest ()
             {
-            Assert.AreEqual("=", ECToSQLMap.ECRelationalOperatorToSQL(RelationalOperator.EQ));
-            Assert.AreEqual(">", ECToSQLMap.ECRelationalOperatorToSQL(RelationalOperator.GT));
-            Assert.AreEqual(">=", ECToSQLMap.ECRelationalOperatorToSQL(RelationalOperator.GTEQ));
-            Assert.AreEqual("IN", ECToSQLMap.ECRelationalOperatorToSQL(RelationalOperator.IN));
-            Assert.AreEqual("LIKE", ECToSQLMap.ECRelationalOperatorToSQL(RelationalOperator.LIKE));
-            Assert.AreEqual("<", ECToSQLMap.ECRelationalOperatorToSQL(RelationalOperator.LT));
-            Assert.AreEqual("<=", ECToSQLMap.ECRelationalOperatorToSQL(RelationalOperator.LTEQ));
-            Assert.AreEqual("<>", ECToSQLMap.ECRelationalOperatorToSQL(RelationalOperator.NE));
-            Assert.AreEqual("NOT IN", ECToSQLMap.ECRelationalOperatorToSQL(RelationalOperator.NOTIN));
-            Assert.AreEqual("NOT LIKE", ECToSQLMap.ECRelationalOperatorToSQL(RelationalOperator.NOTLIKE));
-            Assert.AreEqual("IS NULL", ECToSQLMap.ECRelationalOperatorToSQL(RelationalOperator.ISNULL));
-            Assert.AreEqual("IS NOT NULL", ECToSQLMap.ECRelationalOperatorToSQL(RelationalOperator.ISNOTNULL));
+            //Assert.AreEqual("=", ECToSQLMap.ECRelationalOperatorToSQL(RelationalOperator.EQ));
+            //Assert.AreEqual(">", ECToSQLMap.ECRelationalOperatorToSQL(RelationalOperator.GT));
+            //Assert.AreEqual(">=", ECToSQLMap.ECRelationalOperatorToSQL(RelationalOperator.GTEQ));
+            //Assert.AreEqual("IN", ECToSQLMap.ECRelationalOperatorToSQL(RelationalOperator.IN));
+            //Assert.AreEqual("LIKE", ECToSQLMap.ECRelationalOperatorToSQL(RelationalOperator.LIKE));
+            //Assert.AreEqual("<", ECToSQLMap.ECRelationalOperatorToSQL(RelationalOperator.LT));
+            //Assert.AreEqual("<=", ECToSQLMap.ECRelationalOperatorToSQL(RelationalOperator.LTEQ));
+            //Assert.AreEqual("<>", ECToSQLMap.ECRelationalOperatorToSQL(RelationalOperator.NE));
+            //Assert.AreEqual("NOT IN", ECToSQLMap.ECRelationalOperatorToSQL(RelationalOperator.NOTIN));
+            //Assert.AreEqual("NOT LIKE", ECToSQLMap.ECRelationalOperatorToSQL(RelationalOperator.NOTLIKE));
+            //Assert.AreEqual("IS NULL", ECToSQLMap.ECRelationalOperatorToSQL(RelationalOperator.ISNULL));
+            //Assert.AreEqual("IS NOT NULL", ECToSQLMap.ECRelationalOperatorToSQL(RelationalOperator.ISNOTNULL));
 
-            Assert.Throws<NotImplementedException>(() => ECToSQLMap.ECRelationalOperatorToSQL(RelationalOperator.X));
+            //Assert.Throws<NotImplementedException>(() => ECToSQLMap.ECRelationalOperatorToSQL(RelationalOperator.X));
             }
         }
     }
