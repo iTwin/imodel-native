5f926818c2a183dab1c088af5d82bcb130160a06
Mathieu.St-Pierre <none@none>
2017-06-02 15:15:15 -0400

    Ensure the content extent is correctly set

--- iModelBridgeCore/ScalableMesh/STM/ScalableMesh.cpp
+++ iModelBridgeCore/ScalableMesh/STM/ScalableMesh.cpp
@@ -1207,7 +1207,9 @@ template <class POINT> int ScalableMesh<POINT>::Open()
             }            
 #endif
                        
-        
+
+        m_contentExtent = ComputeTotalExtentFor(&*m_scmIndexPtr);
+        //if (m_contentExtent.isNull() || m_contentExtent.isEmpty() || m_contentExtent.DiagonalDistance() == 0) return BSIERROR;
         for (int i = 0; i < (int)DTMAnalysisType::Qty; ++i)
         {
             m_scalableMeshDTM[i] = ScalableMeshDTM::Create(this);
