f4b6cdc0d2abb9cb823353989550bfc02f539cab
Vincas.Razma <none@none>
2017-06-16 13:08:38 +0300

    Tests: failure and success tests should be seperate

--- iModelBridgeCore/WSClient/Tests/UnitTests/Published/WebServices/Cache/Util/ECDbAdapterTests.cpp
+++ iModelBridgeCore/WSClient/Tests/UnitTests/Published/WebServices/Cache/Util/ECDbAdapterTests.cpp
@@ -66,6 +66,10 @@ SeedFile ECDbAdapterTests::s_seedECDb("ecdbAdapterTest.ecdb",
 
     auto schema = ParseSchema(R"xml(
         <ECSchema schemaName="TestSchema" alias="TS" version="1.0" xmlns="http://www.bentley.com/schemas/Bentley.ECXML.3.1">
+            <ECEntityClass typeName="A" />
+            <ECEntityClass typeName="B">
+                <BaseClass>A</BaseClass>
+            </ECEntityClass>
             <ECEntityClass typeName="TestClass" />
             <ECEntityClass typeName="TestClass2" />
             <ECEntityClass typeName="TestClass3" />
@@ -2352,28 +2356,29 @@ TEST_F(ECDbAdapterTests, DeleteInstances_MultipleClassInstances_DeletesInstances
     EXPECT_EQ(0, adapter.FindInstances(ecClass3).size());
     }
 
+TEST_F(ECDbAdapterTests, ExtractECInstanceKeys_InvalidStatement_ReturnsErrorAndEmptyList)
+    {
+    auto db = GetTestDb();
+    ECDbAdapter adapter(*db);
+
+    ECSqlStatement statement;
+    ECInstanceKeyMultiMap keys;
+
+    BeTest::SetFailOnAssert(false);
+    ASSERT_EQ(ERROR, adapter.PrepareStatement(statement, "Invalid statement"));
+    BeTest::SetFailOnAssert(true);
+    ASSERT_EQ(ERROR, adapter.ExtractECInstanceKeys(statement, keys));
+    EXPECT_EQ(0, keys.size());
+    }
+
 TEST_F(ECDbAdapterTests, ExtractECInstanceKeys_StatementWithIdsPassed_ReturnsKeys)
     {
-    auto schema = ParseSchema(R"(
-        <ECSchema schemaName="TestSchema" nameSpacePrefix="TS" version="1.0" xmlns="http://www.bentley.com/schemas/Bentley.ECXML.2.0">
-            <ECClass typeName="A" />
-            <ECClass typeName="B">
-                <BaseClass>A</BaseClass>
-            </ECClass>
-        </ECSchema>)");
-
-    ObservableECDb db;
-    ASSERT_EQ(DbResult::BE_SQLITE_OK, db.CreateNewDb(":memory:"));
-
-    auto cache = ECSchemaCache::Create();
-    cache->AddSchema(*schema);
-    ASSERT_EQ(SUCCESS, db.Schemas().ImportSchemas(cache->GetSchemas()));
-
-    ECDbAdapter adapter(db);
+    auto db = GetTestDb();
+    ECDbAdapter adapter(*db);
 
     ECInstanceKey a, b;
-    ASSERT_EQ(SUCCESS, JsonInserter(db, *adapter.GetECClass("TestSchema.A"), nullptr).Insert(a, Json::Value()));
-    ASSERT_EQ(SUCCESS, JsonInserter(db, *adapter.GetECClass("TestSchema.B"), nullptr).Insert(b, Json::Value()));
+    ASSERT_EQ(SUCCESS, JsonInserter(*db, *adapter.GetECClass("TestSchema.A"), nullptr).Insert(a, Json::Value()));
+    ASSERT_EQ(SUCCESS, JsonInserter(*db, *adapter.GetECClass("TestSchema.B"), nullptr).Insert(b, Json::Value()));
 
     ECSqlStatement statement;
     ECInstanceKeyMultiMap keys;
@@ -2392,15 +2397,6 @@ TEST_F(ECDbAdapterTests, ExtractECInstanceKeys_StatementWithIdsPassed_ReturnsKey
     EXPECT_CONTAINS(keys, ECDbHelper::ToPair(a));
     EXPECT_CONTAINS(keys, ECDbHelper::ToPair(b));
     EXPECT_EQ(2, keys.size());
-
-    statement.Finalize();
-    keys.clear();
-
-    BeTest::SetFailOnAssert(false);
-    ASSERT_EQ(ERROR, adapter.PrepareStatement(statement, "Invalid statement"));
-    BeTest::SetFailOnAssert(true);
-    ASSERT_EQ(ERROR, adapter.ExtractECInstanceKeys(statement, keys));
-    EXPECT_EQ(0, keys.size());
     }
 
 TEST_F(ECDbAdapterTests, ECDb_DeleteRelationship_ShouldNotDeleteEnds1)
