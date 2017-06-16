a604c7a277ab93304e1d692983a483be249db910
Vincas.Razma <none@none>
2017-06-16 12:55:07 +0300

    EN: add ECDbAdapter::DeleteInstance() for simplification when deleting multiple is not needed

--- iModelBridgeCore/WSClient/Tests/UnitTests/Published/WebServices/Cache/Util/ECDbAdapterTests.cpp
+++ iModelBridgeCore/WSClient/Tests/UnitTests/Published/WebServices/Cache/Util/ECDbAdapterTests.cpp
@@ -1373,6 +1373,30 @@ TEST_F(ECDbAdapterTests, DeleteInstances_DeletingParentThatHoldsChild_DeletesPar
     EXPECT_EQ(0, notDeletedInstances.size());
     }
 
+TEST_F(ECDbAdapterTests, DeleteInstance_DeletingParentThatHoldsChild_DeletesParentAndChild)
+    {
+    auto db = GetTestDb();
+    ECDbAdapter adapter(*db);
+
+    auto ecClass = adapter.GetECClass("TestSchema.TestClass");
+    auto holdingRelClass = adapter.GetECRelationshipClass("TestSchema.HoldingRel");
+
+    ECInstanceKey parent, child, rel;
+    INSERT_INSTANCE(*db, ecClass, parent);
+    INSERT_INSTANCE(*db, ecClass, child);
+    INSERT_RELATIONSHIP(*db, holdingRelClass, parent, child, rel);
+
+    CREATE_MockECDbAdapterDeleteListener(listener);
+    EXPECT_CALL_OnBeforeDelete(listener, db, parent);
+    EXPECT_CALL_OnBeforeDelete(listener, db, child);
+    EXPECT_CALL_OnBeforeDelete(listener, db, rel);
+    adapter.RegisterDeleteListener(&listener);
+
+    EXPECT_EQ(SUCCESS, adapter.DeleteInstance(parent));
+    auto notDeletedInstances = adapter.FindInstances(ecClass);
+    EXPECT_EQ(0, notDeletedInstances.size());
+    }
+
 TEST_F(ECDbAdapterTests, DeleteInstnace_DeletingNotExistingHoldingRelationship_NotifiesBeforeDeleteAndAndSuccess)
     {
     auto db = GetTestDb();
