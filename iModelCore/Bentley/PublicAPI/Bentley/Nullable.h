aad3e1536963c9a50591b99df08b122526f2ee1c
Krischan.Eberle <none@none>
2017-07-06 10:11:43 +0200

    Made Nullable class final

--- iModelCore/Bentley/PublicAPI/Bentley/Nullable.h
+++ iModelCore/Bentley/PublicAPI/Bentley/Nullable.h
@@ -2,7 +2,7 @@
 |
 |     $Source: PublicAPI/Bentley/Nullable.h $
 |
-|  $Copyright: (c) 2016 Bentley Systems, Incorporated. All rights reserved. $
+|  $Copyright: (c) 2017 Bentley Systems, Incorporated. All rights reserved. $
 |
 +--------------------------------------------------------------------------------------*/
 #pragma once
@@ -16,15 +16,15 @@ BEGIN_BENTLEY_NAMESPACE
 // @bsiclass                                                         Affan.Khan   03/16
 //=======================================================================================
 template<typename T>
-struct Nullable
+struct Nullable final
     {
     private:
         T m_value;
-        bool m_isNull;
+        bool m_isNull = true;
 
     public:
-        Nullable() : m_value(T()), m_isNull(true) {}
-        Nullable(std::nullptr_t) : m_value(T()), m_isNull(true) {}
+        Nullable() : m_value(T()) {}
+        Nullable(std::nullptr_t) : m_value(T()) {}
         Nullable(T const& value) : m_value(value), m_isNull(false) {}
         Nullable(Nullable<T> const& rhs) : m_value(rhs.m_value), m_isNull(rhs.m_isNull) {}
         Nullable(Nullable<T>&& rhs) : m_value(std::move(rhs.m_value)), m_isNull(std::move(rhs.m_isNull)) {}
