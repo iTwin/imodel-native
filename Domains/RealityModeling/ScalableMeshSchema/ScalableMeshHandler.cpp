4e2224dd683d10ff419bf4e2181778f59c322a2c
Paul.Connelly <none@none>
2017-07-31 16:16:39 -0400

    WIP: TFS#723420, TFS#706009: Graphics created during tile generation are faceted to a known tolerance. For any other Graphic (e.g., decorations, dynamics, etc) we need the DgnViewport and the coordinate system (world or view) in order to determine appropriate tolerance. ViewContext::CreateGraphic() has been replaced by 2 functions requiring caller to be explicit about this: CreateWorldGraphic() and CreateViewGraphic(). GraphicBuilder::CreateParams similar requires explicit distinction, and takes an optional DgnViewport. ViewContext passes along its viewport, if it has one. NEEDSWORK: Now we need to actually take advantage of the viewport when determining the tolerance.

--- Domains/RealityModeling/ScalableMeshSchema/ScalableMeshHandler.cpp
+++ Domains/RealityModeling/ScalableMeshSchema/ScalableMeshHandler.cpp
@@ -373,7 +373,7 @@ void SMNode::_DrawGraphics(Dgn::TileTree::DrawArgsR args) const
 
     for (auto& mesh : m_meshes)
         {
-        Render::GraphicBuilderPtr graphic = args.m_context.CreateGraphic();
+        Render::GraphicBuilderPtr graphic = args.m_context.CreateWorldGraphic();
 
         GraphicBuilder::TileCorners corners;
         DPoint3d rangeCorners[8];
