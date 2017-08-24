7445c0ed8315f508d4fe006c48b1f8faea78dade
Tomas.Tamasauskas <none@none>
2017-08-24 17:45:50 +0300

    Added GetA2PUrl method to ImsClient.

--- iModelCore/WSClient/PublicAPI/WebServices/Configuration/UrlProvider.h
+++ iModelCore/WSClient/PublicAPI/WebServices/Configuration/UrlProvider.h
@@ -114,6 +114,9 @@ struct UrlProvider
             WSCLIENT_EXPORT static const UrlDescriptor UsageAndFeatureTrackingAPI;
             WSCLIENT_EXPORT static const UrlDescriptor UsageTracking;
             WSCLIENT_EXPORT static const UrlDescriptor ConnectXmpp;
+            WSCLIENT_EXPORT static const UrlDescriptor RepositoryFederationService;
+            WSCLIENT_EXPORT static const UrlDescriptor ImsActiveSTSHelperService;
+            WSCLIENT_EXPORT static const UrlDescriptor ImsPassiveAuthUrl;
             };
     };
 
