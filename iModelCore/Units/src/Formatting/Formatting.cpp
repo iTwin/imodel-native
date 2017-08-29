1d330335f2674fe6bff242b503ae774ff69f3296
David.Fox-Rabinovitz <none@none>
2017-08-29 15:32:45 -0400

    WIP: corrected trivial "not equal"

--- iModelCore/Units/src/Formatting/Formatting.cpp
+++ iModelCore/Units/src/Formatting/Formatting.cpp
@@ -1226,7 +1226,7 @@ Utf8String NumericFormatSpec::StdFormatQuantityTriad(Utf8CP stdName, QuantityTri
      if (m_decPrecision != other.m_decPrecision) return false;
      if (m_fractPrecision != other.m_fractPrecision) return false;
      if (m_barType != other.m_barType) return false;
-     if (m_decimalSeparator != m_decimalSeparator) return false;
+     if (m_decimalSeparator != other.m_decimalSeparator) return false;
      if (m_thousandsSeparator != other.m_thousandsSeparator) return false;
      if (!m_uomSeparator.Equals(other.m_uomSeparator)) return false;
      if (m_statSeparator != other.m_statSeparator) return false;
