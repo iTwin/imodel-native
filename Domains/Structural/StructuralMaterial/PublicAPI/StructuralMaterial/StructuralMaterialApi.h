cd4a3dfc79b36402cdb62b37bc16cda13824c783
Arturas.Mizaras <none@none>
2017-08-31 02:28:56 +0300

    Fixed StructuralDomain build break

--- Domains/Structural/StructuralMaterial/PublicAPI/StructuralMaterial/StructuralMaterialApi.h
+++ Domains/Structural/StructuralMaterial/PublicAPI/StructuralMaterial/StructuralMaterialApi.h
@@ -10,7 +10,6 @@
 //__PUBLISH_SECTION_START__
 
 #include "StructuralMaterialDefinitions.h"
-#include "StructuralMaterialDomain.h"
 #include "MaterialProperties.h"
 #include "SteelMaterialProperties.h"
 #include "ConcreteMaterialProperties.h"
