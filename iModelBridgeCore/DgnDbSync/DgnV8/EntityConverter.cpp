b4a414a695e25aa6899eafbb499076076f20ac16
Carole.MacDonald <none@none>
2017-09-11 10:30:01 -0400

    TFS#726579 - Fix logic of determining when to add a NavigationProperty for relationships without base classes.

--- iModelBridgeCore/DgnDbSync/DgnV8/EntityConverter.cpp
+++ iModelBridgeCore/DgnDbSync/DgnV8/EntityConverter.cpp
@@ -675,15 +675,37 @@ BentleyStatus BisClassConverter::ConvertECRelationshipClass(SchemaConversionCont
             }
         }
 
-    if (inputClass.GetBaseClasses().size() == 0 && inputClass.GetSource().GetMultiplicity().GetUpperLimit() == 1 && 0 == inputClass.GetPropertyCount(false))
+    // If there is no BisCore base class, then ECDb reports this error: It violates against the 'No additional link tables' policy which means that relationship classes with 'Link table' mapping must subclass from relationship classes defined in the ECSchema BisCore
+    // The solution is: The only way it can be converted to a Logical FK relationship is to add a navigation property to this relationship on Target side. Navigation property is prerequisite for EndTable relationship.
+    if (inputClass.GetBaseClasses().size() == 0 && 0 == inputClass.GetPropertyCount(false))
         {
-        // If there is no BisCore base class, then ECDb reports this error: It violates against the 'No additional link tables' policy which means that relationship classes with 'Link table' mapping must subclass from relationship classes defined in the ECSchema BisCore
-        // The solution is: The only way it can be converted to a Logical FK relationship is to add a navigation property to this relationship on Target side. Navigation property is prerequisite for EndTable relationship.
-        for (ECClassCP constraintClass : inputClass.GetTarget().GetConstraintClasses())
+        // 1:N->nav prop on target side
+        // N:1->nav prop on source side
+        // 1:1 and StrengthDirection == Forward->nav prop on target side
+        // 1:1 and StrengthDirection == Backward->nav prop on source side
+        if (inputClass.GetSource().GetMultiplicity().GetUpperLimit() == 1)
             {
-            ECEntityClassP target = const_cast<ECEntityClassP>(constraintClass->GetEntityClassCP());
-            NavigationECPropertyP navProp = nullptr;
-            target->CreateNavigationProperty(navProp, inputClass.GetName(), inputClass, ECRelatedInstanceDirection::Backward, ECN::PRIMITIVETYPE_Long, false);
+            if (inputClass.GetTarget().GetMultiplicity().IsUpperLimitUnbounded() || (inputClass.GetTarget().GetMultiplicity().GetUpperLimit() == 1 && inputClass.GetStrengthDirection() == ECRelatedInstanceDirection::Forward))
+                {
+                for (ECClassCP constraintClass : inputClass.GetTarget().GetConstraintClasses())
+                    {
+                    ECEntityClassP target = const_cast<ECEntityClassP>(constraintClass->GetEntityClassCP());
+                    NavigationECPropertyP navProp = nullptr;
+                    target->CreateNavigationProperty(navProp, inputClass.GetName(), inputClass, ECRelatedInstanceDirection::Backward, ECN::PRIMITIVETYPE_Long, false);
+                    }
+                }
+            }
+        else if (inputClass.GetTarget().GetMultiplicity().GetUpperLimit() == 1)
+            {
+            if (inputClass.GetSource().GetMultiplicity().IsUpperLimitUnbounded() || (inputClass.GetSource().GetMultiplicity().GetUpperLimit() == 1 && inputClass.GetStrengthDirection() == ECRelatedInstanceDirection::Backward))
+                {
+                for (ECClassCP constraintClass : inputClass.GetSource().GetConstraintClasses())
+                    {
+                    ECEntityClassP source = const_cast<ECEntityClassP>(constraintClass->GetEntityClassCP());
+                    NavigationECPropertyP navProp = nullptr;
+                    source->CreateNavigationProperty(navProp, inputClass.GetName(), inputClass, ECRelatedInstanceDirection::Forward, ECN::PRIMITIVETYPE_Long, false);
+                    }
+                }
             }
         }
 
