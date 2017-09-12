908adbb5946989ec7f0757020a509d08cb57bbf9
Krischan.Eberle <none@none>
2017-09-12 18:08:26 +0200

    Reacted to JSON macro renames in ECJsonUtilities. TFS#749292

--- iModelCore/WSClient/Cache/Persistence/Core/CacheSchema.h
+++ iModelCore/WSClient/Cache/Persistence/Core/CacheSchema.h
@@ -118,7 +118,7 @@ BEGIN_BENTLEY_WEBSERVICES_NAMESPACE
 #define ECSql_CachedFileInfoToFileInfo                              "[WSC].[" CLASS_CachedFileInfoToFileInfo "]"
 #define ECSql_ObjectInfoToCachedFileInfoOwnership                   "[WSC].[" CLASS_CachedFileInfoToFileInfoOwnership "]"
 
-#define NavPropId                                                   JSON_NAVIGATION_ID_KEY
+#define NavPropId                                                   JSON_NavigationId
 
 //! Values for property CLASS_CachedObjectInfo_PROPERTY_InstanceState
 enum class CachedInstanceState
