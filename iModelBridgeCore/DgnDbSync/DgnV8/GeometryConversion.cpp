e949ff31dce25536e575b75e06467f08e0d4d118
Brien.Bastings <none@none>
2017-09-14 15:37:43 -0400

    wip: TFS#752781 Give up on post-instancing of BReps and normal cells. It's too wip:  slow and creates too many singleton parts.

--- iModelBridgeCore/DgnDbSync/DgnV8/GeometryConversion.cpp
+++ iModelBridgeCore/DgnDbSync/DgnV8/GeometryConversion.cpp
@@ -1416,6 +1416,47 @@ static T_PartGeom* GetCache(Bentley::ElementRefP elRef, double v8SymbolScale, bo
 }; // DgnV8PartReferenceAppData
 
 /*=================================================================================**//**
+* @bsiclass                                                     Brien.Bastings  09/17
++===============+===============+===============+===============+===============+======*/
+struct PostInstancePartCacheAppData: BeSQLite::Db::AppData
+{
+protected:
+
+typedef bmap<DgnGeometryPartId, DgnGeometryPartCPtr> T_PartIdToGeom;
+T_PartIdToGeom m_map;
+
+public:
+
+static BeSQLite::Db::AppData::Key const& GetKey()
+    {
+    static BeSQLite::Db::AppData::Key s_key;
+    return s_key;
+    }
+
+static DgnGeometryPartCPtr GetPart(DgnGeometryPartId partId, DgnDbR db)
+    {
+    auto cache = (PostInstancePartCacheAppData*) db.FindAppData(GetKey());
+
+    if (nullptr == cache)
+        {
+        cache = new PostInstancePartCacheAppData();
+        db.AddAppData(PostInstancePartCacheAppData::GetKey(), cache);
+        }
+
+    T_PartIdToGeom::iterator found = cache->m_map.find(partId);
+
+    if (found == cache->m_map.end())
+        {
+        cache->m_map[partId] = db.Elements().Get<DgnGeometryPart>(partId);
+        found = cache->m_map.find(partId);
+        }
+
+    return found->second;
+    }
+
+}; // PostInstancePartCacheAppData
+
+/*=================================================================================**//**
 * @bsiclass
 +===============+===============+===============+===============+===============+======*/
 struct V8GraphicsCollector : DgnV8Api::IElementGraphicsProcessor
@@ -2238,6 +2279,7 @@ void CreatePartReferences(bvector<DgnV8PartReference>& geomParts, TransformCR ba
 +---------------+---------------+---------------+---------------+---------------+------*/
 bool IsValidForPostInstancing(GeometricPrimitiveR geometry, DgnV8Api::DisplayPath const& path)
     {
+#if defined (NOT_NOW_TOO_SLOW_TFS_752781)
     if (!m_model.Is3d())
         return false;
 
@@ -2258,12 +2300,10 @@ bool IsValidForPostInstancing(GeometricPrimitiveR geometry, DgnV8Api::DisplayPat
     if (DgnV8Api::SHARED_CELL_ELM == elRef->GetElementType() || DgnV8Api::SHAREDCELL_DEF_ELM == elRef->GetElementType())
         return false;
 
-    // Normal cell components and BReps are good candidates for post-instancing...
-    if (GeometricPrimitive::GeometryType::BRepEntity == geometry.GetGeometryType())
-        return true;
-
+    // Normal cell components are good candidates for post-instancing...
     if (DgnV8Api::CELL_HEADER_ELM == elRef->GetElementType() && !elRef->GetUnstableMSElementCP()->hdr.dhdr.props.b.h)
         return true;
+#endif
 
     return false;
     }
@@ -2278,7 +2318,7 @@ bool IsMatchingPartGeometry(DgnGeometryPartId partId, DgnDbR db, GeometricPrimit
     if (GeometricPrimitive::GeometryType::TextString == geomType)
         return false; // Don't post-instance text...
 
-    DgnGeometryPartCPtr partGeometry = db.Elements().Get<DgnGeometryPart>(partId);
+    DgnGeometryPartCPtr partGeometry = PostInstancePartCacheAppData::GetPart(partId, db);
 
     if (!partGeometry.IsValid())
         return false;
