3eb1863393f975d157480d4c8ccd1f144bf45d35
Carole.MacDonald <none@none>
2017-10-09 10:43:27 -0400

    When validating a relationship constraint, if validation fails because the base constraint is not polymorphic, and 'resolveIssues' is true, then try setting the constraint to polymorphic to see if that solves the problem.

--- iModelCore/ecobjects/src/ECClass.cpp
+++ iModelCore/ecobjects/src/ECClass.cpp
@@ -3132,7 +3132,16 @@ ECObjectsStatus ECRelationshipConstraint::ValidateAbstractConstraint(ECClassCP a
                     }
                 
                 if (!baseConstraint.SupportsClass(*abstractConstraint))
-                    valid = false;
+                    {
+                    if (resolveIssues && !baseConstraint.GetIsPolymorphic())
+                        {
+                        baseConstraint.SetIsPolymorphic(true);
+                        if (!baseConstraint.SupportsClass(*abstractConstraint))
+                            valid = false;
+                        }
+                    else
+                        valid = false;
+                    }
                 }
             else
                 {
