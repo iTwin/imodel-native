d1608ea7e27cbd878fc1e98859fca880bc8ea170
Saulius.Skliutas <none@none>
2017-10-19 11:14:08 +0300

    TFS#760830:Embedding presentation rulesets in ECDb & TFS#760831:API to embed rulesets into ECDb Reviewed by Grigas Petraitis

---
+++ iModelCore/ECPresentation/Source/RulesDriven/RulesEngine/RuleSetEmbedder.cpp
@@ -0,0 +1,30 @@
+/*--------------------------------------------------------------------------------------+
+|
+|     $Source: Source/RulesDriven/RulesEngine/RuleSetEmbedder.cpp $
+|
+|  $Copyright: (c) 2017 Bentley Systems, Incorporated. All rights reserved. $
+|
++--------------------------------------------------------------------------------------*/
+#include <ECPresentationPch.h>
+#include <ECPresentation/RulesDriven/RuleSetEmbedder.h>
+#include <BeSQLite/BeSQLite.h>
+
+USING_NAMESPACE_BENTLEY_SQLITE
+USING_NAMESPACE_BENTLEY_SQLITE_EC
+
+const Utf8CP RuleSetEmbedder::FILE_TYPE_PresentationRuleSet = "PresentationRuleSet";
+/*---------------------------------------------------------------------------------**//**
+* @bsimethod                                    Saulius.Skliutas                10/2017
++---------------+---------------+---------------+---------------+---------------+------*/
+DbResult RuleSetEmbedder::Embed(PresentationRuleSetR ruleset)
+    {
+    DbEmbeddedFileTable& embeddedFiles = m_connection.EmbeddedFiles();
+
+    Utf8String rulesetName = ruleset.GetFullRuleSetId();
+    Utf8String rulesetXml = ruleset.WriteToXmlString();
+    DbResult result = embeddedFiles.AddEntry(rulesetName.c_str(), FILE_TYPE_PresentationRuleSet);
+    if (BE_SQLITE_OK != result)
+        return result;
+
+    return embeddedFiles.Save(rulesetXml.c_str(), rulesetXml.SizeInBytes(), rulesetName.c_str());
+    }
