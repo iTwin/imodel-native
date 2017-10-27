a69ab18f9115effe4908bed710166ee9d9e185a8
Dan.East <none@none>
2017-10-27 09:55:36 -0400

    EN: TFS#762220:Messages -- Implement Activity Messages

--- iModelCore/DgnPlatform/PublicAPI/DgnPlatform/NotificationManager.h
+++ iModelCore/DgnPlatform/PublicAPI/DgnPlatform/NotificationManager.h
@@ -117,6 +117,41 @@ public:
 
 
 //=======================================================================================
+//! Specifies the details of an activity message to be displayed to the user.
+// @bsiclass                                                      Dan.East      10/17
+//=======================================================================================
+struct           ActivityMessageDetails
+{
+//__PUBLISH_SECTION_END__
+    friend struct NotificationManager;
+
+//__PUBLISH_SECTION_START__
+private:
+    bool            m_showProgressBar;
+    bool            m_showPercentInMessage;
+    bool            m_supportsCancellation;
+
+public:
+    //! Construct a ActivityMessageDetails
+    //! @param[in]  showProgressBar         Indicates whether to show the progress bar in the activity message dialog.
+    //! @param[in]  showPercentInMessage    Indicates whether to show the percentage complete in the activity message text.
+    //! @param[in]  supportsCancellation    Indicates whether to show the Cancel button, giving the user the ability to cancel the operation.
+    ActivityMessageDetails(bool showProgressBar, bool showPercentInMessage, bool supportsCancellation)
+        {
+        m_showProgressBar = showProgressBar;
+        m_showPercentInMessage = showPercentInMessage;
+        m_supportsCancellation = supportsCancellation;
+        }
+
+    bool GetShowProgressBar() const { return m_showProgressBar; } //!< Gets the property to show the progress bar in the activity message dialog.
+    bool GetShowPercentInMessage() const { return m_showPercentInMessage; } //!< Gets the property to show the percentage complete in the activity message text.
+    bool GetSupportsCancellation() const { return m_supportsCancellation; } //!< Gets the property to show the Cancel button, giving the user the ability to cancel the operation.
+
+    //! Implement if cancellation is supported.
+    virtual void _ActivityCancelled () const {}
+};
+
+//=======================================================================================
 //! The NotificationManager controls the interaction with the user for prompts, error messages, and alert dialogs.
 //! Implementations of the NotificationManager may present the information in different ways. For example, in
 //! non-interactive sessions, these messages may be saved to a log file or simply discarded.
@@ -176,6 +211,20 @@ public:
     //! @param[in] icon         The MessageBox icon type.
     //! @return the response from the user.
     DGNPLATFORM_EXPORT static MessageBoxValue OpenMessageBox(MessageBoxType mbType, Utf8CP message, MessageBoxIconType icon);
+
+    //! Set up for activity messages.
+    //! @param[in] details      The activity message details.
+    //! @return SUCCESS if the message was displayed, ERROR if an invalid priority is specified.
+    DGNPLATFORM_EXPORT static StatusInt SetupActivityMessage(ActivityMessageDetails const& details);
+
+    //! Output an activity message to the user. Once percentComplete reaches 100, the message will be dismissed.
+    //! @param[in] messageText      The message text.
+    //! @param[in] percentComplete  The percentage of completion.
+    //! @return SUCCESS if the message was displayed, ERROR if the message could not be displayed.
+    DGNPLATFORM_EXPORT static StatusInt OutputActivityMessage(Utf8CP messageText, int32_t percentComplete);
+
+    //! Cancel an activity message.
+    DGNPLATFORM_EXPORT static void      CancelActivityMessage();
 };
 
 END_BENTLEY_DGN_NAMESPACE
