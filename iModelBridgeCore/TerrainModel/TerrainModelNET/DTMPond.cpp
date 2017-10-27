2cba5fa9138c4e7f91a1fd9429c737409a1c9c11
Daryl.Holmwood <none@none>
2017-10-27 15:53:14 +0100

    Fixed rename of enum or maintain compatiblity

--- iModelBridgeCore/TerrainModel/TerrainModelNET/DTMPond.cpp
+++ iModelBridgeCore/TerrainModel/TerrainModelNET/DTMPond.cpp
@@ -35,33 +35,33 @@ BEGIN_BENTLEY_TERRAINMODELNET_NAMESPACE
 ///<date>6/2011</date>
 
 Bentley::TerrainModelNET::DTMPondDesignCriteria::DTMPondResult DTMPondDesignCriteria::CreatePond([Out] DTM^% pondDTM)
-	{
-	BcDTMP fillTargetP = nullptr;
-	long numPoints = (long)m_points->Length;
-	// Convert to Native
-	pin_ptr<BGEO::DPoint3d const> tPoint = &m_points[0];
-	if (m_targetDTM != nullptr)
-		fillTargetP = m_targetDTM->Handle;
+    {
+    BcDTMP fillTargetP = nullptr;
+    long numPoints = (long)m_points->Length;
+    // Convert to Native
+    pin_ptr<BGEO::DPoint3d const> tPoint = &m_points[0];
+    if (m_targetDTM != nullptr)
+        fillTargetP = m_targetDTM->Handle;
 
-	// Create the Unmanaged pond Criteria
-	DtmPondDesignCriteria pondDesignCrit((::DTMPondDesignMethod)m_designMethod, (::DPoint3d*)&tPoint[0], numPoints, SideSlope, FreeBoard, (::DTMPondTarget)m_pondTarget, m_targetElevation, m_targetVolume,
-		IsBerm, BermSlope, BermWidth, IsCrown, CrownWidth, CornerStrokeTolerance, IsBermFillOnly, fillTargetP);
+    // Create the Unmanaged pond Criteria
+    DtmPondDesignCriteria pondDesignCrit((::DTMPondDesignMethod)m_designMethod, (::DPoint3d*)&tPoint[0], numPoints, SideSlope, FreeBoard, (::DTMPondTarget)m_pondTarget, m_targetElevation, m_targetVolume,
+        IsBerm, BermSlope, BermWidth, IsCrown, CrownWidth, CornerStrokeTolerance, IsBermFillOnly, fillTargetP);
 
-	::DTMPondResult pondFlag;
-	BcDTMPtr dtmP;
-	double outPondElevation = 0.0, outPondVolume = 0.0;
-	// Call the unmanaged function to get the result dtm. 
-	pondFlag = pondDesignCrit.CreatePond(&outPondElevation, &outPondVolume, dtmP);
-	if (dtmP != NULL)
-		{
-		pondDTM = gcnew DTM(dtmP.get());
- 		Bentley::TerrainModelNET::DTMPondDesignCriteria::AchievedPondElevation = outPondElevation;
- 		Bentley::TerrainModelNET::DTMPondDesignCriteria::AchievedPondVolume = outPondVolume;
-		return (Bentley::TerrainModelNET::DTMPondDesignCriteria::DTMPondResult) (int (pondFlag));
-		}
-	else
-		return Bentley::TerrainModelNET::DTMPondDesignCriteria::DTMPondResult::UnknownError;
-	}
+    ::DTMPondResult pondFlag;
+    BcDTMPtr dtmP;
+    double outPondElevation = 0.0, outPondVolume = 0.0;
+    // Call the unmanaged function to get the result dtm. 
+    pondFlag = pondDesignCrit.CreatePond(&outPondElevation, &outPondVolume, dtmP);
+    if (dtmP != NULL)
+        {
+        pondDTM = gcnew DTM(dtmP.get());
+        Bentley::TerrainModelNET::DTMPondDesignCriteria::AchievedPondElevation = outPondElevation;
+        Bentley::TerrainModelNET::DTMPondDesignCriteria::AchievedPondVolume = outPondVolume;
+        return (Bentley::TerrainModelNET::DTMPondDesignCriteria::DTMPondResult) (int (pondFlag));
+        }
+    else
+        return Bentley::TerrainModelNET::DTMPondDesignCriteria::DTMPondResult::UnknownError;
+    }
 
 
 END_BENTLEY_TERRAINMODELNET_NAMESPACE
--- iModelBridgeCore/TerrainModel/TerrainModelNET/DTMPond.h
+++ iModelBridgeCore/TerrainModel/TerrainModelNET/DTMPond.h
@@ -27,14 +27,14 @@ public ref class DTMPondDesignCriteria
 
     public:
         ///<summary> Enum for pond target - voume or elevation</summary>
-        enum class DTMPondTarget
+        enum class DTMPondTargetEnum
             {
             Volume = 1,
             Elevation = 2
             };
 
         ///<summary> Enum for pond reference points - perimeter or invert</summary>
-        enum class DTMPondDesignMethod
+        enum class DTMPondDesignMethodEnum
             {
             BottomUp = 1,
             TopDown = 2        
@@ -56,215 +56,215 @@ public ref class DTMPondDesignCriteria
         array<BGEO::DPoint3d>^ m_points;
         double m_sideSlope;
         double m_freeBoard;
-        DTMPondTarget m_pondTarget;
-        DTMPondDesignMethod  m_designMethod;
+        DTMPondTargetEnum m_pondTarget;
+        DTMPondDesignMethodEnum  m_designMethod;
         double m_targetElevation;
         double m_targetVolume;
-		bool m_isBerm;
-		double m_bermSlope;
-		double m_bermWidth;
-		bool m_isCrown;
-		double m_crownWidth;
-		bool m_isBermFillOnly;
+        bool m_isBerm;
+        double m_bermSlope;
+        double m_bermWidth;
+        bool m_isCrown;
+        double m_crownWidth;
+        bool m_isBermFillOnly;
         double m_pondElevation;
         double m_pondVolume;
-		double m_cornerStrokeTolerance;
-		double m_bermToTargetDtmSlope;
-		DTMPondResult m_lastResult;
-		DTM^ m_targetDTM;
+        double m_cornerStrokeTolerance;
+        double m_bermToTargetDtmSlope;
+        DTMPondResult m_lastResult;
+        DTM^ m_targetDTM;
 
     public:
 
-		DTMPondDesignCriteria()
+        DTMPondDesignCriteria()
             {
             // Set default params
-			m_designMethod = DTMPondDesignMethod::BottomUp;
+            m_designMethod = DTMPondDesignMethodEnum::BottomUp;
             m_sideSlope = 1.0;
             m_freeBoard = 0.0;
-            m_pondTarget = DTMPondTarget::Volume;
+            m_pondTarget = DTMPondTargetEnum::Volume;
             m_targetVolume = 100000.0;
             m_targetElevation = 0.0;
-			m_isBerm = false;
-			m_bermSlope = 1.0;
-			m_bermWidth = 5.0;
-			m_isCrown = false;
-			m_crownWidth = 5.0;
+            m_isBerm = false;
+            m_bermSlope = 1.0;
+            m_bermWidth = 5.0;
+            m_isCrown = false;
+            m_crownWidth = 5.0;
             m_pondElevation = 0.0;
             m_pondVolume = 0.0;
-			m_isBermFillOnly = true;
-			m_cornerStrokeTolerance = 10.0;
-			m_bermToTargetDtmSlope = 1.0;
+            m_isBermFillOnly = true;
+            m_cornerStrokeTolerance = 10.0;
+            m_bermToTargetDtmSlope = 1.0;
             m_lastResult = DTMPondResult::TargetObtained;
             };   
 
-		DTMPondDesignCriteria(DTMPondDesignMethod perimeterOrInvert, array<BGEO::DPoint3d>^ perimerOrInvertPoints, double sideSlope, double freeBoard, DTMPondTarget pondTarget, double target,
-			bool IsBerm, double bermSlope, double bermWidth, double bermToTargetDtmSlope, bool isBermFillOnly, bool IsCrown, double crownWidth, double cornerStrokeTolerance, DTM^ targetDTM)
-			{
-			// Set design params
-			m_designMethod = perimeterOrInvert;
-			SetReferencePoints(perimerOrInvertPoints);
-			m_sideSlope = sideSlope;
-			m_freeBoard = freeBoard;
-			m_pondTarget = pondTarget;
-			if (pondTarget == DTMPondTarget::Volume)
-				m_targetVolume = target;
-			else
-				m_targetElevation = target;
+        DTMPondDesignCriteria(DTMPondDesignMethodEnum perimeterOrInvert, array<BGEO::DPoint3d>^ perimerOrInvertPoints, double sideSlope, double freeBoard, DTMPondTargetEnum pondTarget, double target,
+            bool IsBerm, double bermSlope, double bermWidth, double bermToTargetDtmSlope, bool isBermFillOnly, bool IsCrown, double crownWidth, double cornerStrokeTolerance, DTM^ targetDTM)
+            {
+            // Set design params
+            m_designMethod = perimeterOrInvert;
+            SetReferencePoints(perimerOrInvertPoints);
+            m_sideSlope = sideSlope;
+            m_freeBoard = freeBoard;
+            m_pondTarget = pondTarget;
+            if (pondTarget == DTMPondTargetEnum::Volume)
+                m_targetVolume = target;
+            else
+                m_targetElevation = target;
 
-			m_isBerm = IsBerm;
-			m_bermSlope = bermSlope;
-			m_bermWidth = bermWidth;
-			m_isCrown = IsCrown;
-			m_crownWidth = CrownWidth;
-			m_isBermFillOnly = isBermFillOnly;
-			m_cornerStrokeTolerance = cornerStrokeTolerance;
-			m_bermToTargetDtmSlope = bermToTargetDtmSlope;
-			m_pondElevation = 0.0;
-			m_pondVolume = 0.0;
-			m_lastResult = DTMPondResult::TargetObtained;
-			m_targetDTM = targetDTM;
-			};
+            m_isBerm = IsBerm;
+            m_bermSlope = bermSlope;
+            m_bermWidth = bermWidth;
+            m_isCrown = IsCrown;
+            m_crownWidth = CrownWidth;
+            m_isBermFillOnly = isBermFillOnly;
+            m_cornerStrokeTolerance = cornerStrokeTolerance;
+            m_bermToTargetDtmSlope = bermToTargetDtmSlope;
+            m_pondElevation = 0.0;
+            m_pondVolume = 0.0;
+            m_lastResult = DTMPondResult::TargetObtained;
+            m_targetDTM = targetDTM;
+            };
 
-		DTMPondDesignCriteria(DTMPondDesignMethod perimeterOrInvert, array<BGEO::DPoint3d>^ perimerOrInvertPoints, double sideSlope, double freeBoard, DTMPondTarget pondTarget, double target)
-			{
-			m_designMethod = perimeterOrInvert;
-			SetReferencePoints(perimerOrInvertPoints);
-			m_sideSlope = sideSlope;
-			m_freeBoard = freeBoard;
-			m_pondTarget = pondTarget;
-			if (pondTarget == DTMPondTarget::Volume)
-				m_targetVolume = target;
-			else
-				m_targetElevation = 0.0;
+        DTMPondDesignCriteria(DTMPondDesignMethodEnum perimeterOrInvert, array<BGEO::DPoint3d>^ perimerOrInvertPoints, double sideSlope, double freeBoard, DTMPondTargetEnum pondTarget, double target)
+            {
+            m_designMethod = perimeterOrInvert;
+            SetReferencePoints(perimerOrInvertPoints);
+            m_sideSlope = sideSlope;
+            m_freeBoard = freeBoard;
+            m_pondTarget = pondTarget;
+            if (pondTarget == DTMPondTargetEnum::Volume)
+                m_targetVolume = target;
+            else
+                m_targetElevation = 0.0;
 
-			m_isBerm = false;
-			m_bermSlope = 1.0;
-			m_bermWidth = 5.0;
-			m_isCrown = false;
-			m_crownWidth = 5.0;
-			m_pondElevation = 0.0;
-			m_pondVolume = 0.0;
-			m_isBermFillOnly = true;
-			m_cornerStrokeTolerance = 10.0;
-			m_bermToTargetDtmSlope = 1.0;
-			m_lastResult = DTMPondResult::TargetObtained;
-			}
+            m_isBerm = false;
+            m_bermSlope = 1.0;
+            m_bermWidth = 5.0;
+            m_isCrown = false;
+            m_crownWidth = 5.0;
+            m_pondElevation = 0.0;
+            m_pondVolume = 0.0;
+            m_isBermFillOnly = true;
+            m_cornerStrokeTolerance = 10.0;
+            m_bermToTargetDtmSlope = 1.0;
+            m_lastResult = DTMPondResult::TargetObtained;
+            }
 
-		property array<BGEO::DPoint3d>^ PerimPoints
-			{
-			array<BGEO::DPoint3d>^ get()
-				{
-				return m_points;
-				}
-			}
+        property array<BGEO::DPoint3d>^ PerimPoints
+            {
+            array<BGEO::DPoint3d>^ get()
+                {
+                return m_points;
+                }
+            }
 
-		property DTM^ TargetDTM
-			{
-			Bentley::TerrainModelNET::DTM^ get()
-				{
-				return m_targetDTM;
-				}
-			void set(DTM^ value)
-				{
-				m_targetDTM = value;
-				}
-			}
+        property DTM^ TargetDTM
+            {
+            Bentley::TerrainModelNET::DTM^ get()
+                {
+                return m_targetDTM;
+                }
+            void set(DTM^ value)
+                {
+                m_targetDTM = value;
+                }
+            }
 
-		property double BermToTargetDtmSlope
-			{
-			double get()
-				{
-				return m_bermToTargetDtmSlope;
-				}
-			void set(double value)
-				{
-				m_bermToTargetDtmSlope = value;
-				}
-			}
+        property double BermToTargetDtmSlope
+            {
+            double get()
+                {
+                return m_bermToTargetDtmSlope;
+                }
+            void set(double value)
+                {
+                m_bermToTargetDtmSlope = value;
+                }
+            }
 
-		property double CornerStrokeTolerance
-			{
-			double get()
-				{
-				return m_cornerStrokeTolerance;
-				}
-			void set(double value)
-				{
-				m_cornerStrokeTolerance = value;
-				}
-			}
+        property double CornerStrokeTolerance
+            {
+            double get()
+                {
+                return m_cornerStrokeTolerance;
+                }
+            void set(double value)
+                {
+                m_cornerStrokeTolerance = value;
+                }
+            }
 
-		property bool IsBermFillOnly
-			{
-			bool get()
-				{
-				return (bool) m_isBermFillOnly;
-				}
-			void set(bool value)
-				{
-				m_isBermFillOnly = value;
-				}
-			}
+        property bool IsBermFillOnly
+            {
+            bool get()
+                {
+                return (bool) m_isBermFillOnly;
+                }
+            void set(bool value)
+                {
+                m_isBermFillOnly = value;
+                }
+            }
 
-		property bool IsCrown
-			{
-			bool get()
-				{
-				return m_isCrown;
-				}
-			void set(bool value)
-				{
-				m_isCrown = value;
-				}
-			}
+        property bool IsCrown
+            {
+            bool get()
+                {
+                return m_isCrown;
+                }
+            void set(bool value)
+                {
+                m_isCrown = value;
+                }
+            }
 
-		property bool IsBerm
-			{
-			bool get()
-				{
-				return m_isBerm;
-				}
-			void set(bool value)
-				{
-				m_isBerm = value;
-				}
-			}
+        property bool IsBerm
+            {
+            bool get()
+                {
+                return m_isBerm;
+                }
+            void set(bool value)
+                {
+                m_isBerm = value;
+                }
+            }
 
-		property double CrownWidth
-			{
-			double get()
-				{
-				return m_crownWidth;
-				}
-			void set(double value)
-				{
-				m_crownWidth = value;
-				}
-			}
+        property double CrownWidth
+            {
+            double get()
+                {
+                return m_crownWidth;
+                }
+            void set(double value)
+                {
+                m_crownWidth = value;
+                }
+            }
 
-		property double BermWidth
-			{
-			double get()
-				{
-				return m_bermWidth;
-				}
-			void set(double value)
-				{
-				m_bermWidth = value;
-				}
-			}
+        property double BermWidth
+            {
+            double get()
+                {
+                return m_bermWidth;
+                }
+            void set(double value)
+                {
+                m_bermWidth = value;
+                }
+            }
 
-		property double BermSlope
-			{
-			double get()
-				{
-				return m_bermSlope;
-				}
-			void set(double value)
-				{
-				m_bermSlope = value;
-				}
-			}
+        property double BermSlope
+            {
+            double get()
+                {
+                return m_bermSlope;
+                }
+            void set(double value)
+                {
+                m_bermSlope = value;
+                }
+            }
 
         property double SideSlope
             {
@@ -290,25 +290,25 @@ public ref class DTMPondDesignCriteria
                 }
             }
 
-        property DTMPondTarget PondTarget
+        property DTMPondTargetEnum PondTarget
             {
-            DTMPondTarget get()
+            DTMPondTargetEnum get()
                 {
                 return m_pondTarget;
                 }
-            void set(DTMPondTarget value)
+            void set(DTMPondTargetEnum value)
                 {
                 m_pondTarget = value;
                 }
             }
 
-        property DTMPondDesignMethod PondDesignMethod
+        property DTMPondDesignMethodEnum PondDesignMethod
             {
-            DTMPondDesignMethod get()
+            DTMPondDesignMethodEnum get()
                 {
                 return m_designMethod;
                 }
-            void set(DTMPondDesignMethod value)
+            void set(DTMPondDesignMethodEnum value)
                 {
                 m_designMethod = value;
                 }
@@ -357,10 +357,10 @@ public ref class DTMPondDesignCriteria
                 {
                 return m_pondElevation;
                 }
-			void set(double value)
-				{
-				m_pondElevation = value;
-				}
+            void set(double value)
+                {
+                m_pondElevation = value;
+                }
             }
 
         property double AchievedPondVolume
@@ -369,25 +369,25 @@ public ref class DTMPondDesignCriteria
                 {
                 return m_pondVolume;
                 }
-			void set(double value)
-				{
-				m_pondVolume = value;
-				}
+            void set(double value)
+                {
+                m_pondVolume = value;
+                }
             }
 
         property DTMPondResult LastResult
             {
-			DTMPondResult get()
+            DTMPondResult get()
                 {
                 return m_lastResult;
                 }
-			void set(DTMPondResult value)
-				{
-				m_lastResult = value;
-				}
+            void set(DTMPondResult value)
+                {
+                m_lastResult = value;
+                }
             }
 
-		DTMPondResult DTMPondDesignCriteria::CreatePond([Out] DTM^% pondDTM);
+        DTMPondResult DTMPondDesignCriteria::CreatePond([Out] DTM^% pondDTM);
 
     };
 
