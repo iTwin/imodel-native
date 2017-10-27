245b4a6abe8b5e5e05d5492eda27b5f1ea05047e
Haroldas.Vitunskas <none@none>
2017-10-27 14:36:19 +0300

    fixed default normal vector for radial grid being invalid

--- Domains/Grids/Elements/PublicApi/RadialGridPortion.h
+++ Domains/Grids/Elements/PublicApi/RadialGridPortion.h
@@ -49,7 +49,7 @@ public:
         //! @param[in] height               height of grid lines
         //! @param[in] extendHeight         true if grid should be extended to both ends in Z axis
         CreateParams(Dgn::SpatialLocationModelCP model, Dgn::DgnElementId modeledElementId, int planeCount, int circularCount, double planeIterationAngle,
-                               double circularInterval, double length, double height, Utf8CP name, bool extendHeight = false, DVec3d normal = DVec3d::From(0, 0, 0)) :
+                               double circularInterval, double length, double height, Utf8CP name, bool extendHeight = false, DVec3d normal = DVec3d::From(0, 0, 1)) :
             T_Super(DgnElement::CreateParams(model->GetDgnDb(), model->GetModelId(), QueryClassId(model->GetDgnDb()), Dgn::DgnCode(model->GetDgnDb().CodeSpecs().QueryCodeSpecId(GRIDS_AUTHORITY_RadialGridPortion), modeledElementId, name))),
             m_model(model),
             m_planeCount(planeCount),
