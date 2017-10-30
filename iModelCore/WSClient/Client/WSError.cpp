3841489f5c34c788e90c859bca66bc684b079cde
Vincas.Razma <none@none>
2017-10-30 18:59:20 +0200

    BF: fix mistake in 4c7874e6db3e due to failing test

--- iModelCore/WSClient/Client/WSError.cpp
+++ iModelCore/WSClient/Client/WSError.cpp
@@ -124,7 +124,7 @@ WSError::WSError(HttpResponseCR httpResponse) : WSError()
     if (ImsClient::IsLoginRedirect(httpResponse) ||                 // Bentley CONNECT login redirect
         HttpStatus::Unauthorized == httpResponse.GetHttpStatus())   // Bentley CONNECT token could not be retrieved
         {
-        m_message = HttpError(httpResponse).GetDisplayMessage();
+        m_message = HttpError(ConnectionStatus::OK, HttpStatus::Unauthorized).GetDisplayMessage();
         m_status = Status::ReceivedError;
         m_id = Id::LoginFailed;
         return;
