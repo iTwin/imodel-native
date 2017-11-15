78c611472700cd90f5035889110fb8f785fc2c3d
Ray.Bentley <none@none>
2017-11-15 09:14:09 -0500

    BF: Fix partition handling - Test case from TFS# 778151 - Instanced BReps could cause missing geometry.

--- iModelCore/DgnPlatform/DgnCore/MeshTile.cpp
+++ iModelCore/DgnPlatform/DgnCore/MeshTile.cpp
@@ -2488,7 +2488,7 @@ bool TileGeometryProcessor::_ProcessBody(IBRepEntityCR solid, SimplifyGraphic& g
     PSolidThreadUtil::WorkerThreadInnerMark     innerMark;
 #endif
 
-    IBRepEntityPtr  clone = const_cast<IBRepEntityP>(&solid);
+    IBRepEntityPtr  clone = solid.Clone();      // Clone to move the body to our thread's (lightweight) partition.
     DRange3d        range = clone->GetEntityRange();
     Transform       localToTile = Transform::FromProduct(m_transformFromDgn, gf.GetLocalToWorldTransform());
 
@@ -2618,7 +2618,6 @@ private:
         }
 
     StatusInt _VisitElement(DgnElementId elementId, bool allowLoad) override;
-    Render::GraphicPtr _StrokeGeometry(GeometrySourceCR, double) override;
 
     static Render::ViewFlags GetDefaultViewFlags()
         {
@@ -2715,43 +2714,6 @@ template<typename T> StatusInt TileGeometryProcessorContext<T>::_VisitElement(Dg
         }
 
     return status;
-    }
-
-/*---------------------------------------------------------------------------------**//**
-* @bsimethod                                                    Brien.Bastings 09/2017
-+---------------+---------------+---------------+---------------+---------------+------*/
-#if defined (BENTLEYCONFIG_PARASOLID) 
-static bool hasBRep(GeometrySourceCR source)
-    {
-    GeometryCollection collection(source);
-
-    for (auto iter : collection)
-        {
-        if (GeometryCollection::Iterator::EntryType::BRepEntity == iter.GetEntryType())
-            return true;
-        }
-
-    return false;
-    }
-#endif
-    
-/*---------------------------------------------------------------------------------**//**
-* @bsimethod                                                    Paul.Connelly   09/16
-+---------------+---------------+---------------+---------------+---------------+------*/
-template<typename T> Render::GraphicPtr TileGeometryProcessorContext<T>::_StrokeGeometry(GeometrySourceCR source, double pixelSize)
-    {
-#if defined (BENTLEYCONFIG_PARASOLID) 
-    // If the geometry has a parasolid BRep then create a mark so that work is done in this threads lightweight partition and
-    // error handling works properly..
-    PSolidThreadUtil::WorkerThreadInnerMarkPtr        innerMark;
-    if (hasBRep(source))
-        innerMark = new PSolidThreadUtil::WorkerThreadInnerMark();
-
-    Render::GraphicPtr graphic = source.Draw(*this, pixelSize);
-    return WasAborted() ? nullptr : graphic;
-#else
-    return nullptr;
-#endif
     }
 
 /*---------------------------------------------------------------------------------**//**
