eb5f361206cabc070b6176341bda206dc45ae9ac
Jeff.Marker <none@none>
2017-11-27 11:44:24 -0500

    compile error (cannot convert nullptr to ...)

--- iModelCore/BeHttp/BeHttp/Curl/CurlHttpRequest.cpp
+++ iModelCore/BeHttp/BeHttp/Curl/CurlHttpRequest.cpp
@@ -440,8 +440,8 @@ void CurlHttpRequest::SetupCurl()
         }
     else
         {
-        curl_easy_setopt(m_curl, CURLOPT_USERNAME, nullptr);
-        curl_easy_setopt(m_curl, CURLOPT_PASSWORD, nullptr);
+        curl_easy_setopt(m_curl, CURLOPT_USERNAME, NULL);
+        curl_easy_setopt(m_curl, CURLOPT_PASSWORD, NULL);
         }
 
     // Request method
@@ -530,7 +530,7 @@ void CurlHttpRequest::SetupCurl()
         {
         curl_easy_setopt(m_curl, CURLOPT_SSL_VERIFYPEER, 0L);
         curl_easy_setopt(m_curl, CURLOPT_SSL_VERIFYHOST, 0L);
-        curl_easy_setopt(m_curl, CURLOPT_CAINFO, nullptr);
+        curl_easy_setopt(m_curl, CURLOPT_CAINFO, NULL);
         }
 
     if (LOG.isSeverityEnabled(NativeLogging::LOG_DEBUG))
