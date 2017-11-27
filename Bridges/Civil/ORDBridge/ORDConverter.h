9afa9a29fa49bed7ef08ae6be95da82c01a90fac
Diego.Diaz <none@none>
2017-11-27 10:25:23 -0500

    React to underlying API changes.

--- Bridges/Civil/ORDBridge/ORDConverter.h
+++ Bridges/Civil/ORDBridge/ORDConverter.h
@@ -15,16 +15,26 @@ BEGIN_ORDBRIDGE_NAMESPACE
 
 struct ORDConverter
 {
+public:
+    struct Params
+        {
+        Params(BeFileNameCR dgnFileName, Dgn::SubjectCR subject, Dgn::iModelBridgeSyncInfoFile::ChangeDetector& changeDetector, Dgn::iModelBridgeSyncInfoFile::ROWID fileScopeId) :
+            dgnFileName(dgnFileName), subjectCPtr(&subject), changeDetectorP(&changeDetector), fileScopeId(fileScopeId), spatialDataTransformHasChanged(false)
+            {}
+
+        BeFileName dgnFileName;
+        Dgn::SubjectCPtr subjectCPtr;
+        Dgn::iModelBridgeSyncInfoFile::ChangeDetector* changeDetectorP;
+        Dgn::iModelBridgeSyncInfoFile::ROWID fileScopeId;
+        bool spatialDataTransformHasChanged;
+        };
+
 private:
-    void ConvertAlignments(Bentley::Cif::GeometryModel::SDK::GeometricModel const&, 
-        Dgn::DgnDbSync::DgnV8::ConverterLibrary& converterLib,
-        Dgn::iModelBridgeSyncInfoFile::ChangeDetector& changeDetector, Dgn::iModelBridgeSyncInfoFile::ROWID fileScopeId);
-    void ConvertCorridors(Bentley::Cif::GeometryModel::SDK::GeometricModel const&, 
-        Dgn::DgnDbSync::DgnV8::ConverterLibrary& converterLib,
-        Dgn::iModelBridgeSyncInfoFile::ChangeDetector& changeDetector, Dgn::iModelBridgeSyncInfoFile::ROWID fileScopeId);
+    void ConvertAlignments(Bentley::Cif::GeometryModel::SDK::GeometricModel const&, Dgn::DgnDbSync::DgnV8::ConverterLibrary& converterLib, Params& params);
+    void ConvertCorridors(Bentley::Cif::GeometryModel::SDK::GeometricModel const&, Dgn::DgnDbSync::DgnV8::ConverterLibrary& converterLib, Params& params);
 
 public:
-    void ConvertORDData(BeFileNameCR dgnFileName, Dgn::SubjectCR subject, Dgn::iModelBridgeSyncInfoFile::ChangeDetector& changeDetector, Dgn::iModelBridgeSyncInfoFile::ROWID fileScopeId);
+    void ConvertORDData(Params& params);
 }; // ORDConverter
 
 END_ORDBRIDGE_NAMESPACE
