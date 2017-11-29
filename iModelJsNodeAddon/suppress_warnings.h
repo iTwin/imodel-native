1b90fbda9d63e3156ca7cb2d521c47f7f15cdab7
Sam.Wilson <none@none>
2017-11-29 13:39:48 -0500

    the imodeljs node addon

---
+++ iModelJsNodeAddon/suppress_warnings.h
@@ -0,0 +1,13 @@
+/*--------------------------------------------------------------------------------------+
+|
+|     $Source: suppress_warnings.h $
+|
+|  $Copyright: (c) 2017 Bentley Systems, Incorporated. All rights reserved. $
+|
++--------------------------------------------------------------------------------------*/
+#pragma once
+
+#if defined (_MSC_VER)
+#pragma warning(disable:4100) // unreferenced formal parameter
+#pragma warning(disable:4996) // deprecated
+#endif
