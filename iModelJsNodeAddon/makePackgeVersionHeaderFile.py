f301b0c4abd0c5c02cdd8ad6902dd2a6e937a50b
Sam.Wilson <none@none>
2017-12-04 16:44:59 -0500

    work around problem passing arguments to Linux command line program.

---
+++ iModelJsNodeAddon/makePackgeVersionHeaderFile.py
@@ -0,0 +1,33 @@
+#--------------------------------------------------------------------------------------
+#
+#     $Source: makePackgeVersionHeaderFile.py $
+#
+#  $Copyright: (c) 2017 Bentley Systems, Incorporated. All rights reserved. $
+#
+#--------------------------------------------------------------------------------------
+import os
+import sys
+import re
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print "Syntax: ", sys.argv[0], " filename packageversionfile"
+        exit(1)
+
+    filename = sys.argv[1]
+    packageVersionFileName = sys.argv[2]
+
+    # The package semantic version number (n.m.p) is stored in a file. Inject this version number into all of the package files that we generate.
+    packageVersion = "";
+    with open(packageVersionFileName, 'r') as pvf:
+        packageVersion = pvf.read()
+    packageVersion = packageVersion.strip().lower();
+
+    with open(filename, 'r+') as f:
+        str = f.read()
+        str = str.replace(r'${PACKAGE_VERSION}', packageVersion)
+        f.seek(0)
+        f.write(str)
+        f.truncate()
+
+    exit(0)
