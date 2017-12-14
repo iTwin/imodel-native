60fef06d9be423067b1340263ebcc337b48d78ef
Farhad.Kabir <none@none>
2017-12-14 17:12:41 +0500

    TFS#778427 - Streamlining iModelMock operations

---
+++ iModelBridgeCore/WSClient/Tests/iModelHubClient/Integration/RequestHandler.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "../BackDoor/PublicAPI/BackDoor/WebServices/iModelHub/BackDoor.h"
+//#include "C:\BSW\Bim0200Dev\src\WSClient\iModelHubClient\Utils.h"
+#include <WebServices\iModelHub\Utils.h>
+
+BEGIN_BENTLEY_IMODELHUB_UNITTESTS_NAMESPACE
+USING_NAMESPACE_BENTLEY_HTTP
+
+class RequestHandler
+    {
+    public:
+        RequestHandler();
+        ~RequestHandler();
+        Response PerformGetRequest(Request req);
+        Response PerformOtherRequest(Request req);
+
+    private:
+
+    };
+
+END_BENTLEY_IMODELHUB_UNITTESTS_NAMESPACE
