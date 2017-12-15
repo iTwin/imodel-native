fc8432132861728d9b51082d6a1daf040033bed5
Saulius.Skliutas <none@none>
2017-12-15 10:04:29 +0200

    Should take create second ContentSetItem for the same instance

--- iModelCore/ECPresentation/Tests/Published/RulesEngine/PresentationManagerContentTests.cpp
+++ iModelCore/ECPresentation/Tests/Published/RulesEngine/PresentationManagerContentTests.cpp
@@ -8263,7 +8263,7 @@ TEST_F(RulesDrivenECPresentationManagerContentTests, GetDistinctNavigationProper
     }
 
 /*---------------------------------------------------------------------------------**//**
-* @bsitest                                      Saulius.Skliutas                09/2017
+* @bsitest                                      Saulius.Skliutas                12/2017
 +---------------+---------------+---------------+---------------+---------------+------*/
 DEFINE_SCHEMA(DoesntLoadCompositeContentIfInstanceDoesntHaveCompositeProperty, R"*(
     <ECStructClass typeName="MyStruct">
@@ -8307,6 +8307,67 @@ TEST_F(RulesDrivenECPresentationManagerContentTests, DoesntLoadCompositeContentI
     ASSERT_TRUE(content.IsValid());
 
     // validate content set
+    DataContainer<ContentSetItemCPtr> contentSet = content->GetContentSet();
+    ASSERT_EQ(2, contentSet.GetSize());
+    }
+
+/*---------------------------------------------------------------------------------**//**
+* @bsitest                                      Saulius.Skliutas                12/2017
++---------------+---------------+---------------+---------------+---------------+------*/
+DEFINE_SCHEMA(DoesntTakeContentTwiceForTheSameInstance, R"*(
+    <ECEntityClass typeName="Element">
+        <ECProperty propertyName="StructProperty" typeName="int" />
+        <ECNavigationProperty propertyName="Parent" relationshipName="ElementOwnsChildElements" direction="Backward" description="The parent element that owns this element.">
+            <ECCustomAttributes>
+                <ForeignKeyConstraint xmlns="ECDbMap.2.0">
+                    <OnDeleteAction>NoAction</OnDeleteAction>
+                </ForeignKeyConstraint>
+            </ECCustomAttributes>
+        </ECNavigationProperty>
+    </ECEntityClass>
+
+    <ECRelationshipClass typeName="ElementOwnsChildElements" strength="embedding" modifier="None">
+        <Source multiplicity="(0..1)" roleLabel="owns child" polymorphic="true">
+            <Class class="Element"/>
+        </Source>
+        <Target multiplicity="(0..*)" roleLabel="is owned by parent" polymorphic="true">
+            <Class class="Element"/>
+        </Target>
+    </ECRelationshipClass>
+)*");
+TEST_F(RulesDrivenECPresentationManagerContentTests, DoesntTakeContentTwiceForTheSameInstance)
+    {
+    // set up data set
+    ECClassCP elementClass = GetClass("Element");
+    ECRelationshipClassCP elementOwnsChildElement = GetClass("ElementOwnsChildElements")->GetRelationshipClassCP();
+    IECInstancePtr rootElement = RulesEngineTestHelpers::InsertInstance(s_project->GetECDb(), *elementClass);
+    IECInstancePtr childElement = RulesEngineTestHelpers::InsertInstance(s_project->GetECDb(), *elementClass);
+    RulesEngineTestHelpers::InsertRelationship(s_project->GetECDb(), *elementOwnsChildElement, *rootElement, *childElement);
+    
+    // create the rule set
+    PresentationRuleSetPtr rules = PresentationRuleSet::CreateInstance("DoesntTakeContentTwiceForTheSameInstance", 1, 0, false, "", "", "", false);
+    m_locater->AddRuleSet(*rules);
+
+    ContentRuleP rule = new ContentRule("", 1, false);
+    rules->AddPresentationRule(*rule);
+    rule->AddSpecification(*new ContentRelatedInstancesSpecification(1, 0, true, "", RequiredRelationDirection::RequiredRelationDirection_Forward,
+        elementOwnsChildElement->GetFullName(), elementClass->GetFullName()));
+    rule->AddSpecification(*new SelectedNodeInstancesSpecification());
+
+    // options
+    RulesDrivenECPresentationManager::ContentOptions options(rules->GetRuleSetId().c_str());
+    SelectionInfo selection("", false, *NavNodeKeyListContainer::Create({ECInstanceNodeKey::Create(*rootElement), ECInstanceNodeKey::Create(*childElement)}));
+
+    // validate descriptor
+    ContentDescriptorCPtr descriptor = IECPresentationManager::GetManager().GetContentDescriptor(s_project->GetECDb(), nullptr, selection, options.GetJson()).get();
+    ASSERT_TRUE(descriptor.IsValid());
+    ASSERT_EQ(2, descriptor->GetVisibleFields().size());
+
+    // request for content
+    ContentCPtr content = IECPresentationManager::GetManager().GetContent(s_project->GetECDb(), *descriptor, selection, PageOptions(), options.GetJson()).get();
+    ASSERT_TRUE(content.IsValid());
+
+    // expect 2 content set items
     DataContainer<ContentSetItemCPtr> contentSet = content->GetContentSet();
     ASSERT_EQ(2, contentSet.GetSize());
     }
