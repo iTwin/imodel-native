8a91c5064c188753233a65e038c31b413ef4571f
Farhad.Kabir <none@none>
2017-12-18 17:00:46 +0500

    TFS#778427 - Streamlining mock operations

--- iModelCore/WSClient/Tests/iModelHubClient/Integration/RequestHandler.h
+++ iModelCore/WSClient/Tests/iModelHubClient/Integration/RequestHandler.h
@@ -3,17 +3,20 @@
 #include "../BackDoor/PublicAPI/BackDoor/WebServices/iModelHub/BackDoor.h"
 //#include "C:\BSW\Bim0200Dev\src\WSClient\iModelHubClient\Utils.h"
 #include <WebServices\iModelHub\Utils.h>
+#include "FakeServer.h"
 
 BEGIN_BENTLEY_IMODELHUB_UNITTESTS_NAMESPACE
 USING_NAMESPACE_BENTLEY_HTTP
 
 class RequestHandler
     {
+    WCharCP serverPath;
     public:
         RequestHandler();
         ~RequestHandler();
         Response PerformGetRequest(Request req);
         Response PerformOtherRequest(Request req);
+        Response CreateiModel(Request req);
 
     private:
 
