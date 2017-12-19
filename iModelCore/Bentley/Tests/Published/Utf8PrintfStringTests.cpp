d9a10bca2575d068f0898515413fa61429477daa
Vincas.Razma <none@none>
2017-12-19 14:55:26 +0200

    EN: add positional formatting tests for Utf8PrintfString 4195bb1ce6b6 SRB:Jeff.Marker

--- iModelCore/Bentley/Tests/Published/Utf8PrintfStringTests.cpp
+++ iModelCore/Bentley/Tests/Published/Utf8PrintfStringTests.cpp
@@ -11,7 +11,7 @@
 
 class Utf8PrintfStringTests : public ::testing::Test {};
 
-/*---------------------------------------------------------------------------------**//**
+/*--------------------------------------------------------------------------------------+
 * @bsimethod                                    Vincas.Razma                      10/16
 +---------------+---------------+---------------+---------------+---------------+------*/
 TEST_F(Utf8PrintfStringTests, Ctor_EmptyStringPassed_Empty)
@@ -19,7 +19,7 @@ TEST_F(Utf8PrintfStringTests, Ctor_EmptyStringPassed_Empty)
     EXPECT_EQ("", Utf8PrintfString(""));
     }
 
-/*---------------------------------------------------------------------------------**//**
+/*--------------------------------------------------------------------------------------+
 * @bsimethod                                    Vincas.Razma                      10/16
 +---------------+---------------+---------------+---------------+---------------+------*/
 TEST_F(Utf8PrintfStringTests, Ctor_FormatAndArgumentPassed_FormattedString)
@@ -27,7 +27,7 @@ TEST_F(Utf8PrintfStringTests, Ctor_FormatAndArgumentPassed_FormattedString)
     EXPECT_EQ("Foo", Utf8PrintfString("%s", "Foo"));
     }
 
-/*---------------------------------------------------------------------------------**//**
+/*--------------------------------------------------------------------------------------+
 * @bsimethod                                    Vincas.Razma                      10/16
 +---------------+---------------+---------------+---------------+---------------+------*/
 TEST_F(Utf8PrintfStringTests, Ctor_FormatAndMultipleArgumentsPassed_FormattedString)
@@ -47,10 +47,29 @@ static void Utf8PrintfStringArgs(Utf8CP expectedStr, Utf8CP fmt, ...)
     va_end(args);
     EXPECT_TRUE(str.Equals(expectedStr)) << "Expected: " << expectedStr << "\nActual: " << str.c_str();// << str.size();
     }
+
 //---------------------------------------------------------------------------------------
 // @bsimethod                                     Farhad.Kabir                        12/16
 //---------------------------------------------------------------------------------------
 TEST_F(Utf8PrintfStringTests, Utf8PrintfStringTest)
     {
     Utf8PrintfStringArgs("No. 5", "No. %d", 5);
+    }
+
+/*--------------------------------------------------------------------------------------+
+* @bsimethod                                    Vincas.Razma                      10/16
++---------------+---------------+---------------+---------------+---------------+------*/
+TEST_F(Utf8PrintfStringTests, Ctor_PositionalFormatWithStrings_FormattedString)
+    {
+    EXPECT_STREQ("A B C D", Utf8PrintfString("A %1$s C %2$s", "B", "D").c_str());
+    EXPECT_STREQ("A B C D", Utf8PrintfString("A %2$s C %1$s", "D", "B").c_str());
+    }
+
+/*--------------------------------------------------------------------------------------+
+* @bsimethod                                    Vincas.Razma                      10/16
++---------------+---------------+---------------+---------------+---------------+------*/
+TEST_F(Utf8PrintfStringTests, Ctor_PositionalFormatWithMixedTypes_FormattedString)
+    {
+    EXPECT_STREQ("A 111 B Foo", Utf8PrintfString("A %1$d B %2$s", 111, "Foo").c_str());
+    EXPECT_STREQ("A Foo B 111", Utf8PrintfString("A %2$s B %1$d", 111, "Foo").c_str());
     }
