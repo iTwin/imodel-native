0b87d51fc96022f339085e720fdde506b2a4d1a5
Paul.Connelly <none@none>
2017-12-19 08:10:06 -0500

    BF: TFS#796257: Semantics of _HasGraphics() changed, but _DrawGraphics() still assumed it meant the primary graphic was valid. Additionally, because we potentially render partial tiles while they are being refined, must be more careful about synchronization.

--- iModelCore/DgnPlatform/DgnCore/ElementTileTree.cpp
+++ iModelCore/DgnPlatform/DgnCore/ElementTileTree.cpp
@@ -10,6 +10,7 @@
 #include <DgnPlatform/TileIO.h>
 #include <folly/BeFolly.h>
 #include <DgnPlatform/RangeIndex.h>
+#include <numeric>
 #if defined (BENTLEYCONFIG_PARASOLID) 
 #include <DgnPlatform/DgnBRep/PSolidUtil.h>
 #endif
@@ -1026,6 +1027,7 @@ BentleyStatus Loader::_LoadTile()
     for (auto const& mesh : geometry.Meshes())
         mesh->GetGraphics (graphics, *system, args, root.GetDgnDb());
 
+    GraphicPtr batch;
     if (!graphics.empty())
         {
         GraphicPtr graphic;
@@ -1035,36 +1037,47 @@ BentleyStatus Loader::_LoadTile()
                 break;
             case 1:
                 graphic = *graphics.begin();
+                BeAssert(graphic.IsValid());
                 break;
             default:
+                BeAssert(std::accumulate(graphics.begin(), graphics.end(), true, [](bool cur, GraphicPtr const& gf) { return cur && gf.IsValid(); }));
                 graphic = system->_CreateGraphicList(std::move(graphics), root.GetDgnDb());
+                BeAssert(graphic.IsValid());
                 break;
             }
 
         if (graphic.IsValid())
             {
             geometry.Meshes().m_features.SetModelId(root.GetModelId());
-            tile.SetGraphic(*system->_CreateBatch(*graphic, std::move(geometry.Meshes().m_features)));
+            batch = system->_CreateBatch(*graphic, std::move(geometry.Meshes().m_features));
+            BeAssert(batch.IsValid());
+            tile.SetGraphic(*batch);
             }
         }
 
-    if (!tile._IsPartial())
+    return tile.GetElementRoot().UnderMutex([&]()
         {
-        tile.ClearBackupGraphic();
-        tile.SetIsReady();
-        return SUCCESS;
-        }
-    else
-        {
-        // Mark partial tile as canceled so it becomes 'not loaded' again and we can resume tile generation from where we left off...
-        BeAssert(nullptr != m_loads);
-        m_loads->SetCanceled();
+        if (batch.IsValid())
+            tile.SetGraphic(*batch);
 
-        // Also notify host that a new tile has become available, though it's only partial - otherwise it won't know to recreate the scene...
-        T_HOST._OnNewTileReady(root.GetDgnDb());
+        if (!tile._IsPartial())
+            {
+            tile.ClearBackupGraphic();
+            // tile.SetIsReady();
+            return SUCCESS;
+            }
+        else
+            {
+            // Mark partial tile as canceled so it becomes 'not loaded' again and we can resume tile generation from where we left off...
+            BeAssert(nullptr != m_loads);
+            m_loads->SetCanceled();
 
-        return ERROR;
-        }
+            // Also notify host that a new tile has become available, though it's only partial - otherwise it won't know to recreate the scene...
+            T_HOST._OnNewTileReady(root.GetDgnDb());
+
+            return ERROR;
+            }
+        });
     }
 
 /*---------------------------------------------------------------------------------**//**
@@ -1577,16 +1590,13 @@ Utf8String Tile::_GetTileCacheKey() const
 +---------------+---------------+---------------+---------------+---------------+------*/
 void Tile::_DrawGraphics(TileTree::DrawArgsR args) const
     {
-    if (IsReady() || _IsPartial())
+    GetElementRoot().UnderMutex([&]()
         {
-        BeAssert(_HasGraphics());
-        if (_HasGraphics())
+        if (m_graphic.IsValid())
             args.m_graphics.Add(*m_graphic);
-        }
-    else if (m_backupGraphic.IsValid())
-        {
-        args.m_graphics.Add(*m_backupGraphic);
-        }
+        else if (m_backupGraphic.IsValid())
+            args.m_graphics.Add(*m_backupGraphic);
+        });
 
     auto debugGraphic = GetDebugGraphics(GetElementRoot().GetDebugOptions());
     if (debugGraphic.IsValid())
@@ -1635,25 +1645,28 @@ void Tile::InitTolerance()
 +---------------+---------------+---------------+---------------+---------------+------*/
 void Tile::_Invalidate()
     {
-    m_backupGraphic = m_graphic;
-    m_graphic = nullptr;
-    m_debugGraphics.Reset();
-    m_generator.reset();
+    GetElementRoot().UnderMutex([&]()
+        {
+        m_backupGraphic = m_graphic;
+        m_graphic = nullptr;
+        m_debugGraphics.Reset();
+        m_generator.reset();
 
-    m_contentRange = ElementAlignedBox3d();
+        m_contentRange = ElementAlignedBox3d();
 
-    m_isLeaf = false;
-    m_hasZoomFactor = false;
-    m_zoomFactor = 1.0;
+        m_isLeaf = false;
+        m_hasZoomFactor = false;
+        m_zoomFactor = 1.0;
 
-    InitTolerance();
+        InitTolerance();
 
-    if (nullptr != GetParent())
-        return;
+        if (nullptr != GetParent())
+            return;
 
-    // Root tile...
-    GeometricModelPtr model = GetElementRoot().GetModel();
-    m_displayable = isElementCountLessThan(s_minElementsPerTile, *model->GetRangeIndex());
+        // Root tile...
+        GeometricModelPtr model = GetElementRoot().GetModel();
+        m_displayable = isElementCountLessThan(s_minElementsPerTile, *model->GetRangeIndex());
+        });
     }
 
 /*---------------------------------------------------------------------------------**//**
