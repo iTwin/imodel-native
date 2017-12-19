b843a62bdbbf92b83a05a300d9ecfa9ae1d62bac
Mindaugas.Butkus <none@none>
2017-12-19 14:34:11 +0200

    created main BuildingShared header

---
+++ Domains/BuildingShared/PublicApi/BuildingShared/BuildingSharedApi.h
@@ -0,0 +1,12 @@
+/*--------------------------------------------------------------------------------------+
+|
+|     $Source: PublicApi/BuildingShared/BuildingSharedApi.h $
+|
+|  $Copyright: (c) 2017 Bentley Systems, Incorporated. All rights reserved. $
+|
++--------------------------------------------------------------------------------------*/
+#pragma once
+
+#include "BuildingSharedMacros.h"
+#include <BuildingShared/Units/UnitsApi.h>
+#include <BuildingShared/Utils/UtilsApi.h>
---
+++ Domains/BuildingShared/Units/PublicApi/UnitsApi.h
@@ -0,0 +1,11 @@
+/*--------------------------------------------------------------------------------------+
+|
+|     $Source: Units/PublicApi/UnitsApi.h $
+|
+|  $Copyright: (c) 2017 Bentley Systems, Incorporated. All rights reserved. $
+|
++--------------------------------------------------------------------------------------*/
+#pragma once
+
+#include "GeometryDebug.h"
+#include "UnitConverter.h"
