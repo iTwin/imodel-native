2d6bc7928cbbe9dc62c58d69389b4da313d6d309
Mindaugas.Butkus <none@none>
2017-12-20 18:26:19 +0200

    added property setters/getters by key (string)

--- Domains/BuildingShared/GeometryManipulationStrategies/GeometryManipulationStrategyBase.cpp
+++ Domains/BuildingShared/GeometryManipulationStrategies/GeometryManipulationStrategyBase.cpp
@@ -8,6 +8,7 @@
 #include <BuildingShared/BuildingSharedApi.h>
 #include "PublicApi/GeometryManipulationStrategyBase.h"
 
+USING_NAMESPACE_BENTLEY_DGN
 USING_NAMESPACE_BUILDING_SHARED
 
 //--------------------------------------------------------------------------------------
@@ -15,5 +16,18 @@ USING_NAMESPACE_BUILDING_SHARED
 //---------------+---------------+---------------+---------------+---------------+------
 GeometryManipulationStrategyBase::GeometryManipulationStrategyBase()
     {
-
     }
+
+#define SET_PROPERTY_IMPL(value_type) \
+    void GeometryManipulationStrategyBase::SetProperty(Utf8CP key, value_type const& value) { _SetProperty(key, value); }
+#define TRY_GET_PROPERTY_IMPL(value_type) \
+    bool GeometryManipulationStrategyBase::TryGetProperty(Utf8CP key, value_type& value) { return _TryGetProperty(key, value); }
+#define SET_TRYGET_PROPERTY_IMPL(value_type) \
+    SET_PROPERTY_IMPL(value_type) \
+    TRY_GET_PROPERTY_IMPL(value_type)
+
+SET_TRYGET_PROPERTY_IMPL(int)
+SET_TRYGET_PROPERTY_IMPL(double)
+SET_TRYGET_PROPERTY_IMPL(DgnElementId)
+SET_TRYGET_PROPERTY_IMPL(DgnElement)
+SET_TRYGET_PROPERTY_IMPL(Utf8String)
--- Domains/BuildingShared/GeometryManipulationStrategies/PublicApi/GeometryManipulationStrategyBase.h
+++ Domains/BuildingShared/GeometryManipulationStrategies/PublicApi/GeometryManipulationStrategyBase.h
@@ -9,10 +9,39 @@
 
 BEGIN_BUILDING_SHARED_NAMESPACE
 
+#define GMS_V_SET_PROPERTY_TYPE(value_type) \
+    virtual void _SetProperty(Utf8CP key, value_type const& value) {}
+#define GMS_V_TRYGET_PROPERTY_TYPE(value_type) \
+    virtual bool _TryGetProperty(Utf8CP key, value_type& value) { return false; }
+#define GMS_V_SET_TRYGET_PROPERTY_TYPE(value_type) \
+    GMS_V_SET_PROPERTY_TYPE(value_type) \
+    GMS_V_TRYGET_PROPERTY_TYPE(value_type)
+#define GMS_SET_PROPERTY_TYPE(value_type) \
+    GEOMETRYMANIPULATIONSTRATEGIES_EXPORT void SetProperty(Utf8CP key, value_type const& value);
+#define GMS_TRYGET_PROPERTY_TYPE(value_type) \
+    GEOMETRYMANIPULATIONSTRATEGIES_EXPORT bool TryGetProperty(Utf8CP key, value_type& value);
+#define GMS_SET_TRYGET_PROPERTY_TYPE(value_type) \
+    GMS_SET_PROPERTY_TYPE(value_type) \
+    GMS_TRYGET_PROPERTY_TYPE(value_type)
+#define GMS_PROPERTY_TYPE(value_type) \
+    protected: \
+        GMS_V_SET_TRYGET_PROPERTY_TYPE(value_type) \
+    public: \
+        GMS_SET_TRYGET_PROPERTY_TYPE(value_type)
+
+//=======================================================================================
+// @bsiclass                                     Mindaugas.Butkus               12/2017
+//=======================================================================================
 struct GeometryManipulationStrategyBase : RefCountedBase
     {
     protected:
         GEOMETRYMANIPULATIONSTRATEGIES_EXPORT GeometryManipulationStrategyBase();
+
+        GMS_PROPERTY_TYPE(int)
+        GMS_PROPERTY_TYPE(double)
+        GMS_PROPERTY_TYPE(Dgn::DgnElementId)
+        GMS_PROPERTY_TYPE(Dgn::DgnElement)
+        GMS_PROPERTY_TYPE(Utf8String)
     };
 
 END_BUILDING_SHARED_NAMESPACE
