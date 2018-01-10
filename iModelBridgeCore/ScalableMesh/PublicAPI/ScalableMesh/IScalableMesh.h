f4938120dca182ab758d8587b7d80a7f13a0f751
carole.macdonald <none@none>
2018-01-10 09:38:04 -0500

    Fixed build break by properly qualifing ClipVectorPtr.  Otherwise, it is an ambiguous reference when used with the VersionedV8 API.

--- iModelBridgeCore/ScalableMesh/PublicAPI/ScalableMesh/IScalableMesh.h
+++ iModelBridgeCore/ScalableMesh/PublicAPI/ScalableMesh/IScalableMesh.h
@@ -210,7 +210,7 @@ struct IScalableMesh abstract:  IRefCounted
 
         virtual int                                 _Generate3DTiles(const WString& outContainerName, const WString& outDatasetName, SMCloudServerType server, IScalableMeshProgressPtr progress = nullptr, BENTLEY_NAMESPACE_NAME::Dgn::ClipVectorPtr clips = nullptr, uint64_t coverageId = (uint64_t)-1) const = 0;
 
-        virtual int                                 _SaveAs(const WString& destination, ClipVectorPtr clips = nullptr, IScalableMeshProgressPtr progress = nullptr) = 0;
+        virtual int                                 _SaveAs(const WString& destination, BENTLEY_NAMESPACE_NAME::Dgn::ClipVectorPtr clips = nullptr, IScalableMeshProgressPtr progress = nullptr) = 0;
 
 #ifdef SCALABLE_MESH_ATP
         virtual int                                 _ChangeGeometricError(const WString& outContainerName, const WString& outDatasetName = L"", SMCloudServerType server = SMCloudServerType::LocalDisk, const double& newGeometricErrorValue = 0.0) const = 0;
@@ -467,7 +467,7 @@ struct IScalableMesh abstract:  IRefCounted
 
         BENTLEY_SM_EXPORT int                    Generate3DTiles(const WString& outContainerName, WString outDatasetName = L"", SMCloudServerType server = SMCloudServerType::LocalDisk, IScalableMeshProgressPtr progress = nullptr, BENTLEY_NAMESPACE_NAME::Dgn::ClipVectorPtr clips = nullptr) const;
 
-        BENTLEY_SM_EXPORT int                    SaveAs(const WString& destination, ClipVectorPtr clips = nullptr, IScalableMeshProgressPtr progress = nullptr);
+        BENTLEY_SM_EXPORT int                    SaveAs(const WString& destination, BENTLEY_NAMESPACE_NAME::Dgn::ClipVectorPtr clips = nullptr, IScalableMeshProgressPtr progress = nullptr);
 
         BENTLEY_SM_EXPORT void                   ImportTerrainSM(WString terrainPath);
 
