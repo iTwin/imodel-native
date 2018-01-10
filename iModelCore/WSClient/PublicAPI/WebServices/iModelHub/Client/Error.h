2363e60dcd858dee7015abc4e90ded82ad51d909
Benas.Kikutis <none@none>
2018-01-10 16:07:11 +0200

    TFS#798347:Query with multiple where criteria fails with iModelHub.DatabaseOperationFailed

--- iModelCore/WSClient/PublicAPI/WebServices/iModelHub/Client/Error.h
+++ iModelCore/WSClient/PublicAPI/WebServices/iModelHub/Client/Error.h
@@ -2,7 +2,7 @@
 |
 |     $Source: PublicAPI/WebServices/iModelHub/Client/Error.h $
 |
-|  $Copyright: (c) 2017 Bentley Systems, Incorporated. All rights reserved. $
+|  $Copyright: (c) 2018 Bentley Systems, Incorporated. All rights reserved. $
 |
 +--------------------------------------------------------------------------------------*/
 #pragma once
@@ -57,6 +57,7 @@ public:
         FailedToGetProjectPermissions,
         ChangeSetAlreadyHasVersion,
         VersionAlreadyExists,
+        QueryIdsNotSpecified,
 
         //Long Running Processes Errors
         FileIsNotYetInitialized = 100,
