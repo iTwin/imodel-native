ee231cfe62bb085a8afbe9c3e066f7e4403afb1d
Vincas.Razma <none@none>
2018-01-30 18:24:33 +0200

    EN: improve and cleanup RunWSClientCompatibilityTestToolForKnownServices.bat SRB:Arturas.Januska

--- iModelCore/WSClient/Tests/CompatibilityTests/RunTests/RunWSClientCompatibilityTestToolForKnownServices.bat
+++ iModelCore/WSClient/Tests/CompatibilityTests/RunTests/RunWSClientCompatibilityTestToolForKnownServices.bat
@@ -1,9 +1,16 @@
 
+@echo;
+@echo Running specific test cases. Get test list:
+@echo    RunWSClientCompatibilityTestToolForKnownServices.bat "--gtest_list_tests"
+@echo Filter on test number:
+@echo    RunWSClientCompatibilityTestToolForKnownServices.bat "--gtest_filter=*/12"
+@echo;
+
 set workdir=%OutRoot%WSClientCompatibilityTestTool\
 call %OutRoot%Winx64\Product\WSClientCompatibilityTestTool\WSClientCompatibilityTests.exe %1 %2 %3 %4 %5 %6 %7 %8 %9 ^
     --gtest_output=xml:%workdir%results.xml ^
     --workdir %workdir% ^
     --config %~dp0\RunWSClientCompatibilityTestToolForKnownServices.config
 
-call python %~dp0\..\Other\TestReport\makereport.py "%workdir%results.xml" > %workdir%results.htm
+call python %~dp0\TestReport\makereport.py "%workdir%results.xml" > %workdir%results.htm
 start %workdir%results.htm
