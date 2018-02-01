f8b7b792054a3759805f37eb27aa5deedef2e27e
Viktorija.Adomauskai <none@none>
2018-02-01 11:49:23 +0200

    If object not found on server remove it from cache. SRB:Vincas.Razma

--- iModelCore/WSClient/Cache/FileDownloadManager.cpp
+++ iModelCore/WSClient/Cache/FileDownloadManager.cpp
@@ -2,7 +2,7 @@
 |
 |     $Source: Cache/FileDownloadManager.cpp $
 |
-|  $Copyright: (c) 2016 Bentley Systems, Incorporated. All rights reserved. $
+|  $Copyright: (c) 2018 Bentley Systems, Incorporated. All rights reserved. $
 |
 +--------------------------------------------------------------------------------------*/
 
@@ -95,10 +95,7 @@ FileCache cacheLocation
         auto txn = m_ds->StartCacheTransaction();
         if (!fileResult.IsSuccess())
             {
-            WSError::Id errorId = fileResult.GetError().GetId();
-
-            if (WSError::Id::InstanceNotFound == errorId ||
-                WSError::Id::NotEnoughRights == errorId)
+            if (fileResult.GetError().IsInstanceNotAvailableError())
                 {
                 txn.GetCache().RemoveFile(objectId);
                 }
