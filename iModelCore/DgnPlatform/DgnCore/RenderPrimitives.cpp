e686f692595e7d095b6ba72a54076e631d5124b7
Ray.Bentley <none@none>
2018-02-16 14:19:45 -0500

    CM: Add test for invalid (<3 point) triangles.

--- iModelCore/DgnPlatform/DgnCore/RenderPrimitives.cpp
+++ iModelCore/DgnPlatform/DgnCore/RenderPrimitives.cpp
@@ -1191,6 +1191,9 @@ void MeshBuilder::AddTriangle(TriangleCR triangle)
 +---------------+---------------+---------------+---------------+---------------+------*/
 void MeshBuilder::AddFromPolyfaceVisitor(PolyfaceVisitorR visitor, TextureMappingCR mappedTexture, DgnDbR dgnDb, FeatureCR feature, bool includeParams, uint32_t fillColor, bool requireNormals)
     {
+    if (points.size() < 3)
+        return;
+
     auto const&     points = visitor.Point();
     bool const*     visitorVisibility = visitor.GetVisibleCP();
     size_t          nTriangles = points.size() - 2;
