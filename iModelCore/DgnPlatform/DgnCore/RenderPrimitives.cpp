d54cc360d92ed052c9fb8dc5f46aacaa013929f7
Ray.Bentley <none@none>
2018-02-16 14:21:09 -0500

    CM: Fix test for nPoints in triangle visitor.

--- iModelCore/DgnPlatform/DgnCore/RenderPrimitives.cpp
+++ iModelCore/DgnPlatform/DgnCore/RenderPrimitives.cpp
@@ -1191,7 +1191,7 @@ void MeshBuilder::AddTriangle(TriangleCR triangle)
 +---------------+---------------+---------------+---------------+---------------+------*/
 void MeshBuilder::AddFromPolyfaceVisitor(PolyfaceVisitorR visitor, TextureMappingCR mappedTexture, DgnDbR dgnDb, FeatureCR feature, bool includeParams, uint32_t fillColor, bool requireNormals)
     {
-    if (points.size() < 3)
+    if (visitor.Point().size() < 3)
         return;
 
     auto const&     points = visitor.Point();
