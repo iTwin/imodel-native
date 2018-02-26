157bbe219a8a11f5ef712650637e02f4d1fcabb6
Mathieu.St-Pierre <none@none>
2018-02-26 10:23:09 -0500

    Ensure that the extent is correctly initialized even when the first feature point is outside the clip extent

--- iModelBridgeCore/ScalableMesh/STM/ScalableMeshSourceCreator.cpp
+++ iModelBridgeCore/ScalableMesh/STM/ScalableMeshSourceCreator.cpp
@@ -6,7 +6,7 @@
 |       $Date: 2015/07/15 10:41:29 $
 |     $Author: Elenie.Godzaridis $
 |
-|  $Copyright: (c) 2017 Bentley Systems, Incorporated. All rights reserved. $
+|  $Copyright: (c) 2018 Bentley Systems, Incorporated. All rights reserved. $
 |
 +--------------------------------------------------------------------------------------*/
 
