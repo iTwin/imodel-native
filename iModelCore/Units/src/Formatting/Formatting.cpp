b56a2a059dadd0d7dd674efa1e7d02c151867e24
David.Fox-Rabinovitz <none@none>
2018-02-27 22:02:07 -0500

    WIP: removed pragma once in .cpp file

--- iModelCore/Units/src/Formatting/Formatting.cpp
+++ iModelCore/Units/src/Formatting/Formatting.cpp
@@ -5,7 +5,6 @@
 |  $Copyright: (c) 2018 Bentley Systems, Incorporated. All rights reserved. $
 |
 +--------------------------------------------------------------------------------------*/
-#pragma once
 #include <UnitsPCH.h>
 #include <locale>
 #include <Formatting/FormattingApi.h>
