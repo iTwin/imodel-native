edf00551cc5d68a2eb3066fa8b228401d9b9c843
Satyakam.Khadilkar <none@none>
2018-03-20 08:21:58 -0400

    WIP: Unify iOS and Mac Test

--- iModelJsMobile/nonport/Test.cpp
+++ iModelJsMobile/nonport/Test.cpp
@@ -9,6 +9,7 @@
 #include "Test.h"
 #include <iostream>
 #include <iModelJs/iModelJs.h>
+#include <iModelJs/iModelJsServicesTier.h>
 
 struct TestObject : Napi::ObjectWrap<TestObject>
 {
@@ -47,9 +48,50 @@ void Test () {
     std::cout << "NAPI - JavaScriptCore Tests\n";
     BentleyApi::iModelJs::Js::Runtime runtime;
     
-//    Napi::Env& env = runtime.Env();
-    
+    Napi::Env& env = runtime.Env();
+//    BentleyApi::iModelJs::ServicesTier::UvHost host;
+//    while (!host.IsReady()) { ; }
+
     auto result = runtime.EvaluateScript ("1 + 1");
     assert (result.status == BentleyB0200::iModelJs::Js::EvaluateStatus::Success);
     assert (result.value.As<Napi::Number>().DoubleValue() == 2.0);
+    
+    auto u = env.Undefined();
+    assert (u.IsUndefined());
+    
+    auto n = env.Null();
+    assert (n.IsNull());
+    
+    auto o = Napi::Object::New(env);
+    assert (o.IsObject());
+    
+    auto a = Napi::Array::New(env);
+    assert (a.IsArray());
+    
+    auto b = Napi::Boolean::New(env, true);
+    assert (b.IsBoolean());
+    assert (b.Value() == true);
+    
+    auto n2 = Napi::Number::New(env, 2.0);
+    assert (n2.IsNumber());
+    assert (n2.DoubleValue() == 2.0);
+    
+    auto s = Napi::String::New(env, "hello");
+    assert (s.IsString());
+    assert (s.Utf8Value() == "hello");
+    
+    auto s2 = s;
+    assert (s2.Utf8Value() == s.Utf8Value());
+    
+    TestObject::Init(env, env.Global());
+    auto result1 = runtime.EvaluateScript ("Multiply(7.0,6.0);");
+    assert (result1.status == BentleyB0200::iModelJs::Js::EvaluateStatus::Success);
+    assert (result1.value.As<Napi::Number>().DoubleValue() == 42.0);
+    
+    auto result2 = runtime.EvaluateScript ("var a = new TestObject(1.0);");
+    auto result3 = runtime.EvaluateScript ("a.TestFunc()");
+    assert (result3.value.As<Napi::Number>().DoubleValue() == 42.0);
+    
+    auto result4 = runtime.EvaluateScript ("TestObject.TestStaticFunc()");
+    assert (result4.value.As<Napi::Number>().DoubleValue() == 3.14);
 }
