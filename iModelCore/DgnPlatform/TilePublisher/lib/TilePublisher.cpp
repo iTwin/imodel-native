c1f91574b48c62bfe7ae3964c97f98271cdafb2f
Paul.Connelly <none@none>
2018-03-20 10:58:22 -0400

    WIP: TFS#865301: Disable publishing of 3D view attachments to Cesium.

--- iModelCore/DgnPlatform/TilePublisher/lib/TilePublisher.cpp
+++ iModelCore/DgnPlatform/TilePublisher/lib/TilePublisher.cpp
@@ -3476,6 +3476,9 @@ Json::Value PublisherContext::GetViewAttachmentsJson(Sheet::ModelCR sheet, DgnMo
         if (viewedModels.empty())
             continue;
 
+        if (view->IsSpatialView())
+            continue; // WIP - we will publish these differently - for now just skip.
+
         // ###TODO: Spatial view attachments with multiple models unhandled...
         DgnModelId baseModelId = *viewedModels.begin();
         attachedModels.insert(baseModelId);
@@ -3752,7 +3755,11 @@ void    PublisherContext::GetViewedModelsFromView (DgnModelIdSet& viewedModels, 
                 auto attachId = stmt->GetValueId<DgnElementId>(0);
                 auto attach = GetDgnDb().Elements().Get<Sheet::ViewAttachment>(attachId);
                 if (attach.IsValid())
-                    GetViewedModelsFromView(viewedModels, attach->GetAttachedViewId(), false); // view attachments don't nest...
+                    {
+                    // Exclude spatial views...publish as raster tiles directly into sheet tileset.
+                    if (GetDgnDb().Elements().Get<ViewDefinition2d>(attach->GetAttachedViewId()).IsValid())
+                        GetViewedModelsFromView(viewedModels, attach->GetAttachedViewId(), false); // view attachments don't nest...
+                    }
                 }
             }
         }
