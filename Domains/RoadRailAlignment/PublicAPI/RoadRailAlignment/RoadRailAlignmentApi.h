a06c087fda83b46ee76d258c9031d57ca0c78d29
Carl Hinkle <carl.hinkle@bentley.com>
2018-04-02 12:46:34 -0400

    Add documentation group for Road Rail Alignment.  Document Alignment PublishedAPI.

--- Domains/RoadRailAlignment/PublicAPI/RoadRailAlignment/RoadRailAlignmentApi.h
+++ Domains/RoadRailAlignment/PublicAPI/RoadRailAlignment/RoadRailAlignmentApi.h
@@ -8,6 +8,15 @@
 #pragma once
 
 //__PUBLISH_SECTION_START__
+
+/** @cond BENTLEY_SDK_RoadRailAlignment */
+/**
+
+@defgroup GROUP_RoadRailAlignment Road Rail Alignment
+
+*/
+/* @endcond */
+
 #include "RoadRailAlignment.h"
 #include "AlignmentModel.h"
 #include "Alignment.h"
