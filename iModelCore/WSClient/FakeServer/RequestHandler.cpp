b910f673890548df9f191702ea7f06af11a54147
Farhad.Kabir <none@none>
2018-04-03 11:55:22 +0500

    fix iOS

--- iModelCore/WSClient/FakeServer/RequestHandler.cpp
+++ iModelCore/WSClient/FakeServer/RequestHandler.cpp
@@ -114,7 +114,7 @@ void RequestHandler::CheckDb()
         if(!m_db.TableExists("Users"))
             CreateTable("Users", m_db, "UserCreated STRING, CreatedDate STRING");
         if(!m_db.TableExists("ChangeSets"))
-            CreateTable("ChangeSets", m_db, "Id STRING, Index INTEGER, iModelId STRING, ParentId STRING, UserCreated STRING, ChangeSetId STRING, CreatedDate STRING");
+            CreateTable("ChangeSets", m_db, "Id STRING, Description STRING, iModelId STRING, FileSize INTEGER, BriefcaseId INTEGER, ParentId STRING, UserCreated STRING, CreatedDate STRING");
         if(!m_db.TableExists("SeedFile"))
             CreateTable("SeedFile", m_db, "Id STRING, FileName STRING, FileDescription STRING, FileSize INTEGER, iModelId STRING, IsUploaded BOOLEAN, UserUploaded STRING, UploadedDate STRING");
 
@@ -636,36 +636,48 @@ Response RequestHandler::DeleteiModels(Request req)
 
 Response RequestHandler::PushChangeSetMetadata(Request req)
     {
-    BeGuid projGuid(true);
+    bvector<Utf8String> args = ParseUrl(req);
+    Utf8String iModelid = GetInstanceid(args[4]);
     Json::Value settings = ParsedJson(req);
+    
+    bvector<Utf8String> input = {  
+        settings["instance"]["properties"]["Id"].asString(),
+        settings["instance"]["properties"]["Description"].asString(),
+        settings["instance"]["properties"]["ParentId"].asString(),
+        settings["instance"]["properties"]["FileSize"].asString(),
+        settings["instance"]["properties"]["BriefcaseId"].asString(),};
 
-    bvector<Utf8String> input = {   projGuid.ToString(),  
-        settings["instance"]["properties"]["Name"].asString(),
-        settings["instance"]["properties"]["Description"].asString()};
-
-    Utf8String fileName(settings["instance"]["properties"]["Name"].asString());
     CheckDb();
     BeFileName dbPath = GetDbPath();
     BentleyB0200::BeSQLite::Db m_db;
     if (DbResult::BE_SQLITE_OK == m_db.OpenBeSQLiteDb(dbPath, BentleyB0200::BeSQLite::Db::OpenParams(BentleyB0200::BeSQLite::Db::OpenMode::ReadWrite, DefaultTxn::Yes)))
         {
+        Statement st;
+        st.Prepare(m_db, "INSERT INTO Instances(Id, Description, iModelId, FileSize, BriefcaseId, ParentId) VALUES (?,?,?,?,?,?)");
+        st.BindText(1, input[0], Statement::MakeCopy::No);
+        st.BindText(2, input[1], Statement::MakeCopy::No);
+        st.BindText(3, iModelid, Statement::MakeCopy::No);
+        st.BindInt(4, settings["instance"]["properties"]["FileSize"].asInt());
+        st.BindInt(5, settings["instance"]["properties"]["BriefcaseId"].asInt());
+        st.BindText(6, input[2], Statement::MakeCopy::No);
+        st.Step();
 
         Json::Value changesetMetadata(Json::objectValue);
         JsonValueR changedInstance = changesetMetadata[ServerSchema::ChangedInstance] = Json::objectValue;
         changedInstance["change"] = "Created";
         JsonValueR InstanceAfterChange = changedInstance[ServerSchema::InstanceAfterChange] = Json::objectValue;
-        InstanceAfterChange[ServerSchema::InstanceId] = "";
+        InstanceAfterChange[ServerSchema::InstanceId] = input[0];
         InstanceAfterChange[ServerSchema::SchemaName] = ServerSchema::Schema::iModel;
         InstanceAfterChange[ServerSchema::ClassName] = ServerSchema::Class::ChangeSet;
         JsonValueR properties = InstanceAfterChange[ServerSchema::Properties] = Json::objectValue;
         properties[ServerSchema::Property::BriefcaseId] = 2;
         properties[ServerSchema::Property::ContainingChanges] = 0;
-        properties[ServerSchema::Property::Description] = "";
-        properties[ServerSchema::Property::FileSize] = "";
-        properties[ServerSchema::Property::Id] = "";
-        properties[ServerSchema::Property::IsUploaded] = "";
-        properties[ServerSchema::Property::ParentId] = "";
-        properties[ServerSchema::Property::SeedFileId] = "";
+        properties[ServerSchema::Property::Description] = input[1];
+        properties[ServerSchema::Property::FileSize] = input[3];
+        properties[ServerSchema::Property::Id] = input[0];
+        properties[ServerSchema::Property::IsUploaded] = false;
+        properties[ServerSchema::Property::ParentId] = input[2];
+        properties[ServerSchema::Property::SeedFileId] = input[4];
         properties[ServerSchema::Property::Index] = "";
         JsonValueR relationshipInstances = InstanceAfterChange[ServerSchema::RelationshipInstances] = Json::objectValue;
         relationshipInstances[ServerSchema::InstanceId] = "";
@@ -677,8 +689,8 @@ Response RequestHandler::PushChangeSetMetadata(Request req)
         relatedInstance[ServerSchema::SchemaName] = "iModelScope";
         relatedInstance[ServerSchema::ClassName] = "AccessKey";
         JsonValueR propertiesl1 = relationshipInstances[ServerSchema::Properties] = Json::objectValue;
-        properties[ServerSchema::Property::UploadUrl] = 2;
-        properties[ServerSchema::Property::DownloadUrl] = NULL;
+        propertiesl1[ServerSchema::Property::UploadUrl] = "";
+        propertiesl1[ServerSchema::Property::DownloadUrl] = NULL;
 
         Utf8String contentToWrite(Json::FastWriter().write(changesetMetadata));
 
