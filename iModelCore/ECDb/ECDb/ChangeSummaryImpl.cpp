02542df16a72bc6c22f9cb3e69c521795ee93442
Ramanujam.Raman <none@none>
2018-04-05 12:10:28 -0400

    Reduced redundant logging when processing change summaries.

--- iModelCore/ECDb/ECDb/ChangeSummaryImpl.cpp
+++ iModelCore/ECDb/ECDb/ChangeSummaryImpl.cpp
@@ -488,13 +488,21 @@ BentleyStatus ChangeExtractor::FromChangeSet(IChangeSet& changeSet, ExtractOptio
     {
     ChangeIterator iter(m_ecdb, changeSet);
 
+    ChangeIterator::TableMap const* lastTableMap = nullptr;
     for (ChangeIterator::RowEntry const& rowEntry : iter)
-    {
-        if (!rowEntry.IsMapped())
         {
-            LOG.warningv("ChangeSet includes changes to unmapped table %s", rowEntry.GetTableName().c_str());
+        if (!rowEntry.IsMapped())
+            {
+                if (LOG.isSeverityEnabled(NativeLogging::LOG_WARNING))
+                {
+                if (lastTableMap != rowEntry.GetTableMap())
+                    {
+                    LOG.warningv("ChangeSet includes changes to unmapped table %s", rowEntry.GetTableName().c_str());
+                    lastTableMap = rowEntry.GetTableMap();
+                    }
+                }
             continue; // There are tables which are just not mapped to EC that we simply don't care about (e.g., be_Prop table)
-        }
+            }
 
         ECClassCP primaryClass = rowEntry.GetPrimaryClass();
         ECInstanceId primaryInstanceId = rowEntry.GetPrimaryInstanceId();
