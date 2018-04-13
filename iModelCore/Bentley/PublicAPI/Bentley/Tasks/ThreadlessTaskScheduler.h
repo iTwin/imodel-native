aa71e97838bb005ef2fe48dbe26a75947f1f4f42
Vincas.Razma <none@none>
2018-04-13 14:50:36 +0300

    EN: add ThreadlessTaskScheduler for BeHttp bugfixes to avoid deadlocks in some cases - allows using no addtional thread for Then() tasks SRB:Benediktas.Lipnickas

---
+++ iModelCore/Bentley/PublicAPI/Bentley/Tasks/ThreadlessTaskScheduler.h
@@ -0,0 +1,42 @@
+/*--------------------------------------------------------------------------------------+
+ |
+ |     $Source: PublicAPI/Bentley/Tasks/ThreadlessTaskScheduler.h $
+ |
+ |  $Copyright: (c) 2018 Bentley Systems, Incorporated. All rights reserved. $
+ |
+ +--------------------------------------------------------------------------------------*/
+#pragma once
+//__PUBLISH_SECTION_START__
+
+#include <Bentley/Tasks/TaskScheduler.h>
+
+BEGIN_BENTLEY_TASKS_NAMESPACE
+
+/*--------------------------------------------------------------------------------------+
+* @bsiclass                                                     Vincas.Razma      04/18
+* For internal use only.
+* Scheduler that does not use its own threads.
+* Can be used to execute small sections of code that needs to avoid using additional or default threads.
+* When used with task->Then() - code is executed in same thread as task or current thread if task is already completed.
+* When used with ExecuteAsync() or ExecuteAsyncWithoutAttachingToCurrentTask() - code is exectued in current thread.
++---------------+---------------+---------------+---------------+---------------+------*/
+struct ThreadlessTaskScheduler : ITaskScheduler
+    {
+    void Push(std::shared_ptr<AsyncTask> task, AsyncTask::Priority priority = AsyncTask::Priority::Inherited)
+        {
+        Push(task, AsyncTasksManager::GetCurrentThreadAsyncTask(), priority);
+        };
+    void Push(std::shared_ptr<AsyncTask> task, std::shared_ptr<AsyncTask> parentTask, AsyncTask::Priority priority = AsyncTask::Priority::Inherited)
+        {
+        if (parentTask)
+            parentTask->AddSubTask(task);
+        task->Execute();
+        };
+    std::shared_ptr<AsyncTask> WaitAndPop() { return nullptr; };
+    std::shared_ptr<AsyncTask> TryPop() { return nullptr; };
+    int GetQueueTaskCount() const { return 0; };
+    bool HasRunningTasks() const { return false; };
+    AsyncTaskPtr<void> OnEmpty() const { return CreateCompletedAsyncTask(); };
+    };
+
+END_BENTLEY_TASKS_NAMESPACE
