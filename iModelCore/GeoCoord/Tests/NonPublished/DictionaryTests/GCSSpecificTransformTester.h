c8d6725ed875fcef7f85b0c742c18eb8b50746e7
Alain.Robert <none@none>
2018-04-16 16:44:16 -0400

    EN - Augmentated Automated Unit Tests

--- iModelCore/GeoCoord/Tests/NonPublished/DictionaryTests/GCSSpecificTransformTester.h
+++ iModelCore/GeoCoord/Tests/NonPublished/DictionaryTests/GCSSpecificTransformTester.h
@@ -2,18 +2,32 @@
 //:>
 //:>     $Source: Tests/NonPublished/DictionaryTests/GCSSpecificTransformTester.h $
 //:>
-//:>  $Copyright: (c) 2016 Bentley Systems, Incorporated. All rights reserved. $
+//:>  $Copyright: (c) 2018 Bentley Systems, Incorporated. All rights reserved. $
 //:>
 //:>+--------------------------------------------------------------------------------------
 // Class : GCSSpecificTransformTester
 //-----------------------------------------------------------------------------
 
 #pragma once
-
+/*---------------------------------------------------------------------------------**//**
+* @bsi                                                   Alain.Robert  04/2018
++---------------+---------------+---------------+---------------+---------------+------*/
+struct conversionTest
+    {
+    WString m_sourceGCS;
+    WString m_targetGCS;
+    double  m_inputCoordinateX;
+    double  m_inputCoordinateY;
+    double  m_inputCoordinateZ;
+    double  m_outputCoordinateX;
+    double  m_outputCoordinateY;
+    double  m_outputCoordinateZ;
+    bool    m_linearUnit;
+    };
 
  
 // Preparation of required environement
-class GCSSpecificTransformTester : public ::testing::TestWithParam< WString >
+class GCSSpecificTransformTester : public ::testing::TestWithParam< conversionTest >
     {   
 
 public:
