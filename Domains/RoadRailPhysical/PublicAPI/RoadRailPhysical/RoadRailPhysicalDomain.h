c4ffd0aba13b2c46b21feb30db9c27322a411173
Carl Hinkle <carl.hinkle@bentley.com>
2018-04-18 08:29:20 -0400

    Docs

--- Domains/RoadRailPhysical/PublicAPI/RoadRailPhysical/RoadRailPhysicalDomain.h
+++ Domains/RoadRailPhysical/PublicAPI/RoadRailPhysical/RoadRailPhysicalDomain.h
@@ -21,20 +21,42 @@ struct RoadRailPhysicalDomain : Dgn::DgnDomain
 DOMAIN_DECLARE_MEMBERS(RoadRailPhysicalDomain, ROADRAILPHYSICAL_EXPORT)
 
 protected:
+    //! @private
     void _OnSchemaImported(Dgn::DgnDbR dgndb) const override;
 
 public:
+    //! @private
     RoadRailPhysicalDomain();
 
+    //! @private
     ROADRAILPHYSICAL_EXPORT static Dgn::DgnDbStatus SetUpModelHierarchy(Dgn::SubjectCR, Utf8CP physicalPartitionName);
+
+    //! @private
     ROADRAILPHYSICAL_EXPORT static Dgn::DgnViewId SetUpDefaultViews(Dgn::SubjectCR, Utf8CP alignmentPartitionName = nullptr, Utf8CP physicalPartitionName = nullptr,
         bvector<Dgn::DgnCategoryId> const* additionalCategoriesForSelector = nullptr);
+
+    //! Query for the physical model
+    //! @param[in] parentSubject The parent subject of the physical model with \p modelName
+    //! @param[in] modelName The name of the physical model in the \p parentSubject
     ROADRAILPHYSICAL_EXPORT static Dgn::PhysicalModelPtr QueryPhysicalModel(Dgn::SubjectCR parentSubject, Utf8CP modelName);
+
+    //! The name of the default Physical partition
     static Utf8CP GetDefaultPhysicalPartitionName() { return "Roads/Rail Physical"; }
+
+    //! The name of the default Standards Partition
     static Utf8CP GetDefaultStandardsPartitionName() { return "Roadway Standards"; }
 
+    //! @private
     ROADRAILPHYSICAL_EXPORT static Dgn::DgnDbStatus SetGeometricElementAsBoundingContentForSheet(Dgn::GeometricElementCR boundingElm, Dgn::Sheet::ElementCR sheet);
+    
+    //! Get the DgnElementIdSet containing the DgnElementIds of all of the NamedBoundaries that are the bounding elements for drawings in SheetModels
+    //! @param[in] dgnDb The DgnDb to search for bounding elements
+    //! @return DgnElementSet containing boundary DgnElementIds
     ROADRAILPHYSICAL_EXPORT static Dgn::DgnElementIdSet QueryElementIdsBoundingContentForSheets(Dgn::DgnDbCR dgnDb);
+
+    //! Get the DgnElementIdSet of any SheetModles that are bounded by the \p boundingElm.  
+    //! @param[in] A geometric element that bounds a SheetModel.  This can be obtained by getting the GeometricElement from the DgnElementIds returned by QueryElementIdsBoundingContentForSheets()
+    //! @return ElementIdSet containing all of the sheets that are physically located within the area defined by \p boundingElm
     ROADRAILPHYSICAL_EXPORT static Dgn::DgnElementIdSet QuerySheetIdsBoundedBy(Dgn::GeometricElementCR boundingElm);
 
 private:
