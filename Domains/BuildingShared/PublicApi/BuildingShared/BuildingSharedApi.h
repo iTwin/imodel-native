f3ebbb1a8d1446b7b630735f64bd9de298b04d8b
Jonas.Valiunas <none@none>
2018-04-27 10:49:29 +0300

    TFS#869272 WIP: add IBCSSerializable interface, load floors in floormanager

--- Domains/BuildingShared/PublicApi/BuildingShared/BuildingSharedApi.h
+++ Domains/BuildingShared/PublicApi/BuildingShared/BuildingSharedApi.h
@@ -8,6 +8,7 @@
 #pragma once
 
 #include "BuildingSharedMacros.h"
+#include "Interfaces.h"
 #include <BuildingShared/Units/UnitsApi.h>
 #include <BuildingShared/Utils/UtilsApi.h>
 #include <BuildingShared/DgnUtils/BuildingDgnUtilsApi.h>
