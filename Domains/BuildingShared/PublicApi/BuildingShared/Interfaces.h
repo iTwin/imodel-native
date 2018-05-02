8cf89ed675dcb9c6bb1990b0d34a07c1fe9375e9
Jonas.Valiunas <none@none>
2018-05-02 15:08:04 +0300

    TFS#869272 WIP:add element insertion/update handling, display names, fixes on findNodeById

---
+++ Domains/BuildingShared/PublicApi/BuildingShared/Interfaces.h
@@ -0,0 +1,37 @@
+/*--------------------------------------------------------------------------------------+
+|
+|     $Source: PublicApi/BuildingShared/Interfaces.h $
+|
+|  $Copyright: (c) 2018 Bentley Systems, Incorporated. All rights reserved. $
+|
++--------------------------------------------------------------------------------------*/
+#pragma once
+
+#include <DgnPlatform/DgnPlatformApi.h>
+#include "BuildingSharedMacros.h"
+
+BEGIN_BUILDING_SHARED_NAMESPACE
+
+
+#define BCSSERIALIZABLE_ELEMENT_ClassName                   "ClassName"
+#define BCSSERIALIZABLE_ELEMENT_ElementId                   "AllocationId"
+#define BCSSERIALIZABLE_ELEMENT_Name                        "Name"
+#define BCSSERIALIZABLE_ELEMENT_CodeValue                   "CodeValue"
+
+
+//=======================================================================================
+// @bsiclass                                     Jonas.Valiunas                 04/2018
+//=======================================================================================
+struct IBCSSerializable
+    {
+    public:
+        //! Serializes element data to a JSON object
+        //! @param[in]  elementData JSON object that will hold serialized data
+        virtual void SerializeProperties(Json::Value& elementData) const = 0;
+
+        //! Formats serialized element data in a JSON object
+        //! @param[in]  elementData JSON object that holds serialized data
+        virtual void FormatSerializedProperties(Json::Value& elementData) const = 0;
+    };
+
+END_BUILDING_SHARED_NAMESPACE
