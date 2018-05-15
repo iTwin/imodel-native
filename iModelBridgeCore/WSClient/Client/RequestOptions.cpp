60b507a8cfb2f95808c7962d5c261b989282c6a7
Julius.Senkus <none@none>
2018-05-15 11:25:39 +0300

    Added custom request options TFS#855762 SRB:Vincas.Razma

--- iModelBridgeCore/WSClient/Client/RequestOptions.cpp
+++ iModelBridgeCore/WSClient/Client/RequestOptions.cpp
@@ -2,13 +2,15 @@
  |
  |     $Source: Client/RequestOptions.cpp $
  |
- |  $Copyright: (c) 2017 Bentley Systems, Incorporated. All rights reserved. $
+ |  $Copyright: (c) 2018 Bentley Systems, Incorporated. All rights reserved. $
  |
  +--------------------------------------------------------------------------------------*/
 #include "ClientInternal.h"
 #include <WebServices/Client/RequestOptions.h>
 
 USING_NAMESPACE_BENTLEY_WEBSERVICES
+
+#define CUSTOM_REQUEST_PROPERTY_CustomOptions "CustomOptions"
 
 /*--------------------------------------------------------------------------------------+
 * @bsimethod
@@ -56,4 +58,9 @@ void RequestOptions::ToJson(JsonValueR jsonOut) const
     jsonOut["FailureStrategy"] = GetFailureStrategyStr(m_failureStrategy);
     jsonOut["ResponseContent"] = GetResponseContentStr(m_responseContent);
     jsonOut["RefreshInstances"] = m_refreshInstances;
+
+    if (!m_customRequestOptions.empty())
+        {
+        jsonOut[CUSTOM_REQUEST_PROPERTY_CustomOptions] = m_customRequestOptions;
+        }
     }
