d3d2d62b348c905b2f362d0eb7c31a629547299a
Jonas.Valiunas <none@none>
2018-05-16 13:38:58 +0300

    TFS#869272 WIP:make delete button work

--- Domains/BuildingShared/PublicApi/BuildingShared/Interfaces.h
+++ Domains/BuildingShared/PublicApi/BuildingShared/Interfaces.h
@@ -26,11 +26,15 @@ struct IBCSSerializable
     {
     public:
         //! Serializes element data to a JSON object
-        //! @param[in]  elementData JSON object that will hold serialized data
+        //! @param[in,out]  elementData JSON object that will hold serialized data
         virtual void SerializeProperties(Json::Value& elementData) const = 0;
+        
+        //! Updates element from Json properties
+        //! @param[in]  elementData JSON object that holds serialized data
+        virtual void UpdateFromJson(Json::Value const& elementData) = 0;
 
         //! Formats serialized element data in a JSON object
-        //! @param[in]  elementData JSON object that holds serialized data
+        //! @param[in, out]  elementData JSON object that holds serialized data
         virtual void FormatSerializedProperties(Json::Value& elementData) const = 0;
     };
 
