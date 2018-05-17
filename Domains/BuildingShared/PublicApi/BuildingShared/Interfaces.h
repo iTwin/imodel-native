d5d5d8e06846aedd15d43e0bb31f0abf29fe0753
Jonas.Valiunas <none@none>
2018-05-17 17:43:11 +0300

    TFS#869272 WIP:refactor the update/delete and other requests

--- Domains/BuildingShared/PublicApi/BuildingShared/Interfaces.h
+++ Domains/BuildingShared/PublicApi/BuildingShared/Interfaces.h
@@ -14,28 +14,57 @@ BEGIN_BUILDING_SHARED_NAMESPACE
 
 
 #define BCSSERIALIZABLE_ELEMENT_ClassName                   "ClassName"
+#define BCSSERIALIZABLE_ELEMENT_SchemaName                  "SchemaName"
 #define BCSSERIALIZABLE_ELEMENT_ElementId                   "AllocationId"
 #define BCSSERIALIZABLE_ELEMENT_Name                        "Name"
 #define BCSSERIALIZABLE_ELEMENT_CodeValue                   "CodeValue"
 
+
+#define BCSJSONACTIONPERFORMER_ACTION                       "Action"
+#define BCSJSONACTIONPERFORMER_ACTIONPARAMS                 "Parameters"
+
+#define BCSJSONACTIONPERFORMER_ACTION_UPDATE                "Update"
+#define BCSJSONACTIONPERFORMER_ACTION_DELETE                "Delete"
+
+//=======================================================================================
+// @bsiclass                                     Jonas.Valiunas                 05/2018
+//=======================================================================================
+struct IBCSJsonActionPerformer
+    {
+    protected:
+    
+        //! performs action based on the json message
+        //! @param[in]  actionData JSON object that holds serialized action data
+        virtual void _PerformJsonAction(Json::Value const& actionData) = 0;
+
+    public:
+        //! performs action based on the json message
+        //! @param[in]  actionData JSON object that holds serialized action data
+        void PerformJsonAction(Json::Value const& actionData) { _PerformJsonAction(actionData); };
+    };
 
 //=======================================================================================
 // @bsiclass                                     Jonas.Valiunas                 04/2018
 //=======================================================================================
 struct IBCSSerializable
     {
-    public:
+    protected:
         //! Serializes element data to a JSON object
         //! @param[in,out]  elementData JSON object that will hold serialized data
-        virtual void SerializeProperties(Json::Value& elementData) const = 0;
-        
-        //! Updates element from Json properties
-        //! @param[in]  elementData JSON object that holds serialized data
-        virtual void UpdateFromJson(Json::Value const& elementData) = 0;
+        virtual void _SerializeProperties(Json::Value& elementData) const = 0;
 
         //! Formats serialized element data in a JSON object
         //! @param[in, out]  elementData JSON object that holds serialized data
-        virtual void FormatSerializedProperties(Json::Value& elementData) const = 0;
+        virtual void _FormatSerializedProperties(Json::Value& elementData) const = 0;
+
+    public:
+        //! Serializes element data to a JSON object
+        //! @param[in,out]  elementData JSON object that will hold serialized data
+        void SerializeProperties(Json::Value& elementData) const { _SerializeProperties(elementData);};
+
+        //! Formats serialized element data in a JSON object
+        //! @param[in, out]  elementData JSON object that holds serialized data
+        void FormatSerializedProperties(Json::Value& elementData) const { _FormatSerializedProperties(elementData);};
     };
 
 END_BUILDING_SHARED_NAMESPACE
