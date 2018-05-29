74d239469f49802c2742565675fa1f40263672f2
Taslim.Murad <none@none>
2018-05-29 12:52:33 +0500

    TFS#897731  added multi aspects element performance tests

--- iModelCore/DgnPlatform/Tests/DgnProject/Performance/ElementAspectPerformance.cpp
+++ iModelCore/DgnPlatform/Tests/DgnProject/Performance/ElementAspectPerformance.cpp
@@ -55,7 +55,7 @@ void ElementAspectTests1::LogTiming (StopWatch& timer, Utf8CP description, Utf8C
 /*=================================================================================**//**
 * @bsiclass                                                     Taslim.Murad      05/18
 +===============+===============+===============+===============+===============+======*/
-TEST_F (ElementAspectTests1, UniqueAspectPerformance_AspectInsert)
+TEST_F (ElementAspectTests1, UniqueAspectPerformance_Insert)
 {
     SetupSeedProject ();
     ECN::ECClassCR aclass = *TestUniqueAspect::GetECClass (*m_db);
@@ -73,13 +73,13 @@ TEST_F (ElementAspectTests1, UniqueAspectPerformance_AspectInsert)
         ASSERT_TRUE ((e2[i]).IsValid ());
     }
     timer1.Stop ();
-    LogTiming (timer1, "Insert Element With Aspect", "TestElement", false, 0, count);
+    LogTiming (timer1, "Insert Element With a Unique Aspect", "TestElement", false, 0, count);
 }
 
 /*=================================================================================**//**
 * @bsiclass                                                     Taslim.Murad      05/18
 +===============+===============+===============+===============+===============+======*/
-TEST_F (ElementAspectTests1, UniqueAspectPerformance_SimpleInsert)
+TEST_F (ElementAspectTests1, SimpleElementPerformance_Insert)
 {
     SetupSeedProject ();
     ECN::ECClassCR aclass = *TestUniqueAspect::GetECClass (*m_db);
@@ -96,7 +96,7 @@ TEST_F (ElementAspectTests1, UniqueAspectPerformance_SimpleInsert)
         ASSERT_TRUE ((el[i]).IsValid ());
     }
     timer.Stop ();
-    LogTiming (timer, "Insert Element Without Aspect", "TestElement", false, 0, count);
+    LogTiming (timer, "Insert Simple Element Without Aspect", "TestElement", false, 0, count);
 }
 
 
@@ -104,7 +104,7 @@ TEST_F (ElementAspectTests1, UniqueAspectPerformance_SimpleInsert)
 /*=================================================================================**//**
 * @bsiclass                                                     Taslim.Murad      05/18
 +===============+===============+===============+===============+===============+======*/
-TEST_F (ElementAspectTests1, UniqueAspectPerformance_SimpleUpdate)
+TEST_F (ElementAspectTests1, SimpleElementPerformance_Update)
 {
     SetupSeedProject ();
     ECN::ECClassCR aclass = *TestUniqueAspect::GetECClass (*m_db);
@@ -128,13 +128,13 @@ TEST_F (ElementAspectTests1, UniqueAspectPerformance_SimpleUpdate)
         tempEl[i]->Update (&status);
     }
     timer1.Stop ();
-    LogTiming (timer1, "Update Element Without Aspect", "TestElement", false, count, count);
+    LogTiming (timer1, "Update Simple Element Without Aspect", "TestElement", false, count, count);
 }
 
 /*=================================================================================**//**
 * @bsiclass                                                     Taslim.Murad      05/18
  +===============+===============+===============+===============+===============+======*/
-TEST_F (ElementAspectTests1, UniqueAspectPerformance_AspectUpdate)
+TEST_F (ElementAspectTests1, UniqueAspectPerformance_Update)
 {
     SetupSeedProject ();
     ECN::ECClassCR aclass = *TestUniqueAspect::GetECClass (*m_db);
@@ -164,14 +164,14 @@ TEST_F (ElementAspectTests1, UniqueAspectPerformance_AspectUpdate)
         ASSERT_TRUE (m_db->Elements ().Update (*tempE3[i]).IsValid ());
     }
     timer1.Stop ();
-    LogTiming (timer1, "Update Element With Aspect", "TestElement2", false, count, count);
+    LogTiming (timer1, "Update Element With a Unique Aspect", "TestElement2", false, count, count);
 }
 
 
 /*=================================================================================**//**
 * @bsiclass                                                     Taslim.Murad      05/18
 +===============+===============+===============+===============+===============+======*/
-TEST_F (ElementAspectTests1, UniqueAspectPerformance_SimpleDelete)
+TEST_F (ElementAspectTests1, SimpleElementPerformance_Delete)
 {
     SetupSeedProject ();
     ECN::ECClassCR aclass = *TestUniqueAspect::GetECClass (*m_db);
@@ -193,13 +193,13 @@ TEST_F (ElementAspectTests1, UniqueAspectPerformance_SimpleDelete)
         tempEl[i]->Delete ();
     }
     timer.Stop ();
-    LogTiming (timer, "Delete Element Without Aspect", "TestElement", false, count, count);
+    LogTiming (timer, "Delete Simple Element Without any Aspect", "TestElement", false, count, count);
 }
 
 /*=================================================================================**//**
 * @bsiclass                                                     Taslim.Murad      05/18
 +===============+===============+===============+===============+===============+======*/
-TEST_F (ElementAspectTests1, UniqueAspectPerformance_AspectDelete)
+TEST_F (ElementAspectTests1, UniqueAspectPerformance_Delete)
 {
     SetupSeedProject ();
     ECN::ECClassCR aclass = *TestUniqueAspect::GetECClass (*m_db);
@@ -221,5 +221,117 @@ TEST_F (ElementAspectTests1, UniqueAspectPerformance_AspectDelete)
         tempEl[i]->Delete ();
     }
     timer1.Stop ();
-    LogTiming (timer1, "Delete Element With Aspect", "TestElement", false, count, count);
+    LogTiming (timer1, "Delete Element With a Unique Aspect", "TestElement", false, count, count);
+}
+
+/*=================================================================================**//**
+* @bsiclass                                                     Taslim.Murad      05/18
++===============+===============+===============+===============+===============+======*/
+TEST_F (ElementAspectTests1, MultiAspectPerformance_Insert)
+{
+    SetupSeedProject ();
+    ECN::ECClassCR aclass = *TestMultiAspect::GetECClass (*m_db);
+    TestElementCPtr el;
+    int const count = 1000;
+    TestMultiAspectPtr a1[count];
+    EC::ECInstanceId a1id[count];
+
+    //  Insert an element with a multi aspect...
+    StopWatch timer1 (true);
+    TestElementPtr tempEl = TestElement::Create (*m_db, m_defaultModelId, m_defaultCategoryId, "TestElement");
+    for (int i = 1; i < count; i++)
+    {
+        a1[i] = TestMultiAspect::Create ("test");
+        DgnElement::MultiAspect::AddAspect (*tempEl, *(a1[i]));
+    }
+    el = m_db->Elements ().Insert (*tempEl);
+    ASSERT_TRUE (el.IsValid ());
+    timer1.Stop ();
+    LogTiming (timer1, "Insert an Element With Multi Aspect " , "TestElement", false, 0, 1);
+
+    for (int i = 1; i < count; i++)
+    {
+        a1id[i] = a1[i]->GetAspectInstanceId ();
+        ASSERT_TRUE (a1id[i].IsValid ());
+    }
+}
+
+/*=================================================================================**//**
+* @bsiclass                                                     Taslim.Murad      05/18
++===============+===============+===============+===============+===============+======*/
+TEST_F (ElementAspectTests1, MultiAspectPerformance_Update)
+{
+    SetupSeedProject ();
+    ECN::ECClassCR aclass = *TestMultiAspect::GetECClass (*m_db);
+    TestElementCPtr el;
+    int const count = 1000;
+    TestMultiAspectPtr a1[count];
+    EC::ECInstanceId a1id[count];
+
+    TestElementPtr tempEl = TestElement::Create (*m_db, m_defaultModelId, m_defaultCategoryId, "TestElement");
+    for (int i = 1; i < count; i++)
+    {
+        a1[i] = TestMultiAspect::Create ("test");
+        DgnElement::MultiAspect::AddAspect (*tempEl, *(a1[i]));
+    }
+    el = m_db->Elements ().Insert (*tempEl);
+    ASSERT_TRUE (el.IsValid ());
+    for (int i = 1; i < count; i++)
+    {
+        a1id[i] = a1[i]->GetAspectInstanceId ();
+        ASSERT_TRUE (a1id[i].IsValid ());
+    }
+
+    TestElementPtr tempE2 = el->MakeCopy<TestElement> ();
+    TestMultiAspectP aspect[count];
+    for (int i = 1; i < count; i++)
+    {
+         aspect[i] = DgnElement::MultiAspect::GetP<TestMultiAspect> (*tempE2, aclass, a1id[i]);
+         TestMultiAspectCP aspectPersist = DgnElement::MultiAspect::Get<TestMultiAspect> (*el, aclass, a1id[i]);
+         ASSERT_TRUE (aspectPersist != nullptr);
+         ASSERT_EQ (aspectPersist, DgnElement::MultiAspect::Get<TestMultiAspect> (*el, aclass, a1id[i]));
+         ASSERT_EQ (aspect[i], DgnElement::MultiAspect::GetP<TestMultiAspect> (*tempE2, aclass, a1id[i]));
+        
+    }
+
+    StopWatch timer1 (true);
+    for (int i = 1; i < count; i++)
+    {
+        aspect[i]->SetTestMultiAspectProperty ("updated");
+    }
+    ASSERT_TRUE (m_db->Elements ().Update (*tempE2).IsValid ());
+    timer1.Stop ();
+    LogTiming (timer1, "Update an Element With Multiple Aspect", "TestElement", false, 1, 1);
+}
+
+/*=================================================================================**//**
+* @bsiclass                                                     Taslim.Murad      05/18
++===============+===============+===============+===============+===============+======*/
+TEST_F (ElementAspectTests1, MultiAspectPerformance_Delete)
+{
+    SetupSeedProject ();
+    ECN::ECClassCR aclass = *TestMultiAspect::GetECClass (*m_db);
+    TestElementCPtr el;
+    int const count = 1000;
+    TestMultiAspectPtr a1[count];
+    EC::ECInstanceId a1id[count];
+
+    TestElementPtr tempEl = TestElement::Create (*m_db, m_defaultModelId, m_defaultCategoryId, "TestElement");
+    for (int i = 1; i < count; i++)
+    {
+        a1[i] = TestMultiAspect::Create ("test");
+        DgnElement::MultiAspect::AddAspect (*tempEl, *(a1[i]));
+    }
+    el = m_db->Elements ().Insert (*tempEl);
+    ASSERT_TRUE (el.IsValid ());
+    for (int i = 1; i < count; i++)
+    {
+        a1id[i] = a1[i]->GetAspectInstanceId ();
+        ASSERT_TRUE (a1id[i].IsValid ());
+    }
+
+    StopWatch timer1 (true);
+    tempEl->Delete ();
+    timer1.Stop ();
+    LogTiming (timer1, "Delete an Element With Multi Aspects", "TestElement", false, 1, 1);
 }
