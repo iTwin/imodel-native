8828d1f50f183fc6246c4e30a3e96b984537f247
Martynas.Saulius <none@none>
2018-06-01 16:19:35 +0300

    TFS#896007: Removed Duplicate code from ClassificationSystems and Grids

--- Domains/ClassificationSystems/Tests/NonPublished/ClassificationSystemsTestsBase.h
+++ Domains/ClassificationSystems/Tests/NonPublished/ClassificationSystemsTestsBase.h
@@ -4,39 +4,12 @@
 #include <DgnPlatform/UnitTests/ScopedDgnHost.h>
 #include <DgnClientFx/TestHelpers/DgnClientFxTests.h>
 #include <UnitTests/BackDoor/DgnPlatform/DgnPlatformTestDomain.h>
-#include <DgnPlatform/UnitTests/RepositoryManagerUtil.h>
+#include <BuildingShared\SharedRepositoryManagerTest.h>
 
 
 BEGIN_CLASSIFICATIONSYSTEMS_NAMESPACE
 
-
-struct MyTestApp : DgnClientFx::DgnClientApp, Dgn::DgnPlatformLib::Host::RepositoryAdmin
-    {
-    protected:
-        mutable Dgn::TestRepositoryManager   m_server;
-        Dgn::IRepositoryManagerP _GetRepositoryManager(Dgn::DgnDbR) const override { return &m_server; }
-
-    };
-
-/*---------------------------------------------------------------------------------**//**
-* @bsistruct                                                    Martynas.Saulius   05/18
-+---------------+---------------+---------------+---------------+---------------+------*/
-struct RepositoryManagerTest : public BentleyApi::DgnClientFx::TestHelpers::DgnClientFxTests
-    {
-    public:
-        typedef Dgn::IRepositoryManager::Request Request;
-        typedef Dgn::IRepositoryManager::Response Response;
-        typedef Dgn::IBriefcaseManager::ResponseOptions ResponseOptions;
-
-
-        DgnClientFx::DgnClientApp* _CreateApp() override { return new MyTestApp(); }
-
-        void ClearRevisions(Dgn::DgnDbR db);
-
-    };
-
-
-struct ClassificationSystemsTestsBase : RepositoryManagerTest
+struct ClassificationSystemsTestsBase : BENTLEY_BUILDING_SHARED_NAMESPACE_NAME::SharedRepositoryManagerTest
     {
     private:
         static BeFileName GetSeedProjectPath();
@@ -44,7 +17,6 @@ struct ClassificationSystemsTestsBase : RepositoryManagerTest
         static void SetUpTestCase();
         virtual void SetUp() override;
         virtual void TearDown() override;
-        DgnClientFx::DgnClientApp* _CreateApp() override { return new DgnClientFx::DgnClientApp(); }
         BeSQLite::L10N::SqlangFiles _GetApplicationSqlangs() override;
     public:
         BeFileName GetProjectPath() const;
