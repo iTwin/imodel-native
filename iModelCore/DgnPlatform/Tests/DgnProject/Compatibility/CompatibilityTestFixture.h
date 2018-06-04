e02153349ae55b8f0130ca9312acb38abe99b396
Krischan.Eberle <none@none>
2018-06-04 18:47:05 +0200

    [Compatibility test framework] * Added helper methods to create test files * Separate test fixture for creating test files * Finished testing of pre EC32 enums

--- iModelCore/DgnPlatform/Tests/DgnProject/Compatibility/CompatibilityTestFixture.h
+++ iModelCore/DgnPlatform/Tests/DgnProject/Compatibility/CompatibilityTestFixture.h
@@ -15,6 +15,8 @@
 #include <Bentley/BeTimeUtilities.h>
 #include <UnitTests/BackDoor/DgnPlatform/DgnDbTestUtils.h>
 #include <UnitTests/BackDoor/DgnPlatform/ScopedDgnHost.h>
+#include <json/json.h>
+#include <ostream>
 
 #define PROFILE_NAME_BEDB "bedb"
 #define PROFILE_NAME_ECDB "ecdb"
@@ -207,7 +209,20 @@ struct ProfileManager final : NonCopyableClass
 
         Profile& GetProfile(ProfileType) const;
     };
-//
+
+//=======================================================================================    
+// @bsiclass                                   Krischan.Eberle                  06/18
+//=======================================================================================    
+struct SchemaItem final
+    {
+    private:
+        Utf8String m_xmlString;
+
+    public:
+        explicit SchemaItem(Utf8StringCR xmlString) : m_xmlString(xmlString) {}
+        Utf8StringCR GetXml() const { return m_xmlString; }
+    };
+
 //======================================================================================
 // @bsiclass                                                 Affan.Khan          03/2018
 //======================================================================================
@@ -215,4 +230,55 @@ struct CompatibilityTestFixture : ::testing::Test
     {
 protected:
     ProfileManager& ProfileManager() const { return ProfileManager::Get(); }
+    static ECN::ECSchemaReadContextPtr DeserializeSchemas(ECDbCR, std::vector<SchemaItem> const&);
+    static ECN::ECSchemaReadContextPtr DeserializeSchema(ECDbCR ecdb, SchemaItem const& schema) { return DeserializeSchemas(ecdb, {schema}); }
+    };
+
+
+//=======================================================================================
+// Wrapper for JsonCpp that allows for easy use in GTest macros
+// @bsiclass                                                 Krischan.Eberle     06/18
+//=======================================================================================    
+struct JsonValue final
+    {
+    public:
+        Json::Value m_value = Json::Value(Json::nullValue);
+
+        JsonValue() {}
+        explicit JsonValue(Json::ValueType type) : m_value(type) {}
+        explicit JsonValue(JsonValueCR json) : m_value(json) {}
+        explicit JsonValue(Utf8CP json);
+        explicit JsonValue(Utf8StringCR json) : JsonValue(json.c_str()) {}
+
+        bool operator==(JsonValue const& rhs) const;
+        bool operator!=(JsonValue const& rhs) const { return !(*this == rhs); }
+
+        Utf8String ToString() const { return m_value.ToString(); }
+    };
+
+void PrintTo(JsonValue const&, std::ostream*);
+
+//=======================================================================================
+//! Disables "Fail On Assertion" for the lifetime of this object.
+//! Use this helper classes instead of BeTest::SetFailOnAssert as it automatically resets
+//! the FailOnAssert state when the ScopedDisableFailOnAssertion object goes out of scope
+// @bsiclass                                               Krischan.Eberle     06/2018
+//=======================================================================================    
+struct ScopedDisableFailOnAssertion final
+    {
+    private:
+        bool m_isNoop = false;
+
+    public:
+        explicit ScopedDisableFailOnAssertion(bool disable = true) : m_isNoop(!BeTest::GetFailOnAssert() || !disable)
+            {
+            if (!m_isNoop)
+                BeTest::SetFailOnAssert(false);
+            }
+
+        ~ScopedDisableFailOnAssertion()
+            {
+            if (!m_isNoop)
+                BeTest::SetFailOnAssert(true);
+            }
     };
