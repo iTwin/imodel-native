23b06b747edbcb91404a0e4907a5d783c6e1ea4b
Martynas.Saulius <none@none>
2018-06-04 17:31:16 +0300

    TFS#896007:Moved CreateSeedDb to SharedRepositoryManagerTestBase, and added RegisterDomain methods

--- Domains/Grids/Tests/NonPublished/GridsTestFixtureBase.cpp
+++ Domains/Grids/Tests/NonPublished/GridsTestFixtureBase.cpp
@@ -17,128 +17,37 @@
 #include "GridsTestFixtureBase.h"
 
 USING_NAMESPACE_BENTLEY_DGN
-USING_NAMESPACE_BENTLEY_SQLITE
 USING_NAMESPACE_GRIDS
 USING_NAMESPACE_BENTLEY_DGNCLIENTFX
-
-
-
+USING_NAMESPACE_BUILDING_SHARED
 #define PROJECT_SEED_NAME       L"BuildingTests/BuildingTestSeed.bim"
-#define PROJECT_DEFAULT_NAME    L"BuildingTests/Default.bim"
-#define EMPTY_DGNDB             L"DgnDb\\empty.bim"
-
-//--------------------------------------------------------------------------------------
-// @bsimethod                                    Haroldas.Vitunskas              05/2017
-//---------------+---------------+---------------+---------------+---------------+------
-void CreatSeedDb (WCharCP seedFileName)
-    {
-    DgnDbPtr dgnDbPtr;
-    ScopedDgnHost host;
-    {
-    BeFileName  temporaryDir;
-
-    PhysicalModelPtr siteModel;
-    DgnViewId viewId;
-
-    DgnClientUi::InitializeBeJsEngine ();
-    BeFileName applicationSchemaDir;
-    BeTest::GetHost ().GetDgnPlatformAssetsDirectory (applicationSchemaDir);
-
-    BeTest::GetHost ().GetOutputRoot (temporaryDir);
-
-    BeSQLite::EC::ECDb::Initialize (temporaryDir, &applicationSchemaDir);
-
-    temporaryDir.AppendToPath (L"BuildingTests");
-    BeFileName::CreateNewDirectory (temporaryDir);
 
 
-    ASSERT_TRUE (SUCCESS == DgnDomains::RegisterDomain (Grids::GridsDomain::GetDomain (), DgnDomain::Required::Yes)) << "Failed to register Grids domain";
-
-    dgnDbPtr = DgnDbTestUtils::CreateSeedDb (seedFileName);
-    ASSERT_TRUE (dgnDbPtr.IsValid ());
-
-    dgnDbPtr->GeoLocation ().SetProjectExtents (AxisAlignedBox3d (DPoint3d::From (-500.0, -500.0, 0.0), DPoint3d::From (500.0, 500.0, 500.0)));
-
-    dgnDbPtr->SetAsBriefcase(BeBriefcaseId(2));
-    dgnDbPtr->SaveChanges ();
-    dgnDbPtr->SaveSettings ();
+//---------------------------------------------------------------------------------------
+// @bsimethod                                   Martynas.Saulius                06/18
+//---------------------------------------------------------------------------------------
+void StaticRegisterDomains() {
+    DgnDomains::RegisterDomain(Grids::GridsDomain::GetDomain(), DgnDomain::Required::Yes);
     }
-    dgnDbPtr->CloseDb();
-
-    Dgn::T_HOST.GetScriptAdmin ().TerminateOnThread ();
-    }
-
 //---------------------------------------------------------------------------------------
-// @bsimethod                                   Grigas.Petraitis                07/17
+// @bsimethod                                   Martynas.Saulius                06/18
 //---------------------------------------------------------------------------------------
-BeFileName GridsTestFixtureBase::GetSeedProjectPath ()
+void GridsTestFixtureBase::RegisterDomains() 
     {
-    BeFileName seedPath;
-    BeTest::GetHost ().GetOutputRoot (seedPath);
-    seedPath.AppendToPath (PROJECT_SEED_NAME);
-    return seedPath;
+    StaticRegisterDomains();
     }
-
 //---------------------------------------------------------------------------------------
-// @bsimethod                                   Grigas.Petraitis                07/17
+// @bsimethod                                   Martynas.Saulius                06/18
 //---------------------------------------------------------------------------------------
-BeFileName GridsTestFixtureBase::GetProjectPath () const
+void GridsTestFixtureBase::SetUpTestCase()
     {
-    BeFileName projectPath;
-    BeTest::GetHost ().GetOutputRoot (projectPath);
-    projectPath.AppendToPath (PROJECT_DEFAULT_NAME);
-    return projectPath;
-    }
-
-//---------------------------------------------------------------------------------------
-// @bsimethod                                   Grigas.Petraitis                05/12
-//---------------------------------------------------------------------------------------
-void GridsTestFixtureBase::SetUpTestCase ()
-    {
-    BeFileName seedPath = GetSeedProjectPath ();
+    BeFileName seedPath = SharedRepositoryManagerTestBase::GetSeedProjectPath();
     BeFileName outPath;
-    BeTest::GetHost ().GetOutputRoot (outPath);
-    BeFileName::CreateNewDirectory (outPath);
-    seedPath.BeDeleteFile ();
-    CreatSeedDb (PROJECT_SEED_NAME);
+    BeTest::GetHost().GetOutputRoot(outPath);
+    BeFileName::CreateNewDirectory(outPath);
+    seedPath.BeDeleteFile();
+    CreateSeedDb(PROJECT_SEED_NAME, &StaticRegisterDomains);
     }
 
-//---------------------------------------------------------------------------------------
-// @bsimethod                                   Grigas.Petraitis                05/12
-//---------------------------------------------------------------------------------------
-void GridsTestFixtureBase::SetUp ()
-    {
-    BentleyApi::DgnClientFx::TestHelpers::DgnClientFxTests::SetUp ();
-    BeFileName projectPath = GetProjectPath ();
-    projectPath.BeDeleteFile ();
-    BeFileName::BeCopyFile (GetSeedProjectPath (), projectPath.c_str (), true);
-
-    DgnDomains::RegisterDomain (Grids::GridsDomain::GetDomain (), DgnDomain::Required::Yes);
-
-    EXPECT_EQ (BE_SQLITE_OK, DgnClientUi::OpenDgnDb (projectPath, DgnDb::OpenParams (Db::OpenMode::ReadWrite, DefaultTxn::Yes)));
-    DgnDbR db = *DgnClientFx::DgnClientApp::App ().Project ();
-    ASSERT_TRUE (db.IsBriefcase ());
-    ASSERT_TRUE (db.Txns ().IsTracking ());
-    }
-
-//---------------------------------------------------------------------------------------
-// @bsimethod                                   Grigas.Petraitis                05/12
-//---------------------------------------------------------------------------------------
-void GridsTestFixtureBase::TearDown ()
-    {
-    BentleyApi::DgnClientFx::TestHelpers::DgnClientFxTests::TearDown ();
-    }
-
-//---------------------------------------------------------------------------------------
-// @bsimethod                                   Grigas.Petraitis                07/17
-//---------------------------------------------------------------------------------------
-L10N::SqlangFiles GridsTestFixtureBase::_GetApplicationSqlangs ()
-    {
-    BeFileName documentsRoot;
-    BeTest::GetHost ().GetDocumentsRoot (documentsRoot);
-
-    BeFileName sqlangDbPath = BeFileName (documentsRoot).AppendToPath (L"../sqlang/DgnClientFx_pseudo.sqlang.db3");
-    return L10N::SqlangFiles (sqlangDbPath);
-    }
 
 
--- Domains/Grids/Tests/NonPublished/GridsTestFixtureBase.h
+++ Domains/Grids/Tests/NonPublished/GridsTestFixtureBase.h
@@ -10,30 +10,21 @@
 #include <DgnPlatform\DgnPlatformApi.h>
 #include <DgnPlatform/UnitTests/ScopedDgnHost.h>
 #include <DgnClientFx/TestHelpers/DgnClientFxTests.h>
-//#include "DgnHandlersTests.h"
 #include <Bentley/BeTest.h>
 #include <DgnPlatform/DgnMaterial.h>
 #include <UnitTests/BackDoor/DgnPlatform/DgnPlatformTestDomain.h>
-#include <BuildingShared\SharedRepositoryManagerTest.h>
+#include <BuildingShared\SharedRepositoryManagerTestBase.h>
 
 
 #define POINTS_MATCH_TOLERANCE 1.0E-7    //1mm
 
 BEGIN_GRIDS_NAMESPACE
 
-struct GridsTestFixtureBase : BENTLEY_BUILDING_SHARED_NAMESPACE_NAME::SharedRepositoryManagerTest
+struct GridsTestFixtureBase : BENTLEY_BUILDING_SHARED_NAMESPACE_NAME::SharedRepositoryManagerTestBase
     {
-    private:
-        static BeFileName GetSeedProjectPath ();
-    public:
-        static void SetUpTestCase ();
-        virtual void SetUp () override;
-        virtual void TearDown () override;
-        BeSQLite::L10N::SqlangFiles _GetApplicationSqlangs () override;
-    public:
-        BeFileName GetProjectPath () const;
+    public:   
+        virtual void RegisterDomains() override;
+        static void SetUpTestCase();
     };
-
-
 
 END_GRIDS_NAMESPACE
