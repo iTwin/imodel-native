53a61fe52afcdd52aae35947ba617609b606ceb5
Martynas.Saulius <none@none>
2018-06-04 17:25:03 +0300

    TFS#896007:Moved CreateSeedDb and added function pass as parameter for DomainRegisters

--- Domains/BuildingShared/TestUtils/SharedRepositoryManagerTest/SharedRepositoryManagerTest.h
+++ Domains/BuildingShared/TestUtils/SharedRepositoryManagerTest/SharedRepositoryManagerTest.h
@@ -6,6 +6,7 @@
 #include <DgnClientFx/TestHelpers/DgnClientFxTests.h>
 #include <Bentley/BeTest.h>
 
+
 BEGIN_BUILDING_SHARED_NAMESPACE
 
 /*---------------------------------------------------------------------------------**//**
---
+++ Domains/BuildingShared/TestUtils/SharedRepositoryManagerTest/SharedRepositoryManagerTestBase.h
@@ -0,0 +1,27 @@
+#include <BuildingShared/BuildingSharedApi.h>
+#include <DgnPlatform\DgnPlatformApi.h>
+#include "SharedRepositoryManagerTest.h"
+#include <UnitTests/BackDoor/DgnPlatform/DgnPlatformTestDomain.h>
+#include <DgnPlatform/UnitTests/RepositoryManagerUtil.h>
+#include <BuildingShared/BuildingSharedApi.h>
+#include <DgnPlatform\DgnPlatformApi.h>
+#include <DgnPlatform/UnitTests/ScopedDgnHost.h>
+#include <DgnClientFx/TestHelpers/DgnClientFxTests.h>
+#include <Bentley/BeTest.h>
+
+BEGIN_BUILDING_SHARED_NAMESPACE
+
+struct SharedRepositoryManagerTestBase : BENTLEY_BUILDING_SHARED_NAMESPACE_NAME::SharedRepositoryManagerTest
+    {
+        protected:
+            static BeFileName GetSeedProjectPath();
+        public:
+            static void CreateSeedDb(WCharCP seedFileName, void (*RegisterDomainFunction)());
+            virtual void RegisterDomains() = 0;
+            virtual void SetUp() override;
+            virtual void TearDown() override;
+            BeFileName GetProjectPath() const;
+            BeSQLite::L10N::SqlangFiles _GetApplicationSqlangs() override;
+    };
+
+END_BUILDING_SHARED_NAMESPACE
