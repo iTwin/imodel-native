703628c91d30aa2f536b6a9f714aa1c9ec56991a
Sam.Wilson <Sam.Wilson@Bentley.com>
2018-06-11 11:42:00 -0400

    WIP build imodel-bank addon

---
+++ imodel-bank.messages.h
@@ -0,0 +1,13 @@
+/*--------------------------------------------------------------------------------------+
+|
+|     $Source: PrivateApi/DgnPlatformInternal/DgnCore/DgnCoreL10N.h $
+|
+|  $Copyright: (c) 2016 Bentley Systems, Incorporated. All rights reserved. $
+|
++--------------------------------------------------------------------------------------*/
+#pragma once
+#include <BeSQLite/L10N.h>
+
+BENTLEY_TRANSLATABLE_STRINGS_START(imodel_bank_l10n,imodel_bank_msg)
+    L10N_STRING(Title)                                         // =="iModelBank"==
+BENTLEY_TRANSLATABLE_STRINGS_END
