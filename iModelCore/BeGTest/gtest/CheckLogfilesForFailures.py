c461c9387bcccef2142e625c6216566e684e546c
Jeff.Marker <none@none>
2018-06-13 11:38:04 -0400

    HACK... CheckLogfilesForFailures.py importing IgnoreFilters.py causes python to write a PYC file into the build context during a DgnClientSdk build, which is not allowed. The quick hack is to copy/paste the one function in IgnoreFilters until a better solution can be found.

--- iModelCore/BeGTest/gtest/CheckLogfilesForFailures.py
+++ iModelCore/BeGTest/gtest/CheckLogfilesForFailures.py
@@ -16,7 +16,45 @@ runpat = re.compile (r"RUN\s*]\s*(\w+\.\w+)", re.I)
 
 filedir=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(filedir)
-import IgnoreFilters as filters
+
+# HACK is to copy/paste this function until we can figure out why it generates a PYC file in the build context.
+# import IgnoreFilters as filters
+#-------------------------------------------------------------------------------------------
+# bsimethod                                     Ridha.Malik      06/2018
+#-------------------------------------------------------------------------------------------
+def PreventAutoIgnoreTests(FailingTestsList):
+    filedir=os.path.dirname(os.path.abspath(__file__))
+    filepath=os.path.join(filedir,"IgnoreFilters.txt")
+    filteredFile = open (filepath, 'r')
+    lines = filteredFile.readlines()
+    filteredlist=[]
+    for filteredline in lines:
+        # strip comments
+        startComment = filteredline.find ('#')
+        if startComment != -1 :
+            filteredline = filteredline[0:startComment]
+            # strip leading and trailing blanks
+            filteredline = filteredline.strip()
+            # see if the line identifies anything other than performance tests (which we always suppress)
+            if len(filteredline) == 0:
+                continue
+            if filteredline.lower().find ('performance') != -1:
+                continue
+            filteredlist.append(filteredline)
+        else:
+            if not filteredline.strip():continue
+            filteredline=filteredline.strip()
+            filteredlist.append(filteredline)
+    for filters in filteredlist:
+        for failingtest in FailingTestsList:
+            if filters.startswith("*") and filters.endswith("*"):
+               filters=filters.replace("*",'')
+            pattern=re.compile(filters)
+            testobj=re.search(pattern,failingtest)
+            if testobj != None:
+                print "*** compatibility test failed",failingtest
+                return 1
+    return 0
 
 #-------------------------------------------------------------------------------------------
 # bsimethod                                     Jeff.Marker
