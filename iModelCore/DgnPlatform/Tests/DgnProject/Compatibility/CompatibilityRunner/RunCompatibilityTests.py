3f8a223ee2636283a4e5cce6c753df4c403c6690
Krischan.Eberle <none@none>
2018-06-26 13:01:33 +0200

    [Compatibility tests] Enhanced output of RunCompatibilityTests.py script.

--- iModelCore/DgnPlatform/Tests/DgnProject/Compatibility/CompatibilityRunner/RunCompatibilityTests.py
+++ iModelCore/DgnPlatform/Tests/DgnProject/Compatibility/CompatibilityRunner/RunCompatibilityTests.py
@@ -16,14 +16,23 @@ from shutil import rmtree
 def main():
     if len(sys.argv) < 2:
         print "Must give the test root"
-        return
-    
+        return 1
+        
+    hasError = False
     testRoot = sys.argv[1]
 
     for subdir in os.listdir(testRoot):
         fullPath = os.path.join(testRoot, subdir);
         if (os.path.isdir(fullPath) and subdir != "Assets"):
-            subprocess.check_call(os.path.join(fullPath, "iModelSchemaEvolution.exe"))
+            try:
+                subprocess.check_call(os.path.join(fullPath, "iModelSchemaEvolution.exe"))
+                print "Compatibility test runner for '" + subdir + "' succeeded."
+            except subprocess.CalledProcessError as err:
+                print >> sys.stderr, "Compatibility test runner for '{0}' failed: {1}".format(subdir, err)
+                hasError = True
 
+    if hasError:
+        return 1
+        
 if __name__ == "__main__":
     main()
