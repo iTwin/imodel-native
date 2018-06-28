94dee27cc90388eda4d067e64667a5f18b0bcc7a
Krischan.Eberle <none@none>
2018-06-28 10:04:42 +0200

    [Compatibility tests] Merge from ec32 branch.

--- iModelCore/DgnPlatform/Tests/DgnProject/Compatibility/TestHelper.h
+++ iModelCore/DgnPlatform/Tests/DgnProject/Compatibility/TestHelper.h
@@ -13,14 +13,24 @@
 //! Provides helper methods for testing certain areas of a DgnDb or ECDb file in the compatibility tests
 // @bsiclass                                                 Krischan.Eberle     06/2018
 //=======================================================================================    
-struct TestHelper final
+struct TestDb
     {
-private:
+protected:
     TestFile const& m_testFile;
-    ECDbCR m_db;
+
+private:
+    virtual ECDbR _GetDb() const = 0;
+    virtual ECDb::OpenParams const& _GetOpenParams() const = 0;
+
+protected:
+    explicit TestDb(TestFile const& testFile) : m_testFile(testFile) {}
 
 public:
-    TestHelper(TestFile const& testFile, ECDbCR bimOrECDb) : m_testFile(testFile), m_db(bimOrECDb) {}
+    virtual ~TestDb() {}
+    TestFile const& GetTestFile() const { return m_testFile; }
+    ECDbR GetDb() const { return _GetDb(); }
+    ECDb::OpenParams const& GetOpenParams() const { return _GetOpenParams(); }
+    Utf8String GetDescription() const;
 
     JsonValue ExecuteECSqlSelect(Utf8CP ecsql) const;
     SchemaVersion GetSchemaVersion(Utf8CP schemaName) const;
@@ -37,4 +47,70 @@ public:
     void AssertPhenomenon(Utf8CP schemaName, Utf8CP phenName, Utf8CP expectedDisplayLabel, Utf8CP expectedDescription, Utf8CP definition) const;
 
     void AssertLoadSchemas() const;
+    };
+
+//=======================================================================================
+//! Provides helper methods for testing certain areas of an ECDb file in the compatibility tests
+// @bsiclass                                                 Krischan.Eberle     06/2018
+//=======================================================================================    
+struct TestECDb final : TestDb
+    {
+private:
+    ECDb m_ecdb;
+    ECDb::OpenParams m_openParams;
+
+    ECDbR _GetDb() const override { return const_cast<ECDbR> (m_ecdb); }
+    ECDb::OpenParams const& _GetOpenParams() const override { return m_openParams; }
+public:
+    TestECDb(TestFile const& testFile, ECDb::OpenParams const& openParams = ECDb::OpenParams(ECDb::OpenMode::Readonly)): TestDb(testFile), m_openParams(openParams) {}
+    ~TestECDb() { Close(); }
+    DbResult Open() { return m_ecdb.OpenBeSQLiteDb(m_testFile.GetPath(), m_openParams); }
+    void Close()
+        {
+        if (m_ecdb.IsDbOpen())
+            m_ecdb.CloseDb();
+        }
+
+    void AssertProfileVersion() const;
+
+    static std::vector<std::unique_ptr<TestECDb>> CreateFor(TestFile const& testFile)
+        {
+        std::vector<std::unique_ptr<TestECDb>> testECDbs {std::make_unique<TestECDb>(testFile, ECDb::OpenParams(ECDb::OpenMode::Readonly)), std::make_unique<TestECDb>(testFile, ECDb::OpenParams(ECDb::OpenMode::ReadWrite))};
+        if (testFile.GetAge() == ProfileState::Age::Older)
+            testECDbs.pusb_back(std::make_unique<TestECDb>(testFile, ECDb::OpenParams(ECDb::OpenMode::ReadWrite, ECDb::ProfileUpgradeOptions::Upgrade)));
+
+        return testECDbs;
+        }
+    };
+
+//=======================================================================================
+//! Provides helper methods for testing certain areas of an iModel file in the compatibility tests
+// @bsiclass                                                 Krischan.Eberle     06/2018
+//=======================================================================================    
+struct TestIModel final : TestDb
+    {
+    private:
+        DgnDbPtr m_dgndb = nullptr;
+        DgnDb::OpenParams m_openParams;
+
+        ECDbR _GetDb() const override { return *m_dgndb; }
+        ECDb::OpenParams const& _GetOpenParams() const override { return m_openParams; }
+
+    public:
+        TestIModel(TestFile const& testFile, DgnDb::OpenParams const& openParams = DgnDb::OpenParams(DgnDb::OpenMode::Readonly)) : TestDb(testFile), m_openParams(openParams) {}
+        ~TestIModel() { Close(); }
+        DbResult Open()
+            { 
+            DbResult stat = BeSQLite::BE_SQLITE_OK;
+            m_dgndb = DgnDb::OpenDgnDb(&stat, m_testFile.GetPath(), m_openParams);
+            return stat;
+            }
+
+        void Close()
+            {
+            if (m_dgndb != nullptr && m_dgndb->IsDbOpen())
+                m_dgndb->CloseDb();
+            }
+
+        void AssertProfileVersion() const;
     };
