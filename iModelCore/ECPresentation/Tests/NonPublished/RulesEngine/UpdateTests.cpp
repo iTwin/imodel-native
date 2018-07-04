23a2918687ab2d8580058706c2db4239f039ba0c
Grigas.Petraitis <none@none>
2018-07-04 14:34:32 +0300

    TFS#909312: Linux & MacOS ECPresentation test failures Enabled failing unit tests. The issue was in Bentley library Utf8PrinftString class which sometimes failed to printf string correctly.

--- iModelCore/ECPresentation/Tests/NonPublished/RulesEngine/UpdateTests.cpp
+++ iModelCore/ECPresentation/Tests/NonPublished/RulesEngine/UpdateTests.cpp
@@ -149,7 +149,6 @@ struct HierarchyUpdateTests : UpdateTests
 /*---------------------------------------------------------------------------------**//**
 * @betest                                       Grigas.Petraitis                02/2016
 +---------------+---------------+---------------+---------------+---------------+------*/
-#if !defined(BENTLEYCONFIG_OS_APPLE_MACOS) && !defined(BENTLEYCONFIG_OS_LINUX)
 TEST_F (HierarchyUpdateTests, RemovesECInstanceNodeAfterECInstanceDelete)
     {
     // insert some widget instances
@@ -5326,7 +5325,6 @@ TEST_F (HierarchyUpdateTests, UpdatesDataSourceAfterInsertWhenItAlreadyHasManyTo
     EXPECT_EQ(ChangeType::Update, m_updateRecordsHandler->GetRecords()[1].GetChangeType());
     EXPECT_EQ(*childNodes[1]->GetKey(), *m_updateRecordsHandler->GetRecords()[1].GetNode()->GetKey());
     }
-#endif
 
 /*=================================================================================**//**
 * @bsiclass                                     Grigas.Petraitis                10/2016
@@ -5757,7 +5755,6 @@ struct SelectionUpdateTests : UpdateTests
 /*---------------------------------------------------------------------------------**//**
 * @betest                                       Grigas.Petraitis                10/2017
 +---------------+---------------+---------------+---------------+---------------+------*/
-#if !defined(BENTLEYCONFIG_OS_APPLE_MACOS) && !defined(BENTLEYCONFIG_OS_LINUX)
 TEST_F(SelectionUpdateTests, RemovesECInstanceNodeKeyFromSelection)
     {
     // insert some widget instances
@@ -6118,4 +6115,3 @@ TEST_F(SelectionUpdateTests, DoesntRefreshSelectionWhenUnaffectedGroupingNodeCha
     // expect no selection change events
     EXPECT_EQ(0, selectionChangeEvents.size());
     }
-#endif
