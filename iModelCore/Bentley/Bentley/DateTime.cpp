13b43a958a50853dbf8280b5824773723673da34
Vincas Razma <vincas.razma@bentley.com>
2018-07-05 15:46:46 +0300

    Merge

--- iModelCore/Bentley/Bentley/DateTime.cpp
+++ iModelCore/Bentley/Bentley/DateTime.cpp
@@ -2,7 +2,7 @@
 |
 |     $Source: Bentley/DateTime.cpp $
 |
-|  $Copyright: (c) 2016 Bentley Systems, Incorporated. All rights reserved. $
+|  $Copyright: (c) 2018 Bentley Systems, Incorporated. All rights reserved. $
 |
 +--------------------------------------------------------------------------------------*/
 #include <Bentley/DateTime.h>
