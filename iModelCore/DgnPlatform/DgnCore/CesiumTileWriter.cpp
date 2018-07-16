4750d86e6b22737ed0f4188abf8abbd83035b4dc
Mantas.Boiko <none@none>
2018-07-16 10:14:53 +0300

    fix compiler errors
    13: error: '_BeginProcessModel' overrides a member function but is not marked 'override' [-Werror,-Winconsistent-missing-override]
        [c:/build/src/mds06t/DgnPlatform/DgnCore/CesiumTileWriter.cpp (903)]
    13: error: '_EndProcessModel' overrides a member function but is not marked 'override' [-Werror,-Winconsistent-missing-override]
        [c:/build/src/mds06t/DgnPlatform/DgnCore/CesiumTileWriter.cpp (911)]

--- iModelCore/DgnPlatform/DgnCore/CesiumTileWriter.cpp
+++ iModelCore/DgnPlatform/DgnCore/CesiumTileWriter.cpp
@@ -900,7 +900,7 @@ BeFileNameStatus InitializeDirectories()
 /*---------------------------------------------------------------------------------**//**
 * @bsimethod                                                    Ray.Bentley     08/2017
 +---------------+---------------+---------------+---------------+---------------+------*/
-WriteStatus _BeginProcessModel(GeometricModelCR model)
+WriteStatus _BeginProcessModel(GeometricModelCR model) override
     {
     return (BeFileNameStatus::Success == InitializeDirectories()) ? WriteStatus::Success : WriteStatus::UnableToOpenFile;
     }
@@ -908,7 +908,7 @@ WriteStatus _BeginProcessModel(GeometricModelCR model)
 /*---------------------------------------------------------------------------------**//**
 * @bsimethod                                                    Ray.Bentley     08/2017
 +---------------+---------------+---------------+---------------+---------------+------*/
-WriteStatus _EndProcessModel(GeometricModelCR model, TransformCR tileToDb, PublishedTileCR rootTile, WriteStatus status)
+WriteStatus _EndProcessModel(GeometricModelCR model, TransformCR tileToDb, PublishedTileCR rootTile, WriteStatus status) override
     {
     if (WriteStatus::Success != status)
         {
