35c68562203d7d6d0bf1a2fdf1033a063c965315
Carole.MacDonald <none@none>
2018-07-18 14:14:23 -0400

    TFS#918802 - During an update, add new saved views.

--- iModelBridgeCore/DgnDbSync/DgnV8/RootModelConverter.cpp
+++ iModelBridgeCore/DgnDbSync/DgnV8/RootModelConverter.cpp
@@ -19,9 +19,6 @@ void RootModelConverter::_ConvertSpatialViews()
     if (!IsFileAssignedToBridge(*GetRootV8File()))
         return;
 
-    if (IsUpdating())
-        return;         // pity, but we don't have the logic to *update* previously converted views.
-
     if (!m_viewGroup.IsValid())
         m_viewGroup = m_rootFile->GetViewGroupsR().FindByModelId(GetRootModelP()->GetModelId(), true, -1);
     if (!m_viewGroup.IsValid())
