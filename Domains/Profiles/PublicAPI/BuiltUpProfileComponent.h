c8f110aff43593a886ea42741ed235b65270ba3c
Nick.Purcell <none@none>
2018-07-24 16:15:59 -0500

    Added some comments to xml and fixed issue with linker

--- Domains/Profiles/PublicAPI/BuiltUpProfileComponent.h
+++ Domains/Profiles/PublicAPI/BuiltUpProfileComponent.h
@@ -11,7 +11,6 @@ BEGIN_BENTLEY_PROFILES_NAMESPACE
 struct EXPORT_VTABLE_ATTRIBUTE BuiltUpProfileComponent : Dgn::DgnElement::MultiAspect
     {
     DGNASPECT_DECLARE_MEMBERS(BENTLEY_PROFILES_SCHEMA_NAME, PROFILES_CLASS_BuiltUpProfileComponent, Dgn::DgnElement::MultiAspect);
-        
     friend struct  BuiltUpProfileComponentHandler;
 
 private:
@@ -22,24 +21,26 @@ private:
     DPoint2d _offsetPoint;
     double _rotation;
     Dgn::DgnElementId _profileId;
+
 public:
-    EXPORT_VTABLE_ATTRIBUTE static BuiltUpProfileComponentPtr Create() {return new BuiltUpProfileComponent();}
-    EXPORT_VTABLE_ATTRIBUTE void SetIndex(int index) { _index = index; }
-    EXPORT_VTABLE_ATTRIBUTE int GetIndex() const{ return _index; }
-    EXPORT_VTABLE_ATTRIBUTE void SetPlacementPointId(int placementPointId) { _placementPointId = placementPointId; }
-    EXPORT_VTABLE_ATTRIBUTE int GetPlacementPointId() const{ return _placementPointId; }
-    EXPORT_VTABLE_ATTRIBUTE void SetMirrorProfileAboutYAxis(bool mirrorProfileAboutYAxis) 
+    PROFILES_DOMAIN_EXPORT static BuiltUpProfileComponentPtr Create() {return new BuiltUpProfileComponent();}
+    PROFILES_DOMAIN_EXPORT void SetIndex(int index) { _index = index; }
+    PROFILES_DOMAIN_EXPORT int GetIndex() const{ return _index; }
+    PROFILES_DOMAIN_EXPORT void SetPlacementPointId(int placementPointId) { _placementPointId = placementPointId; }
+    PROFILES_DOMAIN_EXPORT int GetPlacementPointId() const{ return _placementPointId; }
+    PROFILES_DOMAIN_EXPORT void SetMirrorProfileAboutYAxis(bool mirrorProfileAboutYAxis) 
         { _mirrorProfileAboutYAxis = mirrorProfileAboutYAxis; }
-    EXPORT_VTABLE_ATTRIBUTE bool GetMirrorProfileAboutYAxis() const{ return _mirrorProfileAboutYAxis; }
-    EXPORT_VTABLE_ATTRIBUTE void SetOffsetPoint(DPoint2d offsetPoint) { _offsetPoint = offsetPoint; }
-    EXPORT_VTABLE_ATTRIBUTE DPoint2d GetOffsetPoint() const{ return _offsetPoint; }
-    EXPORT_VTABLE_ATTRIBUTE void SetRotation(double rotation) { _rotation = rotation; }
-    EXPORT_VTABLE_ATTRIBUTE double GetRotation() const{ return _rotation; }
-    EXPORT_VTABLE_ATTRIBUTE void SetProfileId(Dgn::DgnElementId profileId) { _profileId = profileId; }
-    EXPORT_VTABLE_ATTRIBUTE Dgn::DgnElementId GetProfileId() const{ return _profileId; }
+    PROFILES_DOMAIN_EXPORT bool GetMirrorProfileAboutYAxis() const{ return _mirrorProfileAboutYAxis; }
+    PROFILES_DOMAIN_EXPORT void SetOffsetPoint(DPoint2d offsetPoint) { _offsetPoint = offsetPoint; }
+    PROFILES_DOMAIN_EXPORT DPoint2d GetOffsetPoint() const{ return _offsetPoint; }
+    PROFILES_DOMAIN_EXPORT void SetRotation(double rotation) { _rotation = rotation; }
+    PROFILES_DOMAIN_EXPORT double GetRotation() const{ return _rotation; }
+    PROFILES_DOMAIN_EXPORT void SetProfileId(Dgn::DgnElementId profileId) { _profileId = profileId; }
+    PROFILES_DOMAIN_EXPORT Dgn::DgnElementId GetProfileId() const{ return _profileId; }
+
 protected:
-    EXPORT_VTABLE_ATTRIBUTE Dgn::DgnDbStatus _LoadProperties(Dgn::DgnElementCR) override;
-    EXPORT_VTABLE_ATTRIBUTE Dgn::DgnDbStatus _UpdateProperties(Dgn::DgnElementCR, BeSQLite::EC::ECCrudWriteToken const*) override;
+    PROFILES_DOMAIN_EXPORT Dgn::DgnDbStatus _LoadProperties(Dgn::DgnElementCR) override;
+    PROFILES_DOMAIN_EXPORT Dgn::DgnDbStatus _UpdateProperties(Dgn::DgnElementCR, BeSQLite::EC::ECCrudWriteToken const*) override;
     Dgn::DgnDbStatus _GetPropertyValue(ECN::ECValueR, Utf8CP, Dgn::PropertyArrayIndex const&) const override { return Dgn::DgnDbStatus::NotEnabled; }
     Dgn::DgnDbStatus _SetPropertyValue(Utf8CP, ECN::ECValueCR, Dgn::PropertyArrayIndex const&) override { return Dgn::DgnDbStatus::NotEnabled; }
     };
