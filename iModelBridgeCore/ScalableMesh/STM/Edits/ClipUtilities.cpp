b07231239ff7ec6a9258f73ca98173ee676946d6
Mathieu.St-Pierre <none@none>
2018-08-20 12:53:35 -0400

    Fix compile error

--- iModelBridgeCore/ScalableMesh/STM/Edits/ClipUtilities.cpp
+++ iModelBridgeCore/ScalableMesh/STM/Edits/ClipUtilities.cpp
@@ -2214,7 +2214,11 @@ void MeshClipper::SetClipGeometry(const bmap<size_t, uint64_t>& idsForPrimitives
         mapOfVector[id].id = id;
         if (nullptr == mapOfVector[id].clip)
         {
+#ifdef VANCOUVER_API
             vectorDefs.push_back(ClipVector::CreateFromPrimitive(primitive));
+#else
+            vectorDefs.push_back(ClipVector::CreateFromPrimitive(primitive.get()));
+#endif
             mapOfVector[id].clip = vectorDefs.back().get();
         }
         else const_cast<ClipVector*>(mapOfVector[id].clip)->push_back(primitive);
@@ -2240,7 +2244,11 @@ void MeshClipper::SetClipGeometry(const bmap<size_t, uint64_t>& idsForPrimitives
         mapOfVector[id].id =id;
         if (nullptr == mapOfVector[id].clip)
             {
+#ifdef VANCOUVER_API
             vectorDefs.push_back(ClipVector::CreateFromPrimitive(primitive));
+#else
+            vectorDefs.push_back(ClipVector::CreateFromPrimitive(primitive.get()));
+#endif
             mapOfVector[id].clip = vectorDefs.back().get();
             }
         else const_cast<ClipVector*>(mapOfVector[id].clip)->push_back(primitive);
