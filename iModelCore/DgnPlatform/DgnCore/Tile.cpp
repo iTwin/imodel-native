76e88c8c092090e0fa444522369d3dc1f13d41a1
Paul.Connelly <none@none>
2018-08-29 10:39:54 -0400

    Tile content/node IDs.

--- iModelCore/DgnPlatform/DgnCore/Tile.cpp
+++ iModelCore/DgnPlatform/DgnCore/Tile.cpp
@@ -6,7 +6,7 @@
 |
 +--------------------------------------------------------------------------------------*/
 #include "DgnPlatformInternal.h"
-#include <DgnPlatform/NewTileTree.h>
+#include <DgnPlatform/Tile.h>
 #include <numeric>
 #include <DgnPlatform/RangeIndex.h>
 #include <GeomJsonWireFormat/JsonUtils.h>
@@ -16,7 +16,67 @@
 #include <DgnPlatform/DgnBRep/PSolidUtil.h>
 #endif
 
-USING_NAMESPACE_NEWTILETREE
+USING_NAMESPACE_TILE
+
+BEGIN_UNNAMED_NAMESPACE
+
+/*---------------------------------------------------------------------------------**//**
+* @bsimethod                                                    Paul.Connelly   01/18
++---------------+---------------+---------------+---------------+---------------+------*/
+static DRange3d bisectRange2d(DRange3dCR range, bool takeLow)
+    {
+    DVec3d diag = range.DiagonalVector();
+    DRange3d subRange = range;
+
+    double bisect;
+    double* replace;
+    if (diag.x > diag.y)
+        {
+        bisect = (range.low.x + range.high.x) / 2.0;
+        replace = takeLow ? &subRange.high.x : &subRange.low.x;
+        }
+    else
+        {
+        bisect = (range.low.y + range.high.y) / 2.0;
+        replace = takeLow ? &subRange.high.y : &subRange.low.y;
+        }
+
+    *replace = bisect;
+    return subRange;
+    }
+
+/*---------------------------------------------------------------------------------**//**
+* @bsimethod                                                    Paul.Connelly   12/16
++---------------+---------------+---------------+---------------+---------------+------*/
+static DRange3d bisectRange(DRange3dCR range, bool takeLow)
+    {
+    DVec3d diag = range.DiagonalVector();
+    DRange3d subRange = range;
+
+    double bisect;
+    double* replace;
+    if (diag.x > diag.y && diag.x > diag.z)
+        {
+        bisect = (range.low.x + range.high.x) / 2.0;
+        replace = takeLow ? &subRange.high.x : &subRange.low.x;
+        }
+    else if (diag.y > diag.z)
+        {
+        bisect = (range.low.y + range.high.y) / 2.0;
+        replace = takeLow ? &subRange.high.y : &subRange.low.y;
+        }
+    else
+        {
+        bisect = (range.low.z + range.high.z) / 2.0;
+        replace = takeLow ? &subRange.high.z : &subRange.low.z;
+        }
+
+    *replace = bisect;
+    return subRange;
+    }
+
+END_UNNAMED_NAMESPACE
+
 
 /*---------------------------------------------------------------------------------**//**
 * @bsimethod                                                    Paul.Connelly   08/18
@@ -53,5 +113,44 @@ bool ContentId::FromString(Utf8CP str)
 
     *this = ContentId(depth, i, j, k, mult);
     return true;
+    }
+
+/*---------------------------------------------------------------------------------**//**
+* @bsimethod                                                    Paul.Connelly   08/18
++---------------+---------------+---------------+---------------+---------------+------*/
+NodeId NodeId::ComputeParentId() const
+    {
+    auto depth = GetDepth();
+    if (depth <= 1)
+        {
+        BeAssert(depth == 1);
+        return RootId();
+        }
+
+    return NodeId(depth - 1, m_i >> 2, m_j >> 2, m_k >> 2);
+    }
+
+/*---------------------------------------------------------------------------------**//**
+* @bsimethod                                                    Paul.Connelly   08/18
++---------------+---------------+---------------+---------------+---------------+------*/
+DRange3d NodeId::ComputeRange(DRange3dCR rootRange, bool is2d) const
+    {
+    if (IsRoot() || rootRange.IsNull())
+        return rootRange;
+
+    auto parentId = ComputeParentId();
+    auto parentRange = parentId.ComputeRange(rootRange, is2d);
+
+    bool lowI = 0 == m_i % 2,
+         lowJ = 0 == m_j % 2,
+         lowK = 0 == m_k % 2;
+
+    // We should never subdivide along z for 2d tiles...###TODO Use a quad-tree for 2d, not an oct-tree.
+    auto bisect = is2d ? bisectRange2d : bisectRange;
+    DRange3d range = bisect(parentRange, lowI);
+    range = bisect(range, lowJ);
+    range = bisect(range, lowK);
+
+    return range;
     }
 
--- iModelCore/DgnPlatform/PublicAPI/DgnPlatform/Tile.h
+++ iModelCore/DgnPlatform/PublicAPI/DgnPlatform/Tile.h
@@ -11,11 +11,14 @@
 #include "DgnPlatform.h"
 #include <DgnPlatform/RealityDataCache.h>
 
-#define BEGIN_NEWTILETREE_NAMESPACE    BEGIN_BENTLEY_DGN_NAMESPACE namespace NewTileTree {
-#define END_NEWTILETREE_NAMESPACE      } END_BENTLEY_DGN_NAMESPACE
-#define USING_NAMESPACE_NEWTILETREE    using namespace BentleyApi::Dgn::NewTileTree;
+#define BEGIN_TILE_NAMESPACE    BEGIN_BENTLEY_DGN_NAMESPACE namespace Tile {
+#define END_TILE_NAMESPACE      } END_BENTLEY_DGN_NAMESPACE
+#define USING_NAMESPACE_TILE    using namespace BentleyApi::Dgn::Tile;
 
-BEGIN_NEWTILETREE_NAMESPACE
+BEGIN_TILE_NAMESPACE
+
+DEFINE_POINTER_SUFFIX_TYPEDEFS(NodeId);
+DEFINE_POINTER_SUFFIX_TYPEDEFS(ContentId);
 
 //=======================================================================================
 // @bsistruct                                                   Paul.Connelly   08/18
@@ -27,38 +30,80 @@ enum class TreeType : uint8_t
 };
 
 //=======================================================================================
+//! Uniquely identifies a volume within the oct-tree by its depth and range.
 // @bsistruct                                                   Paul.Connelly   08/18
 //=======================================================================================
-struct ContentId
+struct NodeId
 {
-private:
+protected:
     uint64_t    m_i;
     uint64_t    m_j;
     uint64_t    m_k;
     uint8_t     m_depth;
+
+    static int8_t Compare(NodeIdCR lhs, NodeIdCR rhs)
+        {
+        if (lhs.m_depth != rhs.m_depth) return lhs.m_depth < rhs.m_depth ? -1 : 1;
+        else if (lhs.m_i != rhs.m_i) return lhs.m_i < rhs.m_i ? -1 : 1;
+        else if (lhs.m_j != rhs.m_j) return lhs.m_j < rhs.m_j ? -1 : 1;
+        else if (lhs.m_k != rhs.m_k) return lhs.m_k < rhs.m_k ? -1 : 1;
+        else return 0;
+        }
+
+    void EnforceDepth()
+        {
+        if (0 == m_depth)
+            {
+            BeAssert(0 == m_i && 0 == m_j && 0 == m_k);
+            m_i = m_j = m_k = 0;
+            }
+        }
+public:
+    NodeId(uint8_t depth, uint64_t i, uint64_t j, uint64_t k) : m_i(i), m_j(j), m_k(k), m_depth(depth) { EnforceDepth(); }
+    NodeId() : m_i(0), m_j(0), m_k(0), m_depth(0) { }
+
+    static NodeId RootId() { return NodeId(); }
+    bool IsRoot() const { return 0 == GetDepth(); }
+
+    uint8_t GetDepth() const { return m_depth; }
+
+    bool operator==(NodeIdCR other) const { return 0 == Compare(*this, other); }
+    bool operator!=(NodeIdCR other) const { return !(*this == other); }
+    bool operator<(NodeIdCR rhs) const { return Compare(*this, rhs) < 0; }
+
+    DGNPLATFORM_EXPORT NodeId ComputeParentId() const;
+    DGNPLATFORM_EXPORT DRange3d ComputeRange(DRange3dCR rootRange, bool is2d) const;
+};
+
+//=======================================================================================
+// @bsistruct                                                   Paul.Connelly   08/18
+//=======================================================================================
+struct ContentId : NodeId
+{
+private:
+    DEFINE_T_SUPER(NodeId);
+
     uint8_t     m_mult;
 public:
-    ContentId(uint8_t depth, uint64_t i, uint64_t j, uint64_t k, uint8_t mult) : m_i(i), m_j(j), m_k(k), m_depth(depth), m_mult(0 == mult ? 1 : mult) { }
+    ContentId(NodeIdCR nodeId, uint8_t mult) : T_Super(nodeId), m_mult(0 == mult ? 1 : mult) { }
+    ContentId(uint8_t depth, uint64_t i, uint64_t j, uint64_t k, uint8_t mult) : ContentId(NodeId(depth, i, j, k), mult) { }
     ContentId() : ContentId(0, 0, 0, 0, 1) { }
 
-    static ContentId RootId() { return ContentId(); }
-
-    bool operator==(ContentId const& other) const { return m_depth == other.m_depth && m_i == other.m_i && m_j == other.m_j && m_k == other.m_k && m_mult == other.m_mult; }
-    bool operator!=(ContentId const& other) const { return !(*this == other); }
-    bool operator<(ContentId const& rhs) const
+    bool operator==(ContentIdCR other) const { return T_Super::operator==(other) && m_mult == other.m_mult; }
+    bool operator!=(ContentIdCR other) const { return !(*this == other); }
+    bool operator<(ContentIdCR rhs) const
         {
-        // memcmp could be faster, or not - let optimizer figure it out. We don't actually care about the ordering, only that it's strict-weak.
-        if (m_depth != rhs.m_depth) return m_depth < rhs.m_depth;
-        else if (m_i != rhs.m_i) return m_i < rhs.m_i;
-        else if (m_j != rhs.m_j) return m_j < rhs.m_j;
-        else if (m_k != rhs.m_k) return m_k < rhs.m_k;
-        else if (m_mult != rhs.m_mult) return m_mult != rhs.m_mult;
+        auto compId = Compare(*this, rhs);
+        if (0 != compId) return compId < 0;
+        else if (m_mult != rhs.m_mult) return m_mult < rhs.m_mult;
         else return false;
         }
 
     DGNPLATFORM_EXPORT Utf8String ToString() const;
     DGNPLATFORM_EXPORT bool FromString(Utf8CP str);
+
+    double GetSizeMultiplier() const { BeAssert(0 != m_mult); return static_cast<double>(m_mult); }
 };
 
-END_NEWTILETREE_NAMESPACE
+END_TILE_NAMESPACE
 
