6773d70d2716d2dd17d5bf60801b90b83450550c
Carole.MacDonald <none@none>
2018-08-30 12:20:27 -0400

    Clean up the ignore_list.  Get tests working again by updating expected values in reaction to changes in the dgndb logic.

--- iModelBridgeCore/DgnDbSync/DgnV8/Tests/DataValidationTests.cpp
+++ iModelBridgeCore/DgnDbSync/DgnV8/Tests/DataValidationTests.cpp
@@ -2,7 +2,7 @@
 |
 |  $Source: DgnV8/Tests/DataValidationTests.cpp $
 |
-|  $Copyright: (c) 2017 Bentley Systems, Incorporated. All rights reserved. $
+|  $Copyright: (c) 2018 Bentley Systems, Incorporated. All rights reserved. $
 |
 +--------------------------------------------------------------------------------------*/
 
