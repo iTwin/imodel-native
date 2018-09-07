0c56c51b2138660278ce89f70ed22549fa028d73
Paul.Connelly <none@none>
2018-09-07 06:36:12 -0400

    More NodeId tests.

--- iModelCore/DgnPlatform/Tests/DgnProject/Published/Tile_Test.cpp
+++ iModelCore/DgnPlatform/Tests/DgnProject/Published/Tile_Test.cpp
@@ -60,6 +60,53 @@ TEST(NodeId, ChildNodes)
     }
 
 /*---------------------------------------------------------------------------------**//**
+* @bsimethod                                                    Paul.Connelly   09/18
++---------------+---------------+---------------+---------------+---------------+------*/
+TEST(NodeId, GrandchildNodes)
+    {
+    DRange3d rootRange;
+    rootRange.low = DPoint3d::From(0, 0, 0);
+    rootRange.high = DPoint3d::From(400, 420, 440);
+
+    // Depth 2, takes lower half of every subdivision
+    NodeId lllChild(2, 0, 0, 0);
+
+    // Confirm parent ID
+    NodeId parent = lllChild.ComputeParentId();
+    EXPECT_EQ(parent, NodeId(1, 0, 0, 0));
+
+    // COnfirm parent range
+    DRange3d parentRange = parent.ComputeRange(rootRange, false);
+    EXPECT_TRUE(parentRange.low.IsEqual(rootRange.low));
+    EXPECT_TRUE(parentRange.high.IsEqual(DPoint3d::From(200, 210, 220)));
+
+    // Confirm child range
+    DRange3d childRange = lllChild.ComputeRange(rootRange, false);
+    EXPECT_TRUE(childRange.low.IsEqual(parentRange.low));
+    EXPECT_TRUE(childRange.high.IsEqual(DPoint3d::From(100, 105, 110)));
+
+    // Depth 2, takes upper half of every subdivision
+    NodeId uuuChild(2, 1, 1, 1);
+    parent = uuuChild.ComputeParentId();
+    EXPECT_EQ(parent, NodeId(1, 0, 0, 0));
+
+    // Confirm child range
+    childRange = uuuChild.ComputeRange(rootRange, false);
+    EXPECT_TRUE(childRange.low.IsEqual(DPoint3d::From(100, 105, 110)));
+    EXPECT_TRUE(childRange.high.IsEqual(parentRange.high));
+
+    // Depth 2, takes upper, lower, upper
+    NodeId uluChild(2, 1, 0, 1);
+    parent = uluChild.ComputeParentId();
+    EXPECT_EQ(parent, NodeId(1, 0, 0, 0));
+
+    // Confirm child range
+    childRange = uluChild.ComputeRange(rootRange, false);
+    EXPECT_TRUE(childRange.low.IsEqual(DPoint3d::From(100, 0, 110)));
+    EXPECT_TRUE(childRange.high.IsEqual(DPoint3d::From(200, 105, 220)));
+    }
+
+/*---------------------------------------------------------------------------------**//**
 * @bsimethod                                                    Paul.Connelly   08/18
 +---------------+---------------+---------------+---------------+---------------+------*/
 TEST(ContentId, ToFromString)
