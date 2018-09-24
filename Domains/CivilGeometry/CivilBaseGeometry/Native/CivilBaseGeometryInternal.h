a6ff0c795fb5880b62deef3bb3ca7491639adb8e
Arun.George <none@none>
2018-09-24 08:52:12 -0400

    fixed bentleylogging include for android build

--- Domains/CivilGeometry/CivilBaseGeometry/Native/CivilBaseGeometryInternal.h
+++ Domains/CivilGeometry/CivilBaseGeometry/Native/CivilBaseGeometryInternal.h
@@ -23,7 +23,7 @@ USING_NAMESPACE_BENTLEY_CIVILGEOMETRY
     #define CIVILBASEGEOMETRY_LOGW(...) __android_log_print(ANDROID_LOG_WARN,  LOGGER_NAMESPACE_CIVILBASEGEOMETRY, __VA_ARGS__);
     #define CIVILBASEGEOMETRY_LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOGGER_NAMESPACE_CIVILBASEGEOMETRY, __VA_ARGS__);
 #else
-    #include <Logging/BentleyLogging.h>
+    #include <Logging/bentleylogging.h>
     #define CIVILBASEGEOMETRY_LOG                 (*NativeLogging::LoggingManager::GetLogger (LOGGER_NAMESPACE_CIVILBASEGEOMETRY))
     #define CIVILBASEGEOMETRY_LOGD(...)           CIVILBASEGEOMETRY_LOG.debugv (__VA_ARGS__);
     #define CIVILBASEGEOMETRY_LOGI(...)           CIVILBASEGEOMETRY_LOG.infov (__VA_ARGS__);
