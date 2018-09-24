7cc3bf244921edb9bba92189e3e521501f9d2db1
Caleb.Shafer <none@none>
2018-09-24 07:59:00 -0400

    Update presentation utils to use currentFormatId instead of currentFusId

--- iModelJsNodeAddon/ECPresentationUtils.cpp
+++ iModelJsNodeAddon/ECPresentationUtils.cpp
@@ -928,7 +928,7 @@ struct IModelJsECPresentationSerializer : IECPresentationSerializer
         json.AddMember("name", rapidjson::StringRef(koq.GetFullName().c_str()), allocator);
         json.AddMember("label", rapidjson::StringRef(koq.GetDisplayLabel().c_str()), allocator);
         json.AddMember("persistenceUnit", rapidjson::Value(koq.GetPersistenceUnit()->GetName().c_str(), allocator), allocator);
-        json.AddMember("currentFusId", rapidjson::Value(koq.GetDefaultPresentationFormat()->GetName().c_str(), allocator), allocator);
+        json.AddMember("currentFormatId", rapidjson::Value(koq.GetDefaultPresentationFormat()->GetName().c_str(), allocator), allocator);
         return json;
         }
 
