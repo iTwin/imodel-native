4254bf39c96c2f2845ce0cb9a3dff46f03c6a6a4
Shaun.Sewall <none@none>
2018-09-28 13:58:39 -0400

    - Update "installnativeplatform.bat" to keep copy of LICENSE.md in the "imodeljs" repository in sync.

--- iModelJsNodeAddon/installnativeplatform.bat
+++ iModelJsNodeAddon/installnativeplatform.bat
@@ -21,6 +21,9 @@ xcopy /Q /Y /I %OutRoot%Winx64\packages\imodeljs-native-platform-api %tempDir%\c
 xcopy /Q /Y /I /S %OutRoot%Winx64\packages\imodeljs-n_8-win32-x64    %tempDir%\common\temp\node_modules\@bentley\imodeljs-native-platform-api\lib\@bentley\imodeljs-n_8-win32-x64
 xcopy /Q /Y /I /S %OutRoot%Winx64\packages\imodeljs-e_2-win32-x64    %tempDir%\common\temp\node_modules\@bentley\imodeljs-native-platform-api\lib\@bentley\imodeljs-e_2-win32-x64
 
+REM Copy the LICENSE.md mastered in the iModelJsNodeAddon repository into the imodeljs repository so that we can link to it when imodeljs is posted to GitHub
+copy %OutRoot%Winx64\packages\imodeljs-native-platform-api\LICENSE.md %tempDir%\core\backend\src\imodeljs-native-LICENSE.md
+
 REM Create a semaphore file that identifies that this is a dev build, and helps bypass version checks
 set libDir=%tempDir%\core\backend\lib
 if not exist %libDir% mkdir %libDir%
