ae217e739aaca3fcc6872cd902d5bcf1091c71d1
Mantas.Boiko <none@none>
2018-10-02 14:29:11 +0300

    fix logging for windows
    there was ) missing
    but instead of using (*obj). I switched to obj->

--- iModelCore/DgnPlatform/DgnCore/DgnCoreLog.h
+++ iModelCore/DgnPlatform/DgnCore/DgnCoreLog.h
@@ -24,10 +24,10 @@
 #define LOGF(...) __android_log_print(ANDROID_LOG_FATAL, "DgnCore", __VA_ARGS__)
 #else
 #define LOG_IsSeverityEnabled(...) (*NativeLogging::LoggingManager::GetLogger (L"DgnCore").isSeverityEnabled (__VA_ARGS__)
-#define LOGT(...) (*NativeLogging::LoggingManager::GetLogger (L"DgnCore").tracev (__VA_ARGS__)
-#define LOGD(...) (*NativeLogging::LoggingManager::GetLogger (L"DgnCore").debugv (__VA_ARGS__)
-#define LOGI(...) (*NativeLogging::LoggingManager::GetLogger (L"DgnCore").infov (__VA_ARGS__)
-#define LOGW(...) (*NativeLogging::LoggingManager::GetLogger (L"DgnCore").warningv (__VA_ARGS__)
-#define LOGE(...) (*NativeLogging::LoggingManager::GetLogger (L"DgnCore").errorv (__VA_ARGS__)
-#define LOGF(...) (*NativeLogging::LoggingManager::GetLogger (L"DgnCore").fatalv (__VA_ARGS__)
+#define LOGT(...) NativeLogging::LoggingManager::GetLogger (L"DgnCore")->tracev (__VA_ARGS__)
+#define LOGD(...) NativeLogging::LoggingManager::GetLogger (L"DgnCore")->debugv (__VA_ARGS__)
+#define LOGI(...) NativeLogging::LoggingManager::GetLogger (L"DgnCore")->infov (__VA_ARGS__)
+#define LOGW(...) NativeLogging::LoggingManager::GetLogger (L"DgnCore")->warningv (__VA_ARGS__)
+#define LOGE(...) NativeLogging::LoggingManager::GetLogger (L"DgnCore")->errorv (__VA_ARGS__)
+#define LOGF(...) NativeLogging::LoggingManager::GetLogger (L"DgnCore")->fatalv (__VA_ARGS__)
 #endif
