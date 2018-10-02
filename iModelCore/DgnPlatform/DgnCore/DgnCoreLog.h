e14d568199419d743db39e5d3c7c7e372c0102e2
Mantas.Boiko <none@none>
2018-10-02 15:38:03 +0300

    Backed out changeset: c53d082bfc56
    based on Paul Connelly suggestion

--- iModelCore/DgnPlatform/DgnCore/DgnCoreLog.h
+++ iModelCore/DgnPlatform/DgnCore/DgnCoreLog.h
@@ -23,7 +23,7 @@
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, "DgnCore", __VA_ARGS__)
 #define LOGF(...) __android_log_print(ANDROID_LOG_FATAL, "DgnCore", __VA_ARGS__)
 #else
-#define LOG_IsSeverityEnabled(...) NativeLogging::LoggingManager::GetLogger (L"DgnCore")->isSeverityEnabled (__VA_ARGS__)
+#define LOG_IsSeverityEnabled(...) (*NativeLogging::LoggingManager::GetLogger (L"DgnCore").isSeverityEnabled (__VA_ARGS__)
 #define LOGT(...) NativeLogging::LoggingManager::GetLogger (L"DgnCore")->tracev (__VA_ARGS__)
 #define LOGD(...) NativeLogging::LoggingManager::GetLogger (L"DgnCore")->debugv (__VA_ARGS__)
 #define LOGI(...) NativeLogging::LoggingManager::GetLogger (L"DgnCore")->infov (__VA_ARGS__)
