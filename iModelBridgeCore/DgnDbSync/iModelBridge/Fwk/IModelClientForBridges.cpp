7fc40422e4eae52f95952fbcff5ab82886141212
Abeesh.Basheer <none@none>
2018-10-12 16:13:46 -0500

    Fix: Compile error after rename.

--- iModelBridgeCore/DgnDbSync/iModelBridge/Fwk/IModelClientForBridges.cpp
+++ iModelBridgeCore/DgnDbSync/iModelBridge/Fwk/IModelClientForBridges.cpp
@@ -5,7 +5,7 @@
 |  $Copyright: (c) 2018 Bentley Systems, Incorporated. All rights reserved. $
 |
 +--------------------------------------------------------------------------------------*/
-#include "IModelClientForBridges.h"
+#include <iModelBridge/IModelClientForBridges.h>
 #include <DgnPlatform/DgnProgressMeter.h>
 
 #include <WebServices/iModelHub/Client/Client.h>
