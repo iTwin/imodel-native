c724035a4bd1cc1184ef36f06e50fec6c1ed41a2
Vincas.Razma <none@none>
2018-10-18 17:51:07 +0300

    CM: cleanup API and formatting SRB:Julius.Cepukenas

--- iModelCore/WSClient/PublicAPI/WebServices/Connect/SimpleConnectTokenProvider.h
+++ iModelCore/WSClient/PublicAPI/WebServices/Connect/SimpleConnectTokenProvider.h
@@ -10,14 +10,9 @@
 
 #include <WebServices/Connect/IConnectTokenProvider.h>
 #include <WebServices/Connect/ISecurityToken.h>
-#include <WebServices/Connect/SecurityToken.h>
-#include <Bentley/Tasks/AsyncTask.h>
-
-#include <WebServices/Connect/IConnectAuthenticationPersistence.h>
-#include <WebServices/Connect/IConnectTokenProvider.h>
-#include <WebServices/Connect/IImsClient.h>
 
 BEGIN_BENTLEY_WEBSERVICES_NAMESPACE
+
 /*--------------------------------------------------------------------------------------+
 * @bsiclass                                                julius.cepukenas    10/2018
 +---------------+---------------+---------------+---------------+---------------+------*/
@@ -25,31 +20,28 @@ typedef std::shared_ptr<struct SimpleConnectTokenProvider> SimpleConnectTokenPro
 struct SimpleConnectTokenProvider : IConnectTokenProvider
     {
 public:
-    typedef std::function<AsyncTaskPtr<SecurityTokenPtr>()> UpdateTokenCallback;
+    typedef std::function<AsyncTaskPtr<ISecurityTokenPtr>()> UpdateTokenCallback;
 
 private:
-    SecurityTokenPtr m_token;
+    ISecurityTokenPtr m_token;
     UpdateTokenCallback m_onUpdate;
 
-private:
-    static UpdateTokenCallback GetDefaultTokenCallback ()
-        {
-        auto lambda([]() {return CreateCompletedAsyncTask(std::make_shared<SecurityToken>()); });
-        return std::cref(lambda);
-        }
-
 public:
-    SimpleConnectTokenProvider(SecurityTokenPtr token, UpdateTokenCallback callback = GetDefaultTokenCallback())
-        : m_token(token), m_onUpdate(callback) {};
- 
-    AsyncTaskPtr<ISecurityTokenPtr> UpdateToken() override {return m_onUpdate()
-        ->Then<ISecurityTokenPtr>([=](SecurityTokenPtr token)
+    SimpleConnectTokenProvider(ISecurityTokenPtr token, UpdateTokenCallback onUpdate = [] { return CreateCompletedAsyncTask(std::make_shared<SecurityToken>()); })
+        : m_token(token), m_onUpdate(onUpdate) {};
+
+    //! Call update callback and cache token
+    AsyncTaskPtr<ISecurityTokenPtr> UpdateToken() override
+        {
+        return m_onUpdate()->Then<ISecurityTokenPtr>([=] (ISecurityTokenPtr token)
             {
-            m_token = std::static_pointer_cast<SecurityToken>(token);
+            m_token = token;
             return m_token;
             });
-    }
-    std::shared_ptr<ISecurityToken> GetToken() override { return m_token; };
+        }
+
+    //! Return original or cached token
+    ISecurityTokenPtr GetToken() override { return m_token; };
     };
- 
+
 END_BENTLEY_WEBSERVICES_NAMESPACE
