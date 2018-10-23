60156282c13628621a5be3f639ecb28f79a8f6f9
Jedrzej.Kosinski <none@none>
2018-10-23 13:16:00 -0500

    Added initial doxygen docs and necessary files

--- iModelCore/LicensingCrossPlatform/PublicAPI/Licensing/Client.h
+++ iModelCore/LicensingCrossPlatform/PublicAPI/Licensing/Client.h
@@ -42,22 +42,48 @@ private:
         );
 
 public:
-    LICENSING_EXPORT static ClientPtr Create
+	//! Client Creator
+	/*!
+	* Initializes an instance of Client, returns a ClientPtr to the prepared Client instance.
+	* 
+	* 
+	*/
+	LICENSING_EXPORT static ClientPtr Create
         (
-		const ConnectSignInManager::UserInfo& userInfo,
-        ClientInfoPtr clientInfo,
-        std::shared_ptr<IConnectAuthenticationProvider> authenticationProvider,
-        BeFileNameCR dbPath,
-        bool offlineMode,
-        Utf8String projectId = "",
-        Utf8String featureString = "",
-        IHttpHandlerPtr customHttpHandler = nullptr
+		const ConnectSignInManager::UserInfo& userInfo, /** For determining UserID */
+        ClientInfoPtr clientInfo, /** A ClientInfoPtr */
+        std::shared_ptr<IConnectAuthenticationProvider> authenticationProvider, /** For  */
+        BeFileNameCR dbPath, /** Path for LicenseClient database */
+        bool offlineMode, /** If offline, pushes usage in discrete intervals. If not offline, pushes usage continuously via stream */
+        Utf8String projectId = "", /** ProjectID string, defaults to an empty string */
+        Utf8String featureString = "", /** FeatureString, defaults to an empty string */
+        IHttpHandlerPtr customHttpHandler = nullptr /** CustomHttpHandler, defaults to a nullptr */
         );
 
+	//! StartApplication performs actions and creates threads required for usage posting and policy requests, returns LicenseStatus
     // TODO: Return more than BentleyStatus to indicate to the app if the user has rights to use this app or it's crippled etc...
+	/*!
+	* This method should be called at when the application is started or when it needs to be started again after being stopped.
+	* A database is opened for use with the LicenseClient.
+	* A policy is requested to check license status; if license status does not allow for proper runtime, it is returned and no further action takes place.
+	* Otherwise, threads are created for policy requests and usage posting and then the license status is returned.
+	*/
     LICENSING_EXPORT LicenseStatus StartApplication(); 
+	//! StopApplication signals background threads to stop and cleans up resources.
+	/*!
+	* Threads are signalled to stop, any remaining usage logs are saved, and the database is closed.
+	*/
+	/*!
+	* Stop Application Comment
+	*/
     LICENSING_EXPORT BentleyStatus StopApplication();
+	/*!
+	* Start Feature Comment
+	*/
     LICENSING_EXPORT BentleyStatus StartFeature();
+	/*!
+	* Stop Feature Comment
+	*/
     LICENSING_EXPORT BentleyStatus StopFeature();
 };
 
