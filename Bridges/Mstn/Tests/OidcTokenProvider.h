9a1c276dd3ed85bd6acc0dcbeeac01ceb1927fbb
Abeesh.Basheer <none@none>
2018-11-19 10:17:03 -0600

    WIP: Oidc unit tests.

---
+++ Bridges/Mstn/Tests/OidcTokenProvider.h
@@ -0,0 +1,33 @@
+/*--------------------------------------------------------------------------------------+
+|
+|     $Source: Tests/OidcTokenProvider.h $
+|
+|  $Copyright: (c) 2018 Bentley Systems, Incorporated. All rights reserved. $
+|
++--------------------------------------------------------------------------------------*/
+#pragma once
+
+#include "../../Common.h"
+#include "OidcToken.h"
+#include <Bentley/BeTimeUtilities.h>
+
+BEGIN_BENTLEY_IMODELHUB_UNITTESTS_NAMESPACE
+USING_NAMESPACE_BENTLEY_WEBSERVICES
+
+/*--------------------------------------------------------------------------------------+
+* @bsiclass                                     Algirdas.Mikoliunas    08/2018
++---------------+---------------+---------------+---------------+---------------+------*/
+struct OidcTokenProvider : IConnectTokenProvider
+    {
+    private:
+        OidcTokenPtr m_token;
+        BeTimePoint m_tokenValidUntil;
+        Credentials m_credentials;
+
+    public:
+        OidcTokenProvider(Credentials credentials);
+        AsyncTaskPtr<ISecurityTokenPtr> UpdateToken() override;
+        ISecurityTokenPtr GetToken() override;
+    };
+
+END_BENTLEY_IMODELHUB_UNITTESTS_NAMESPACE
