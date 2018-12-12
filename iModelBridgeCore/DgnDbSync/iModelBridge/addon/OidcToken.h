108debcd31be34d6215777c53c9bafa022f77f63
John.Majerle <none@none>
2018-12-12 14:43:09 -0600

    Added OidcTokenProvider support

---
+++ iModelBridgeCore/DgnDbSync/iModelBridge/addon/OidcToken.h
@@ -0,0 +1,38 @@
+/*--------------------------------------------------------------------------------------+
+|
+|     $Source: iModelBridge/addon/OidcToken.h $
+|
+|  $Copyright: (c) 2018 Bentley Systems, Incorporated. All rights reserved. $
+|
++--------------------------------------------------------------------------------------*/
+#pragma once
+
+#include <Bentley\WString.h>
+#include <WebServices\Connect\ISecurityToken.h>
+#include <iModelBridge/iModelBridgeFwkRegistry.h>
+
+BEGIN_BENTLEY_DGN_NAMESPACE
+/*--------------------------------------------------------------------------------------+
+* @bsiclass                                     Algirdas.Mikoliunas    08/2018
++---------------+---------------+---------------+---------------+---------------+------*/
+typedef std::shared_ptr<struct OidcToken> OidcTokenPtr;
+struct OidcToken : public WebServices::ISecurityToken
+    {
+    private:
+        Utf8String m_token;
+
+    public:
+        //! Construct token from string
+        OidcToken(Utf8String token);
+        OidcToken(OidcToken const& other);
+        //! Check whenver given token is valid xml and supported
+        bool IsSupported() const override;
+        //! Create string for authorization header
+        Utf8String ToAuthorizationString() const override;
+        //! Return original token representation
+        Utf8StringCR AsString() const override;
+        //! Compare contents of two tokens for equality
+        bool operator==(ISecurityToken const& other) const override;
+    };
+
+END_BENTLEY_DGN_NAMESPACE
