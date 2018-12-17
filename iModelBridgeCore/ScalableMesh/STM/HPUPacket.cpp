4418454a84e2ede19ba58d199439efb86f6177c8
Elenie.Godzaridis <none@none>
2018-12-17 20:12:18 +0100

    Stopped using HPUPacket.hpp

--- iModelBridgeCore/ScalableMesh/STM/HPUPacket.cpp
+++ iModelBridgeCore/ScalableMesh/STM/HPUPacket.cpp
@@ -7,10 +7,43 @@
 //:>+--------------------------------------------------------------------------------------
 #include "ScalableMeshPCH.h"
 #include <ImagePP/h/ImageppAPI.h>
+#include "HPUPacket.h"
 #ifndef VANCOUVER_API
-#include "HPUPacket.hpp"
    
 namespace HPU {       
+
+    HCDPacket& Packet::EditPacket()
+    {
+        HPRECONDITION(!IsSharedPacket());
+        return *m_pDataPacket;
+    }
+
+    const HCDPacket& Packet::GetPacket() const
+    {
+        return *m_pDataPacket;
+    }
+
+    Packet::iterator Packet::BeginEdit()
+    {
+        return Edit();
+    }
+
+    Packet::iterator Packet::EndEdit()
+    {
+        return Edit() + GetSize();
+    }
+
+    Packet::iterator Packet::Erase(const_iterator  pi_Position,
+        size_t          pi_Size)
+    {
+        return Erase(pi_Position, pi_Position + pi_Size);
+    }
+
+    void swap(Packet& pio_rLeft,
+        Packet& pio_rRight)
+    {
+        pio_rLeft.Swap(pio_rRight);
+    }
 
 /*---------------------------------------------------------------------------------**//**`
 * @description  Construct a packet around a c string. Packet will contain the string
--- iModelBridgeCore/ScalableMesh/STM/HPUPacket.hpp
+++ iModelBridgeCore/ScalableMesh/STM/HPUPacket.hpp
@@ -2,40 +2,40 @@
 //:>
 //:>     $Source: STM/HPUPacket.hpp $
 //:>
-//:>  $Copyright: (c) 2016 Bentley Systems, Incorporated. All rights reserved. $
+//:>  $Copyright: (c) 2018 Bentley Systems, Incorporated. All rights reserved. $
 //:>
 //:>+--------------------------------------------------------------------------------------
 
 
 
-inline HCDPacket& Packet::EditPacket ()
+HCDPacket& Packet::EditPacket ()
     {
     HPRECONDITION(!IsSharedPacket());
     return *m_pDataPacket;
     }
 
-inline const HCDPacket& Packet::GetPacket () const
+const HCDPacket& Packet::GetPacket () const
     {
     return *m_pDataPacket;
     }
 
-inline Packet::iterator Packet::BeginEdit ()
+Packet::iterator Packet::BeginEdit ()
     {
     return Edit();
     }
 
-inline Packet::iterator Packet::EndEdit ()
+Packet::iterator Packet::EndEdit ()
     {
     return Edit() + GetSize();
     }
 
-inline Packet::iterator Packet::Erase  (const_iterator  pi_Position,
+Packet::iterator Packet::Erase  (const_iterator  pi_Position,
                                         size_t          pi_Size)
     {
     return Erase(pi_Position, pi_Position + pi_Size);
     }
 
-inline void swap   (Packet& pio_rLeft,
+void swap   (Packet& pio_rLeft,
                     Packet& pio_rRight)
     {
     pio_rLeft.Swap(pio_rRight);
