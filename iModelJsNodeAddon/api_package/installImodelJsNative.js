42b11782e1ca0d9a2ad8faf92a6c0dc6715f908c
Keith Bentley <keith.bentley@bentley.com>
2018-12-18 12:09:38 -0500

    find napi.dll by augmenting PATH

--- iModelJsNodeAddon/api_package/installImodelJsNative.js
+++ iModelJsNodeAddon/api_package/installImodelJsNative.js
@@ -1,12 +1,12 @@
 /*--------------------------------------------------------------------------------------+
 |  $Copyright: (c) 2018 Bentley Systems, Incorporated. All rights reserved. $
 +--------------------------------------------------------------------------------------*/
-var exec = require('child_process').exec;
-var path = require('path');
-var fs = require('fs');
-var os = require('os');
-let version = require("./package.json").version;
-var formatPackageName = require('./formatPackageName.js').formatPackageName;
+const exec = require('child_process').exec;
+const path = require('path');
+const fs = require('fs');
+const os = require('os');
+const version = require("./package.json").version;
+const formatPackageName = require('./formatPackageName.js').formatPackageName;
 
 function copyFolderRecursiveSync(source, target) {
     if (!fs.existsSync(target))
@@ -27,19 +27,19 @@ function copyFolderRecursiveSync(source, target) {
 // We have to run npm install in a temp directory. If we try to run it in the current directory,
 // it will fight over a (file?) lock that the parent npm install holds.
 // Note that we have to copy the current package.json to the temp directory, or else npm install will object.
-let installDir = path.join(os.tmpdir(), "install-imodeljs-native");
+const installDir = path.join(os.tmpdir(), "install-imodeljs-native");
 try { fs.mkdirSync(installDir); } catch (err) { }
 fs.copyFileSync(path.join(__dirname, "package.json"),  path.join(installDir, "package.json"));
 
-// We will then copy the results of the install from teh temp directory into the targetDirectory
-// We know that this script is in:               <somewhere>/node_modules/@bentley/imodeljs-native-platform-api
+// We will then copy the results of the install from the temp directory into the targetDirectory
+// We know that this script is in: <somewhere>/node_modules/@bentley/imodeljs-native-platform-api
 // We want to put the native addon packages in:  <somewhere>/node_modules/@bentley/imodeljs-native-platform-api/lib
-let currdir = process.cwd();
-let targetDir = path.normalize(path.join(currdir, "lib")); // See comment below
+const currdir = process.cwd();
+const targetDir = path.normalize(path.join(currdir, "lib")); // See comment below
 
 // Install in tmp dir and copy into target dir
 function installNativePlatformPackage(packages) {
-    let cmdLine = `npm install --no-save ${packages}`;
+    const cmdLine = `npm install --no-save ${packages}`;
     console.log(cmdLine);
     exec(cmdLine, { cwd: installDir }, (error, stdout, stderr) => {
         if (error)
@@ -50,18 +50,4 @@ function installNativePlatformPackage(packages) {
     });
 }
 
-// Install both flavors of the addon for the current platform
-// *** KEEP THIS CONSISTENT WITH iModelJsNodeAddon/MakePackages.py ***
-let engineVersion = process.version.substring(1).split(".")[0];
-let electronVersion = (engineVersion == "10")? "3": "?";    // Record well-known correspondence between node and electron versions here.
-
-console.log(`Node major version is ${engineVersion}`);
-console.log(`Electron major version is ${electronVersion}`);
-
-let packages;
-if (process.os == "win32")
-    packages = `${formatPackageName('n_'+engineVersion)}@${version} ${formatPackageName('e_'+electronVersion)}@${version}`;
-else
-    packages = `${formatPackageName('n_'+engineVersion)}@${version}`;
-
-installNativePlatformPackage(packages);
+installNativePlatformPackage(`${formatPackageName()}@${version}`);
--- iModelJsNodeAddon/makePackages.py
+++ iModelJsNodeAddon/makePackages.py
@@ -34,14 +34,12 @@ def publishPackage(packagedir, doPublish, tag):
         exit(1);
 
 # Replace ${macros} with values in specified file
-def setMacros(packagefile, NODE_VERSION_CODE = None, NODE_OS = None, NODE_CPU = None, PACKAGE_VERSION = None, COMPATIBLE_API_PACKAGE_VERSIONS = None, NODE_ENGINES = None):
+def writePackageJson(packagefile, NODE_OS = None, NODE_CPU = None, PACKAGE_VERSION = None, COMPATIBLE_API_PACKAGE_VERSIONS = None, NODE_ENGINES = None):
     str = ''
     with open(packagefile, 'r') as pf:
         str = pf.read()
 
     with open(packagefile, 'w') as pf:
-        if (NODE_VERSION_CODE):
-            str = str.replace(r'${NODE_VERSION_CODE}', NODE_VERSION_CODE.lower())
         if (NODE_OS):
             str = str.replace(r'${NODE_OS}', NODE_OS.lower())
         if (NODE_CPU):
@@ -59,61 +57,31 @@ def setMacros(packagefile, NODE_VERSION_CODE = None, NODE_OS = None, NODE_CPU = 
 def compute_compatible_api_version_range(packageVersion):
     return "<=" + packageVersion + "  >=" + packageVersion.split('.')[0] + ".0.0";
 
-# Tell copytree to ignore binary files that should not be in the addon
-def filterOutUnwantedFiles(dirname, files):
-    if dirname.endswith('Assets'):
-        return ['DgnGeoCoord'];
-    return ['v8B02.dll', 'v8_libbaseB02.dll', 'v8_libplatformB02.dll']
-
 # Copy a version-specific addon into place
 # @param outdirParent The path to the output package's parent directory
 # @param inputProductdir The path to the Product that contains the ingredients, e.g., D:\bim0200dev\out\Winx64\product\iModelJsNodeAddon-Windows
-# @param versionsubdir The name of the subdirectory that contains the addon. The name of the subdir encodes the version of node that this addon is for.  E.g., N_8
 # @param nodeOS The target platform (using Node terminology)
 # @param nodeCPU The target CPU (using Node terminology)
 # @param packageVersion The semantic version number for the generated package
 # @param sourceDir The source directory, i.e., %SrcRoot%iModelJsNodeAddon
 # @return the full path to the generated package directory
-def generate_addon_for_platform(outdirParent, inputProductdir, versionsubdir, nodeOS, nodeCPU, packageVersion, sourceDir):
-
-    # Generate the name of the addon package
-    versionsubdir = versionsubdir.lower();
+def generate_package_for_platform(outdirParent, inputProductdir, nodeOS, nodeCPU, packageVersion, sourceDir):
 
     # Compute the name of a directory that we can use to stage this package. This is just a temporary name.
     # The real name of the package is inside the package.json file.
-    outputpackagename = 'imodeljs-' + versionsubdir + '-' + nodeOS + '-' + nodeCPU
+    outputpackagename = 'imodeljs-' + nodeOS + '-' + nodeCPU
 
     outputpackagedir = os.path.join(outdirParent, outputpackagename)
 
     if os.path.exists(outputpackagedir):
         shutil.rmtree(outputpackagedir)
 
-    # The input product's directory structure should look like this:
-    # D:\bim0200dev\out\Winx64\product\iModelJsNodeAddon-Windows\Addon\N_8\imodeljs.node
-    # D:\bim0200dev\out\Winx64\product\iModelJsNodeAddon-Windows\Support
-
-    srcsupportdir = os.path.join(inputProductdir, "Support");
-    srcnNapiFile = os.path.join(os.path.join(os.path.join(inputProductdir, "Addon"), versionsubdir), "napi.dll")
     srcpackagefile = os.path.join(sourceDir, "package.json.template")
-
-    if not os.path.exists(srcsupportdir) or not os.path.exists(srcpackagefile):
-        print '***'
-        print('*** ' + inputProductdir + ' -- invalid or incomplete iModelJsNodeAddon Product.')
-        if not os.path.exists(srcnodefile):
-            print ' ***   not found: ' + srcnodefile
-        if not os.path.exists(srcsupportdir):
-            print ' ***   not found: ' + srcsupportdir
-        print('***')
-        exit(1)
-
     dstpackagefile = os.path.join(outputpackagedir, 'package.json')
     dstaddondir = os.path.join(outputpackagedir, 'addon')
 
     # NB: shutil.copytree insists on creating dstaddondir and will throw an exception if it already exists. That is why we don't call os.makedirs(dest...) here.
-    shutil.copytree(srcsupportdir, dstaddondir, False, filterOutUnwantedFiles)
-
-    if os.path.exists(srcnNapiFile):
-        shutil.copy(srcnNapiFile, dstaddondir)
+    shutil.copytree(inputProductdir, outputpackagedir, False)
 
     shutil.copyfile(srcpackagefile, dstpackagefile)
 
@@ -121,14 +89,7 @@ def generate_addon_for_platform(outdirParent, inputProductdir, versionsubdir, no
 
     shutil.copyfile(os.path.join(sourceDir, 'api_package', 'LICENSE.md'), os.path.join(outputpackagedir, 'LICENSE.md'))
 
-    # The node addon is specific to a version of node.
-    # *** NEEDS WORK: We don't inject this constraint for Electron addons -- not sure how to specify electron engines.
-    nodeEngines = ' '
-    if (versionsubdir.lower().startswith('n_')):
-        nodeEngineVersion = versionsubdir[2:].replace('_', '.')
-        nodeEngines = '"engines": {"node": "' + nodeEngineVersion + '"},'
-
-    setMacros(dstpackagefile, NODE_VERSION_CODE = versionsubdir, NODE_OS = nodeOS, NODE_CPU = nodeCPU, PACKAGE_VERSION = packageVersion, NODE_ENGINES = nodeEngines)
+    writePackageJson(dstpackagefile, NODE_OS = nodeOS, NODE_CPU = nodeCPU, PACKAGE_VERSION = packageVersion, NODE_ENGINES = ' ')
 
     return outputpackagedir
 
@@ -157,7 +118,7 @@ def generate_imodeljs_native_platform_api(outdirParent, parentSourceDir, package
     dstpackagefile = os.path.join(outputpackagedir, 'package.json')
     shutil.copyfile(os.path.join(apiSourceDir, packageTemplateFileName), dstpackagefile);
 
-    setMacros(dstpackagefile, PACKAGE_VERSION = packageVersion)
+    writePackageJson(dstpackagefile, PACKAGE_VERSION = packageVersion)
 
     return outputpackagedir;
 
@@ -193,9 +154,6 @@ if __name__ == '__main__':
     packageVersion = packageVersion.strip()
 
     addonDir = os.path.join(productdir, 'Addon')
-    if not os.path.basename(productdir).startswith('iModelJsNodeAddon') or not os.path.exists(addonDir):
-        print '*** ' + productdir + ' does not appear to be the path to an iModelJsNodeAddon product directory';
-        exit(1)
 
     if os.path.exists(outdirParent):
         print '*** ' + outdirParent + ' already exists. Remove output directory before calling this script';
@@ -203,14 +161,7 @@ if __name__ == '__main__':
 
     os.makedirs(outdirParent)
 
-    # Generate a platform-specific package fo each platform that was built
-    for versionsubdir in os.listdir(addonDir):
-        # We are looking for the version-specific addon subdirectories. They tell us the names of the addons
-        if (re.match(r'^([a-z])_(\d+)_(\d+)_(\d+)$', versionsubdir) is None and re.match(r'^([a-z])_(\d+)_(\d+)$', versionsubdir) is None and re.match(r'^([a-z])_(\d+)$', versionsubdir) is None):
-            print '*** ' + versionsubdir + ' is unexpected. Only version-specific subdirectories should appear under addon.';
-            continue
-
-        publishPackage(generate_addon_for_platform(outdirParent, productdir, versionsubdir, nodeOS, nodeCPU, packageVersion, sourceDir), doPublish, tag);
+    publishPackage(generate_package_for_platform(outdirParent, productdir, nodeOS, nodeCPU, packageVersion, sourceDir), doPublish, tag);
 
     # Generate the api package - PUBLISH ONLY ON WINDOWS - Builds for all other platforms only publish their platform-specific addon packages.
     if nodeOS != 'win32':
