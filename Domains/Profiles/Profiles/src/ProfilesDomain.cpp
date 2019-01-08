0cf1ae9b3e75901faa5d2a1683b7f28f36c52143
Karolis.Zukauskas <none@none>
2019-01-08 17:10:35 +0200

    Reordering ProfilesApi includes and Handler registrations.

--- Domains/Profiles/Profiles/src/ProfilesDomain.cpp
+++ Domains/Profiles/Profiles/src/ProfilesDomain.cpp
@@ -6,43 +6,7 @@
 |
 +--------------------------------------------------------------------------------------*/
 #include "ProfilesInternal.h"
-#include <Profiles\ProfilesDomain.h>
-
-#include <Profiles\Profile.h>
-#include <Profiles\SinglePerimeterProfile.h>
-#include <Profiles\CompositeProfile.h>
-#include <Profiles\ArbitraryCenterLineProfile.h>
-#include <Profiles\ArbitraryCompositeProfile.h>
-#include <Profiles\ParametricProfile.h>
-#include <Profiles\DerivedProfile.h>
-#include <Profiles\AsymmetricIShapeProfile.h>
-#include <Profiles\BentPlateProfile.h>
-#include <Profiles\CapsuleProfile.h>
-#include <Profiles\CenterLineCShapeProfile.h>
-#include <Profiles\CenterLineLShapeProfile.h>
-#include <Profiles\CenterLineZShapeProfile.h>
-#include <Profiles\CShapeProfile.h>
-#include <Profiles\ArbitraryShapeProfile.h>
-#include <Profiles\DoubleCShapeProfile.h>
-#include <Profiles\DoubleLShapeProfile.h>
-#include <Profiles\EllipseProfile.h>
-#include <Profiles\CircleProfile.h>
-#include <Profiles\HollowCircleProfile.h>
-#include <Profiles\HollowRectangleProfile.h>
-#include <Profiles\IShapeProfile.h>
-#include <Profiles\LShapeProfile.h>
-#include <Profiles\RectangleProfile.h>
-#include <Profiles\RegularPolygonProfile.h>
-#include <Profiles\RoundedRectangleProfile.h>
-#include <Profiles\SchifflerizedLShapeProfile.h>
-#include <Profiles\StandardProfileAspect.h>
-#include <Profiles\CustomCardinalPointsAspect.h>
-#include <Profiles\TrapeziumProfile.h>
-#include <Profiles\TShapeProfile.h>
-#include <Profiles\TTShapeProfile.h>
-#include <Profiles\ZShapeProfile.h>
-#include <Profiles\MaterialProfileDefinition.h>
-#include <Profiles\MaterialProfile.h>
+#include <Profiles\ProfilesApi.h>
 
 BEGIN_BENTLEY_PROFILES_NAMESPACE
 
@@ -53,39 +17,39 @@ DOMAIN_DEFINE_MEMBERS (ProfilesDomain)
 +---------------+---------------+---------------+---------------+---------------+------*/
 ProfilesDomain::ProfilesDomain() : DgnDomain (PRF_SCHEMA_NAME, "Bentley Profiles Domain", 1)
     {
-    RegisterHandler (StandardProfileAspectHandler::GetHandler());
-    RegisterHandler (CustomCardinalPointsAspectHandler::GetHandler());
     RegisterHandler (ProfileHandler::GetHandler());
-    RegisterHandler (CompositeProfileHandler::GetHandler());
-    RegisterHandler (DoubleLShapeProfileHandler::GetHandler());
-    RegisterHandler (DoubleCShapeProfileHandler::GetHandler());
-    RegisterHandler (ArbitraryCompositeProfileHandler::GetHandler());
     RegisterHandler (SinglePerimeterProfileHandler::GetHandler());
+    RegisterHandler (DerivedProfileHandler::GetHandler());
     RegisterHandler (ArbitraryShapeProfileHandler::GetHandler());
     RegisterHandler (ArbitraryCenterLineProfileHandler::GetHandler());
-    RegisterHandler (DerivedProfileHandler::GetHandler());
     RegisterHandler (ParametricProfileHandler::GetHandler());
-    RegisterHandler (AsymmetricIShapeProfileHandler::GetHandler());
-    RegisterHandler (BentPlateProfileHandler::GetHandler());
-    RegisterHandler (CapsuleProfileHandler::GetHandler());
-    RegisterHandler (CenterLineCShapeProfileHandler::GetHandler());
-    RegisterHandler (CenterLineLShapeProfileHandler::GetHandler());
-    RegisterHandler (CenterLineZShapeProfileHandler::GetHandler());
     RegisterHandler (CShapeProfileHandler::GetHandler());
-    RegisterHandler (EllipseProfileHandler::GetHandler());
-    RegisterHandler (CircleProfileHandler::GetHandler());
-    RegisterHandler (HollowCircleProfileHandler::GetHandler());
-    RegisterHandler (HollowRectangleProfileHandler::GetHandler());
     RegisterHandler (IShapeProfileHandler::GetHandler());
+    RegisterHandler (AsymmetricIShapeProfileHandler::GetHandler());
     RegisterHandler (LShapeProfileHandler::GetHandler());
     RegisterHandler (SchifflerizedLShapeProfileHandler::GetHandler());
-    RegisterHandler (RectangleProfileHandler::GetHandler());
-    RegisterHandler (RegularPolygonProfileHandler::GetHandler());
-    RegisterHandler (RoundedRectangleProfileHandler::GetHandler());
-    RegisterHandler (TrapeziumProfileHandler::GetHandler());
     RegisterHandler (TShapeProfileHandler::GetHandler());
     RegisterHandler (TTShapeProfileHandler::GetHandler());
     RegisterHandler (ZShapeProfileHandler::GetHandler());
+    RegisterHandler (CenterLineCShapeProfileHandler::GetHandler());
+    RegisterHandler (CenterLineLShapeProfileHandler::GetHandler());
+    RegisterHandler (CenterLineZShapeProfileHandler::GetHandler());
+    RegisterHandler (BentPlateProfileHandler::GetHandler());
+    RegisterHandler (RectangleProfileHandler::GetHandler());
+    RegisterHandler (RoundedRectangleProfileHandler::GetHandler());
+    RegisterHandler (HollowRectangleProfileHandler::GetHandler());
+    RegisterHandler (CircleProfileHandler::GetHandler());
+    RegisterHandler (HollowCircleProfileHandler::GetHandler());
+    RegisterHandler (EllipseProfileHandler::GetHandler());
+    RegisterHandler (TrapeziumProfileHandler::GetHandler());
+    RegisterHandler (RegularPolygonProfileHandler::GetHandler());
+    RegisterHandler (CapsuleProfileHandler::GetHandler());
+    RegisterHandler (CompositeProfileHandler::GetHandler());
+    RegisterHandler (DoubleCShapeProfileHandler::GetHandler());
+    RegisterHandler (DoubleLShapeProfileHandler::GetHandler());
+    RegisterHandler (ArbitraryCompositeProfileHandler::GetHandler());
+    RegisterHandler (StandardProfileAspectHandler::GetHandler());
+    RegisterHandler (CustomCardinalPointsAspectHandler::GetHandler());
     RegisterHandler (MaterialProfileDefinitionHandler::GetHandler());
     RegisterHandler (MaterialProfileHandler::GetHandler());
     }
