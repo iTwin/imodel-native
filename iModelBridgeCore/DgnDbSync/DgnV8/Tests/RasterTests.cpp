6eb7f721d354466256d4b7715d8cc314b9be412b
Carole.MacDonald <none@none>
2019-01-10 11:17:27 -0500

    VSTS#58475 - Ensure setting the file time on the raster file worked before calling update.

--- iModelBridgeCore/DgnDbSync/DgnV8/Tests/RasterTests.cpp
+++ iModelBridgeCore/DgnDbSync/DgnV8/Tests/RasterTests.cpp
@@ -1003,8 +1003,10 @@ TEST_F(RasterTests, CRUD)
     time_t mtime;
     popotons.GetFileTime(nullptr, nullptr, &mtime);
     mtime += 1000;
-    popotons.SetFileTime(nullptr, &mtime);
-    DoUpdate(m_dgnDbFileName, m_v8FileName, false, true);
+    BentleyApi::BeFileNameStatus status = popotons.SetFileTime(nullptr, &mtime);
+    EXPECT_TRUE(BentleyApi::BeFileNameStatus::Success == status) << "Failed to set time on raster file: " << (int) status;
+    if (BentleyApi::BeFileNameStatus::Success == status)
+        DoUpdate(m_dgnDbFileName, m_v8FileName, false, true);
 
     // This test only works if either uploading to the reality data server or storing to local server
     Bentley::WString uploadConfigVar;
