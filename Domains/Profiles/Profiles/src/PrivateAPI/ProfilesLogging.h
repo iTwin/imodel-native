4e46aa28a100314fac62492a3d628cadbc3705df
Karolis.Zukauskas <none@none>
2019-01-16 12:25:46 +0200

    Adding new log message: ProfilesLog::FailedCopyFrom_InvalidElement.

--- Domains/Profiles/Profiles/src/PrivateAPI/ProfilesLogging.h
+++ Domains/Profiles/Profiles/src/PrivateAPI/ProfilesLogging.h
@@ -19,6 +19,9 @@ struct ProfilesLog
     {
     ProfilesLog() = delete;
 
+    /*---------------------------------------------------------------------------------**//**
+    * @bsimethod                                                                     01/2019
+    +---------------+---------------+---------------+---------------+---------------+------*/
     static void FailedDelete_ProfileHasReference (Utf8CP pReferenecedProfileName, Dgn::DgnElementId referencedProfileId,
                                                   Utf8CP pReferencingProfileName, Dgn::DgnElementId referencingProfileId)
         {
@@ -31,6 +34,22 @@ struct ProfilesLog
         Utf8String errorString = "Failed to delete " + Utf8String (pReferenecedProfileName) + " instance (id: %s), because it is being referenced by " +
                                  Utf8String (pReferencingProfileName) + " instance (id: %s).";
         PROFILES_LOG.errorv (errorString.c_str(), referencedIdBuffer, referencingIdBuffer);
+        }
+
+    /*---------------------------------------------------------------------------------**//**
+    * @bsimethod                                                                     01/2019
+    +---------------+---------------+---------------+---------------+---------------+------*/
+    static void FailedCopyFrom_InvalidElement (Utf8CP pProfileClassName, Dgn::DgnElementId profileId, Dgn::DgnElementId sourceElementId)
+        {
+        Utf8Char profileIdBuffer[Dgn::DgnElementId::ID_STRINGBUFFER_LENGTH];
+        Utf8Char sourceElementIdBuffer[Dgn::DgnElementId::ID_STRINGBUFFER_LENGTH];
+
+        profileId.ToString (profileIdBuffer, BeInt64Id::UseHex::Yes);
+        sourceElementId.ToString (sourceElementIdBuffer, BeInt64Id::UseHex::Yes);
+
+        Utf8String errorString = "Failed to copy from DgnElement instance (id: %s) to a " + Utf8String (pProfileClassName) + " instance (id: %s)"
+                                 ", because DgnElement class is incompatible (failed cast).";
+        PROFILES_LOG.errorv (errorString.c_str(), profileIdBuffer, sourceElementIdBuffer);
         }
 
     };
