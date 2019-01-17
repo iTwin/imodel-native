fc8ab831f59db0120172528b5248f3405da9d675
Carole.MacDonald <none@none>
2019-01-17 11:03:20 -0500

    VSTS#58475 - code cleanup

--- iModelBridgeCore/DgnDbSync/DgnV8/Tests/RasterTests.cpp
+++ iModelBridgeCore/DgnDbSync/DgnV8/Tests/RasterTests.cpp
@@ -1008,23 +1008,5 @@ TEST_F(RasterTests, CRUD)
     if (BentleyApi::BeFileNameStatus::Success == status)
         DoUpdate(m_dgnDbFileName, m_v8FileName, false, true);
 
-    // This test only works if either uploading to the reality data server or storing to local server
-    Bentley::WString uploadConfigVar;
-    Bentley::WString serverConfigVar;
-    bool doCheck = false;
-    if (SUCCESS == DgnV8Api::ConfigurationManager::GetVariable(uploadConfigVar, L"DGNDB_REALITY_MODEL_UPLOAD"))
-        doCheck = true;
-    else if (SUCCESS == DgnV8Api::ConfigurationManager::GetVariable(serverConfigVar, L"DGNDB_REALITY_MODEL_TEMPDIR"))
-        doCheck = true;
-
-    if (doCheck)
-        {
-        BentleyApi::BeFileName popotons = GetOutputFileName(L"popotons.jpg");
-        time_t mtime;
-        popotons.GetFileTime(nullptr, nullptr, &mtime);
-        mtime += 1000;
-        popotons.SetFileTime(nullptr, &mtime);
-        DoUpdate(m_dgnDbFileName, m_v8FileName, false, true);
-        }
     }
 
