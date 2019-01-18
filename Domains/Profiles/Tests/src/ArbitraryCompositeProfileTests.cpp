a60b6d2c8a6c43009dd666b7615d5ddf91c95e93
Karolis.Zukauskas <none@none>
2019-01-18 14:24:54 +0200

    TFS#973163: Updating and extending tests for ArbitraryCompositeProfile.

--- Domains/Profiles/Tests/src/ArbitraryCompositeProfileTests.cpp
+++ Domains/Profiles/Tests/src/ArbitraryCompositeProfileTests.cpp
@@ -8,6 +8,9 @@
 #include "ProfileValidationTestCase.h"
 
 USING_NAMESPACE_BENTLEY_DGN
+USING_NAMESPACE_BENTLEY_EC
+USING_NAMESPACE_BENTLEY_SQLITE
+USING_NAMESPACE_BENTLEY_SQLITE_EC
 USING_NAMESPACE_BENTLEY_PROFILES
 
 /*---------------------------------------------------------------------------------**//**
@@ -91,19 +94,56 @@ TEST_F (ArbitraryCompositeProfileTestCase, Insert_SingleProfileInstances_FailedI
 +---------------+---------------+---------------+---------------+---------------+------*/
 TEST_F (ArbitraryCompositeProfileTestCase, GetProperties_ProfileInstance_ValidProperties)
     {
-    ArbitraryCompositeProfileComponent component = CreateDefaultComponent();
-    CreateParams params (GetModel(), "Composite", bvector<ArbitraryCompositeProfileComponent> { component });
+    bvector<ArbitraryCompositeProfileComponent> initialComponents = DefaultComponentVector();
+    EXPECT_EQ (-1, initialComponents[0].GetMemberPriority()) << "MemberPriority of a user-constructed ArbitraryCompositeProfileCompoent should be invalid (-1)";
+
+    CreateParams params (GetModel(), "Composite", bvector<ArbitraryCompositeProfileComponent> { initialComponents });
 
     ArbitraryCompositeProfilePtr profilePtr = CreateProfile (params);
     ASSERT_TRUE (profilePtr.IsValid());
 
     bvector<ArbitraryCompositeProfileComponent> components = profilePtr->GetComponents();
-    ArbitraryCompositeProfileComponent& firstComponent = components[0];
     EXPECT_EQ ("Composite", profilePtr->GetName());
-    EXPECT_EQ (component.singleProfileId, firstComponent.singleProfileId);
-    EXPECT_EQ (component.offset, firstComponent.offset);
-    EXPECT_EQ (component.mirrorAboutYAxis, firstComponent.mirrorAboutYAxis);
-    EXPECT_EQ (component.rotation.Radians(), firstComponent.rotation.Radians());
+    EXPECT_EQ (initialComponents[0].singleProfileId, components[0].singleProfileId);
+    EXPECT_EQ (initialComponents[0].offset, components[0].offset);
+    EXPECT_EQ (initialComponents[0].mirrorAboutYAxis, components[0].mirrorAboutYAxis);
+    EXPECT_EQ (initialComponents[0].rotation.Radians(), components[0].rotation.Radians());
+    EXPECT_EQ (0, components[0].GetMemberPriority()) << "MemberPriority of a created profile instance should be valid";
+
+    ArbitraryCompositeProfileCPtr insertedProfilePtr = profilePtr->Insert();
+    bvector<ArbitraryCompositeProfileComponent> insertedComponents = insertedProfilePtr->GetComponents();
+
+    EXPECT_EQ (initialComponents[0].singleProfileId, insertedComponents[0].singleProfileId);
+    EXPECT_EQ (initialComponents[0].offset, insertedComponents[0].offset);
+    EXPECT_EQ (initialComponents[0].mirrorAboutYAxis, insertedComponents[0].mirrorAboutYAxis);
+    EXPECT_EQ (initialComponents[0].rotation.Radians(), insertedComponents[0].rotation.Radians());
+    EXPECT_EQ (0, insertedComponents[0].GetMemberPriority()) << "MemberPriority of an inserted profile instance should be valid";
+    }
+
+/*---------------------------------------------------------------------------------**//**
+* @bsimethod                                                                     01/2019
++---------------+---------------+---------------+---------------+---------------+------*/
+TEST_F (ArbitraryCompositeProfileTestCase, GetProperties_LoadedInstance_ValidProperties)
+    {
+    bvector<ArbitraryCompositeProfileComponent> initialComponents = DefaultComponentVector();
+    CreateParams params (GetModel(), "Composite", initialComponents);
+
+    ArbitraryCompositeProfilePtr profilePtr = InsertElement<ArbitraryCompositeProfile> (params);
+    ASSERT_TRUE (profilePtr.IsValid());
+
+    GetDb().Elements().ClearCache();
+
+    ArbitraryCompositeProfileCPtr loadedProfilePtr = ArbitraryCompositeProfile::Get (GetDb(), profilePtr->GetElementId());
+    bvector<ArbitraryCompositeProfileComponent> loadedComponents = loadedProfilePtr->GetComponents();
+
+    for (int i = 0; i < 2; ++i)
+        {
+        EXPECT_EQ (initialComponents[i].singleProfileId, loadedComponents[i].singleProfileId);
+        EXPECT_EQ (initialComponents[i].offset, loadedComponents[i].offset);
+        EXPECT_EQ (initialComponents[i].rotation.Radians(), loadedComponents[i].rotation.Radians());
+        EXPECT_EQ (initialComponents[i].mirrorAboutYAxis, loadedComponents[i].mirrorAboutYAxis);
+        EXPECT_EQ (i, loadedComponents[i].GetMemberPriority());
+        }
     }
 
 /*---------------------------------------------------------------------------------**//**
@@ -121,12 +161,12 @@ TEST_F (ArbitraryCompositeProfileTestCase, SetProperties_ProfileInstance_ValidPr
     profilePtr->SetComponents (bvector<ArbitraryCompositeProfileComponent> { component });
 
     bvector<ArbitraryCompositeProfileComponent> components = profilePtr->GetComponents();
-    ArbitraryCompositeProfileComponent& firstComponent = components[0];
     EXPECT_EQ ("Composite", profilePtr->GetName());
-    EXPECT_EQ (component.singleProfileId, firstComponent.singleProfileId);
-    EXPECT_EQ (component.offset, firstComponent.offset);
-    EXPECT_EQ (component.mirrorAboutYAxis, firstComponent.mirrorAboutYAxis);
-    EXPECT_EQ (component.rotation.Radians(), firstComponent.rotation.Radians());
+    EXPECT_EQ (component.singleProfileId, components[0].singleProfileId);
+    EXPECT_EQ (component.offset, components[0].offset);
+    EXPECT_EQ (component.mirrorAboutYAxis, components[0].mirrorAboutYAxis);
+    EXPECT_EQ (component.rotation.Radians(), components[0].rotation.Radians());
+    EXPECT_EQ (0, components[0].GetMemberPriority());
     }
 
 /*---------------------------------------------------------------------------------**//**
@@ -164,4 +204,57 @@ TEST_F (ArbitraryCompositeProfileTestCase, Insert_InvalidSingleProfileId_FailedI
 
     CreateParams params (GetModel(), "Composite", bvector<ArbitraryCompositeProfileComponent> { component1, component2 });
     EXPECT_FAIL_Insert (params) << "Profile should fail with invalid SinglieProfile id.";
+    }
+
+/*---------------------------------------------------------------------------------**//**
+* @bsimethod                                                                     01/2019
++---------------+---------------+---------------+---------------+---------------+------*/
+TEST_F (ArbitraryCompositeProfileTestCase, Insert_QueryAspects_TwoRows)
+    {
+    CreateParams params (GetModel(), "Composite", bvector<ArbitraryCompositeProfileComponent> { CreateDefaultComponent(), CreateDefaultComponent() });
+    ProfilePtr profilePtr = InsertElement<ArbitraryCompositeProfile> (params);
+
+    Utf8CP pSqlString = "SELECT * FROM " PRF_SCHEMA ("ArbitraryCompositeProfileAspect") " WHERE Element.Id=?";
+
+    ECSqlStatement sqlStatement;
+    sqlStatement.Prepare (GetDb(), pSqlString);
+    sqlStatement.BindId (1, profilePtr->GetElementId());
+
+    ASSERT_EQ (BE_SQLITE_ROW, sqlStatement.Step()) << "Aspect for first component should exist";
+    ASSERT_EQ (BE_SQLITE_ROW, sqlStatement.Step()) << "Aspect for second component should exist";
+    ASSERT_EQ (BE_SQLITE_DONE, sqlStatement.Step()) << "No more aspects should exist for this profile";
+    }
+
+/*---------------------------------------------------------------------------------**//**
+* @bsimethod                                                                     01/2019
++---------------+---------------+---------------+---------------+---------------+------*/
+TEST_F (ArbitraryCompositeProfileTestCase, Delete_QueryAspects_ZeroRows)
+    {
+    CreateParams params (GetModel(), "Composite", bvector<ArbitraryCompositeProfileComponent> { CreateDefaultComponent(), CreateDefaultComponent() });
+    ProfilePtr profilePtr = InsertElement<ArbitraryCompositeProfile> (params);
+    profilePtr->Delete();
+
+    Utf8CP pSqlString = "SELECT * FROM " PRF_SCHEMA ("ArbitraryCompositeProfileAspect") " WHERE Element.Id=?";
+
+    ECSqlStatement sqlStatement;
+    sqlStatement.Prepare (GetDb(), pSqlString);
+    sqlStatement.BindId (1, profilePtr->GetElementId());
+
+    ASSERT_EQ (BE_SQLITE_DONE, sqlStatement.Step()) << "No aspects should exist for this profile";
+    }
+
+/*---------------------------------------------------------------------------------**//**
+* @bsimethod                                                                     01/2019
++---------------+---------------+---------------+---------------+---------------+------*/
+TEST_F (ArbitraryCompositeProfileTestCase, DeleteReferencedProfile_ExistingCompositeProfile_FailedDelete)
+    {
+    ProfilePtr singleProfilePtr = InsertElement<CircleProfile> (CircleProfile::CreateParams (GetModel(), "c", 1.0));
+    bvector<ArbitraryCompositeProfileComponent> components { CreateComponent (singleProfilePtr->GetElementId()), CreateComponent (singleProfilePtr->GetElementId()) };
+
+    CreateParams params (GetModel(), "Composite", components);
+    ProfilePtr profilePtr = InsertElement<ArbitraryCompositeProfile> (params);
+
+    ASSERT_EQ (DgnDbStatus::ForeignKeyConstraint, singleProfilePtr->Delete()) << "SingleProfile should fail to delete if it is being referenced by ArbitraryCompositeProfile";
+    profilePtr->Delete();
+    ASSERT_EQ (DgnDbStatus::Success, singleProfilePtr->Delete()) << "SingleProfile should succeed to delete when it's not referenced by ArbitraryCompositeProfile";
     }
