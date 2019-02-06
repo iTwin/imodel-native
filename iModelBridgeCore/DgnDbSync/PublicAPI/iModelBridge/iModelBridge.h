99898b5aa7509ce79a846ddfe8e674dc5e3488bd
Abeesh.Basheer <none@none>
2019-02-06 15:21:07 -0600

    EN: Remove the element aspect schema from bridges and point to the biscore version.

--- iModelBridgeCore/DgnDbSync/PublicAPI/iModelBridge/iModelBridge.h
+++ iModelBridgeCore/DgnDbSync/PublicAPI/iModelBridge/iModelBridge.h
@@ -43,8 +43,8 @@ typedef std::shared_ptr<struct ClientInfo> ClientInfoPtr;
 //! @brief Eneds a table of translatable strings contained in an iModelBridge.
 #define IMODELBRIDGEFX_TRANSLATABLE_STRINGS_END };
 
-#define XTRN_SRC_ASPCT_ECSCHEMA_NAME            "SourceInfo"
-#define XTRN_SRC_ASPCT_SCHEMA(name)             XTRN_SRC_ASPCT_ECSCHEMA_NAME "." name
+
+#define XTRN_SRC_ASPCT_SCHEMA(name)             BIS_ECSCHEMA_NAME "." name
 #define XTRN_SRC_ASPCT_CLASS                    "ExternalSourceAspect"
 #define XTRN_SRC_ASPCT_FULLCLASSNAME            XTRN_SRC_ASPCT_SCHEMA(XTRN_SRC_ASPCT_CLASS)
 #define XTRN_SRC_ASPCT_Scope                    "Scope"
@@ -1054,8 +1054,6 @@ public:
     //! @}
 
     bool HadAnyChanges() const { return m_hadAnyChanges; }
-
-    IMODEL_BRIDGE_EXPORT BentleyStatus  ImportElementAspectSchema(bool& madeChanges, bool force, DgnDbR dgndb);
 
     //! @name Feature Flags
     //! @{
--- iModelBridgeCore/DgnDbSync/SyncInfo.cpp
+++ iModelBridgeCore/DgnDbSync/SyncInfo.cpp
@@ -1852,42 +1852,6 @@ BentleyStatus SyncInfo::AttachToProject(DgnDb& targetProject, BeFileNameCR dbNam
     return OnAttach(targetProject);
     }
 
-/*---------------------------------------------------------------------------------**//**
-* @bsimethod                                    Abeesh.Basheer                  11/2018
-+---------------+---------------+---------------+---------------+---------------+------*/
-static BentleyStatus   importElementAspectSchema(DgnDbR db)
-    {
-    if (db.Schemas().ContainsSchema(XTRN_SRC_ASPCT_ECSCHEMA_NAME))
-        return BSISUCCESS;
-
-    BeFileName schemaPathname = T_HOST.GetIKnownLocationsAdmin().GetDgnPlatformAssetsDirectory();
-    schemaPathname.AppendToPath(L"ECSchemas/Application/SourceInfo.ecschema.xml");
-
-    if (!schemaPathname.DoesPathExist())
-        {
-        LOG.errorv("Error reading schema %ls", schemaPathname.GetName());
-        return BSIERROR;
-        }
-
-    ECN::ECSchemaPtr schema;
-    ECN::ECSchemaReadContextPtr schemaContext = ECN::ECSchemaReadContext::CreateContext();
-    schemaContext->AddSchemaLocater(db.GetSchemaLocater());
-    ECN::SchemaReadStatus status = ECN::ECSchema::ReadFromXmlFile(schema, schemaPathname.GetName(), *schemaContext);
-
-    // CreateSearchPathSchemaFileLocater
-    if (ECN::SchemaReadStatus::Success != status)
-        {
-        LOG.errorv("Error reading schema %ls", schemaPathname.GetName());
-        return BSIERROR;
-        }
-
-    bvector<ECN::ECSchemaCP> schemas;
-    schemas.push_back(schema.get());
-    if (SchemaStatus::Success != db.ImportV8LegacySchemas(schemas))
-        return BSIERROR;
-
-    return BSISUCCESS;
-    }
 
 /*---------------------------------------------------------------------------------**//**
 * @bsimethod                                    Sam.Wilson                      12/18
@@ -2449,8 +2413,7 @@ BentleyStatus SyncInfo::OnAttach(DgnDb& project)
     {
     m_dgndb = &project;
 
-    importElementAspectSchema(*m_dgndb);
-
+    
     if (!m_dgndb->TableExists(SYNCINFO_ATTACH(SYNC_TABLE_File)))
         {
         // We are creating a new syncinfo file
