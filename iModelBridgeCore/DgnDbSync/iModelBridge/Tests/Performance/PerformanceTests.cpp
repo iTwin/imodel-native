fcaad99efae9e9c9c51f3fb2f5f13af60b27c96d
Abeesh.Basheer <none@none>
2019-02-12 10:56:03 -0600

    Added a test that creates million elements to test the impact of aspect change.

--- iModelBridgeCore/DgnDbSync/iModelBridge/Tests/Performance/PerformanceTests.cpp
+++ iModelBridgeCore/DgnDbSync/iModelBridge/Tests/Performance/PerformanceTests.cpp
@@ -129,16 +129,19 @@ struct TestSourceItemNoId : iModelBridgeSyncInfoFile::ISourceItem
 //=======================================================================================
 struct TestSourceItemWithId : iModelBridgeSyncInfoFile::ISourceItem
     {
-    Utf8String m_id;
-    Utf8String m_content;
-    Utf8String m_type;
-    TestSourceItemWithId(Utf8StringCR id, Utf8StringCR content, Utf8StringCR type) : m_id(id), m_content(content), m_type(type) {}
-    Utf8String _GetId() override  {return m_id;}
+    int m_id;
+    int m_content;
+    iModelBridgeSyncInfoFile::ROWID m_scope;
+    Utf8String m_kind;
+    TestSourceItemWithId(int id, int content, iModelBridgeSyncInfoFile::ROWID scope, Utf8StringCR kind)
+        : m_id(id),m_content(content),m_scope(scope),m_kind(kind)
+    {}
+    Utf8String _GetId() override  {return Utf8PrintfString("%d",m_id);}
     double _GetLastModifiedTime() override {return 0.0;}
     Utf8String _GetHash() override
         {
         SHA1 sha1;
-        sha1(m_content);
+        sha1(Utf8PrintfString("%d", m_content));
         return sha1.GetHashString();
         }
     };
@@ -162,7 +165,6 @@ static BeFileName getiModelBridgeTestsOutputDir(WCharCP subdir)
 BEGIN_BENTLEY_DGN_NAMESPACE
 struct TestIModelHubFwkClientForBridges : TestIModelHubClientForBridges
     {
-    std::deque < bool> m_expect;
     TestIModelHubFwkClientForBridges(BeFileNameCR testWorkDir) 
         : TestIModelHubClientForBridges(testWorkDir)
         {  }
@@ -174,29 +176,23 @@ END_BENTLEY_DGN_NAMESPACE
 //=======================================================================================
 // @bsistruct                                                   Sam.Wilson   04/17
 //=======================================================================================
-struct iModelBridgeTests_Test1_Bridge : iModelBridgeWithSyncInfoBase
+struct iModelBridgeTests_PerfTest_Bridge : iModelBridgeWithSyncInfoBase
 {
-    std::vector <TestSourceItemWithId> m_foo_items;
-    
     TestIModelHubFwkClientForBridges& m_testIModelHubClientForBridges;
     iModelBridgeSyncInfoFile::ROWID m_docScopeId;
     bool m_jobTransChanged = false;
     int m_changeCount = 0;
 
-    struct
-        {
-        bool findJobSubject = false;
-        bool anyChanges = false;
-        bool anyDeleted = false;
-        bool assignmentCheck = false;
-        bool jobTransChanged = false;
-        bvector<Utf8String> docsDeleted;
-        } m_expect;
+    
+    int m_numItems;
+    int m_numScopes;
+    std::vector< iModelBridgeSyncInfoFile::ROWID> m_scopes;
+    std::function <TestSourceItemWithId (int )> m_generator;
 
     Utf8String ComputeJobSubjectCodeValue()
         {
         // note that the jobs are root specific and so the job subject's code must include the input filename
-        Utf8String jobCode("iModelBridgeTests_Test1_Bridge - ");
+        Utf8String jobCode("iModelBridgeTests_PerfTest_Bridge - ");
         jobCode.append(Utf8String(_GetParams().GetInputFileName()).c_str());
         return jobCode;
         }
@@ -207,34 +203,17 @@ struct iModelBridgeTests_Test1_Bridge : iModelBridgeWithSyncInfoBase
 
     void _OnDocumentDeleted(Utf8StringCR docId, iModelBridgeSyncInfoFile::ROWID docrid) override
         {
-        ASSERT_TRUE(std::find(m_expect.docsDeleted.begin(), m_expect.docsDeleted.end(), docId) != m_expect.docsDeleted.end());
-
-        for (auto rec : m_syncInfo.MakeIteratorByScope(docrid))
-            {
-            ASSERT_TRUE(m_expect.anyDeleted);
-            if (!rec.GetDgnElementId().IsValid())
-                continue; // not every record in syncinfo is an element
-            auto el = GetDgnDbR().Elements().GetElement(rec.GetDgnElementId());
-            ASSERT_TRUE(el.IsValid());
-            el->Delete();
-            }
-
-        m_testIModelHubClientForBridges.m_expect.push_back(m_expect.anyDeleted);
         }
 
     SubjectCPtr _FindJob() override
         {
         DgnCode jobCode = Subject::CreateCode(*GetDgnDbR().Elements().GetRootSubject(), ComputeJobSubjectCodeValue().c_str());
         auto jobId = GetDgnDbR().Elements().QueryElementIdByCode(jobCode);
-        EXPECT_EQ(m_expect.findJobSubject, jobId.IsValid());
         return GetDgnDbR().Elements().Get<Subject>(jobId);
         }
 
     SubjectCPtr _InitializeJob() override
         {
-        EXPECT_TRUE(!m_expect.findJobSubject);
-
-        
         // Set up the model and category that my superclass's DoTest method uses
         DgnCode partitionCode = PhysicalPartition::CreateCode(*GetDgnDbR().Elements().GetRootSubject(), "PhysicalModel");
         if (!GetDgnDbR().Elements().QueryElementIdByCode(partitionCode).IsValid())
@@ -245,9 +224,7 @@ struct iModelBridgeTests_Test1_Bridge : iModelBridgeWithSyncInfoBase
 
         auto subjectObj = Subject::Create(*GetDgnDbR().Elements().GetRootSubject(), ComputeJobSubjectCodeValue().c_str());
         JobSubjectUtils::InitializeProperties(*subjectObj, _GetParams().GetBridgeRegSubKeyUtf8());
-        m_testIModelHubClientForBridges.m_expect.push_back(true);
-        m_expect.findJobSubject = true;
-
+        
         if (!GetDgnDbR().TableExists(DGN_TABLE_ProvenanceFile))
             DgnV8FileProvenance::CreateTable(GetDgnDbR());
         if (!GetDgnDbR().TableExists(DGN_TABLE_ProvenanceModel))
@@ -269,17 +246,13 @@ struct iModelBridgeTests_Test1_Bridge : iModelBridgeWithSyncInfoBase
 
     void DoConvertToBim(SubjectCR jobSubject);
 
-    void ConvertItem(TestSourceItemWithId& item, iModelBridgeSyncInfoFile::ChangeDetector&);
+    DgnElementId ConvertItem(TestSourceItemWithId& item, iModelBridgeSyncInfoFile::ChangeDetector&);
 
-    iModelBridgeTests_Test1_Bridge(TestIModelHubFwkClientForBridges& tc)
+    iModelBridgeTests_PerfTest_Bridge(TestIModelHubFwkClientForBridges& tc)
         :
         iModelBridgeWithSyncInfoBase(),
         m_testIModelHubClientForBridges(tc)
         {
-        m_foo_items.push_back(TestSourceItemWithId("0", "foo i0 - initial", "Foo"));
-        m_foo_items.push_back(TestSourceItemWithId("1", "foo i1 - initial", "Foo"));
-        m_foo_items.push_back(TestSourceItemWithId("0", "bar i0 - initial", "Bar"));
-        m_foo_items.push_back(TestSourceItemWithId("1", "bar i1 - initial", "Bar"));
         }
 };
 
@@ -317,20 +290,7 @@ DgnRevisionPtr TestIModelHubFwkClientForBridges::CaptureChangeSet(DgnDbP db, Utf
     BeAssert(db != nullptr);
 
     BeAssert(db->IsBriefcase());
-    bool expectedResult = false;
-    if (!m_expect.empty())
-        {
-        expectedResult = m_expect.front();
-        m_expect.pop_front();
-        }
-    else
-        {
-        fprintf(stderr, "m_expect is empty. Push comment = \"%s\"", comment);
-        BeAssert(false && "m_expect is empty");
-        }
-
-    BeAssert(expectedResult == anyTxnsInFile(*db));
-
+    
     // printf("*** %s : %d\n", comment, anyTxnsInFile(*db));
 
     if (!anyTxnsInFile(*db))
@@ -341,58 +301,50 @@ DgnRevisionPtr TestIModelHubFwkClientForBridges::CaptureChangeSet(DgnDbP db, Utf
     if (!changeSet.IsValid())
         {
         db->Revisions().FinishCreateRevision();
-        BeAssert(!expectedResult);
         return changeSet;
         }
 
     if (comment)
         changeSet->SetSummary(comment);
 
-    BeAssert(expectedResult);
-
     BeAssert(changeSet.IsValid());
-    BeAssert(Dgn::RevisionStatus::Success ==  db->Revisions().FinishCreateRevision());
-    BeAssert(BE_SQLITE_OK == db->SaveChanges());
+    auto revStatus = db->Revisions().FinishCreateRevision();
+    BeAssert(Dgn::RevisionStatus::Success == revStatus);
+    auto dbStatus = db->SaveChanges();
+    BeAssert(BE_SQLITE_OK == dbStatus);
 
-    // *** TBD: test for expected changes
-    changeSet->Dump(*db);
     return changeSet;
     }
 
 /*---------------------------------------------------------------------------------**//**
 * @bsimethod                                                    Sam.Wilson   10/17
 +---------------+---------------+---------------+---------------+---------------+------*/
-void iModelBridgeTests_Test1_Bridge::ConvertItem(TestSourceItemWithId& item, iModelBridgeSyncInfoFile::ChangeDetector& changeDetector)
+DgnElementId iModelBridgeTests_PerfTest_Bridge::ConvertItem(TestSourceItemWithId& item, iModelBridgeSyncInfoFile::ChangeDetector& changeDetector)
     {
-    Utf8CP itemKind = "ItemKind";   // we don't use kinds in this test
-
-    auto change = changeDetector._DetectChange(m_docScopeId, itemKind, item, nullptr, m_jobTransChanged);
+    auto change = changeDetector._DetectChange(item.m_scope, item.m_kind.c_str(), item, nullptr, m_jobTransChanged);
     if (iModelBridgeSyncInfoFile::ChangeDetector::ChangeType::Unchanged == change.GetChangeType())
         {
         changeDetector._OnElementSeen(change.GetSyncInfoRecord().GetDgnElementId());
-        return;
+        return change.GetSyncInfoRecord().GetDgnElementId();
         }
     
     ++m_changeCount;
-    ASSERT_TRUE(m_expect.anyChanges);
     iModelBridgeSyncInfoFile::ConversionResults results;
     results.m_element = iModelBridgePerformanceTests::CreateGenericPhysicalObject(*m_db);
     DgnElementTransformer::ApplyTransformTo(*results.m_element, GetSpatialDataTransform());
-    ASSERT_EQ(BentleyStatus::SUCCESS, changeDetector._UpdateBimAndSyncInfo(results, change));
+    auto status = changeDetector._UpdateBimAndSyncInfo(results, change);
+    BeAssert(BE_SQLITE_OK == status);
+    return results.m_element->GetElementId();
     }
 
 /*---------------------------------------------------------------------------------**//**
 * @bsimethod                                                    Sam.Wilson   10/17
 +---------------+---------------+---------------+---------------+---------------+------*/
-void iModelBridgeTests_Test1_Bridge::DoConvertToBim(SubjectCR jobSubject)
+void iModelBridgeTests_PerfTest_Bridge::DoConvertToBim(SubjectCR jobSubject)
     {
     // (Note: superclass iModelBridgeWithSyncInfoBase::_OnConvertToBim has already attached my syncinfo file to the bim.)
 
-    if (m_expect.assignmentCheck)
-        {
-        ASSERT_TRUE(IsFileAssignedToBridge(_GetParams().GetInputFileName()));
-        }
-
+    
     iModelBridgeSyncInfoFile::ChangeDetectorPtr changeDetector = GetSyncInfo().GetChangeDetectorFor(*this);
 
     iModelBridgeSyncInfoFile::ConversionResults docLink = RecordDocument(*changeDetector, _GetParams().GetInputFileName(), nullptr,
@@ -406,23 +358,23 @@ void iModelBridgeTests_Test1_Bridge::DoConvertToBim(SubjectCR jobSubject)
 
     Transform _newTrans, _oldTrans;
     m_jobTransChanged = DetectSpatialDataTransformChange(_newTrans, _oldTrans, *changeDetector, m_docScopeId, "JT", "JT");
-    ASSERT_EQ(m_expect.jobTransChanged, m_jobTransChanged);
-
-    // Convert the "items" in my (non-existant) source file.
-    for (auto item : m_foo_items)
+    //Create scopes
+    m_scopes.clear();
+    for (int index = 0; index < m_numScopes; ++index)
         {
-        if (item.m_type.EqualsI(Utf8String(_GetParams().GetInputFileName())))
-            ConvertItem(item, *changeDetector);
+        TestSourceItemWithId scopeItem(index, index, m_docScopeId, "Scope");
+        m_scopes.push_back(iModelBridgeSyncInfoFile::ROWID (ConvertItem(scopeItem, *changeDetector).GetValue()));
+        }
+    
+    // Convert the "items" in my (non-existant) source file.
+    for (int index =0; index <m_numItems; ++ index)
+        {
+        TestSourceItemWithId item = m_generator(index);
+        ConvertItem(item, *changeDetector);
         }
 
     //  Garbage-collect the elements that were abandoned.
     changeDetector->DeleteElementsNotSeenInScope(m_docScopeId);
-
-    bool anyChanges = (changeDetector->GetElementsConverted() != 0);
-
-    ASSERT_EQ((m_expect.anyChanges || m_expect.anyDeleted), anyChanges);
-
-    m_testIModelHubClientForBridges.m_expect.push_back(anyChanges);
     }
 
 /*---------------------------------------------------------------------------------**//**
@@ -430,7 +382,7 @@ void iModelBridgeTests_Test1_Bridge::DoConvertToBim(SubjectCR jobSubject)
 +---------------+---------------+---------------+---------------+---------------+------*/
 TEST_F(iModelBridgePerformanceTests, PerformanceTest)
     {
-    auto bridgeRegSubKey = L"iModelBridgeTests_Test1_Bridge";
+    auto bridgeRegSubKey = L"iModelBridgeTests_PerfTest_Bridge";
 
     auto testDir = getiModelBridgeTestsOutputDir(L"Test1");
 
@@ -467,7 +419,7 @@ TEST_F(iModelBridgePerformanceTests, PerformanceTest)
     iModelBridgeFwk::SetIModelClientForBridgesForTesting(testIModelHubClientForBridges);
 
     // Register the test bridge that fwk should run
-    iModelBridgeTests_Test1_Bridge testBridge(testIModelHubClientForBridges);
+    iModelBridgeTests_PerfTest_Bridge testBridge(testIModelHubClientForBridges);
     iModelBridgeFwk::SetBridgeForTesting(testBridge);
 
     BeFileName assignDbName(testDir);
@@ -479,70 +431,66 @@ TEST_F(iModelBridgePerformanceTests, PerformanceTest)
     testRegistry.AddRef(); // prevent ~iModelBridgeFwk from deleting this object.
     iModelBridgeFwk::SetRegistryForTesting(testRegistry);   // (takes ownership of pointer)
 
+    static const int numFiles = 120;
+    static const int numElementPerFile = 10000;
+
+    testBridge.m_numScopes = numFiles;
+    testBridge.m_numItems = 0;
+
     if (true)
         {
-        testIModelHubClientForBridges.m_expect.push_back(false);// Clear this flag at the outset. It is set by the test bridge as it runs.
-        testBridge.m_expect.findJobSubject = false;
-        testBridge.m_expect.anyChanges = true;
-        testBridge.m_expect.anyDeleted = false;
         // Ask the framework to run our test bridge to do the initial conversion and create the repo
         iModelBridgeFwk fwk;
         bvector<WCharCP> argptrs;
         MAKE_ARGC_ARGV(argptrs, args);
         ASSERT_EQ(BentleyApi::BSISUCCESS, fwk.ParseCommandLine(argc, argv));
         ASSERT_EQ(0, fwk.Run(argc, argv));
-        testIModelHubClientForBridges.m_expect.clear();
         }
-
+    
+    testBridge.m_numScopes = numFiles;
+    testBridge.m_numItems = numFiles * numElementPerFile;
     if (true)
         {
-        // Modify an item 
-        testBridge.m_foo_items[0].m_content = "changed";
-
-        // and run an update
-        // This time, we expect to find the repo and briefcase already there.
-        testIModelHubClientForBridges.m_expect.push_back(false);// iModelBridgeTests_Test1_Bridge - Foo (6640b375-a539-4e73-b3e1-2c0ceb912551) -  : 0
-        testIModelHubClientForBridges.m_expect.push_back(true);// iModelBridgeTests_Test1_Bridge - Foo (6640b375-a539-4e73-b3e1-2c0ceb912551) - dynamic schemas : 1
-        testIModelHubClientForBridges.m_expect.push_back(true);// iModelBridgeTests_Test1_Bridge - Foo (6640b375-a539-4e73-b3e1-2c0ceb912551) - definitions : 1
-        testIModelHubClientForBridges.m_expect.push_back(true);// iModelBridgeTests_Test1_Bridge - Foo (6640b375-a539-4e73-b3e1-2c0ceb912551) - comment in quotes : 1
-        testBridge.m_expect.findJobSubject = true;
-        testBridge.m_expect.anyChanges = true;
-        testBridge.m_expect.anyDeleted = false;
+        // Add items
+        //testBridge.m_foo_items[0].m_content = "changed";
+        testBridge.m_generator = [&](int index){
+        int scopeId = index % numElementPerFile;
+        TestSourceItemWithId item(index, index, testBridge.m_scopes[scopeId], "Item");
+        return item;
+        };
         iModelBridgeFwk fwk;
         bvector<WCharCP> argptrs;
         MAKE_ARGC_ARGV(argptrs, args);
         ASSERT_EQ(BentleyApi::BSISUCCESS, fwk.ParseCommandLine(argc, argv));
         ASSERT_EQ(0, fwk.Run(argc, argv));
-        testIModelHubClientForBridges.m_expect.clear();
         }
 
+    if (true)
+        {
+        // and run an update
+        testBridge.m_generator = [&](int index) {
+            int scopeId = index % numElementPerFile;
+            TestSourceItemWithId item(index, 2 * index, testBridge.m_scopes[scopeId], "Item");
+            return item;
+        };
+        iModelBridgeFwk fwk;
+        bvector<WCharCP> argptrs;
+        MAKE_ARGC_ARGV(argptrs, args);
+        ASSERT_EQ(BentleyApi::BSISUCCESS, fwk.ParseCommandLine(argc, argv));
+        ASSERT_EQ(0, fwk.Run(argc, argv));
+        }
     if (true)
         {
         // Run an update with no changes
-        testIModelHubClientForBridges.m_expect.push_back(false);// Clear this flag at the outset. It is set by the test bridge as it runs.
-        testBridge.m_expect.findJobSubject = true;
-        testBridge.m_expect.anyChanges = false;
-        testBridge.m_expect.anyDeleted = false;
+        testBridge.m_generator = [&](int index){
+        int scopeId = index % numElementPerFile;
+        TestSourceItemWithId item(index, 2 * index, testBridge.m_scopes[scopeId], "Item");
+        return item;
+        };
         iModelBridgeFwk fwk;
         bvector<WCharCP> argptrs;
         MAKE_ARGC_ARGV(argptrs, args);
         ASSERT_EQ(BentleyApi::BSISUCCESS, fwk.ParseCommandLine(argc, argv));
         ASSERT_EQ(0, fwk.Run(argc, argv));
         }
-
-    if (true)
-    {
-        // Run an update with deleting an item.
-        //testIModelHubClientForBridges.m_expect.push_back(false);// Clear this flag at the outset. It is set by the test bridge as it runs.
-        testIModelHubClientForBridges.m_expect.push_back(true);// iModelBridgeTests_Test1_Bridge - Foo (6640b375-a539-4e73-b3e1-2c0ceb912551) - delete
-        testBridge.m_expect.findJobSubject = true;
-        testBridge.m_expect.anyChanges = true;
-        testBridge.m_expect.anyDeleted = true;
-        testBridge.m_foo_items.erase(testBridge.m_foo_items.begin());
-        iModelBridgeFwk fwk;
-        bvector<WCharCP> argptrs;
-        MAKE_ARGC_ARGV(argptrs, args);
-        ASSERT_EQ(BentleyApi::BSISUCCESS, fwk.ParseCommandLine(argc, argv));
-        ASSERT_EQ(0, fwk.Run(argc, argv));
-    }
     }
