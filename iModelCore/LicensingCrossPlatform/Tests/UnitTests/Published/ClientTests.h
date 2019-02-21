5ae1615dc12e15f1a9f71d594270e845eb582dbb
Jason.Wichert <none@none>
2019-02-21 10:23:19 -0500

    make mocks member variables, formatting, remove BuddiProvider files that were moved in an earlier commit

--- iModelCore/LicensingCrossPlatform/Tests/UnitTests/Published/ClientTests.h
+++ iModelCore/LicensingCrossPlatform/Tests/UnitTests/Published/ClientTests.h
@@ -10,18 +10,32 @@
 #include "TestsHelper.h"
 #include "Utils/MockHttpHandler.h"
 
+#include "Mocks/BuddiProviderMock.h"
+#include "Mocks/PolicyProviderMock.h"
+#include "Mocks/UlasProviderMock.h"
+
 USING_NAMESPACE_BENTLEY_LICENSING_UNIT_TESTS
 
 class ClientTests : public ::testing::Test
     {
     private:
         std::shared_ptr<MockHttpHandler>    m_handler;
+        std::shared_ptr<BuddiProviderMock>  m_buddiProviderMock;
+        std::shared_ptr<PolicyProviderMock> m_policyProviderMock;
+        std::shared_ptr<UlasProviderMock>   m_ulasProviderMock;
     public:
         ClientTests();
         static void SetUpTestCase();
 
         MockHttpHandler& GetHandler() const;
         std::shared_ptr<MockHttpHandler> GetHandlerPtr() const;
+
+        BuddiProviderMock& GetBuddiProviderMock() const;
+        std::shared_ptr<BuddiProviderMock>  GetBuddiProviderMockPtr() const;
+        PolicyProviderMock& GetPolicyProviderMock() const;
+        std::shared_ptr<PolicyProviderMock> GetPolicyProviderMockPtr() const;
+        UlasProviderMock& GetUlasProviderMock() const;
+        std::shared_ptr<UlasProviderMock>   GetUlasProviderMockPtr() const;
 
         void TearDown();
     };
