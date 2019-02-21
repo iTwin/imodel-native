9ad81e437b589c284583d22c7fd47bfc77cef5c1
Luke.Lindsey <none@none>
2019-02-21 09:39:49 -0600

    Add new files

---
+++ iModelCore/LicensingCrossPlatform/Licensing/Providers/AuthHandlerProvider.cpp
@@ -0,0 +1,30 @@
+/*--------------------------------------------------------------------------------------+
+|
+|     $Source: Licensing/Providers/AuthHandlerProvider.cpp $
+|
+|  $Copyright: (c) 2019 Bentley Systems, Incorporated. All rights reserved. $
+|
++--------------------------------------------------------------------------------------*/
+
+#include "AuthHandlerProvider.h"
+
+USING_NAMESPACE_BENTLEY_LICENSING
+USING_NAMESPACE_BENTLEY_WEBSERVICES
+
+AuthHandlerProvider::AuthHandlerProvider
+    (
+    std::shared_ptr<IConnectAuthenticationProvider> authProvider,
+    IHttpHandlerPtr httpHandler
+    ) :
+    m_authProvider(authProvider),
+    m_httpHandler(httpHandler)
+    {}
+
+/*--------------------------------------------------------------------------------------+
+* @bsimethod Gets the http handler from IConnectAuthenticationProvider 
+* using the passed in handler
++---------------+---------------+---------------+---------------+---------------+------*/
+IHttpHandlerPtr AuthHandlerProvider::GetAuthHandler(Utf8StringCR serverUrl, IConnectAuthenticationProvider::HeaderPrefix prefix)
+    {
+    return m_authProvider->GetAuthenticationHandler(serverUrl, m_httpHandler, prefix);
+    }
