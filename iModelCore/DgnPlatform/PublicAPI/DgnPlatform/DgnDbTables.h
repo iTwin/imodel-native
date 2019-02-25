31501461c86c0ae1690b0c5a75ce80c2ae20e8a7
Carole.MacDonald <none@none>
2019-02-25 11:56:49 -0500

    VSTS#62958 - Add a definition for BIS_CLASS_ExternalSourceAspect since it is now part of the BisCore schema.

--- iModelCore/DgnPlatform/PublicAPI/DgnPlatform/DgnDbTables.h
+++ iModelCore/DgnPlatform/PublicAPI/DgnPlatform/DgnDbTables.h
@@ -2,7 +2,7 @@
 |
 |     $Source: PublicAPI/DgnPlatform/DgnDbTables.h $
 |
-|  $Copyright: (c) 2018 Bentley Systems, Incorporated. All rights reserved. $
+|  $Copyright: (c) 2019 Bentley Systems, Incorporated. All rights reserved. $
 |
 +--------------------------------------------------------------------------------------*/
 #pragma once
@@ -52,6 +52,7 @@
 #define BIS_CLASS_ElementAspect             "ElementAspect"
 #define BIS_CLASS_ElementMultiAspect        "ElementMultiAspect"
 #define BIS_CLASS_ElementUniqueAspect       "ElementUniqueAspect"
+#define BIS_CLASS_ExternalSourceAspect      "ExternalSourceAspect"
 #define BIS_CLASS_GeometricElement          "GeometricElement"
 #define BIS_CLASS_GeometricElement2d        "GeometricElement2d"
 #define BIS_CLASS_GeometricElement3d        "GeometricElement3d"
