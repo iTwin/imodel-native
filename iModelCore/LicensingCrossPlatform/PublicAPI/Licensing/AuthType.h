05b455568de35183157d3ffc87e807828ee3685f
Luke.Lindsey <none@none>
2019-02-28 18:36:59 -0600

    Add missing AuthType.h

---
+++ iModelCore/LicensingCrossPlatform/PublicAPI/Licensing/AuthType.h
@@ -0,0 +1,19 @@
+/*--------------------------------------------------------------------------------------+
+ |
+ |     $Source: PublicAPI/Licensing/AuthType.h $
+ |
+ |  $Copyright: (c) 2019 Bentley Systems, Incorporated. All rights reserved. $
+ |
+ +--------------------------------------------------------------------------------------*/
+#pragma once
+//__PUBLISH_SECTION_START__
+
+BEGIN_BENTLEY_LICENSING_NAMESPACE
+
+enum class AuthType
+    {
+    SAML = 1,
+    OIDC = 2
+    };
+
+END_BENTLEY_LICENSING_NAMESPACE
