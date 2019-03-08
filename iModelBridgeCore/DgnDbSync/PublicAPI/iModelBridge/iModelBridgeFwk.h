1424926099a8714b3aa8e5f3c6187586ab03df09
Wouter.Rombouts <none@none>
2019-03-08 17:53:06 +0100

    EN:Added iModelBridgeFwk::TestFeatureFlag wrapper for OBD Bridge Service checking LD

--- iModelBridgeCore/DgnDbSync/PublicAPI/iModelBridge/iModelBridgeFwk.h
+++ iModelBridgeCore/DgnDbSync/PublicAPI/iModelBridge/iModelBridgeFwk.h
@@ -372,6 +372,8 @@ public:
 
     IMODEL_BRIDGE_FWK_EXPORT void SetTokenProvider(WebServices::IConnectTokenProviderPtr provider);
 
+    IMODEL_BRIDGE_FWK_EXPORT BentleyStatus TestFeatureFlag (CharCP ff, bool& flag);
+
     IRepositoryManagerP GetRepositoryManager(DgnDbR db) const;
 
     //!Internal function.
