18f5870428e6f72ed50b9087c99cbc632abb084b
Jonas.Valiunas <none@none>
2019-04-02 12:01:05 +0300

    react to changes in buildingshared, remove dgnclientfx from buildingdomains build

--- Domains/Building/BuildingDomainInternal.h
+++ Domains/Building/BuildingDomainInternal.h
@@ -2,12 +2,13 @@
 |
 |     $Source: BuildingDomainInternal.h $
 |
-|  $Copyright: (c) 2018 Bentley Systems, Incorporated. All rights reserved. $
+|  $Copyright: (c) 2019 Bentley Systems, Incorporated. All rights reserved. $
 |
 +--------------------------------------------------------------------------------------*/
 #include "BuildingDomain\BuildingDomainApi.h"
 #include <AecUnits\AecUnitsApi.h>
-#include <Grids/GridsApi.h>
+#include <Grids/Domain/GridsDomain.h>
+#include <Grids/Elements/GridElementsAPI.h>
 #include <StructuralDomain/StructuralDomainApi.h>
 #include <Profiles/ProfilesApi.h>
 #include <FormsDomain/FormsDomainApi.h>
