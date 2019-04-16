c4a54a75b2d93446ce8447d607bfa3c224437f0e
Copyright-Script <none@none>
2019-04-16 14:28:49 -0400

    Remove VCS keywords from headers; leave generic copyright

--- Domains/AecUnits/tests/AecUnitsTests.h
+++ Domains/AecUnits/tests/AecUnitsTests.h
@@ -1,8 +1,6 @@
 /*--------------------------------------------------------------------------------------+
 |
-|     $Source: tests/AecUnitsTests.h $
-|
-|  $Copyright: (c) 2017 Bentley Systems, Incorporated. All rights reserved. $
+|  Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 |
 +--------------------------------------------------------------------------------------*/
 #include "AecUnitsBaseFixture.h"
