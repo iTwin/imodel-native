47f79a12d47ec8520c43269dc0ccf52c0d9d72e7
Copyright-Script <none@none>
2019-04-16 14:29:59 -0400

    Remove VCS keywords from headers; leave generic copyright

--- iModelBridgeCore/Pointools/PointoolsCommon/BuildTools/abmake.py
+++ iModelBridgeCore/Pointools/PointoolsCommon/BuildTools/abmake.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python
 # ---------------------------------------------------------------------------------------------------------------------------
-#  $Source: BuildTools/abmake.py $
 #
-#  $Copyright: (c) 2015 Bentley Systems, Incorporated. All rights reserved. $
+#  Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 #
 #  Author      : Lee Bull <Lee.Bull@bentley.com>
 #
