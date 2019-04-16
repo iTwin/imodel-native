09a98ed96f0c2db4150a87c6602ac386191ce10d
Copyright-Script <none@none>
2019-04-16 14:28:55 -0400

    Remove VCS keywords from headers; leave generic copyright

--- Domains/Grids/Elements/PublicApi/SurfaceSet.h
+++ Domains/Grids/Elements/PublicApi/SurfaceSet.h
@@ -1,8 +1,6 @@
 /*--------------------------------------------------------------------------------------+
 |
-|     $Source: Grids/Elements/PublicApi/SurfaceSet.h $
-|
-|  $Copyright: (c) 2018 Bentley Systems, Incorporated. All rights reserved. $
+|  Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 |
 +--------------------------------------------------------------------------------------*/
 #pragma once
--- Domains/Grids/ManipulatorExtensions/SketchLineGridSurfaceManipulatorExtension.cpp
+++ Domains/Grids/ManipulatorExtensions/SketchLineGridSurfaceManipulatorExtension.cpp
@@ -1,8 +1,6 @@
 /*--------------------------------------------------------------------------------------+
 |
-|     $Source: Grids/ManipulatorExtensions/SketchLineGridSurfaceManipulatorExtension.cpp $
-|
-|  $Copyright: (c) 2018 Bentley Systems, Incorporated. All rights reserved. $
+|  Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 |
 +--------------------------------------------------------------------------------------*/
 #include "PublicAPI/GridManipulatorsAPI.h"
