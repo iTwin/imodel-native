edea1dbad441b167340ba4bfd1262fe1fdc56e7c
Copyright-Script <none@none>
2019-04-16 14:13:08 -0400

    added missing copyright headers via addcopyright.py

--- Domains/SpatialComposition/Elements/PublicApi/CompositeVolume.h
+++ Domains/SpatialComposition/Elements/PublicApi/CompositeVolume.h
@@ -1,3 +1,8 @@
+/*--------------------------------------------------------------------------------------+
+|
+|  Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+|
++--------------------------------------------------------------------------------------*/
 #pragma once
 
 BEGIN_SPATIALCOMPOSITION_NAMESPACE
