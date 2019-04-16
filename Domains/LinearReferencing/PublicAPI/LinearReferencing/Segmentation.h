72b201f4efdeba8a18c7b88d06a26426d3ce1316
Copyright-Script <none@none>
2019-04-16 14:28:56 -0400

    Remove VCS keywords from headers; leave generic copyright

--- Domains/LinearReferencing/PublicAPI/LinearReferencing/Segmentation.h
+++ Domains/LinearReferencing/PublicAPI/LinearReferencing/Segmentation.h
@@ -1,8 +1,6 @@
 /*--------------------------------------------------------------------------------------+
 |
-|     $Source: PublicAPI/LinearReferencing/Segmentation.h $
-|
-|  $Copyright: (c) 2018 Bentley Systems, Incorporated. All rights reserved. $
+|  Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 |
 +--------------------------------------------------------------------------------------*/
 #pragma once
