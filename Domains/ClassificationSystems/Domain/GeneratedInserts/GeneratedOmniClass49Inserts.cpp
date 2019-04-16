3e5a69c3cc8c0b08302a8c034027462a0ae8ea14
Copyright-Script <none@none>
2019-04-16 14:28:52 -0400

    Remove VCS keywords from headers; leave generic copyright

--- Domains/ClassificationSystems/Domain/GeneratedInserts/GeneratedOmniClass49Inserts.cpp
+++ Domains/ClassificationSystems/Domain/GeneratedInserts/GeneratedOmniClass49Inserts.cpp
@@ -1,8 +1,6 @@
 /*--------------------------------------------------------------------------------------+
 |
-|  $Source: Domain/GeneratedInserts/GeneratedOmniClass49Inserts.cpp $
-|
-|  $Copyright: (c) 2019 Bentley Systems, Incorporated. All rights reserved. $
+|  Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 |
 +--------------------------------------------------------------------------------------*/
 //===========================================================================================
