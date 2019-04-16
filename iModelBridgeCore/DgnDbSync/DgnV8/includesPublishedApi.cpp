8e9e7bc33a60452c2fd2fe0ac44abd9ec55666ad
Copyright-Script <none@none>
2019-04-16 14:13:00 -0400

    added missing copyright headers via addcopyright.py

--- iModelBridgeCore/DgnDbSync/DgnV8/includesPublishedApi.cpp
+++ iModelBridgeCore/DgnDbSync/DgnV8/includesPublishedApi.cpp
@@ -1,3 +1,8 @@
+/*--------------------------------------------------------------------------------------+
+|
+|  Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+|
++--------------------------------------------------------------------------------------*/
 // the first include is here to make sure that some basic .h files such as bentley.h and dgnplatform.h are included first.
 // this is done because many .h files require but do not include these basic files.
 #include <Windows.h>
