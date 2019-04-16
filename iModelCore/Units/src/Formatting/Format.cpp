8e70ed3a1af655886440b73047d1955ce79fe5a0
Copyright-Script <none@none>
2019-04-16 14:30:17 -0400

    Remove VCS keywords from headers; leave generic copyright

--- iModelCore/Units/src/Formatting/Format.cpp
+++ iModelCore/Units/src/Formatting/Format.cpp
@@ -1,8 +1,6 @@
 ﻿/*--------------------------------------------------------------------------------------+
 |
-|     $Source: src/Formatting/Format.cpp $
-|
-|  $Copyright: (c) 2018 Bentley Systems, Incorporated. All rights reserved. $
+|  Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 |
 +--------------------------------------------------------------------------------------*/
 #include <UnitsPCH.h>
--- iModelCore/Units/src/PhenomenonDefinitions.cpp
+++ iModelCore/Units/src/PhenomenonDefinitions.cpp
@@ -1,8 +1,6 @@
 /*--------------------------------------------------------------------------------------+
 |
-|     $Source: src/PhenomenonDefinitions.cpp $
-|
-|  $Copyright: (c) 2018 Bentley Systems, Incorporated. All rights reserved. $
+|  Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 |
 +--------------------------------------------------------------------------------------*/
 
