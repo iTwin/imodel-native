56aa873c60800f4666cb95776f3058f21659eaf9
Copyright-Script <none@none>
2019-04-16 14:28:51 -0400

    Remove VCS keywords from headers; leave generic copyright

--- Domains/BuildingSpatial/Tests/NonPublished/BuildingSpatialTestFixtureBase.h
+++ Domains/BuildingSpatial/Tests/NonPublished/BuildingSpatialTestFixtureBase.h
@@ -1,8 +1,6 @@
 /*--------------------------------------------------------------------------------------+
 |
-|  $Source: Tests/NonPublished/BuildingSpatialTestFixtureBase.h $
-|
-|  $Copyright: (c) 2019 Bentley Systems, Incorporated. All rights reserved. $
+|  Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 |
 +--------------------------------------------------------------------------------------*/
 #pragma once
