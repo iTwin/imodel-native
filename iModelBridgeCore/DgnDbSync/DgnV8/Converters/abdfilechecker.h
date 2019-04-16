87099cb2536fc22cc4e231beca53c957ffe47a10
Abeesh.Basheer <none@none>
2019-04-16 14:41:56 -0500

    EN #40524  Disable ABD file assignment checker for Microstation bridge.

--- iModelBridgeCore/DgnDbSync/DgnV8/Converters/abdfilechecker.h
+++ iModelBridgeCore/DgnDbSync/DgnV8/Converters/abdfilechecker.h
@@ -2,13 +2,13 @@
 |
 |     $Source: DgnV8/Converters/abdfilechecker.h $
 |
-|  $Copyright: (c) 2018 Bentley Systems, Incorporated. All rights reserved. $
+|  $Copyright: (c) 2019 Bentley Systems, Incorporated. All rights reserved. $
 |
 +--------------------------------------------------------------------------------------*/
 #pragma once
 
 // comment out next line if something is wrong with abdfilechecker.cpp
-#define USEABDFILECHECKER
+//#define USEABDFILECHECKER
 
 #ifdef USEABDFILECHECKER
 bool dgnFile_isABDFile(DgnFilePtr file);
