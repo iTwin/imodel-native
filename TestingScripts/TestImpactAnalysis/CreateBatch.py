7ed8408420184e9974037c1ff7f9bc773298d580
Majd Uddin <Majd.Uddin@bentley.com>
2019-04-17 08:42:19 +0000

    Merged PR 24378: TIA Maps. Fixed long command file issue.
    TIA Maps. Fixed long command file issue.

--- TestingScripts/TestImpactAnalysis/CreateBatch.py
+++ TestingScripts/TestImpactAnalysis/CreateBatch.py
@@ -13,9 +13,15 @@ import os
 # bsimethod                                     Majd.Uddin    02/2018
 #-------------------------------------------------------------------------------------------
 def main():
-    command = sys.argv[1] + ' ' + sys.argv[2] + ' | ' + sys.argv[3] + ' ' + sys.argv[4]
+    command = sys.argv[1] + ' ' + sys.argv[2]
+    filter = ""
+    with open(sys.argv[3]) as f:
+        lines = f.readlines()
+        for line in lines:
+            filter = filter + line
+    command = command + ' ' + filter + ' | ' + sys.argv[4] + ' ' + sys.argv[5]
 
-    logsDir = os.path.dirname(sys.argv[4])
+    logsDir = os.path.dirname(sys.argv[5])
     batFile = os.path.join(logsDir, 'run.bat')
     print 'writing to file: ' + batFile
 
