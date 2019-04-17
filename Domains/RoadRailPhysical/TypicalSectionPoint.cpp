c50f6676aae2a8a47be05a938ba5ef1cc047d972
Copyright-Script <none@none>
2019-04-17 14:51:05 -0400

    Remove VCS keywords from headers; leave generic copyright

--- Domains/RoadRailPhysical/TypicalSectionPoint.cpp
+++ Domains/RoadRailPhysical/TypicalSectionPoint.cpp
@@ -1,8 +1,6 @@
 /*--------------------------------------------------------------------------------------+
 |
-|     $Source: TypicalSectionPoint.cpp $
-|
-|  $Copyright: (c) 2018 Bentley Systems, Incorporated. All rights reserved. $
+|  Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 |
 +--------------------------------------------------------------------------------------*/
 
