e90206c6df20d3f30599dbcae85ac4fc000fcafc
Copyright-Script <none@none>
2019-04-17 14:51:22 -0400

    Remove VCS keywords from headers; leave generic copyright

--- Domains/Structural/StructuralAddition.cpp
+++ Domains/Structural/StructuralAddition.cpp
@@ -1,8 +1,6 @@
 /*--------------------------------------------------------------------------------------+
 |
-|     $Source: StructuralAddition.cpp $
-|
-|  $Copyright: (c) 2018 Bentley Systems, Incorporated. All rights reserved. $
+|  Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 |
 +--------------------------------------------------------------------------------------*/
 #include "PublicAPI\StructuralAddition.h"
--- Domains/Structural/StructuralDomainUtilities.cpp
+++ Domains/Structural/StructuralDomainUtilities.cpp
@@ -1,8 +1,6 @@
 /*--------------------------------------------------------------------------------------+
 |
-|     $Source: StructuralDomainUtilities.cpp $
-|
-|  $Copyright: (c) 2018 Bentley Systems, Incorporated. All rights reserved. $
+|  Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 |
 +--------------------------------------------------------------------------------------*/
 #include "PublicAPI\StructuralDomainUtilities.h"
