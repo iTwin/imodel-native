3b1f3f9ff278ba76e0e8372db3753bd90405e5ca
Vincas.Razma <none@none>
2019-07-12 15:31:58 +0300

    Ignore XCode warning about possible misuse of comma operator SRB:Jeff.Marker

--- iModelCore/libsrc/facebook/VendorApi/folly/Conv.h
+++ iModelCore/libsrc/facebook/VendorApi/folly/Conv.h
@@ -1172,8 +1172,15 @@ FOLLY_WARN_UNUSED_RESULT inline typename std::enable_if<
     std::is_arithmetic<Tgt>::value,
     Expected<StringPiece, ConversionCode>>::type
 parseTo(StringPiece src, Tgt& out) {
+#ifdef __GNUC__
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wcomma"
+#endif
   return detail::convertTo<Tgt>(&src).then(
       [&](Tgt res) { return void(out = res), src; });
+#ifdef __GNUC__
+#pragma GCC diagnostic pop
+#endif
 }
 
 /*******************************************************************************
