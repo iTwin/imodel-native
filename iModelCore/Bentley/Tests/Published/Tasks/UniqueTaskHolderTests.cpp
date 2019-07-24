bd98c54c18970e727c42a6cf6b96729580507773
Vincas.Razma <none@none>
2019-07-24 15:54:38 +0300

    EN: add missing methods to UniqueTaskHolder that handle one task SRB:Grigas.Petraitis

--- iModelCore/Bentley/Tests/Published/Tasks/UniqueTaskHolderTests.cpp
+++ iModelCore/Bentley/Tests/Published/Tasks/UniqueTaskHolderTests.cpp
@@ -8,6 +8,7 @@
 
 #include <Bentley/Tasks/UniqueTaskHolder.h>
 #include <Bentley/Tasks/WorkerThread.h>
+#include "AsyncTestCheckpoint.h"
 
 /*---------------------------------------------------------------------------------**//**
 * @bsimethod                                    Benediktas.Lipnickas                03/16
@@ -103,7 +104,7 @@ TEST_F (UniqueTaskHolderTests, GetRunningTask_CalledTwiceWithThenTasks_ReturnsVa
             EXPECT_EQ ("UniqueTaskResult", result);
             }));
 
-        tasks.insert (taskHolder.GetTask (nullptr)
+        tasks.insert(taskHolder.GetTask([=] { return nullptr; })
                       ->Then ([&] (Utf8String result)
             {
             EXPECT_EQ (2, ++i);
@@ -158,4 +159,210 @@ TEST_F(UniqueTaskHolderTests, GetRunningTask_ReturnsCompletedTask_ExecutesAndPro
             });
         })
     ->Wait();
+    }
+
+//---------------------------------------------------------------------------------------
+// @betest                                      Benediktas.Lipnickas
+//---------------------------------------------------------------------------------------
+TEST_F(UniqueTaskHolderTests, GetTask_CalledAfterTaskIsCanceled_ReturnsDifferentTask)
+    {
+    UniqueTaskHolder<int> holder;
+
+    auto newTaskCallback = [&] (ICancellationTokenPtr token) -> AsyncTaskPtr<int>
+        {    
+        return std::make_shared<PackagedAsyncTask<int>>([=]
+            {
+            return 1;
+            });
+        };
+
+    auto t1 = holder.GetTask(newTaskCallback);
+    holder.CancelTask();
+    auto t2 = holder.GetTask(newTaskCallback);
+
+    EXPECT_FALSE(t1.get() == t2.get());
+    }
+
+//---------------------------------------------------------------------------------------
+// @betest                                      Benediktas.Lipnickas
+//---------------------------------------------------------------------------------------
+TEST_F(UniqueTaskHolderTests, GetTask_CalledAfterSameIdTaskIsCanceledButIsNotFinishedYet_SecondTaskCanAlsoBeCanceled)
+    {
+    auto thread1 = WorkerThread::Create();
+    auto thread2 = WorkerThread::Create();
+    EXPECT_FALSE(thread1->HasRunningTasks());
+    UniqueTaskHolder<int> holder;
+    AsyncTestCheckpoint a, b, c;
+
+    auto t1 = holder.GetTask([&] (ICancellationTokenPtr token)
+        {
+        EXPECT_FALSE(token->IsCanceled());
+        return thread1->ExecuteAsync<int>([&, token]
+            {
+            a.CheckinAndWait();
+            EXPECT_TRUE(token->IsCanceled());
+            b.CheckinAndWait();
+            return 0;
+            });
+        });
+    EXPECT_TRUE(thread1->HasRunningTasks());
+    a.WaitUntilReached();
+    holder.CancelTask();
+    a.Continue();
+
+    auto t2 = holder.GetTask([&] (ICancellationTokenPtr token)
+        {
+        EXPECT_FALSE(token->IsCanceled());
+        return thread2->ExecuteAsync<int>([&, token]
+            {
+            c.CheckinAndWait();
+            EXPECT_TRUE(token->IsCanceled());
+            return 0;
+            });
+        });
+
+    b.Continue();
+    t1->Wait();
+
+    holder.CancelTask();
+
+    c.Continue();
+    t2->Wait();
+    }
+
+//---------------------------------------------------------------------------------------
+// @betest                                      Benediktas.Lipnickas
+//---------------------------------------------------------------------------------------
+TEST_F(UniqueTaskHolderTests, CancelTask_CalledForExistingTask_TaskTokenIsSetToCanceled)
+    {
+    UniqueTaskHolder<int> holder;
+
+    auto t1 = holder.GetTask([&] (ICancellationTokenPtr token)
+        {
+        return std::make_shared<PackagedAsyncTask<int>>([=]
+            {
+            EXPECT_TRUE(token->IsCanceled());
+            return 1;
+            });
+        });
+    holder.CancelTask();
+
+    t1->Execute();
+    }
+
+//---------------------------------------------------------------------------------------
+// @betest                                      Vincas.Razma
+//---------------------------------------------------------------------------------------
+TEST_F(UniqueTaskHolderTests, CancelTask_CalledForExistingTask_ReturnsTask)
+    {
+    UniqueTaskHolder<int> holder;
+
+    auto t1 = holder.GetTask([] (ICancellationTokenPtr token)
+        {
+        return std::make_shared<PackagedAsyncTask<int>>([] {return 1; });
+        });
+    auto t2 = holder.CancelTask();
+
+    EXPECT_EQ(t2, t1);
+    }
+
+//---------------------------------------------------------------------------------------
+// @betest                                      Vincas.Razma
+//---------------------------------------------------------------------------------------
+TEST_F(UniqueTaskHolderTests, CancelTask_CalledForNotExistingTask_ReturnsNull)
+    {
+    UniqueTaskHolder<int> holder;
+
+    auto t1 = holder.CancelTask();
+
+    EXPECT_EQ(t1, nullptr);
+    }
+
+//---------------------------------------------------------------------------------------
+// @betest                                      Benediktas.Lipnickas
+//---------------------------------------------------------------------------------------
+TEST_F(UniqueTaskHolderTests, CancelTask_CalledForExistingTask_TaskTokenForNewTaskIsNotCanceled)
+    {
+    UniqueTaskHolder<int> holder;
+
+    auto t1 = holder.GetTask([&] (ICancellationTokenPtr token)
+        {
+        return std::make_shared<PackagedAsyncTask<int>>([=]
+            {
+            EXPECT_TRUE(token->IsCanceled());
+            return 1;
+            });
+        });
+    holder.CancelTask();
+
+    auto t2 = holder.GetTask([&] (ICancellationTokenPtr token)
+        {
+        return std::make_shared<PackagedAsyncTask<int>>([=]
+            {
+            EXPECT_FALSE(token->IsCanceled());
+            return 1;
+            });
+        });
+
+    t2->Execute();
+    t1->Execute();
+    }
+
+
+//---------------------------------------------------------------------------------------
+// @betest                                      Benediktas.Lipnickas
+//---------------------------------------------------------------------------------------
+TEST_F(UniqueTaskHolderTests, IsTaskRunning_CalledForNotExistingTask_False)
+    {
+    UniqueTaskHolder<int> holder;
+
+    EXPECT_FALSE(holder.IsTaskRunning());
+    }
+
+//---------------------------------------------------------------------------------------
+// @betest                                      Benediktas.Lipnickas
+//---------------------------------------------------------------------------------------
+TEST_F(UniqueTaskHolderTests, IsTaskRunning_FinishedTask_False)
+    {
+    auto thread = WorkerThread::Create();
+    UniqueTaskHolder<int> holder;
+
+    auto t1 = holder.GetTask([&] (ICancellationTokenPtr token)
+        {
+        return thread->ExecuteAsync<int>([]
+            {
+            return 0;
+            });
+        });
+    t1->Wait();
+
+    EXPECT_FALSE(holder.IsTaskRunning());
+    }
+
+//---------------------------------------------------------------------------------------
+// @betest                                      Benediktas.Lipnickas
+//---------------------------------------------------------------------------------------
+TEST_F(UniqueTaskHolderTests, IsTaskRunning_TaskNotYetFinished_True)
+    {
+    auto thread = WorkerThread::Create();
+
+    UniqueTaskHolder<int> holder;
+    AsyncTestCheckpoint a;
+
+    auto t1 = holder.GetTask([&] (ICancellationTokenPtr token)
+        {
+        return thread->ExecuteAsync<int>([&]
+            {
+            a.CheckinAndWait();
+            return 0;
+            });
+        });
+
+    a.WaitUntilReached();
+    EXPECT_TRUE(holder.IsTaskRunning());
+
+    a.Continue();
+    t1->Wait();
+
+    EXPECT_FALSE(holder.IsTaskRunning());
     }
