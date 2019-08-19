09f4768037d156f6284f5eb0ac447e1cd28fbfd2
Matt Gooding <Matt.Gooding@bentley.com>
2019-08-19 19:53:36 +0000

    Merged PR 41003: Basic smoke test for IModelJsNative.exportGraphics
    Basic smoke test for IModelJsNative.exportGraphics

--- iModelJsNodeAddon/api_package/ts/src/test/index.ts
+++ iModelJsNodeAddon/api_package/ts/src/test/index.ts
@@ -3,7 +3,7 @@
 * Licensed under the MIT License. See LICENSE.md in the project root for license terms.
 *--------------------------------------------------------------------------------------------*/
 import * as utils from "./utils";
-import { assert, DbResult, OpenMode } from "@bentley/bentleyjs-core";
+import { assert, DbResult, OpenMode, Id64Array } from "@bentley/bentleyjs-core";
 import { IModelJsNative } from "../IModelJsNative";
 
 // tslint:disable:no-console
@@ -28,8 +28,29 @@ function testSimpleDbQueries() {
     assert(!dgndb.isRedoPossible());
 }
 
+function testExportGraphicsBasics() {
+    utils.logTest("testExportGraphicsBasics");
+    // Find all 3D elements in the test file
+    const elementIdArray: Id64Array = [];
+    const statement = new imodeljsNative.ECSqlStatement();
+    statement.prepare(dgndb, "SELECT ECInstanceId FROM bis.GeometricElement3d");
+    while (statement.step() === DbResult.BE_SQLITE_ROW)
+        elementIdArray.push(statement.getValue(0).getId());
+    assert(elementIdArray.length > 0, "No 3D elements in test file");
+    // Expect a mesh to be generated for each element - valid for test.bim, maybe invalid for future test data
+    const elementsWithGraphics: any = {};
+    const onGraphics = (info: any) => { elementsWithGraphics[info.elementId] = true; };
+
+    const res: DbResult = dgndb.exportGraphics({ elementIdArray, onGraphics });
+
+    assert(res === 0, `IModelDb.exportGraphics returned ${res}`);
+    for (const id of elementIdArray)
+        assert(elementsWithGraphics[id] !== undefined, `No graphics generated for ${id}`);
+}
+
 // Run the tests
 imodeljsNative = utils.loadAddon();
 dgndb = openDgnDb(utils.dbFileName);
 
 testSimpleDbQueries();
+testExportGraphicsBasics();
