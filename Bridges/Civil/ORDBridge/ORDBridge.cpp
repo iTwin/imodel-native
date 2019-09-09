c1aeb87bf1f4e495452adea57462c508e6ee3fa4
Jeff Marker <Jeff.Marker@bentley.com>
2019-09-09 09:22:27 -0400

    merge cleanup

--- Bridges/Civil/ORDBridge/ORDBridge.cpp
+++ Bridges/Civil/ORDBridge/ORDBridge.cpp
@@ -7,8 +7,7 @@
 +--------------------------------------------------------------------------------------*/
 #include "ORDBridgeInternal.h"
 #include <windows.h>
-//#include <BRepCore/PSolidUtil.h>
-#include <DgnPlatform/DgnBRep/PSolidUtil.h>
+#include <BRepCore/PSolidUtil.h>
 
 #define DefaultPhysicalPartitionName    "Physical"
 #define DefaultDesignAlignmentsName     "Road/Rail Design Alignments"
