a9de13c5d29477002bfe45648950992bff7fa9f0
Paul Connelly <22944042+pmconne@users.noreply.github.com>
2022-11-15 15:40:06 -0500

    chunk writing.

--- iModelJsNodeAddon/ElementMesh.cpp
+++ iModelJsNodeAddon/ElementMesh.cpp
@@ -2,6 +2,8 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the repository root for full copyright notice.
 *--------------------------------------------------------------------------------------------*/
+
+#include <GeomSerialization/GeomLibsFlatBufferApi.h>
 #include "IModelJsNative.h"
 #include "DgnDbWorker.h"
 
@@ -16,6 +18,31 @@ struct ElementMeshOptions {
   explicit ElementMeshOptions(BeJsConst);
 };
 
+enum class ChunkType : uint32_t {
+  ElementMeshes = 'HSML',
+  Polyface = 'CFLP',
+};
+
+struct ChunkHeader {
+  ChunkType m_type;
+  // The length of the chunk, excluding the fixed length of this header.
+  uint32_t m_dataLength;
+
+  ChunkHeader(ChunkType type, uint32_t length) : m_type(type), m_dataLength(length) { }
+};
+
+static_assert(sizeof(ChunkHeader) == 8, "unexpected ChunkHeader size");
+
+struct Chunk : ChunkHeader {
+  uint8_t const* m_data;
+
+  Chunk(ChunkType type, uint32_t length, uint8_t const* data) : ChunkHeader(type, length), m_data(data) { }
+
+  uint32_t GetTotalSize() const {
+    return sizeof(ChunkHeader) + m_dataLength;
+  }
+};
+
 struct ElementMeshWorker : DgnDbWorker {
 private:
   // Input
@@ -23,12 +50,30 @@ private:
   ElementMeshOptions m_options;
 
   // Output
+  ByteStream m_result;
 
   void Execute() final;
   void OnOK() final;
 
   ElementMeshWorker(DgnDbR db, Napi::Env env, DgnElementId elementId, ElementMeshOptions const& options)
     : DgnDbWorker(db, env), m_elementId(elementId), m_options(options) {
+    Append(Chunk(ChunkType::ElementMeshes, 0, nullptr));
+  }
+
+  void Append(Chunk const& chunk) {
+    BeAssert((nullptr == chunk.m_data) == (chunk.m_dataLength == 0));
+
+    m_result.Reserve(m_result.GetSize() + chunk.GetTotalSize());
+    m_result.Append(static_cast<ChunkHeader>(chunk));
+    if (nullptr != chunk.m_data)
+      m_result.Append(chunk.m_data, chunk.m_dataLength);
+  }
+
+  void Append(PolyfaceQueryCR polyface) {
+    bvector<uint8_t> flatBuffer;
+    BentleyGeometryFlatBuffer::GeometryToBytes(polyface, flatBuffer);
+    if (!flatBuffer.empty())
+      Append(Chunk(ChunkType::Polyface, static_cast<uint32_t>(flatBuffer.size()), &flatBuffer[0]));
   }
 public:
   static DgnDbWorkerPtr Create(DgnDbR db, Napi::CallbackInfo const& info);
