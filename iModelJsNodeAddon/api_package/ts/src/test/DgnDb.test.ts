f76b69ffc45bb4005e13c86d9a99c3f3b769cf8b
nick.tessier <22119573+nick4598@users.noreply.github.com>
2022-11-22 14:13:53 -0500

    add getASsetsDir import

--- iModelJsNodeAddon/api_package/ts/src/test/DgnDb.test.ts
+++ iModelJsNodeAddon/api_package/ts/src/test/DgnDb.test.ts
@@ -2,7 +2,7 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
-import { copyFile, dbFileName, iModelJsNative } from "./utils";
+import { copyFile, dbFileName, getAssetsDir, iModelJsNative } from "./utils";
 import { DbResult, Id64Array, IModelStatus } from "@itwin/core-bentley";
 import { IModelJsNative } from "../NativeLibrary";
 import { assert, expect } from "chai";
