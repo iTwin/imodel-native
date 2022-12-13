ce40ba3fd8a03f77ab7c7862bae6aebdda67e4b6
Simas Mikelionis <simas.mikelionis@bentley.com>
2022-12-13 12:37:39 +0200

    Preliminary test changes

--- iModelCore/ECDb/Tests/NonPublished/ECDbValidityCheckTests.cpp
+++ iModelCore/ECDb/Tests/NonPublished/ECDbValidityCheckTests.cpp
@@ -14,8 +14,144 @@ struct ECDbValidityCheckTests : ECDbTestFixture
 
     };
 
-TEST_F(ECDbValidityCheckTests, FirstTest) {
-    ASSERT_EQ(true, true);
+TEST_F(ECDbValidityCheckTests, LoadAllSchemas) {
+    ASSERT_EQ(SUCCESS, SetupECDb("loadAllSchemas.ecdb", SchemaItem(R"schema(<?xml version='1.0' encoding='utf-8' ?>
+        <ECSchema schemaName="TestSchema1" alias="ts1" version="01.00.01" xmlns="http://www.bentley.com/schemas/Bentley.ECXML.3.2">
+          <ECSchemaReference name="ECDbMap" version="02.00.00" alias="ecdbmap"/>
+          <ECEntityClass typeName="MyBaseClass">
+            <ECCustomAttributes>
+              <ClassMap xmlns="ECDbMap.02.00.00">
+                <MapStrategy>TablePerHierarchy</MapStrategy>
+              </ClassMap>
+              <ShareColumns xmlns="ECDbMap.02.00.00">
+                  <MaxSharedColumnsBeforeOverflow>4</MaxSharedColumnsBeforeOverflow>
+              </ShareColumns>
+            </ECCustomAttributes>
+          </ECEntityClass>
+          <ECEntityClass typeName="Class1">
+            <BaseClass>MyBaseClass</BaseClass>
+            <ECProperty propertyName="A" typeName="string" />
+            <ECProperty propertyName="B" typeName="string" />
+            <ECProperty propertyName="C" typeName="string" />
+            <ECProperty propertyName="D" typeName="string" />
+          </ECEntityClass>
+          <ECEntityClass typeName="Class2">
+            <BaseClass>MyBaseClass</BaseClass>
+            <ECProperty propertyName="D" typeName="string" />
+            <ECProperty propertyName="C" typeName="string" />
+            <ECProperty propertyName="B" typeName="string" />
+            <ECProperty propertyName="A" typeName="string" />
+          </ECEntityClass>
+          <ECEntityClass typeName="Class3">
+            <BaseClass>MyBaseClass</BaseClass>
+            <ECProperty propertyName="C" typeName="string" />
+            <ECProperty propertyName="A" typeName="string" />
+            <ECProperty propertyName="D" typeName="string" />
+            <ECProperty propertyName="B" typeName="string" />
+          </ECEntityClass>
+        </ECSchema>
+        )schema")));
+
+    ASSERT_EQ(SUCCESS,  GetHelper().ImportSchema(SchemaItem(R"schema(<?xml version='1.0' encoding='utf-8' ?>
+        <ECSchema schemaName="TestSchema2" alias="ts2" version="01.00.01" xmlns="http://www.bentley.com/schemas/Bentley.ECXML.3.2">
+          <ECSchemaReference name="ECDbMap" version="02.00.00" alias="ecdbmap"/>
+          <ECEntityClass typeName="MyBaseClass">
+            <ECCustomAttributes>
+              <ClassMap xmlns="ECDbMap.02.00.00">
+                <MapStrategy>TablePerHierarchy</MapStrategy>
+              </ClassMap>
+              <ShareColumns xmlns="ECDbMap.02.00.00">
+                  <MaxSharedColumnsBeforeOverflow>4</MaxSharedColumnsBeforeOverflow>
+              </ShareColumns>
+            </ECCustomAttributes>
+          </ECEntityClass>
+          <ECEntityClass typeName="Class1">
+            <BaseClass>MyBaseClass</BaseClass>
+            <ECProperty propertyName="A" typeName="string" />
+            <ECProperty propertyName="B" typeName="string" />
+            <ECProperty propertyName="C" typeName="string" />
+            <ECProperty propertyName="D" typeName="string" />
+          </ECEntityClass>
+          <ECEntityClass typeName="Class2">
+            <BaseClass>MyBaseClass</BaseClass>
+            <ECProperty propertyName="D" typeName="string" />
+            <ECProperty propertyName="C" typeName="string" />
+            <ECProperty propertyName="B" typeName="string" />
+            <ECProperty propertyName="A" typeName="string" />
+          </ECEntityClass>
+          <ECEntityClass typeName="Class3">
+            <BaseClass>MyBaseClass</BaseClass>
+            <ECProperty propertyName="C" typeName="string" />
+            <ECProperty propertyName="A" typeName="string" />
+            <ECProperty propertyName="D" typeName="string" />
+            <ECProperty propertyName="B" typeName="string" />
+          </ECEntityClass>
+        </ECSchema>
+        )schema")));
+
+    auto allSchemas = m_ecdb.Schemas().GetSchemas(true);
+
+    ASSERT_NE(0, allSchemas.size());
+}
+
+TEST_F(ECDbValidityCheckTests, LoadAllSchemasFail) {
+	ASSERT_EQ(true, false);
+}
+
+TEST_F(ECDbValidityCheckTests, LoadAllClassMaps) {
+	ASSERT_EQ(true, false);
+}
+
+TEST_F(ECDbValidityCheckTests, ClassIdCheck) {
+    ASSERT_EQ(SUCCESS, SetupECDb("loadAllSchemas.bim", SchemaItem(R"schema(<?xml version='1.0' encoding='utf-8' ?>
+        <ECSchema schemaName="TestSchema1" alias="ts1" version="01.00.01" xmlns="http://www.bentley.com/schemas/Bentley.ECXML.3.2">
+          <ECSchemaReference name="ECDbMap" version="02.00.00" alias="ecdbmap"/>
+          <ECEntityClass typeName="MyBaseClass">
+            <ECCustomAttributes>
+              <ClassMap xmlns="ECDbMap.02.00.00">
+                <MapStrategy>TablePerHierarchy</MapStrategy>
+              </ClassMap>
+              <ShareColumns xmlns="ECDbMap.02.00.00">
+                  <MaxSharedColumnsBeforeOverflow>4</MaxSharedColumnsBeforeOverflow>
+              </ShareColumns>
+            </ECCustomAttributes>
+          </ECEntityClass>
+          <ECEntityClass typeName="Class1">
+            <BaseClass>MyBaseClass</BaseClass>
+            <ECProperty propertyName="A" typeName="string" />
+            <ECProperty propertyName="B" typeName="string" />
+            <ECProperty propertyName="C" typeName="string" />
+            <ECProperty propertyName="D" typeName="string" />
+          </ECEntityClass>
+          <ECEntityClass typeName="Class2">
+            <BaseClass>MyBaseClass</BaseClass>
+            <ECProperty propertyName="D" typeName="string" />
+            <ECProperty propertyName="C" typeName="string" />
+            <ECProperty propertyName="B" typeName="string" />
+            <ECProperty propertyName="A" typeName="string" />
+          </ECEntityClass>
+          <ECEntityClass typeName="Class3">
+            <BaseClass>MyBaseClass</BaseClass>
+            <ECProperty propertyName="C" typeName="string" />
+            <ECProperty propertyName="A" typeName="string" />
+            <ECProperty propertyName="D" typeName="string" />
+            <ECProperty propertyName="B" typeName="string" />
+          </ECEntityClass>
+        </ECSchema>
+        )schema")));
+
+    auto allSchemas = m_ecdb.Schemas().GetSchemas(true);
+
+    ASSERT_NE(0, allSchemas.size());
+
+	ECSqlStatement statement;
+  	ASSERT_EQ(ECSqlStatus::Success, statement.Prepare(m_ecdb,
+    "SELECT COUNT(*) FROM bis.Element LEFT JOIN meta.ECClassDef on Element.ECClassId = ECClassDef.ECInstanceId WHERE ECClassDef.ECInstanceId IS NULL"));
+
+ 	// "SELECT COUNT(*) FROM bis.Element LEFT JOIN meta.ECClassDef on Element.ECClassId = ECClassDef.ECInstanceId WHERE ECClassDef.ECInstanceId IS NULL;" 
+
+	ASSERT_EQ(BE_SQLITE_ROW, statement.Step());
+	ASSERT_EQ(0, statement.GetValueInt64(0));
 }
 
 END_ECDBUNITTESTS_NAMESPACE
