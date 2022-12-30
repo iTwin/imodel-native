17f41e07ecdb4f28c46403eea8db900b1f2532e2
Chuck Kirschman <29206046+chuckkir@users.noreply.github.com>
2022-12-30 10:43:12 -0500

    Removed iModelCoreOpen build strategy.

--- build/buildlinux.sh
+++ build/buildlinux.sh
@@ -3,4 +3,4 @@ SrcRoot="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/..
 export BMAKE_OPT="-I"$SrcRoot"imodel-native/build/PublicSDK"
 export ToolCache="$SrcRoot"imodel-native/build/toolcache/
 export NDEBUG=1
-python3 "$SrcRoot"imodel-native/build/BentleyBuild/BentleyBuild.py -s  "$SrcRoot"imodel-native/build/strategies/iModelCoreOpen.BuildStrategy.xml+"$SrcRoot"imodel-native/build/strategies/iModelJsNodeAddonOpen.BuildStrategy.xml -a linuxx64 --srcroot="$SrcRoot" --outputroot="$SrcRoot"../out build ${@:1}
+python3 "$SrcRoot"imodel-native/build/BentleyBuild/BentleyBuild.py -s "$SrcRoot"imodel-native/build/strategies/iModelJsNodeAddonOpen.BuildStrategy.xml -a linuxx64 --srcroot="$SrcRoot" --outputroot="$SrcRoot"../out build ${@:1}
