3d74ac88dd175fe78bee0e5b9c0ff70d0807141b
Affan Khan <khanaffan@users.noreply.github.com>
2023-04-24 08:24:53 -0400

    lazy init prop_exists func

--- iModelCore/ECDb/ECDb/ECDbSqlFunctions.cpp
+++ iModelCore/ECDb/ECDb/ECDbSqlFunctions.cpp
@@ -21,7 +21,9 @@ void PropExistsFunc::_ComputeScalar(Context& ctx, int nArgs, DbValue* args) {
         ctx.SetResultError("prop_exists(I,S]) expect two args");
         return;
     }
-
+    if (m_propMap.Empty()) {
+        m_propMap.Build(m_ecdb, true);
+    }
     DbValue const& classIdVal = args[0];
     if (classIdVal.IsNull() || classIdVal.GetValueType() != DbValueType::IntegerVal )
         return;
