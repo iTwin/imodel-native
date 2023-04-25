cfdc6fb352ccdf251543eaa3e8f5822cc0cbcd25
saeeedtorabi <114527638+saeeedtorabi@users.noreply.github.com>
2023-04-25 16:20:10 -0400

    Added RayIntersectWithTriangleFast and missing headers
    - Added a fast ray triangle intersection method (which is 2.5 times faster
      than old method)
    - Added bunch of missing headers including Ray3d headers

--- iModelCore/GeomLibs/PublicAPI/Geom/internal2/GeomPrivateApi.h
+++ iModelCore/GeomLibs/PublicAPI/Geom/internal2/GeomPrivateApi.h
@@ -10,7 +10,11 @@ BEGIN_BENTLEY_GEOMETRY_NAMESPACE
 #define MSGEOM_SMALL_SET_SIZE  8
 
     
+#include "polycoff.h"
+#include "rotconic.h"
 #include "capi/smallsetrange1d_capi.h"
 #include "capi/dellipse4d_capi.h"
-#include "rotconic.h"
+#include "capi/qsic_capi.h"
+#include "capi/pencil_capi.h"
+#include "capi/polycoff_capi.h"
 END_BENTLEY_GEOMETRY_NAMESPACE
