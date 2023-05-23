2ba2796b16d582824224c305e54baf955c8983d3
Naveed.Khan <Naveed.Khan@bentley.com>
2023-05-23 15:32:57 +0200

    skip a test for all runners of a specific stream

--- iModelCore/iModelPlatform/Tests/DgnProject/Compatibility/CompatibilityRunner/RunPulledTestRunners.py
+++ iModelCore/iModelPlatform/Tests/DgnProject/Compatibility/CompatibilityRunner/RunPulledTestRunners.py
@@ -11,7 +11,7 @@ from shutil import rmtree
 #------------------------------------------------------------------------
 # bsimethod
 #------------------------------------------------------------------------
-def getIgnoreList(testRunner):
+def getIgnoreList(currentTestRunnerKey):
     scriptDir = os.path.dirname(os.path.realpath(__file__))
     ignoreListPath = os.path.join(scriptDir, "ignore_list.txt")
     fileData = open(ignoreListPath, "r")
@@ -21,26 +21,48 @@ def getIgnoreList(testRunner):
     rawList = list(filter(None, rawList))
 
     ignoreList = []
+    allVersions = False
+
     for item in rawList:
         if item.count(".") <= 1:
-            print("Test format in the ignore list is not correct");
-            print("Please follow the correct format:");
-            print("-> TestRunnerName.version.TestFixtureName.TestName");
-            print("-> Example: testRunnerNuget_bim0200_x64.2019.2.22.1.CompatibilityTestFixture.ECSqlColumnInfoForAliases");
-            print("->    Note: Use `*` in place of TestRunnerName.version for applying it on all old test runners");
+            print("Test format in the ignore list is not correct")
+            print("Please follow the correct format:")
+            print("-> TestRunnerName.version.TestFixtureName.TestName")
+            print("-> Example: testRunnerNuget_bim0200_x64.2019.2.22.1.CompatibilityTestFixture.ECSqlColumnInfoForAliases")
+            print("->    Note: Use `*` in place of TestRunnerName.version for applying it on all old test runners")
+            print("To skip a test for all test runners of only a specific stream, use following format:")
+            print("-> TestRunnerName.*.TestFixtureName.TestName")
+            print("-> Example: testRunnerNuget_bim0200_x64.*.CompatibilityTestFixture.ECSqlColumnInfoForAliases")
             sys.exit(1)
 
+        testRunnerInfo = item.split(".", 2)
+        currentRunnerName = currentTestRunnerKey.split(".", 1)[0]
+        testRunnerName = testRunnerInfo[0]
+
+        if testRunnerInfo[1] == "*":
+            allVersions = True
+        else:
+            allVersions = False
+
         cleanedItem = item.rsplit('.', 2)
-        if cleanedItem[0].lower() == testRunner.lower() or cleanedItem[0].lower() == "*":
+        testRunnerKey = cleanedItem[0]
+
+        # All versions of a specific stream runners
+        if testRunnerName.lower() == currentRunnerName.lower() and allVersions:
             cleanedItem = cleanedItem[1] + "." + cleanedItem[2] 
             ignoreList.append(cleanedItem)
 
+        # Specific key runner or all types of runners
+        if (testRunnerKey.lower() == currentTestRunnerKey.lower() or testRunnerKey.lower() == "*") and not allVersions:
+            cleanedItem = cleanedItem[1] + "." + cleanedItem[2] 
+            ignoreList.append(cleanedItem)
+            
     return ignoreList
 
 #------------------------------------------------------------------------
 # bsimethod
 #------------------------------------------------------------------------
-def createGTestFilter(exeDir, testRunner):
+def createGTestFilter(exeDir, currentTestRunner):
     exePath = os.path.join(exeDir, "iModelEvolutionTests.exe")
     sys.stdout.flush();
     output = subprocess.Popen([exePath, "--gtest_list_tests"], stdout=subprocess.PIPE).communicate()[0].decode()
@@ -57,7 +79,7 @@ def createGTestFilter(exeDir, testRunner):
 
     fixture = ''
     gtestCommand = ''
-    ignoreList = getIgnoreList(testRunner);
+    ignoreList = getIgnoreList(currentTestRunner);
     for item in testsList:
         if item.endswith('.'):
             fixture = item
