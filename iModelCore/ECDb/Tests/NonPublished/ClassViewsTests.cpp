c91ac772d36a3657676183f6f8465863c3ec068a
Affan Khan <khanaffan@users.noreply.github.com>
2023-07-06 14:10:38 +0500

    Add nav prop and link table relationship

--- iModelCore/ECDb/Tests/NonPublished/ClassViewsTests.cpp
+++ iModelCore/ECDb/Tests/NonPublished/ClassViewsTests.cpp
@@ -92,6 +92,97 @@ TEST_F(ClassViewsFixture, prepare_view_and_check_validate_sql_and_data) {
         ASSERT_EQ(ECSqlStatus::InvalidECSql, stmt.Prepare(m_ecdb, "update ts.SchemaClassesView set schemaname = ?"));
     }
 }
+
+/*---------------------------------------------------------------------------------**//**
+* @bsimethod
++---------------+---------------+---------------+---------------+---------------+------*/
+TEST_F(ClassViewsFixture, linktable_relationship_view) {
+    auto testSchema = SchemaItem(R"xml(<?xml version="1.0" encoding="utf-8" ?>
+    <ECSchema
+            schemaName="test_schema"
+            alias="ts"
+            version="1.0.0"
+            xmlns="http://www.bentley.com/schemas/Bentley.ECXML.3.2">
+        <ECSchemaReference name='ClassViews' version='01.00.00' alias='classView' />
+        <ECSchemaReference name='ECDbMeta' version='04.00.00' alias='meta' />
+        <ECRelationshipClass typeName="SchemaClassesView" description="" displayLabel="" strength="referencing" modifier="Abstract">
+            <ECCustomAttributes>
+                <View xmlns="ClassViews.01.00.00">
+                    <Query>
+                        SELECT * FROM meta.SchemaOwnsClasses
+                    </Query>
+                </View>
+            </ECCustomAttributes>
+            <Source multiplicity="(1..1)" roleLabel="contains" polymorphic="false">
+                <Class class="meta:ECSchemaDef"/>
+            </Source>
+            <Target multiplicity="(0..*)" roleLabel="is defined in" polymorphic="false">
+                <Class class="meta:ECClassDef"/>
+            </Target>
+        </ECRelationshipClass>
+    </ECSchema>)xml");
+
+    ASSERT_EQ(BE_SQLITE_OK, SetupECDb("test.ecdb"));
+    ASSERT_EQ(SUCCESS, ImportSchema(testSchema));
+    if (true){
+        ECSqlStatement stmt;
+        ASSERT_EQ(ECSqlStatus::Success, stmt.Prepare(m_ecdb, "SELECT * FROM ts.SchemaClassesView"));
+        auto nativeSql = "SELECT [K0],[K1],[K2],[K3],[K4],[K5] FROM (SELECT [SchemaOwnsClasses].[ECInstanceId] [K0],[SchemaOwnsClasses].[ECClassId] [K1],[SchemaOwnsClasses].[SourceECInstanceId] [K2],[SchemaOwnsClasses].[SourceECClassId] [K3],[SchemaOwnsClasses].[TargetECInstanceId] [K4],[SchemaOwnsClasses].[TargetECClassId] [K5] FROM (SELECT [ec_Class].[Id] ECInstanceId,34 ECClassId,[ec_Class].[SchemaId] SourceECInstanceId,35 SourceECClassId,[ec_Class].[Id] TargetECInstanceId,33 TargetECClassId FROM [main].[ec_Class] WHERE [ec_Class].[SchemaId] IS NOT NULL) [SchemaOwnsClasses])";
+        ASSERT_STREQ(nativeSql, stmt.GetNativeSql());
+        ASSERT_EQ(stmt.Step(), BE_SQLITE_ROW);
+        ASSERT_EQ(stmt.GetValueId<ECInstanceId>(0), ECInstanceId(1ull));
+        ASSERT_EQ(stmt.GetValueId<ECN::ECClassId>(1), ECClassId(34ull));
+        ASSERT_EQ(stmt.GetValueId<ECInstanceId>(2), ECInstanceId(1ull));
+        ASSERT_EQ(stmt.GetValueId<ECClassId>(3), ECClassId(35ull));
+        ASSERT_EQ(stmt.GetValueId<ECInstanceId>(4), ECInstanceId(1ull));
+        ASSERT_EQ(stmt.GetValueId<ECClassId>(5), ECClassId(33ull));
+    }
+}
+/*---------------------------------------------------------------------------------**//**
+* @bsimethod
++---------------+---------------+---------------+---------------+---------------+------*/
+TEST_F(ClassViewsFixture, return_nav_prop_from_view_query) {
+    auto testSchema = SchemaItem(R"xml(<?xml version="1.0" encoding="utf-8" ?>
+    <ECSchema
+            schemaName="test_schema"
+            alias="ts"
+            version="1.0.0"
+            xmlns="http://www.bentley.com/schemas/Bentley.ECXML.3.2">
+        <ECSchemaReference name='ClassViews' version='01.00.00' alias='classView' />
+        <ECSchemaReference name='ECDbMeta' version='04.00.00' alias='meta' />
+        <ECRelationshipClass typeName="SchemaClassesView" description="" displayLabel="" strength="referencing" modifier="Abstract">
+            <Source multiplicity="(1..1)" roleLabel="contains" polymorphic="false">
+                <Class class="meta:ECSchemaDef"/>
+            </Source>
+            <Target multiplicity="(0..*)" roleLabel="is defined in" polymorphic="false">
+                <Class class="ClassDefView"/>
+            </Target>
+        </ECRelationshipClass>
+        <ECEntityClass typeName="ClassDefView" description="" displayLabel="" modifier="Abstract">
+            <ECCustomAttributes>
+                <View xmlns="ClassViews.01.00.00">
+                    <Query>SELECT ECInstanceId, ECClassId, Schema FROM meta.ECClassDef</Query>
+                </View>
+            </ECCustomAttributes>
+            <ECNavigationProperty propertyName="Schema" relationshipName="SchemaClassesView" direction="backward"/>
+        </ECEntityClass>
+    </ECSchema>)xml");
+
+    ASSERT_EQ(BE_SQLITE_OK, SetupECDb("test.ecdb"));
+    ASSERT_EQ(SUCCESS, ImportSchema(testSchema));
+    if (true){
+        ECSqlStatement stmt;
+        ASSERT_EQ(ECSqlStatus::Success, stmt.Prepare(m_ecdb, "SELECT * FROM ts.ClassDefView"));
+        auto nativeSql = "SELECT [K0],[K1],[K2_0],[K2_1] FROM (SELECT [ECClassDef].[ECInstanceId] [K0],[ECClassDef].[ECClassId] [K1],[ECClassDef].[SchemaId] [K2_0],[ECClassDef].[SchemaRelECClassId] [K2_1] FROM (SELECT [Id] ECInstanceId,33 ECClassId,[SchemaId],(CASE WHEN [SchemaId] IS NULL THEN NULL ELSE 34 END) [SchemaRelECClassId] FROM [main].[ec_Class]) [ECClassDef])";
+        ASSERT_STREQ(nativeSql, stmt.GetNativeSql());
+        ASSERT_EQ(stmt.Step(), BE_SQLITE_ROW);
+        ASSERT_EQ(stmt.GetValueId<ECInstanceId>(0), ECInstanceId(1ull));
+        ASSERT_EQ(stmt.GetValueId<ECN::ECClassId>(1), ECClassId(33ull));
+        ECN::ECClassId relId;
+        ASSERT_EQ(stmt.GetValueNavigation<ECInstanceId>(2, &relId), ECInstanceId(1ull));
+        ASSERT_EQ(relId, ECClassId(34ull));
+    }
+}
 /*---------------------------------------------------------------------------------**//**
 * @bsimethod
 +---------------+---------------+---------------+---------------+---------------+------*/
@@ -430,202 +521,5 @@ TEST_F(ClassViewsFixture, all_specified_view_properties_must_return_by_view_quer
     }
 
 }
-// }
-//         listener.Dump("listener");
-
-// // Add issue listener
-// struct TestIssueListener: ECN::IIssueListener {
-//     mutable std::vector<Utf8String> m_issues;
-//     void _OnIssueReported(ECN::IssueSeverity severity, ECN::IssueCategory category, ECN::IssueType type, Utf8CP message) const override {
-//         m_issues.push_back(message);
-//     }
-//     Utf8String const& GetLastError() const { return m_issues.back();}
-//     Utf8String PopLastError() {
-//         Utf8String str = m_issues.back();
-//         m_issues.pop_back();
-//         return str;
-//     }
-//     void Dump (Utf8CP listenerName) {
-//         Utf8String cppCode;
-//         for(auto it = m_issues.rbegin(); it != m_issues.rend(); ++it) {
-//             cppCode.append(Utf8PrintfString("ASSERT_STREQ(\"%s\", %s.PopLastError().c_str());\r\n", (*it).c_str(), listenerName));
-//         }
-//         printf("%s", cppCode.c_str());
-//     }
-//     void Reset() { m_issues.clear(); }
-// };
-// /*---------------------------------------------------------------------------------**//**
-// * @bsimethod
-// +---------------+---------------+---------------+---------------+---------------+------*/
-// TEST_F(ClassViewsFixture, TransientView_Validation_Checks) {
-//     ASSERT_EQ(SUCCESS, SetupECDb("test.ecdb"));
-
-//     TestIssueListener listener;
-//     m_ecdb.AddIssueListener(listener);
-//     // ===================================================================================
-//     ASSERT_EQ(ERROR, ImportSchema(SchemaItem(R"xml(<?xml version="1.0" encoding="utf-8" ?>
-//     <ECSchema
-//             schemaName="test_schema"
-//             alias="ts"
-//             version="1.0.0"
-//             xmlns="http://www.bentley.com/schemas/Bentley.ECXML.3.2">
-//         <ECSchemaReference name='ClassView' version='01.00.00' alias='classView' />
-//         <ECEntityClass typeName="SchemaClassesView1" description="" displayLabel="" modifier="Abstract">
-//             <ECCustomAttributes>
-//                 <TransientView xmlns="ClassView.01.00.00">
-//                     <Query>
-//                         SELECT cd.ECInstanceId, sc.Name, cd.Name
-//                         FROM ts.SchemaClassesView2
-//                     </Query>
-//                 </TransientView>
-//             </ECCustomAttributes>
-//             <ECProperty propertyName="SchemaName" typeName="string" />
-//             <ECProperty propertyName="ClassName"  typeName="string"/>
-//         </ECEntityClass>
-//         <ECEntityClass typeName="SchemaClassesView2" description="" displayLabel="" modifier="Abstract">
-//             <ECCustomAttributes>
-//                 <TransientView xmlns="ClassView.01.00.00">
-//                     <Query>
-//                         SELECT cd.ECInstanceId, sc.Name, cd.Name
-//                         FROM ts.SchemaClassesView1
-//                     </Query>
-//                 </TransientView>
-//             </ECCustomAttributes>
-//             <ECProperty propertyName="SchemaName" typeName="string" />
-//             <ECProperty propertyName="ClassName"  typeName="string"/>
-//         </ECEntityClass>
-//     </ECSchema>)xml"))) << "Transient view cannot reference other views";
-//     ASSERT_STREQ("Failed to import ECClass 'test_schema:SchemaClassesView2'.", listener.PopLastError().c_str());
-//     ASSERT_STREQ("Failed to import ECClass '[test_schema].[SchemaClassesView2]'. It has view definition custom attribute but not configured correctly.", listener.PopLastError().c_str());
-//     ASSERT_STREQ("Invalid view class 'test_schema:SchemaClassesView2'. View query contain reference to another view class 'test_schema:SchemaClassesView1' which is not supported right now.", listener.PopLastError().c_str());
-//     listener.Reset();
-
-//     // ===================================================================================
-//     ASSERT_EQ(ERROR, ImportSchema(SchemaItem(R"xml(<?xml version="1.0" encoding="utf-8" ?>
-//     <ECSchema
-//             schemaName="test_schema"
-//             alias="ts"
-//             version="1.0.0"
-//             xmlns="http://www.bentley.com/schemas/Bentley.ECXML.3.2">
-//         <ECSchemaReference name='ClassView' version='01.00.00' alias='classView' />
-//         <ECEntityClass typeName="SchemaClassesView" description="" displayLabel="" modifier="Abstract">
-//             <ECCustomAttributes>
-//                 <TransientView xmlns="ClassView.01.00.00">
-//                     <Query>
-//                         SELECT cd.ECInstanceId, sc.Name, cd.Name
-//                         FROM ts.SchemaClassesView
-//                     </Query>
-//                 </TransientView>
-//             </ECCustomAttributes>
-//             <ECProperty propertyName="SchemaName" typeName="string" />
-//             <ECProperty propertyName="ClassName"  typeName="string"/>
-//         </ECEntityClass>
-//     </ECSchema>)xml"))) << "Transient view cannot reference it self";
-//     ASSERT_STREQ("Detected invalid views definitions for test_schema:SchemaClassesView.", listener.PopLastError().c_str());
-//     ASSERT_STREQ("Invalid view class 'test_schema:SchemaClassesView'. View query contain self reference to view class which is not allowed.", listener.PopLastError().c_str());
-//     listener.Reset();
-
-//     // ===================================================================================
-//     ASSERT_EQ(ERROR, ImportSchema(SchemaItem(R"xml(<?xml version="1.0" encoding="utf-8" ?>
-//     <ECSchema
-//             schemaName="test_schema"
-//             alias="ts"
-//             version="1.0.0"
-//             xmlns="http://www.bentley.com/schemas/Bentley.ECXML.3.2">
-//         <ECSchemaReference name='ClassView' version='01.00.00' alias='classView' />
-//         <ECEntityClass typeName="SchemaClassesView" description="" displayLabel="" modifier="Sealed">
-//             <ECCustomAttributes>
-//                 <TransientView xmlns="ClassView.01.00.00">
-//                     <Query>
-//                         SELECT cd.ECInstanceId, sc.Name, cd.Name
-//                         FROM meta.ECSchemaDef sc
-//                             JOIN meta.ECClassDef cd ON cd.Schema.Id=sc.ECInstanceId GROUP BY sc.Name, cd.Name LIMIT 10
-//                     </Query>
-//                 </TransientView>
-//             </ECCustomAttributes>
-//             <ECProperty propertyName="SchemaName" typeName="string" />
-//             <ECProperty propertyName="ClassName"  typeName="string"/>
-//         </ECEntityClass>
-//     </ECSchema>)xml"))) << "Transient view class must be abstract";
-//     ASSERT_STREQ("Failed to import ECClass 'test_schema:SchemaClassesView'.", listener.PopLastError().c_str());
-//     ASSERT_STREQ("Failed to import ECClass '[test_schema].[SchemaClassesView]'. It has view definition custom attribute but not configured correctly.", listener.PopLastError().c_str());
-//     ASSERT_STREQ("Invalid view class '[test_schema].[SchemaClassesView]'. 'TransientView' must be applied to a 'Abstract' class.", listener.PopLastError().c_str());
-//     listener.Reset();
-
-//     // ===================================================================================
-//     ASSERT_EQ(ERROR, ImportSchema(SchemaItem(R"xml(<?xml version="1.0" encoding="utf-8" ?>
-//     <ECSchema
-//             schemaName="test_schema"
-//             alias="ts"
-//             version="1.0.0"
-//             xmlns="http://www.bentley.com/schemas/Bentley.ECXML.3.2">
-//         <ECSchemaReference name='ClassView' version='01.00.00' alias='classView' />
-//         <ECEntityClass typeName="SchemaClassesView" description="" displayLabel="" modifier="Abstract">
-//             <ECCustomAttributes>
-//                 <TransientView xmlns="ClassView.01.00.00">
-//                     <Query></Query>
-//                 </TransientView>
-//             </ECCustomAttributes>
-//             <ECProperty propertyName="SchemaName" typeName="string" />
-//             <ECProperty propertyName="ClassName"  typeName="string"/>
-//         </ECEntityClass>
-//     </ECSchema>)xml"))) << "Transient view class must be abstract";
-//     ASSERT_STREQ("Failed to import ECClass 'test_schema:SchemaClassesView'.", listener.PopLastError().c_str());
-//     ASSERT_STREQ("Failed to import ECClass '[test_schema].[SchemaClassesView]'. It has view definition custom attribute but not configured correctly.", listener.PopLastError().c_str());
-//     ASSERT_STREQ("Invalid view class '[test_schema].[SchemaClassesView]'. 'Query' is required attribute for PersistedView/TransientView defintion custom attribute.", listener.PopLastError().c_str());
-//     listener.Reset();
-
-//     // ===================================================================================
-//     ASSERT_EQ(ERROR, ImportSchema(SchemaItem(R"xml(<?xml version="1.0" encoding="utf-8" ?>
-//     <ECSchema
-//             schemaName="test_schema"
-//             alias="ts"
-//             version="1.0.0"
-//             xmlns="http://www.bentley.com/schemas/Bentley.ECXML.3.2">
-//         <ECSchemaReference name='ClassView' version='01.00.00' alias='classView' />
-//         <ECEntityClass typeName="SchemaClassesView" description="" displayLabel="" modifier="Abstract">
-//             <ECCustomAttributes>
-//                 <TransientView xmlns="ClassView.01.00.00">
-//                     <Query>Update table</Query>
-//                 </TransientView>
-//             </ECCustomAttributes>
-//             <ECProperty propertyName="SchemaName" typeName="string" />
-//             <ECProperty propertyName="ClassName"  typeName="string"/>
-//         </ECEntityClass>
-//     </ECSchema>)xml"))) << "Transient view class must be abstract";
-//     ASSERT_STREQ("Detected invalid views definitions for test_schema:SchemaClassesView.", listener.PopLastError().c_str());
-//     ASSERT_STREQ("Invalid view class '[test_schema].[SchemaClassesView]'. Failed to prepare query.", listener.PopLastError().c_str());
-//     ASSERT_STREQ("Failed to parse ECSQL 'Update table': syntax error", listener.PopLastError().c_str());
-//     listener.Reset();
-//     // ===================================================================================
-//     ASSERT_EQ(ERROR, ImportSchema(SchemaItem(R"xml(<?xml version="1.0" encoding="utf-8" ?>
-//     <ECSchema
-//             schemaName="test_schema"
-//             alias="ts"
-//             version="1.0.0"
-//             xmlns="http://www.bentley.com/schemas/Bentley.ECXML.3.2">
-//         <ECSchemaReference name='ClassView' version='01.00.00' alias='classView' />
-//         <ECEntityClass typeName="SchemaClassesView" description="" displayLabel="" modifier="Abstract">
-//             <ECCustomAttributes>
-//                 <TransientView xmlns="ClassView.01.00.00">
-//                     <Query>
-//                         SELECT cd.ECInstanceId, sc.Name, cd.Name
-//                         FROM meta.ECSchemaDef sc
-//                             JOIN meta.ECClassDef cd ON cd.Schema.Id=sc.ECInstanceId GROUP BY sc.Name, cd.Name LIMIT 10
-//                     </Query>
-//                 </TransientView>
-//             </ECCustomAttributes>
-//             <ECProperty propertyName="SchemaName" typeName="string" />
-//             <ECProperty propertyName="ClassName"  typeName="string"/>
-//         </ECEntityClass>
-//     </ECSchema>)xml"))) << "Transient view class must be abstract";
-//     ASSERT_STREQ("Detected invalid views definitions for test_schema:SchemaClassesView.", listener.PopLastError().c_str());
-//     ASSERT_STREQ("Invalid view class '[test_schema].[SchemaClassesView]'. class does not have property with name 'Name' as defined in query at index 1", listener.PopLastError().c_str());
-//     ASSERT_STREQ("Invalid view class '[test_schema].[SchemaClassesView]'. class does not have property with name 'Name' as defined in query at index 1", listener.PopLastError().c_str());
-
-//     listener.Reset();
-// }
-
-
 
 END_ECDBUNITTESTS_NAMESPACE
