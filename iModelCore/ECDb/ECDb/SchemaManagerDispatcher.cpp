c620db784e7652ebad0abb6b2f4cfafa4fc8d057
Robert Schili <rschili@users.noreply.github.com>
2023-08-29 18:44:19 +0200

    Update iModelCore/ECDb/ECDb/SchemaManagerDispatcher.cpp
    Co-authored-by: affank <khanaffan@gmail.com>

--- iModelCore/ECDb/ECDb/SchemaManagerDispatcher.cpp
+++ iModelCore/ECDb/ECDb/SchemaManagerDispatcher.cpp
@@ -727,7 +727,9 @@ bool SchemaManager::Dispatcher::IsClassUnsupported(ECClassId classId) const
         std::function<void(ECClassId,bool)> collectEntitiesThatHaveCA;
         collectEntitiesThatHaveCA = [&baseUnsupportedEntityClasses, &collectEntitiesThatHaveCA, this](ECClassId caClassId,bool indirect) -> void {
             Statement stmt;
-            if (stmt.Prepare(m_ecdb, "SELECT ContainerId FROM main." TABLE_CustomAttribute " WHERE ContainerType = 30 AND ClassId = ?") != DbResult::BE_SQLITE_OK)
+if (stmt.Prepare(m_ecdb, SqlPrintfString("SELECT ContainerId FROM main." TABLE_CustomAttribute " WHERE ContainerType = %d AND ClassId = ?", (int)SchemaPersistenceHelper::GeneralizedCustomAttributeContainerType::Class)) != DbResult::BE_SQLITE_OK)
+                return;
+
                 return;
 
             if (stmt.BindId(1, caClassId) != DbResult::BE_SQLITE_OK)
