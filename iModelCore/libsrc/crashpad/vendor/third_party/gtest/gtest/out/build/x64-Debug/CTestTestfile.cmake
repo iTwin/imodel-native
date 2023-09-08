714f46db13796ebe4d031df2e1c00132575683fa
chuckkir <29206046+chuckkir@users.noreply.github.com>
2023-09-08 15:27:47 -0400

    Vendor version efaebfc482b7a44161015ef7f6661aa4526b1c26 of https://chromium.googlesource.com/crashpad/crashpad/ Committing this on the vendor branch to match up the current revision.

--- iModelCore/libsrc/crashpad/vendor/third_party/gtest/gtest/out/build/x64-Debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/imodel-native/src/imodel-native/iModelCore/libsrc/crashpad/vendor/third_party/gtest/gtest
-# Build directory: D:/imodel-native/src/imodel-native/iModelCore/libsrc/crashpad/vendor/third_party/gtest/gtest/out/build/x64-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
