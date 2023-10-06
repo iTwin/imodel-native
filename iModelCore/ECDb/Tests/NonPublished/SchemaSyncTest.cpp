64ef7bf73634205e8e5d61a353ca122f70c4cc30
Robert Schili <rschili@users.noreply.github.com>
2023-10-06 18:12:28 +0200

    adjust more tests to latest profile

--- iModelCore/ECDb/Tests/NonPublished/SchemaSyncTest.cpp
+++ iModelCore/ECDb/Tests/NonPublished/SchemaSyncTest.cpp
@@ -321,7 +321,7 @@ TEST_F(SchemaSyncTestFixture, PushSchemaWithoutInitializingSchemaChannel)
             ASSERT_EQ(BE_SQLITE_OK, b1->AbandonChanges());
             CheckHashes(*b1);
             //use specific hash below, because schema sync db is not using a seed but creating the db with the latest profile.
-            schemaSyncDb.WithReadOnly([&](ECDbR syncDb) { CheckHashes(syncDb, "74865ac4eeed46821b13f3662e8c34e51d1d2bda85defe2c069fcc8ae7091612", "17adf1cdba0c3c9970ec26d61bf5d8fa91808c3a98ce02f78ba0359e4eda11ee", "c4ca1cdd07de041e71f3e8d4b1942d29da89653c85276025d786688b6f576443"); });
+            schemaSyncDb.WithReadOnly([&](ECDbR syncDb) { CheckHashes(syncDb, "f02cac7910b0a1dd833d9501db2f46fab8cc253dfb874495d98da9e77982915a", "17adf1cdba0c3c9970ec26d61bf5d8fa91808c3a98ce02f78ba0359e4eda11ee", "c4ca1cdd07de041e71f3e8d4b1942d29da89653c85276025d786688b6f576443"); });
             }
     );
     }
