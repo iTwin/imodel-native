119a0a0a191e53f0d9751da0f02cf759fde78483
simjar <simonas.jarukaitis@bentley.com>
2023-10-12 15:12:31 +0300

    revert auto generated lines

--- iModelCore/ECDb/ECDb/ECSql/Parser/SqlFlex.cpp
+++ iModelCore/ECDb/ECDb/ECSql/Parser/SqlFlex.cpp
@@ -1,4 +1,4 @@
-#line 2 "C:\\repos\\imodelNative\\src\\imodel-native\\iModelCore\\ECDb\\Scripts\\\\..\\ECDb\\ECSql\\parser\\SQLflex.l"
+#line 2 "D:\\bsw\\git-native-4\\src\\imodel-native\\iModelCore\\ECDb\\Scripts\\\\..\\ECDb\\ECSql\\parser\\SQLflex.l"
 //------------------------------------------------------------
 //
 // Licensed to the Apache Software Foundation (ASF) under one
