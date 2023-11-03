feba8bc767db3cc7fe9808b75c8da72454bfffbf
Rohit.Patankar <Rohit.Patankar@bentley.com>
2023-11-03 13:16:51 +0530

    Reverted previous changes and created new macro for unit logs

--- iModelCore/Units/PublicAPI/Units/UnitRegistry.h
+++ iModelCore/Units/PublicAPI/Units/UnitRegistry.h
@@ -5,8 +5,11 @@
 #pragma once
 
 #include <Units/Units.h>
+#include "Bentley/Logging.h"
 
 BEGIN_BENTLEY_UNITS_NAMESPACE
+
+#define UNITSLOG (NativeLogging::CategoryLogger("UnitsNative"))
 
 //=======================================================================================
 //! A central place to store registered units with the system.  Users interact
@@ -26,10 +29,173 @@ private:
 
     void AddSystem(UnitSystemR unitSystem); // Used for creating the static definitions.
 
-    PhenomenonP AddPhenomenonInternal(Utf8CP phenomenaName, Utf8CP definition);
-    UnitP AddUnitInternal(Utf8CP phenomName, Utf8CP systemName, Utf8CP unitName, Utf8CP definition, double numerator, double denominator, double offset, bool isConstant);
-    UnitP AddInvertedUnitInternal(Utf8CP parentUnitName, Utf8CP unitName, Utf8CP unitSystemName);
-    UnitSystemP AddSystemInternal(Utf8CP name);
+    PhenomenonP AddPhenomenonInternal(Utf8CP phenomenaName, Utf8CP definition)
+        {
+        if (Utf8String::IsNullOrEmpty(phenomenaName))
+            {
+            UNITSLOG.error("Failed to create Phenomenon because name is null");
+            return nullptr;
+            }
+
+        if (Utf8String::IsNullOrEmpty(definition))
+            {
+            UNITSLOG.error("Failed to create Phenomenon because definition is null");
+            return nullptr;
+            }
+
+        if (NamedItemExists(phenomenaName))
+            {
+            UNITSLOG.errorv("Cannot create Phenomenon '%s' because name is already in use", phenomenaName);
+            return nullptr;
+            }
+
+        if (Utf8String::IsNullOrEmpty(definition))
+            {
+            UNITSLOG.errorv("Cannot create Phenomenon '%s' because the definition is empty.", phenomenaName);
+            return nullptr;
+            }
+
+        auto phenomenon = new Phenomenon(phenomenaName, definition);
+
+        phenomenon->m_unitsContext = this;
+
+        m_phenomena.insert(bpair<Utf8CP, PhenomenonP>(phenomenon->GetName().c_str(), phenomenon));
+
+        return phenomenon;
+        }
+
+
+    UnitP AddUnitInternal(Utf8CP phenomName, Utf8CP systemName, Utf8CP unitName, Utf8CP definition, double numerator, double denominator, double offset, bool isConstant)
+        {
+        if (Utf8String::IsNullOrEmpty(unitName))
+            {
+            UNITSLOG.error("Cannot create base unit because the input name is null");
+            return nullptr;
+            }
+
+        if (NamedItemExists(unitName))
+            {
+            UNITSLOG.errorv("Could not create unit '%s' because that name is already in use", unitName);
+            return nullptr;
+            }
+
+        PhenomenonP phenomenon = _LookupPhenomenonP(phenomName);
+        if (nullptr == phenomenon)
+            {
+            UNITSLOG.errorv("Could not find phenomenon '%s'", phenomName);
+            return nullptr;
+            }
+
+        UnitSystemCP system = LookupUnitSystem(systemName);
+        if (nullptr == system)
+            {
+            UNITSLOG.errorv("Could not find system '%s'", systemName);
+            return nullptr;
+            }
+
+        if (Utf8String::IsNullOrEmpty(definition))
+            {
+            UNITSLOG.errorv("Cannot create Unit '%s' because the definition is empty.", unitName);
+            return nullptr;
+            }
+
+        if (0.0 == numerator || 0.0 == denominator)
+            {
+             UNITSLOG.errorv("Failed to create unit %s because numerator or denominator is 0.  Factor: %.17g / %.17g  Offset: %d", unitName, numerator, denominator, offset);
+            return nullptr;
+            }
+        auto unit = new Unit(*system, *phenomenon, unitName, definition, numerator, denominator, offset, isConstant);
+
+        unit->m_unitsContext = this;
+        phenomenon->AddUnit(*unit);
+
+        m_units.insert(bpair<Utf8CP, UnitP>(unit->GetName().c_str(), unit));
+
+        return unit;
+        }
+
+    UnitP AddInvertedUnitInternal(Utf8CP parentUnitName, Utf8CP unitName, Utf8CP unitSystemName)
+        {
+        if (Utf8String::IsNullOrEmpty(unitName))
+            {
+            UNITSLOG.error("Cannot create unit because the input name is null");
+            return nullptr;
+            }
+
+        if (Utf8String::IsNullOrEmpty(parentUnitName))
+            {
+            UNITSLOG.errorv("Cannot create unit %s because it's parent name is null", unitName);
+            return nullptr;
+            }
+
+        if (Utf8String::IsNullOrEmpty(unitSystemName))
+            {
+            UNITSLOG.errorv("Cannot create unit %s because it's unit system name is null", unitName);
+            return nullptr;
+            }
+
+        UnitCP parentUnit = LookupUnit(parentUnitName);
+        if (nullptr == parentUnit)
+            {
+            UNITSLOG.errorv("Cannot create unit %s because it's parent unit %s cannot be found", unitName, parentUnitName);
+            return nullptr;
+            }
+
+        UnitSystemCP unitSystem = LookupUnitSystem(unitSystemName);
+        if (nullptr == unitSystem)
+            {
+            UNITSLOG.errorv("Cannot create unit %s because it's unit system %s cannot be found", unitName, unitSystemName);
+            return nullptr;
+            }
+
+        if (NamedItemExists(unitName))
+            {
+            UNITSLOG.errorv("Could not create unit '%s' because that name is already in use", unitName);
+            return nullptr;
+            }
+
+        if (parentUnit->HasOffset())
+            {
+             UNITSLOG.errorv("Cannot create inverted unit %s with parent %s because parent has an offset.", unitName, parentUnit->GetName().c_str());
+            return nullptr;
+            }
+        if (parentUnit->IsInvertedUnit())
+            {
+             UNITSLOG.errorv("Cannot create inverted unit %s with parent %s because parent is an inverted unit", unitName, parentUnit->GetName().c_str());
+            return nullptr;
+            }
+
+        auto unit = new Unit(*parentUnit, *unitSystem, unitName);
+
+        unit->m_unitsContext = this;
+
+        PhenomenonP phenomenon = _LookupPhenomenonP(parentUnit->GetPhenomenon()->GetName().c_str());
+        phenomenon->AddUnit(*unit);
+
+        m_units.insert(bpair<Utf8CP, UnitP>(unit->GetName().c_str(), unit));
+
+        return unit;
+        }
+
+    UnitSystemP AddSystemInternal(Utf8CP name)
+        {
+        if (Utf8String::IsNullOrEmpty(name))
+            {
+            UNITSLOG.error("Cannot create UnitSystem because name is null");
+            return nullptr;
+            }
+        if (NamedItemExists(name))
+            {
+            UNITSLOG.errorv("Cannot create UnitSystem '%s' because that name is already in use.", name);
+            return nullptr;
+            }
+
+        auto unitSystem = new UnitSystem(name);
+        unitSystem->m_unitsContext = this;
+        m_systems.Insert(unitSystem->GetName().c_str(), unitSystem);
+
+        return unitSystem;
+        }
 
 protected:
     PhenomenonP _LookupPhenomenonP(Utf8CP name, bool = false) const override {auto val_iter = m_phenomena.find(name); return val_iter == m_phenomena.end() ? nullptr : (*val_iter).second;}
@@ -75,14 +241,15 @@ public:
     //! @param[in] denominator  Denominator for factor
     //! @param[in] offset
     //! @return A Unit if successfully created and added to this registry, nullptr otherwise.
-    UNITS_EXPORT UnitCP AddUnit(Utf8CP phenomName, Utf8CP systemName, Utf8CP unitName, Utf8CP definition, double numerator = 1, double denominator = 1, double offset = 0);
+    UnitCP AddUnit(Utf8CP phenomName, Utf8CP systemName, Utf8CP unitName, Utf8CP definition, double numerator = 1, double denominator = 1, double offset = 0)
+        {return AddUnitInternal(phenomName, systemName, unitName, definition, numerator, denominator, offset, false);}
 
     //! Creates an inverted Unit for the parent Unit and adds it to this registry.
     //! @param[in] parentUnitName Name of the Unit we are creating the inverted Unit for
-    //! @param[in] unitName Name of the Inverted Unit to be created
+    //! @param[in] unitName Name of the Inversting Unit to be created
     //! @param[in] unitSystemName Name of the unit system this inverted unit belongs to
     //! @return An inverted Unit if successfully created and added to this registry, nullptr otherwise.
-    UNITS_EXPORT UnitCP AddInvertedUnit(Utf8CP parentUnitName, Utf8CP unitName, Utf8CP unitSystemName);
+    UnitCP AddInvertedUnit(Utf8CP parentUnitName, Utf8CP unitName, Utf8CP unitSystemName) {return AddInvertedUnitInternal(parentUnitName, unitName, unitSystemName);}
 
     //! Creates a Constant and adds it to this registry.
     //! @param[in] phenomName Name of the Phenomenon the new Constant is needs to be added to
@@ -92,18 +259,19 @@ public:
     //! @param[in] numerator    Numerator for factor
     //! @param[in] denominator  Denominator for factor
     //! @return A constant Unit if successfully created and added to the registry, nullptr otherwise.
-    UNITS_EXPORT UnitCP AddConstant(Utf8CP phenomName, Utf8CP systemName, Utf8CP constantName, Utf8CP definition, double numerator, double denominator = 1);
+    UnitCP AddConstant(Utf8CP phenomName, Utf8CP systemName, Utf8CP constantName, Utf8CP definition, double numerator, double denominator = 1)
+        {return AddUnitInternal(phenomName, systemName, constantName, definition, numerator, denominator, 0, true);}
 
     //! Creates a Phenomenon and adds it to this registry.
     //! @param[in] name Name of the Phenomenon to be created.
     //! @param[in] definition
     //! @return A Phenomenon if successfully created and added to the registry, nullptr otherwise.
-    UNITS_EXPORT PhenomenonCP AddPhenomenon(Utf8CP name, Utf8CP definition);
+    PhenomenonCP AddPhenomenon(Utf8CP name, Utf8CP definition) {return AddPhenomenonInternal(name, definition);}
 
     //! Creates a UnitSystem and adds it to this registry.
     //! @param[in] name Name of the System to be created
     //! @return A UnitSystem if successfully created and added to the registry, nullptr otherwise.
-    UNITS_EXPORT UnitSystemCP AddSystem(Utf8CP name);
+    UnitSystemCP AddSystem(Utf8CP name) {return AddSystemInternal(name);}
 
     //! Gets the Constant from this registry.
     //! @param[in] name Name of the Constant to retrieve.
