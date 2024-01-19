45812e0a2e7d0b25957b8bdae4f9fd5d38706ced
Affan Khan <khanaffan@users.noreply.github.com>
2024-01-19 10:40:10 -0500

    also add check to see if primary connection was open or not.

--- iModelCore/ECDb/ECDb/ConcurrentQueryManagerImpl.cpp
+++ iModelCore/ECDb/ECDb/ConcurrentQueryManagerImpl.cpp
@@ -204,6 +204,9 @@ void CachedConnection::UpdateSqlFunctions(ConnectionAction action) {
 std::vector<CachedConnection::FunctionInfo> CachedConnection::GetPrimaryDbSqlFunctions() const {
     std::vector<FunctionInfo> funcs;
     std::regex funcFilter("^imodel_\\w*", std::regex_constants::ECMAScript | std::regex_constants::icase);
+    if (!m_cache.GetPrimaryDb().IsDbOpen())
+        return funcs;
+
     for(auto func : m_cache.GetPrimaryDb().GetSqlFunctions()) {
         FunctionInfo info(func->GetName(), func->GetNumArgs(), func);
         if (std::regex_match(info.GetName(), funcFilter)) {
