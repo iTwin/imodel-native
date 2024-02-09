900790f3964ff8285e94fb156e23a45673e9c1a3
grigas.petraitis <35135765+grigasp@users.noreply.github.com>
2024-02-09 14:15:18 +0200

    Support creating hierarchy level descriptor for hierarchies that use `parent` symbol in their instance filters

--- iModelCore/ECPresentation/PublicAPI/ECPresentation/Rules/RelatedInstanceSpecification.h
+++ iModelCore/ECPresentation/PublicAPI/ECPresentation/Rules/RelatedInstanceSpecification.h
@@ -12,12 +12,41 @@ BEGIN_BENTLEY_ECPRESENTATION_NAMESPACE
 /*---------------------------------------------------------------------------------**//**
 * @bsiclass
 +---------------+---------------+---------------+---------------+---------------+------*/
+struct RelatedInstanceTargetInstancesSpecification : NoXmlSupport<PresentationKey>
+{
+    DEFINE_T_SUPER(PresentationKey)
+
+private:
+    Utf8String m_className;
+    bvector<ECInstanceId> m_instanceIds;
+
+protected:
+    ECPRESENTATION_EXPORT MD5 _ComputeHash() const override;
+
+    Utf8CP _GetJsonElementTypeAttributeName() const override {return nullptr;}
+    ECPRESENTATION_EXPORT Utf8CP _GetJsonElementType() const override;
+    ECPRESENTATION_EXPORT bool _ReadJson(BeJsConst json) override;
+    ECPRESENTATION_EXPORT void _WriteJson(BeJsValue) const override;
+
+public:
+    RelatedInstanceTargetInstancesSpecification() {}
+    RelatedInstanceTargetInstancesSpecification(Utf8String className, bvector<ECInstanceId> instanceIds)
+        : m_className(className), m_instanceIds(instanceIds)
+        {}
+    Utf8StringCR GetClassName() const {return m_className;}
+    bvector<ECInstanceId> const GetInstanceIds() const {return m_instanceIds;}
+};
+
+/*---------------------------------------------------------------------------------**//**
+* @bsiclass
++---------------+---------------+---------------+---------------+---------------+------*/
 struct RelatedInstanceSpecification : PresentationKey
 {
     DEFINE_T_SUPER(PresentationKey)
 
 private:
     RelationshipPathSpecification m_relationshipPath;
+    std::unique_ptr<RelatedInstanceTargetInstancesSpecification> m_targetInstancesSpecification;
     Utf8String m_alias;
     bool m_isRequired;
 
@@ -35,6 +64,15 @@ protected:
 
 public:
     RelatedInstanceSpecification() : m_isRequired(false) {}
+    RelatedInstanceSpecification(RelatedInstanceSpecification const& other)
+        : m_relationshipPath(other.m_relationshipPath), m_alias(other.m_alias), m_isRequired(other.m_isRequired)
+        {
+        if (other.m_targetInstancesSpecification)
+            m_targetInstancesSpecification = std::make_unique<RelatedInstanceTargetInstancesSpecification>(*other.m_targetInstancesSpecification);
+        }
+    RelatedInstanceSpecification(RelatedInstanceSpecification&& other)
+        : m_relationshipPath(std::move(other.m_relationshipPath)), m_targetInstancesSpecification(std::move(other.m_targetInstancesSpecification)), m_alias(std::move(other.m_alias)), m_isRequired(other.m_isRequired)
+        {}
     RelatedInstanceSpecification(RequiredRelationDirection direction, Utf8String relationshipName, Utf8String className, Utf8String alias, bool isRequired = false)
         : m_alias(alias), m_isRequired(isRequired)
         {
@@ -43,10 +81,18 @@ public:
     RelatedInstanceSpecification(RelationshipPathSpecification relationshipPath, Utf8String alias, bool isRequired = false)
         : m_relationshipPath(relationshipPath), m_alias(alias), m_isRequired(isRequired)
         {}
+    RelatedInstanceSpecification(std::unique_ptr<RelatedInstanceTargetInstancesSpecification> targetInstancesSpecification, Utf8String alias, bool isRequired = false)
+        : m_targetInstancesSpecification(std::move(targetInstancesSpecification)), m_alias(alias), m_isRequired(isRequired)
+        {}
 
     //! Get specification of the relationship path to the related instance
     RelationshipPathSpecification const& GetRelationshipPath() const {return m_relationshipPath;}
     RelationshipPathSpecification& GetRelationshipPath() {return m_relationshipPath;}
+
+    //! Defines a way to access related instance through a class and instance IDs. This should be mutually exclusive
+    //! with relationship path.
+    //! @warning This part of specification is not read from / written to JSON.
+    RelatedInstanceTargetInstancesSpecification const* GetTargetInstancesSpecification() const {return m_targetInstancesSpecification.get();}
 
     //! Related instance alias which will be used to access properties of this instance. Must be unique per parent specification.
     Utf8StringCR GetAlias() const {return m_alias;}
--- iModelJsNodeAddon/presentation/ECPresentationSerializer.h
+++ iModelJsNodeAddon/presentation/ECPresentationSerializer.h
@@ -97,6 +97,7 @@ protected:
 public:
     using IECPresentationSerializer::AsJson;
 
+    rapidjson::Document AsJson(ContextR, ECClassCP, rapidjson::Document::AllocatorType* = nullptr) const;
     rapidjson::Document AsJson(ContextR, NavNodesContainer const&, rapidjson::Document::AllocatorType* = nullptr) const;
     rapidjson::Document AsJson(ContextR, DataContainer<ContentSetItemCPtr> const&, rapidjson::Document::AllocatorType* = nullptr) const;
 
@@ -118,7 +119,7 @@ struct CompressedClassSerializer : IECClassSerializer
 private:
 	bset<ECClassCP> m_ecClasses;
 protected:
-    virtual rapidjson::Document _SerializeECClass(ECClassCR, rapidjson::Document::AllocatorType&) override;
+    virtual rapidjson::Document _SerializeECClass(ECClassCP, rapidjson::Document::AllocatorType&) override;
 public:
     bset<ECClassCP> const& GetClasses() const {return m_ecClasses;};
     rapidjson::Document CreateAccumulatedClassesMap(rapidjson::Document::AllocatorType*) const;
@@ -131,7 +132,7 @@ public:
 struct DefaultClassSerializer : IECClassSerializer
 {
 protected:
-	virtual rapidjson::Document _SerializeECClass(ECClassCR, rapidjson::Document::AllocatorType&) override;
+	virtual rapidjson::Document _SerializeECClass(ECClassCP, rapidjson::Document::AllocatorType&) override;
 };
 
 /*=================================================================================**//**
