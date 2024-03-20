42dcb139b39883c890014ddd46fee3d874edab94
pmconne <22944042+pmconne@users.noreply.github.com>
2024-03-20 07:13:53 -0400

    rm GetDgnDb

--- iModelJsNodeAddon/IModelJsNative.cpp
+++ iModelJsNodeAddon/IModelJsNative.cpp
@@ -978,8 +978,6 @@ struct NativeDgnDb : BeObjectWrap<NativeDgnDb>, SQLiteOps
         return val.As<Napi::Object>().InstanceOf(Constructor().Value());
         }
 
-    DgnDbR GetDgnDb() {return *m_dgndb;}
-
     template<typename STATUSTYPE>
     Napi::Object CreateBentleyReturnErrorObject(STATUSTYPE errCode, Utf8CP msg = nullptr) {return IModelJsNative::CreateBentleyReturnErrorObject(errCode, msg, Env());}
 
