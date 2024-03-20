841d9f86c212bcd765adda7da758cf51f8a8a868
pmconne <22944042+pmconne@users.noreply.github.com>
2024-03-20 08:27:31 -0400

    temporarily add GetDgnDb back in

--- iModelJsNodeAddon/IModelJsNative.cpp
+++ iModelJsNodeAddon/IModelJsNative.cpp
@@ -978,6 +978,13 @@ struct NativeDgnDb : BeObjectWrap<NativeDgnDb>, SQLiteOps<DgnDb>
         return val.As<Napi::Object>().InstanceOf(Constructor().Value());
         }
 
+    DgnDbR GetDgnDb() {
+        if (m_dgndb.IsNull())
+            throw "###TODO remove this method";
+
+        return *m_dgndb;
+    }
+        
     template<typename STATUSTYPE>
     Napi::Object CreateBentleyReturnErrorObject(STATUSTYPE errCode, Utf8CP msg = nullptr) {return IModelJsNative::CreateBentleyReturnErrorObject(errCode, msg, Env());}
 
