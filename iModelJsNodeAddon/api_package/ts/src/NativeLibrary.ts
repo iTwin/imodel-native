ad686de4feb243aeb22fadf124675f0891dc1772
pmconne <22944042+pmconne@users.noreply.github.com>
2024-04-05 10:31:28 -0400

    wip

--- iModelJsNodeAddon/api_package/ts/src/NativeLibrary.ts
+++ iModelJsNodeAddon/api_package/ts/src/NativeLibrary.ts
@@ -19,12 +19,12 @@ import type {
 } from "@itwin/core-bentley";
 import type {
   ChangesetIndexAndId, CodeSpecProperties, CreateEmptyStandaloneIModelProps, DbRequest, DbResponse, ElementAspectProps, ElementGraphicsRequestProps, ElementLoadProps, ElementProps,
-  FilePropertyProps, FontMapProps, GeoCoordinatesRequestProps, GeoCoordinatesResponseProps, GeographicCRSInterpretRequestProps,
+  FilePropertyProps, FontId, FontMapProps, GeoCoordinatesRequestProps, GeoCoordinatesResponseProps, GeographicCRSInterpretRequestProps,
   GeographicCRSInterpretResponseProps, GeometryContainmentResponseProps, IModelCoordinatesRequestProps,
   IModelCoordinatesResponseProps, IModelProps, LocalDirName, LocalFileName, MassPropertiesResponseProps, ModelLoadProps,
   ModelProps, QueryQuota, RelationshipProps, SnapshotOpenOptions, TextureData, TextureLoadProps, TileVersionInfo, UpgradeOptions,
 } from "@itwin/core-common";
-import type { Range3dProps } from "@itwin/core-geometry";
+import type { Range2dProps, Range3dProps } from "@itwin/core-geometry";
 
 /* eslint-disable @typescript-eslint/naming-convention */
 /* eslint-disable no-restricted-syntax */
@@ -487,6 +487,14 @@ export declare namespace IModelJsNative {
     status: IModelStatus;
   }
 
+  interface TextLayoutRangesProps {
+    layout: Range2dProps;
+    justification: Range2dProps;
+  }
+
+  enum BaselineShift { None, Subscript, Superscript }
+  enum TextEmphasis { None = 0, Bold = 1, Italic = 2, BoldItalic = Bold | Italic }
+
   /** The native object for a Briefcase. */
   class DgnDb implements IConcurrentQueryManager, SQLiteOps {
     constructor();
@@ -514,6 +522,7 @@ export declare namespace IModelJsNative {
     public closeFile(): void;
     public completeCreateChangeset(arg: { index: number }): void;
     public computeProjectExtents(wantFullExtents: boolean, wantOutlierIds: boolean): { extents: Range3dProps, fullExtents?: Range3dProps, outliers?: Id64Array };
+    public computeRangesForText(chars: string, fontId: FontId, emphasis: TextEmphasis, shift: BaselineShift): TextLayoutRangesProps;
     public concurrentQueryExecute(request: DbRequest, onResponse: ConcurrentQuery.OnResponse): void;
     public concurrentQueryResetConfig(config?: QueryConfig): QueryConfig;
     public concurrentQueryShutdown(): void;
