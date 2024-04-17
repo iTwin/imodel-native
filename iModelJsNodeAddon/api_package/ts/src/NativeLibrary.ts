71aba0da8f9236c03ed19b8380f000fd5cab6c90
Paul Connelly <22944042+pmconne@users.noreply.github.com>
2024-04-17 04:12:55 -0400

    Fix computeRangesForText (#718)
    Co-authored-by: RohitPtnkr1996 <111407262+RohitPtnkr1996@users.noreply.github.com>

--- iModelJsNodeAddon/api_package/ts/src/NativeLibrary.ts
+++ iModelJsNodeAddon/api_package/ts/src/NativeLibrary.ts
@@ -492,8 +492,6 @@ export declare namespace IModelJsNative {
     justification: Range2dProps;
   }
 
-  enum TextEmphasis { None = 0, Bold = 1, Italic = 2, BoldItalic = Bold | Italic }
-
   /** The native object for a Briefcase. */
   class DgnDb implements IConcurrentQueryManager, SQLiteOps {
     constructor();
@@ -521,7 +519,7 @@ export declare namespace IModelJsNative {
     public closeFile(): void;
     public completeCreateChangeset(arg: { index: number }): void;
     public computeProjectExtents(wantFullExtents: boolean, wantOutlierIds: boolean): { extents: Range3dProps, fullExtents?: Range3dProps, outliers?: Id64Array };
-    public computeRangesForText(chars: string, fontId: FontId, emphasis: TextEmphasis, widthFactor: number, height: number): TextLayoutRangesProps;
+    public computeRangesForText(chars: string, fontId: FontId, bold: boolean, italic: boolean, widthFactor: number, height: number): TextLayoutRangesProps;
     public concurrentQueryExecute(request: DbRequest, onResponse: ConcurrentQuery.OnResponse): void;
     public concurrentQueryResetConfig(config?: QueryConfig): QueryConfig;
     public concurrentQueryShutdown(): void;
