bce9f6e68de0daca57324b4bd4e42cd933bab7ec
Rohit.Patankar <Rohit.Patankar@bentley.com>
2024-07-12 16:04:21 +0530

    Tabs->spaces

--- iModelCore/ECDb/ECDb/IntegrityChecker.h
+++ iModelCore/ECDb/ECDb/IntegrityChecker.h
@@ -86,7 +86,7 @@ public:
     DbResult CheckSchemaLoad(std::function<bool(Utf8CP)>);
 	// Callback(check-name, status)
     DbResult QuickCheck(Checks, std::function<void(Utf8CP, bool, BeDuration)>);
-	DbResult GetRootLinkTableRelationships(std::vector<ECClassId>&);
+    DbResult GetRootLinkTableRelationships(std::vector<ECClassId>&);
 };
 
 END_BENTLEY_SQLITE_EC_NAMESPACE
