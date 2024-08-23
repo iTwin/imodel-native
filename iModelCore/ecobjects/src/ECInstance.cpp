65a6fe54debb12f92ca4b379578000cf37c8db71
akshay05n <44081595+akshay05n@users.noreply.github.com>
2024-08-23 21:46:26 +0530

    Refactor ECInstance.cpp to skip finding schemas that are being pruned

--- iModelCore/ecobjects/src/ECInstance.cpp
+++ iModelCore/ecobjects/src/ECInstance.cpp
@@ -2606,6 +2606,12 @@ struct  InstanceXmlReader
             if (ECObjectsStatus::Success != SchemaKey::ParseSchemaFullName(key, m_fullSchemaName.c_str()))
                 return NULL;
 
+            if (m_context.IsSchemaToBePruned(key))
+                {
+                LOG.infov("Skipping finding of the schema '%s' because it is being pruned.", m_fullSchemaName.c_str());
+                return NULL;
+                }
+
             m_schema = m_context.FindSchemaCP(key, SchemaMatchType::LatestReadCompatible);//Abeesh: Preserving old behavior. Ideally it should be exact
             return m_schema;
             }
@@ -2687,22 +2693,14 @@ struct  InstanceXmlReader
                 return InstanceReadStatus::ECSchemaNotFound;
                 }
 
-            // do not search for the class if its schema is going to be pruned.
-            SchemaKey key;
-            ECObjectsStatus schemaNameParseStatus = SchemaKey::ParseSchemaFullName(key, m_fullSchemaName.c_str());
-            if (ECObjectsStatus::Success == schemaNameParseStatus && m_context.IsSchemaToBePruned(key))
-                {
-                LOG.debugv("Skipping finding of the class '%s' because its schema '%s' is being pruned", m_className.c_str(), m_fullSchemaName.c_str());
-                return InstanceReadStatus::ECClassNotFound;
-                }
-
             // see if we can find the class from the schema.
             m_context.ResolveSerializedClassName(m_className, *schema);
             ECClassCP    foundClass;
             if (NULL == (foundClass = schema->GetClassCP(m_className.c_str())))
                 {
                 ECSchemaReferenceListCR refList = schema->GetReferencedSchemas();
-                if (ECObjectsStatus::Success == schemaNameParseStatus)
+                SchemaKey key;
+                if (ECObjectsStatus::Success == SchemaKey::ParseSchemaFullName(key, m_fullSchemaName.c_str()))
                     {
                     ECSchemaReferenceList::const_iterator schemaIterator = refList.find(key);
                     if (schemaIterator != refList.end())
