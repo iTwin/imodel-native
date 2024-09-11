c2b8581e37d5eb855922f10ac597ac6c9717a9c8
SohamBhattacharjee <Soham.Bhattacharjee@bentley.com>
2024-09-11 22:05:29 +0530

    file spaces and indentations removed

--- iModelCore/ECDb/ECDb/ECSql/ComputedExp.cpp
+++ iModelCore/ECDb/ECDb/ECSql/ComputedExp.cpp
@@ -13,6 +13,7 @@ BEGIN_BENTLEY_SQLITE_EC_NAMESPACE
 
 //NOTE: The code in this file is sorted by the class names. This should make maintenance easier as there are too many types in this single file
 
+
 //*************************** BooleanBinaryExp ******************************************
 //-----------------------------------------------------------------------------------------
 // @bsimethod
