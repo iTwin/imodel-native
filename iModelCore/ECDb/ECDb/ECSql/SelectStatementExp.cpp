4c75b5eca6b6c459e77608ea7e9b291f650c40fe
SohamBhattacharjee <Soham.Bhattacharjee@bentley.com>
2024-09-13 18:12:49 +0530

    comment updated

--- iModelCore/ECDb/ECDb/ECSql/SelectStatementExp.cpp
+++ iModelCore/ECDb/ECDb/ECSql/SelectStatementExp.cpp
@@ -87,7 +87,7 @@ T const* DerivedPropertyExp::GetExpression() const
         return static_cast<T const*>(child);
     return nullptr; 
     }
-// As DerivedPropertyExp can have children of one of these two types ValueExp or BooleanExp so only GetExpression<ValueExp> or GetExpression<BooleanExp> or GetExpression<ComputedExp> is allowed. DON'T USE EXPRESSIONS OF ANY OTHER TYPE WHILE CALLING GetExpression<>().
+// As DerivedPropertyExp can have a child of one of these two types ValueExp or BooleanExp so only GetExpression<ValueExp> or GetExpression<BooleanExp> or GetExpression<ComputedExp> is allowed. DON'T USE EXPRESSIONS OF ANY OTHER TYPE WHILE CALLING GetExpression<>().
 template ComputedExp const* DerivedPropertyExp::GetExpression<ComputedExp>() const;
 template ValueExp const* DerivedPropertyExp::GetExpression<ValueExp>() const;
 template BooleanExp const* DerivedPropertyExp::GetExpression<BooleanExp>() const;
@@ -1173,7 +1173,7 @@ T const* SubqueryExp::GetQuery() const {
         return static_cast<T const*>(child);
     return nullptr; 
     }
-// As subquery can be one of these two types CommonTableExp or SelectStatementExp so only GetQuery<SelectStatementExp> or GetQuery<CommonTableExp> is allowed. DON'T USE EXPRESSIONS OF ANY OTHER TYPE WHILE CALLING GetQuery<>().
+// As SubqueryExp can have a child of one of these two types CommonTableExp or SelectStatementExp so only GetQuery<SelectStatementExp> or GetQuery<CommonTableExp> is allowed. DON'T USE EXPRESSIONS OF ANY OTHER TYPE WHILE CALLING GetQuery<>().
 template CommonTableExp const* SubqueryExp::GetQuery<CommonTableExp>() const;
 template SelectStatementExp const* SubqueryExp::GetQuery<SelectStatementExp>() const;
 //-----------------------------------------------------------------------------------------
