ba6ea329b4f32a6246095fc858f7b2d5186dfba0
Arun George <aruniverse@users.noreply.github.com>
2024-12-11 16:27:18 -0500

    update deps for native pkg, drop node18, add support for 22, update eslint

--- iModelJsNodeAddon/api_package/ts/src/NativeCloudSqlite.ts
+++ iModelJsNodeAddon/api_package/ts/src/NativeCloudSqlite.ts
@@ -6,10 +6,10 @@
  * @module iModels
  */
 
-import * as child_process from "child_process";
-import * as fs from "fs";
-import * as os from "os";
-import * as path from "path";
+import * as child_process from "node:child_process";
+import * as fs from "node:fs";
+import * as os from "node:os";
+import * as path from "node:path";
 import { NativeLibrary } from "./NativeLibrary";
 
 /**
