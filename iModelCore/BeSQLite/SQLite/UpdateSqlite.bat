e75c12714c397b9adab5a383aeab6ff22a301bca
Affan Khan <khanaffan@users.noreply.github.com>
2025-01-06 10:33:45 -0500

    Update sqlite with fix
    [Fix a problem in the sessions extension allowing changesets containing foreign key violations to be committed under some circumstances.](https://github.com/iTwin/sqlite/commit/7a3d03b1fd8241899ab6fadce5a8e9d605f73537)

--- iModelCore/BeSQLite/SQLite/UpdateSqlite.bat
+++ iModelCore/BeSQLite/SQLite/UpdateSqlite.bat
@@ -8,7 +8,7 @@ rem +---------------------------------------------------------------------------
 rem This script will update the SQLite source from a fossil branch, and then create the amalgamation `sqlite.c` source file.
 rem @note that it requires cygwin be installed
 
-set sqlite_tag=itwin-sqlite-v3.47.0-r0
+set sqlite_tag=itwin-sqlite-v3.47.2-r0
 set imodel_native_sqlite=%SrcRoot%imodel-native\iModelCore\BeSQLite\SQLite\
 set sqlite_root=%appdata%\itwin-sqlite
 set build_dir=outdir
