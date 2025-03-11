4826befa019a490e66c6f19636c2a0e6b50f2fe6
Affan Khan <khanaffan@users.noreply.github.com>
2025-03-11 16:27:24 -0400

    fix

--- iModelCore/ECDb/Tests/NonPublished/InstanceRepositoryTest.cpp
+++ iModelCore/ECDb/Tests/NonPublished/InstanceRepositoryTest.cpp
@@ -18,37 +18,7 @@ struct InstanceRepositoryFixture : ECDbTestFixture {
     }
 };
 
-struct FooHandler : InstanceRepository::IClassHandler {
-public:
-    FooHandler(ECDbCR ecdb, ECN::ECClassId classId) : IClassHandler(ecdb, classId) {}
-    ~FooHandler() override = default;
-    PropertyHandlerResult OnBindProperty(InstanceRepository::WriteArgs& args) override {
-        if (args.GetProperty().GetName() != "Sum") {
-            return PropertyHandlerResult::Continue;
-        }
 
-        auto a = args.GetInstance()["a"].asDouble();
-        auto b = args.GetInstance()["b"].asDouble();
-        args.GetBinder().BindDouble(a + b);
-        return PropertyHandlerResult::Handled;
-    }
-};
-
-struct GooHandler : InstanceRepository::IClassHandler {
-public:
-    GooHandler(ECDbCR ecdb, ECN::ECClassId classId) : IClassHandler(ecdb, classId) {}
-    ~GooHandler() override = default;
-    PropertyHandlerResult OnBindProperty(InstanceRepository::WriteArgs& args) override {
-        if (args.GetProperty().GetName() != "Mul") {
-            return PropertyHandlerResult::Continue;
-        }
-
-        auto a = args.GetInstance()["a"].asDouble();
-        auto b = args.GetInstance()["b"].asDouble();
-        args.GetBinder().BindDouble(a * b);
-        return PropertyHandlerResult::Handled;
-    }
-};
 //---------------------------------------------------------------------------------------
 // @bsimethod
 //+---------------+---------------+---------------+---------------+---------------+------
@@ -71,6 +41,45 @@ TEST_F(InstanceRepositoryFixture, basic) {
     ASSERT_EQ(SUCCESS, SetupECDb("first.ecdb", schema));
     m_ecdb.SaveChanges();
     auto& instRepo = m_ecdb.GetInstanceRepository();
+
+    struct FooHandler : InstanceRepository::IClassHandler {
+    public:
+        FooHandler(ECDbCR ecdb, ECN::ECClassId classId) : IClassHandler(ecdb, classId) {}
+        ~FooHandler() override = default;
+        PropertyHandlerResult OnBindProperty(InstanceRepository::WriteArgs& args) override {
+            if (args.GetProperty().GetName() != "Sum") {
+                return PropertyHandlerResult::Continue;
+            }
+
+            auto a = args.GetInstance()["a"].asDouble();
+            auto b = args.GetInstance()["b"].asDouble();
+            args.GetBinder().BindDouble(a + b);
+            return PropertyHandlerResult::Handled;
+        }
+        void OnBeforeInsertInstance(BeJsValue& inst, const BeJsConst&) override {
+            inst["sum"] = 0;
+        }
+    };
+
+    struct GooHandler : InstanceRepository::IClassHandler {
+    public:
+        GooHandler(ECDbCR ecdb, ECN::ECClassId classId) : IClassHandler(ecdb, classId) {}
+        ~GooHandler() override = default;
+        PropertyHandlerResult OnBindProperty(InstanceRepository::WriteArgs& args) override {
+            if (args.GetProperty().GetName() != "Mul") {
+                return PropertyHandlerResult::Continue;
+            }
+
+            auto a = args.GetInstance()["a"].asDouble();
+            auto b = args.GetInstance()["b"].asDouble();
+            args.GetBinder().BindDouble(a * b);
+            return PropertyHandlerResult::Handled;
+        }
+        void OnBeforeInsertInstance(BeJsValue& inst, const BeJsConst&) override {
+            inst["mul"] = 0;
+        }
+    };
+    
     ASSERT_TRUE(instRepo.RegisterClassHandler<FooHandler>("ts:Foo"));
     ASSERT_TRUE(instRepo.RegisterClassHandler<GooHandler>("ts:Goo"));
 
@@ -82,16 +91,13 @@ TEST_F(InstanceRepositoryFixture, basic) {
     auto fooInst1 = PraseJson(R"json({
         "className": "TestSchema.Foo",
         "a": 7,
-        "b": 2,
-        "sum": 0
+        "b": 2
     })json");
 
     auto gooInst1 = PraseJson(R"json({
         "className": "TestSchema.Goo",
         "a": 5,
-        "b": 4,
-        "mul": 0,
-        "sum": 0
+        "b": 4
     })json");
 
     ECInstanceKey fooKey, gooKey;
@@ -100,17 +106,11 @@ TEST_F(InstanceRepositoryFixture, basic) {
 
     m_ecdb.SaveChanges();
 
-    BeJsDocument fooKeyJson;
-    m_ecdb.GetInstanceWriter().ToJson(fooKeyJson, fooKey, JsFormat::JsName);
-
-    BeJsDocument gooKeyJson;
-    m_ecdb.GetInstanceWriter().ToJson(gooKeyJson, gooKey, JsFormat::JsName);
-
     BeJsDocument fooInstRead;
-    ASSERT_EQ(BE_SQLITE_ROW, instRepo.Read(fooKeyJson, fooInstRead, *options, JsFormat::JsName));
+    ASSERT_EQ(BE_SQLITE_ROW, instRepo.Read(fooKey, fooInstRead, *options, JsFormat::JsName));
 
     BeJsDocument gooInstRead;
-    ASSERT_EQ(BE_SQLITE_ROW, instRepo.Read(gooKeyJson, gooInstRead, *options, JsFormat::JsName));
+    ASSERT_EQ(BE_SQLITE_ROW, instRepo.Read(gooKey, gooInstRead, *options, JsFormat::JsName));
 
     // out: {
     //    "id": "0x1",
@@ -128,8 +128,6 @@ TEST_F(InstanceRepositoryFixture, basic) {
     //    "mul": 20.0
     // }
 
-    ASSERT_STRCASEEQ("0x1", fooKeyJson["id"].asString().c_str());
-    ASSERT_STRCASEEQ("0x2", gooKeyJson["id"].asString().c_str());
     ASSERT_EQ(7.0, fooInstRead["a"].asDouble());
     ASSERT_EQ(2.0, fooInstRead["b"].asDouble());
     ASSERT_EQ(9.0, fooInstRead["sum"].asDouble());
