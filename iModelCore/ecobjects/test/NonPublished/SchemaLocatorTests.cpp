34bce1d394ddb094fe2cb95f07ce68e33da8d895
Robert Schili <rschili@users.noreply.github.com>
2025-04-02 11:52:32 +0200

    add test for sanitizing locater

--- iModelCore/ecobjects/test/NonPublished/SchemaLocatorTests.cpp
+++ iModelCore/ecobjects/test/NonPublished/SchemaLocatorTests.cpp
@@ -143,4 +143,71 @@ TEST_F(SchemaLocatorTests, LocatingSchemaContext_CacheLocaterIsBeforePrioritized
     EXPECT_STREQ("TestSchema1", locatedSchema2->GetName().c_str());
     }
 
+TEST_F(SchemaLocatorTests, SanitizingSchemaLocater_CopySchemaUsesNewReference)
+    {
+    StringSchemaLocater locater;
+    SchemaKey bisCoreKey ("BisCore", 1, 0, 0);
+    locater.AddSchemaString(bisCoreKey, 
+        R"schema(<?xml version='1.0' encoding='utf-8' ?>
+        <ECSchema schemaName="BisCore" alias="bis" version="01.00.00" xmlns="http://www.bentley.com/schemas/Bentley.ECXML.3.2">
+            <ECEntityClass typeName="Element" />
+            <ECCustomAttributeClass typeName="ClassHasHandler" appliesTo="Any" />
+        </ECSchema>)schema");
+
+    SchemaKey domainSchemaKey ("DomainSchema", 1, 0, 0);
+    locater.AddSchemaString(domainSchemaKey, 
+        R"schema(<?xml version='1.0' encoding='utf-8' ?>
+        <ECSchema schemaName="DomainSchema" alias="ds" version="01.00.00" xmlns="http://www.bentley.com/schemas/Bentley.ECXML.3.2">
+            <ECSchemaReference name="BisCore" version="01.00.00" alias="bis"/>
+            <ECEntityClass typeName="MyElement">
+                <ECCustomAttributes>
+                    <ClassHasHandler xmlns="BisCore.01.00.00" />
+                </ECCustomAttributes>
+                <BaseClass>bis:Element</BaseClass>
+            </ECEntityClass>
+        </ECSchema>)schema");
+
+    ECSchemaReadContextPtr context = ECSchemaReadContext::CreateContext();
+    context->AddSchemaLocater(locater);
+    auto domainSchema = context->LocateSchema(domainSchemaKey, SchemaMatchType::LatestReadCompatible);
+    ASSERT_TRUE(domainSchema.IsValid());
+
+    ECSchemaReadContextPtr context2 = ECSchemaReadContext::CreateContext();
+    StringSchemaLocater locater2;
+    SchemaKey bisCoreKey2 ("BisCore", 1, 0, 1);
+    locater2.AddSchemaString(bisCoreKey2, 
+        R"schema(<?xml version='1.0' encoding='utf-8' ?>
+        <ECSchema schemaName="BisCore" alias="bis" version="01.00.01" xmlns="http://www.bentley.com/schemas/Bentley.ECXML.3.2">
+            <ECEntityClass typeName="Element" />
+            <ECCustomAttributeClass typeName="ClassHasHandler" appliesTo="Any" />
+        </ECSchema>)schema");
+    SanitizingSchemaLocater sanitizingLocater(locater2);
+    context2->AddSchemaLocater(sanitizingLocater);
+    ECSchemaPtr domainSchema2;
+    ASSERT_EQ(ECObjectsStatus::Success, domainSchema->CopySchema(domainSchema2, context2.get(), false));
+    ASSERT_TRUE(domainSchema2.IsValid());
+    ASSERT_TRUE(domainSchema2->GetSchemaKey().Matches(domainSchemaKey, SchemaMatchType::Exact));
+    ECSchemaCP bisCore2 = nullptr;
+    for(auto& refSchema : domainSchema2->GetReferencedSchemas())
+        {
+        ASSERT_TRUE(bisCore2 == nullptr);
+        bisCore2 = refSchema.second.get();
+        }
+    ASSERT_TRUE(bisCore2 != nullptr);
+    ASSERT_TRUE(bisCore2->GetSchemaKey().Matches(bisCoreKey2, SchemaMatchType::Exact));
+    auto myElement = domainSchema2->GetClassCP("MyElement");
+    ASSERT_TRUE(myElement != nullptr);
+    auto caInstance = myElement->GetCustomAttributeLocal("BisCore", "ClassHasHandler");
+    ASSERT_TRUE(caInstance.IsValid());
+    ASSERT_TRUE(caInstance->GetClass().GetSchema().GetSchemaKey().Matches(bisCoreKey2, SchemaMatchType::Exact)); // Validate CA class schema
+    ECClassCP baseClass = nullptr;
+    for(auto bc : myElement->GetBaseClasses())
+        {
+        ASSERT_TRUE(baseClass == nullptr);
+        baseClass = bc;
+        }
+    ASSERT_TRUE(baseClass != nullptr);
+    ASSERT_TRUE(baseClass->GetSchema().GetSchemaKey().Matches(bisCoreKey2, SchemaMatchType::Exact)); // Validate base class schema
+    }
+
 END_BENTLEY_ECN_TEST_NAMESPACE
