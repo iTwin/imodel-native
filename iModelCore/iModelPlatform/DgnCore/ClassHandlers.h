4b1e65c01ccb60cb383eb7f89b50499da13d5780
Robert Schili <rschili@users.noreply.github.com>
2025-04-14 16:58:17 +0200

    Adjust expected binary. Remove unused test and unused code

--- iModelCore/iModelPlatform/DgnCore/ClassHandlers.h
+++
@@ -1,8 +0,0 @@
-/*---------------------------------------------------------------------------------------------
-* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
-* See LICENSE.md in the repository root for full copyright notice.
-*--------------------------------------------------------------------------------------------*/
-#pragma once
-#include "DgnPlatformInternal.h"
-
-bool RegisterBisCoreHandlers(DgnDbR db);
