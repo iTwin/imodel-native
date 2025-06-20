014d70f5fd1d3d9a4eda5394de230c5a7e381171
Daniel Rodriguez <DanRod1999@users.noreply.github.com>
2025-06-20 13:44:31 -0400

    add xml_sysconfdir path

--- iModelCore/libsrc/libxml2/vendor/libxml.h
+++ iModelCore/libsrc/libxml2/vendor/libxml.h
@@ -37,6 +37,7 @@
         #include "win32/win32config.h"
     #else
         #include "../BeLibxml-unix-config.h"
+        #define XML_SYSCONFDIR "/etc"
     #endif
     #include <libxml/xmlversion.h>
 #endif
