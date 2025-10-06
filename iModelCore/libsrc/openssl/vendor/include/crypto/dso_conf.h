73cf5cbb18fca594e47fa645875710527a2ff5ce
Daniel Rodriguez <DanRod1999@users.noreply.github.com>
2025-10-06 17:45:41 -0400

    dso_conf.h again

--- iModelCore/libsrc/openssl/vendor/include/crypto/dso_conf.h
+++ iModelCore/libsrc/openssl/vendor/include/crypto/dso_conf.h
@@ -13,5 +13,12 @@
 # define OSSL_CRYPTO_DSO_CONF_H
 # pragma once
 
-# define DSO_WIN32
-# define DSO_EXTENSION ".dll"
+#if defined (WIN32)
+  # define DSO_WIN32
+  # define DSO_EXTENSION ".dll"
+#else
+  # define DSO_DLFCN
+  # define HAVE_DLFCN_H
+  # define DSO_EXTENSION ".so"
+#endif
+#endif
