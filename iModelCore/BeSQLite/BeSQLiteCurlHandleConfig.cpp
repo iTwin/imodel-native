de8b98ab1a0b3a7e027451f3b7b0d65eca014396
Travis.Cobbs <77415528+tcobbs-bentley@users.noreply.github.com>
2025-10-30 16:10:43 -0600

    Add proxyres support. This will now use proxyres to look up the system proxy settings and use them for Cloud SQLite connections.

--- iModelCore/BeSQLite/BeSQLiteCurlHandleConfig.cpp
+++ iModelCore/BeSQLite/BeSQLiteCurlHandleConfig.cpp
@@ -4,15 +4,44 @@
 *--------------------------------------------------------------------------------------------*/
 #include "SQLite/sqlite3.h"
 #include <curl/curl.h>
-#ifdef __APPLE__
+#include <proxyres/proxyres.h>
+#include <cstdlib>
+#include <ctime>
+#include <map>
 #include <string>
+#include <utility>
+#ifndef ITWIN_DAEMON
+#include <Bentley/Logging.h>
+#endif // ITWIN_DAEMON
+#ifdef __APPLE__
 #include <mach-o/dyld.h>
 #endif // __APPLE__
+
+static std::string s_empty;
+typedef std::pair<std::time_t, std::string> TimeStringPair;
+static std::map<std::string, TimeStringPair> s_proxyMap;
+static std::string s_envProxy;
+
+#ifndef ITWIN_DAEMON
+
+void logTrace(Utf8CP fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    NativeLogging::Logging::LogMessageVa("CloudSQLiteCurlConfig", NativeLogging::LOG_TRACE, fmt, args);
+    va_end(args);
+}
+
+#else // ITWIN_DAEMON
+
+void logTrace(const char* /*fmt*/, ...) {
+    // Do nothing in daemon mode
+}
+
+#endif // !ITWIN_DAEMON
 
 #ifdef __APPLE__
 
 static std::string s_certsPath;
-static std::string s_empty;
 
 static std::string &getCACertPath() {
     if (s_certsPath.empty()) {
@@ -44,11 +73,100 @@ void besqlite_bcv_set_cacert_path(const std::string& caFilename) {
 
 #endif // __APPLE__
 
+static const std::string& getEnvProxy() {
+    static bool envChecked = false;
+    if (!envChecked) {
+        const char* httpsProxy = std::getenv("https_proxy");
+        if (httpsProxy != nullptr) {
+            s_envProxy = httpsProxy;
+        } else {
+            const char* httpProxy = std::getenv("http_proxy");
+            if (httpProxy != nullptr) {
+                s_envProxy = httpProxy;
+            }
+        }
+        if (!s_envProxy.empty()) {
+            logTrace("Using proxy from environment: <%s>\n", s_envProxy.c_str());
+        }
+        envChecked = true;
+    }
+    return s_envProxy;
+}
+
+static const std::string& getProxyForUrl(const std::string& url) {
+    const std::string& envProxy = getEnvProxy();
+    if (!envProxy.empty()) {
+        printf("\nUsing proxy from environment: <%s>\n", envProxy.c_str());
+        return envProxy;
+    }
+    auto schemeEnd = url.find("://");
+    if (schemeEnd == std::string::npos) {
+        logTrace("\nCannot determine proxy: invalid URL: %s\n", url.c_str());
+        printf("\nInvalid URL: %s\n", url.c_str());
+        return s_empty;
+    }
+    auto hostEnd = url.find('/', schemeEnd + 3);
+    std::string key = url.substr(0, hostEnd);
+    std::time_t now = std::time(nullptr);
+    auto it = s_proxyMap.find(key);
+    if (it != s_proxyMap.end()) {
+        if (now - it->second.first > 3600) { // cache for 1 hour
+            s_proxyMap.erase(it);
+        } else {
+            printf("\nUsing cached proxy for key %s: <%s>\n", key.c_str(), it->second.second.c_str());
+            return it->second.second;
+        }
+    }
+    void* pProxy = proxy_resolver_create();
+    if (nullptr == pProxy) {
+        printf("\nFailed to create proxy resolver!\n");
+        return s_empty;
+    }
+    proxy_resolver_get_proxies_for_url(pProxy, url.c_str());
+    proxy_resolver_wait(pProxy, -1);
+    const char *list = proxy_resolver_get_list(pProxy);
+    std::string proxy;
+    if (list != nullptr) {
+        proxy = list;
+        auto commaSpot = proxy.find(',');
+        if (commaSpot != std::string::npos) {
+            proxy = proxy.substr(0, commaSpot);
+        } else {
+            proxy = list;
+        }
+        if (proxy == "direct://") {
+            proxy = "";
+        }
+    }
+    proxy_resolver_delete(&pProxy);
+    if (!proxy.empty()) {
+        logTrace("Resolved proxy for URL %s: <%s>\n", url.c_str(), proxy.c_str());
+    }
+    printf("\nCaching proxy for key %s: <%s>\n", key.c_str(), proxy.c_str());
+    s_proxyMap[key] = std::make_pair(now, proxy);
+    return s_proxyMap[key].second;
+}
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-int besqlite_bcv_curl_handle_config(CURL * pCurl, int /*eMethod*/, const char * /*zUri*/) {
+int besqlite_bcv_curl_handle_config(CURL * pCurl, int /*eMethod*/, const char * zUri) {
+    static int s_initialized = 0;
+    if (!s_initialized) {
+        proxy_resolver_global_init();
+        proxy_config_global_init();
+        proxy_config_set_auto_config_url_override("http://localhost:3001/pac.js"); // for testing
+        s_initialized = 1;
+    }
+    const std::string& proxy = getProxyForUrl(zUri);
+    if (!proxy.empty()) {
+        printf("Setting proxy to %s for %s\n", proxy.c_str(), zUri);
+        curl_easy_setopt(pCurl, CURLOPT_PROXY, proxy.c_str());
+    } else {
+        printf("No proxy for %s\n", zUri);
+        curl_easy_setopt(pCurl, CURLOPT_PROXY, NULL);
+    }
     curl_easy_setopt(pCurl, CURLOPT_SSL_OPTIONS, CURLSSLOPT_REVOKE_BEST_EFFORT);
 #ifdef __APPLE__
     const std::string& certsPath = getCACertPath();
