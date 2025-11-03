09e8730681f8e9b7a3adf9c28a41015ab5bad55b
soham-bentley <177123878+soham-bentley@users.noreply.github.com>
2025-11-04 00:04:14 +0530

    Removed type traits inclusion

--- iModelCore/ECDb/ECDb/ECSql/Exp.h
+++ iModelCore/ECDb/ECDb/ECSql/Exp.h
@@ -4,7 +4,6 @@
 *--------------------------------------------------------------------------------------------*/
 #pragma once
 
-#include <type_traits>
 #include <cstddef>
 #include "../ECDbInternalTypes.h"
 #include <stack>
