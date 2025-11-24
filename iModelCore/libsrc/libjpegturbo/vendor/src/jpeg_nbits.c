ae62f18a74e57d8c428de71eb5e53cc8f22a217f
Travis.Cobbs <77415528+tcobbs-bentley@users.noreply.github.com>
2025-11-24 16:40:14 -0700

    Fixes for Android and hopefully Linux

--- iModelCore/libsrc/libjpegturbo/vendor/src/jpeg_nbits.c
+++ iModelCore/libsrc/libjpegturbo/vendor/src/jpeg_nbits.c
@@ -26,7 +26,7 @@
 
 #ifdef INCLUDE_JPEG_NBITS_TABLE
 
-const unsigned char HIDDEN jpeg_nbits_table[65536] = {
+const unsigned char jpeg_nbits_table[65536] = {
    0,  1,  2,  2,  3,  3,  3,  3,  4,  4,  4,  4,  4,  4,  4,  4,
    5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,
    6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,
