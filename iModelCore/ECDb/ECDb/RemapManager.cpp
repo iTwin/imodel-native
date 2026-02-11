b776c8d7a2bb162d363136a932919f27b2acb32f
RohitPtnkr1996 <111407262+RohitPtnkr1996@users.noreply.github.com>
2026-02-11 15:03:34 +0530

    Removed error and added tests for the failing scenario

--- iModelCore/ECDb/ECDb/RemapManager.cpp
+++ iModelCore/ECDb/ECDb/RemapManager.cpp
@@ -645,10 +645,9 @@ BentleyStatus RemapManager::RestoreAndProcessCleanedPropertyMaps(SchemaImportCon
         auto ecClass = m_schemaManager.GetClass(classId);
         if(ecClass == nullptr)
             {
-            BeAssert(false && "Failed to find class");
-            Issues().ReportV(IssueSeverity::Error, IssueCategory::BusinessProperties, IssueType::ECDbIssue, ECDbIssueId::ECDb_0251,
-                "Failed to find class %s for ensuring persisted property maps.", pair.second[0].m_className.c_str());
-            return ERROR;
+            // Class was deleted, property maps get handled with the cascade delete of classId. Skip restoring property maps.
+            LOG.infov("Skipping property map restoration for deleted class %s.", pair.second[0].m_className.c_str(), classId.GetValue());
+            continue;
             }
 
         auto classMap = m_schemaManager.GetClassMap(*ecClass);
--- iModelCore/iModelPlatform/Tests/DgnProject/NonPublished/SchemaRemap_Test.cpp
+++ iModelCore/iModelPlatform/Tests/DgnProject/NonPublished/SchemaRemap_Test.cpp
@@ -101,3 +101,112 @@ TEST_F(SchemaRemapTest, SchemaRemapMovePropertyWithV8LegacyImport)
     ASSERT_TRUE(testClassUpdated);
     ASSERT_TRUE(testClassUpdated->GetPropertyP("MovingProperty", false));
     }
+
+TEST_F(SchemaRemapTest, DeletePropertyOverrideAndDerivedClassSimultaneously)
+    {
+    SetupSeedProject();
+    ECN::ECSchemaReadContextPtr context = ECN::ECSchemaReadContext::CreateContext();
+    context->AddSchemaLocater(m_db->GetSchemaLocater());
+
+    // Setup the base schema with necessary classes.
+    // The property Alias is defined in the base entity class "Named_Item" and gets overriden in the child "Device".
+    const auto schemaV1_0_0 = R"xml(<?xml version="1.0" encoding="utf-8" ?>
+      <ECSchema schemaName="TestSchema%d" alias="ts%d" version="1.0.0" xmlns="http://www.bentley.com/schemas/Bentley.ECXML.3.2">
+        <ECSchemaReference name="BisCore" version="1.0.12" alias="bis"/>
+        <ECSchemaReference name="CoreCustomAttributes" version="1.0.3" alias="CoreCA"/>
+
+        <ECCustomAttributes>
+          <DynamicSchema xmlns="CoreCustomAttributes.1.0.3"/>
+        </ECCustomAttributes>
+
+        <ECEntityClass typeName="Named_Item">
+          <BaseClass>bis:PhysicalElement</BaseClass>
+          <ECProperty propertyName="Alias" typeName="string" priority="0" />
+        </ECEntityClass>
+
+        <ECEntityClass typeName="Device">
+          <BaseClass>Named_Item</BaseClass>
+          <ECProperty propertyName="Alias" typeName="string" priority="1003" />
+        </ECEntityClass>
+
+        <ECEntityClass typeName="Equipment">
+          <BaseClass>Device</BaseClass>
+          <ECProperty propertyName="SerialNumber" typeName="string" />
+        </ECEntityClass>
+      </ECSchema>)xml";
+
+    // Minor schema update:
+    // Override the property "Alias" in the child class "Equipment" and also add a derived class "SILO" which will inherit the property and add a property map row.
+    const auto schemaV1_0_1 = R"xml(<?xml version="1.0" encoding="utf-8" ?>
+      <ECSchema schemaName="TestSchema%d" alias="ts%d" version="1.0.1" xmlns="http://www.bentley.com/schemas/Bentley.ECXML.3.2">
+        <ECSchemaReference name="BisCore" version="1.0.12" alias="bis"/>
+        <ECSchemaReference name="CoreCustomAttributes" version="1.0.3" alias="CoreCA"/>
+
+        <ECCustomAttributes>
+          <DynamicSchema xmlns="CoreCustomAttributes.1.0.3"/>
+        </ECCustomAttributes>
+
+        <ECEntityClass typeName="Named_Item">
+          <BaseClass>bis:PhysicalElement</BaseClass>
+          <ECProperty propertyName="Alias" typeName="string" priority="0" />
+        </ECEntityClass>
+
+        <ECEntityClass typeName="Device">
+          <BaseClass>Named_Item</BaseClass>
+          <ECProperty propertyName="Alias" typeName="string" priority="1003" />
+        </ECEntityClass>
+
+        <ECEntityClass typeName="Equipment">
+          <BaseClass>Device</BaseClass>
+          <ECProperty propertyName="SerialNumber" typeName="string" />
+          <ECProperty propertyName="Alias" typeName="string" priority="0" />
+        </ECEntityClass>
+          
+        <ECEntityClass typeName="SILO">
+          <BaseClass>Equipment</BaseClass>
+          <ECProperty propertyName="Height" typeName="double" />
+        </ECEntityClass>
+      </ECSchema>)xml";
+
+    // Major schema upgrade that deletes the overriden property "Alias" from class "Equipment" and also deletes the leaf class "Silo".
+    // Since the property map for alias contains a property path to "Silo", it is persisted.
+    // However, since the class "Silo" itself gets deleted, we end up with an error "Failed to find class SILO for ensuring persisted property maps." when remapping the persisted property maps.
+    const auto schemaV2_0_0 = R"xml(<?xml version="1.0" encoding="utf-8" ?>
+      <ECSchema schemaName="TestSchema%d" alias="ts%d" version="2.0.1" xmlns="http://www.bentley.com/schemas/Bentley.ECXML.3.2">
+        <ECSchemaReference name="BisCore" version="1.0.12" alias="bis"/>
+        <ECSchemaReference name="CoreCustomAttributes" version="1.0.3" alias="CoreCA"/>
+
+        <ECCustomAttributes>
+          <DynamicSchema xmlns="CoreCustomAttributes.1.0.3"/>
+        </ECCustomAttributes>
+
+        <ECEntityClass typeName="Named_Item">
+          <BaseClass>bis:PhysicalElement</BaseClass>
+          <ECProperty propertyName="Alias" typeName="string" priority="0" />
+        </ECEntityClass>
+
+        <ECEntityClass typeName="Device">
+          <BaseClass>Named_Item</BaseClass>
+          <ECProperty propertyName="Alias" typeName="string" priority="1003" />
+        </ECEntityClass>
+
+        <ECEntityClass typeName="Equipment">
+          <BaseClass>Device</BaseClass>
+          <ECProperty propertyName="SerialNumber" typeName="string" />
+        </ECEntityClass>
+      </ECSchema>)xml";
+
+    for (const auto& schemaToImport : { schemaV1_0_0, schemaV1_0_1, schemaV2_0_0 })
+      {
+      ECSchemaPtr schema;
+      ASSERT_EQ(SchemaReadStatus::Success, ECSchema::ReadFromXmlString(schema, Utf8PrintfString(schemaToImport, 1, 1).c_str(), *context));
+      ASSERT_EQ(SchemaStatus::Success, m_db->ImportSchemas({ schema.get() }, true));
+      m_db->SaveChanges();
+
+      // Test the legacy V8 import logic
+      ECSchemaPtr legacyImportSchema;
+      ASSERT_EQ(SchemaReadStatus::Success, ECSchema::ReadFromXmlString(legacyImportSchema, Utf8PrintfString(schemaToImport, 2, 2).c_str(), *context));
+      ASSERT_EQ(SchemaStatus::Success, m_db->ImportV8LegacySchemas({ legacyImportSchema.get() }));
+      m_db->SaveChanges();
+      }
+    }
