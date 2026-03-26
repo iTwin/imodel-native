ffe0c7a8dc6e652a5b8721d563273487a02b7653
soham-bentley <177123878+soham-bentley@users.noreply.github.com>
2026-03-26 14:09:27 +0530

    Update

--- iModelCore/ECDb/ECDb/ECChangesetReaderImpl.cpp
+++ iModelCore/ECDb/ECDb/ECChangesetReaderImpl.cpp
@@ -1173,4 +1173,15 @@ std::unique_ptr<Field> Factory::CreateField(PropertyMap const& propertyMap, Tabl
     return nullptr;
 }
 
+IECSqlValue const& ECChangesetReader::GetValue(Stage stage, int columnIndex) const {
+    DbSchema const& dbSchema = m_ecdb.Schemas().Main().GetDbSchema();
+    DbTable const* dbTable = dbSchema.FindTable(GetTableName());
+    std::vector<Field::Ptr> const& fields = Factory::Create(m_ecdb, *dbTable, *m_changeIter, stage);
+    if(columnIndex < 0 || columnIndex >= (int)fields.size()) {
+        LOG.errorv("Column index %d is out of range for table '%s'.", columnIndex, GetTableName().c_str());
+        return NoopECSqlValue::GetSingleton();
+    }
+    return *(fields[columnIndex]);
+}
+
 END_BENTLEY_SQLITE_EC_NAMESPACE
