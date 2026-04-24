37c100574fbfc1e0df60043950235b2137a7b3e0
soham-bentley <177123878+soham-bentley@users.noreply.github.com>
2026-04-24 20:20:11 +0530

    Update

--- iModelJsNodeAddon/api_package/ts/src/NativeLibrary.ts
+++ iModelJsNodeAddon/api_package/ts/src/NativeLibrary.ts
@@ -802,7 +802,7 @@ export declare namespace IModelJsNative {
     public pullMergeRebaseAbortTxn(): void
     public pullMergeRebaseBegin(): TxnIdString[];
     public pullMergeRebaseEnd(): void;
-    public pullMergeReverseLocalChanges(onReverse?: (txnId: TxnIdString, txnType: TxnProps["type"]) => void): TxnIdString[];
+    public pullMergeReverseLocalChanges(onReverse?: (txnId: TxnIdString, txnType: string) => void): TxnIdString[];
     public stashChanges(args: { stashRootDir: string, description: string, iModelId: string, resetBriefcase?: true}): any;
     public stashRestore(stashFile: string): void;
     public getPendingTxnsHash(includeReversedTxns: boolean): string;
