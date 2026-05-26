a433b45a5e0520097a45001fffcd453c10c5adbf
Travis.Cobbs <77415528+tcobbs-bentley@users.noreply.github.com>
2026-05-26 10:55:43 -0600

    Provide developer instructions if cargo is not installed

--- iModelCore/libsrc/rustls-ffi/cargo.sh
+++ iModelCore/libsrc/rustls-ffi/cargo.sh
@@ -2,7 +2,10 @@
 
 if type -P cargo >/dev/null 2>&1; then
     cargo "$@"
-else
+elif type -P ~/.cargo/bin/cargo >/dev/null 2>&1; then
     # Run the user-installed cargo with all arguments passed through.
     ~/.cargo/bin/cargo "$@"
+else
+    echo "Error: cargo is not installed. Please set INSTALL_RUST_CARGO to install it for the current user." >&2
+    exit 1
 fi
