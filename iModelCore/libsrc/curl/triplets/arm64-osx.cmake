31052fc7ddc1bf80d212e98b7f5cc5ce4b7ca2f9
Travis Cobbs <77415528+tcobbs-bentley@users.noreply.github.com>
2026-08-31 18:46:33 -0700

    Apple: fix deployment target for cmake-built libraries (#1575)
    The vcpkg-built libraries didn't configure the macOS deployment target, and proxyres didn't configure the macOS or iOS deployment target. This fixes both sets of problems.
    
    (cherry picked from commit 2ef815828a68f06d83b43edfc0f4b6b26ced4b65)
    
    # Conflicts:
    #	iModelCore/libsrc/proxyres/proxyres.mke

--- iModelCore/libsrc/curl/triplets/arm64-osx.cmake
+++ iModelCore/libsrc/curl/triplets/arm64-osx.cmake
@@ -9,6 +9,7 @@ set(VCPKG_LIBRARY_LINKAGE static)
 
 set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
 set(VCPKG_OSX_ARCHITECTURES arm64)
+set(VCPKG_OSX_DEPLOYMENT_TARGET 15.0)
 
 # curl's OpenSSL backend verifies the peer certificate chain against the macOS system trust
 # store (Keychain) via Apple SecTrust. vcpkg_cmake_configure forwards this triplet option to
