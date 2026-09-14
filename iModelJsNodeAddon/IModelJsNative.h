5e510fa2ea701f97c32a3e3907f54b7c3849bdd7
Grigas <35135765+grigasp@users.noreply.github.com>
2026-09-14 14:24:19 +0300

    Presentation: Fix `NativeECPresentationManager::SetRulesetVariableValue` throwing on non-object values (#1593)

--- iModelJsNodeAddon/IModelJsNative.h
+++ iModelJsNodeAddon/IModelJsNative.h
@@ -71,6 +71,12 @@ USING_NAMESPACE_BENTLEY_EC
     }\
     Napi::Object var = info[i].As<Napi::Object>();
 
+#define REQUIRE_ARGUMENT_ANY_VALUE(i, var)\
+    if (ARGUMENT_IS_NOT_PRESENT(i)) {\
+        THROW_JS_TYPE_EXCEPTION("Argument " #i " must be defined")\
+    }\
+    Napi::Value var = info[i];
+
 #define REQUIRE_ARGUMENT_OBJ(i, T, var)\
     if (ARGUMENT_IS_NOT_PRESENT(i) || !T::InstanceOf(info[i])) {\
         THROW_JS_TYPE_EXCEPTION("Argument " #i " must be an object of type " #T)\
@@ -643,7 +649,7 @@ struct CRSListResponseProps
     Utf8String m_description;
     bool m_deprecated;
     DRange2d m_crsExtent;
-    Utf8String m_unit;    
+    Utf8String m_unit;
     };
 
 struct GeoServicesInterop
