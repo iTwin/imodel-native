5849157582c4a5efe79ae96fa7237b511529ac82
Robert Schili <rschili@users.noreply.github.com>
2026-09-25 15:32:02 +0200

    Limit the amount of log warnings written due to mapping validation during changeset apply (#1614)
    * Limit duplicate mapping warnings to a maximum of three and add summary warning for excess duplicates
    
    * Cap duplicate and incomplete property-map warnings independently and summarize excess warnings
    
    * Add polymorphic relationship constraints to schema XML in test

--- iModelCore/ECDb/ECDb/IssueReporter.h
+++ iModelCore/ECDb/ECDb/IssueReporter.h
@@ -766,6 +766,8 @@ struct ECDB_EXPORT ECDbIssueId
     static ECN::IssueId ECDb_0744;
     static ECN::IssueId ECDb_0745;
     static ECN::IssueId ECDb_0746;
+    static ECN::IssueId ECDb_0747;
+    static ECN::IssueId ECDb_0748;
     };
 
 //---------------------------------------------------------------------------------------
